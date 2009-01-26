eefebcd553959e18b0d67ea4a03f5430329e5c87
PRIYA <priya@unknown.com>
2009-01-26 18:38:57 +0000

    some more validation

--- SpringCommerceDemo/src/org/springcommerce/controller/validator/UserValidator.java
+++ SpringCommerceDemo/src/org/springcommerce/controller/validator/UserValidator.java
@@ -3,16 +3,19 @@ package org.springcommerce.controller.validator;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.validator.GenericValidator;
+
 import org.springcommerce.util.CreateUser;
+
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class UserValidator implements Validator {
-    /** Logger for this class and subclasses */
-    protected final Log logger = LogFactory.getLog(getClass());
     private static final String REGEX_VALID_NAME = "[A-Za-z'. ]{1,80}";
     private static final String REGEX_VALID_PASSWORD = "[0-9A-Za-z]{4,15}";
+
+    /** Logger for this class and subclasses */
+    protected final Log logger = LogFactory.getLog(getClass());
 
     @SuppressWarnings("unchecked")
     @Override
@@ -22,30 +25,35 @@ public class UserValidator implements Validator {
 
     @Override
     public void validate(Object obj, Errors errors) {
-    	//TODO: need to add some more validation
-    	CreateUser user = (CreateUser) obj;
+        //TODO: need to add some more validation
+        CreateUser user = (CreateUser) obj;
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "firstName.required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "lastName.required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "username.required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "password.required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwordConfirm", "passwordConfirm.required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "emailAddress", "emailAddress.required");
-        
-        if(!user.getFirstName().matches(REGEX_VALID_NAME)){
-        	errors.rejectValue("firstName", "firstName.invalid",null, null);
-        }
-        if(!user.getLastName().matches(REGEX_VALID_NAME)){
-        	errors.rejectValue("lastName", "lastName.invalid", null, null);
-        }
-        if(!user.getPassword().matches(REGEX_VALID_PASSWORD)){
-        	errors.rejectValue("password", "password.invalid", null, null);
-        }
-        
-        if(!user.getPassword().equals(user.getPasswordConfirm())){
-        	errors.rejectValue("password", "passwordConfirm.invalid", null, null);
-        }
-        
-        if(!GenericValidator.isEmail(user.getEmailAddress())){
-        	errors.rejectValue("emailAddress", "emailAddress.invalid", null, null);
+
+        if (!errors.hasErrors()) {
+            if (!user.getFirstName().matches(REGEX_VALID_NAME)) {
+                errors.rejectValue("firstName", "firstName.invalid", null, null);
+            }
+
+            if (!user.getLastName().matches(REGEX_VALID_NAME)) {
+                errors.rejectValue("lastName", "lastName.invalid", null, null);
+            }
+
+            if (!user.getPassword().matches(REGEX_VALID_PASSWORD)) {
+                errors.rejectValue("password", "password.invalid", null, null);
+            }
+
+            if (!user.getPassword().equals(user.getPasswordConfirm())) {
+                errors.rejectValue("password", "passwordConfirm.invalid", null, null);
+            }
+
+            if (!GenericValidator.isEmail(user.getEmailAddress())) {
+                errors.rejectValue("emailAddress", "emailAddress.invalid", null, null);
+            }
         }
     }
 }
