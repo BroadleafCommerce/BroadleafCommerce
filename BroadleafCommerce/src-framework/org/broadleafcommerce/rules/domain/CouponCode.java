296ef4b73db15b7bce20c5e580c5e834d655b7da
Joseph Fridye <jfridye@credera.com>
2009-02-09 16:45:39 +0000

    added coupon code

--- BroadleafCommerce/src-framework/org/broadleafcommerce/rules/domain/CouponCode.java
+++ BroadleafCommerce/src-framework/org/broadleafcommerce/rules/domain/CouponCode.java
@@ -1,38 +1,8 @@
 package org.broadleafcommerce.rules.domain;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
-@Entity
-@Table(name = "COUPON_CODE_DEFINITION", uniqueConstraints = { @UniqueConstraint(columnNames = { "CODE" }) })
-public class CouponCode implements Serializable {
+public class CouponCode {
 	
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue
-	@Column(name = "COUPON_CODE_ID")
-	private Long id;
-	
-	@Column(name = "CODE")
 	private String code;
-	
-	@Column(name = "DESCRIPTION")
-	private String description;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getCode() {
 		return code;
@@ -42,13 +12,4 @@ public class CouponCode implements Serializable {
 		this.code = code;
 	}
 
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	
 }
