0ecb6ec0b36f7f60408ea1f661748db494f18bcf
Joseph Fridye <jfridye@credera.com>
2009-02-09 16:44:02 +0000

    rules execution

---
+++ src-framework/org/broadleafcommerce/rules/dao/RuleBaseDao.java
@@ -0,0 +1,11 @@
+package org.broadleafcommerce.rules.dao;
+
+import java.util.List;
+
+import org.drools.rule.Package;
+
+public interface RuleBaseDao {
+
+	public List<Package> getRulePackages();
+
+}
