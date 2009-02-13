9f3acff9351257f494ee023761e4d822f4c611b0
Joseph Fridye <jfridye@credera.com>
2009-02-13 21:31:19 +0000

    promotional rule proof of concept simplified

--- BroadleafCommerce/src-framework/org/broadleafcommerce/rules/dao/RuleDao.java
+++ BroadleafCommerce/src-framework/org/broadleafcommerce/rules/dao/RuleDao.java
@@ -1,19 +1,9 @@
 package org.broadleafcommerce.rules.dao;
 
-import java.util.List;
-
-import org.broadleafcommerce.rules.domain.CouponCode;
-import org.broadleafcommerce.rules.domain.PromotionRuleCategory;
 import org.broadleafcommerce.rules.domain.ShoppingCartPromotion;
 
 public interface RuleDao {
-	
-	public List<PromotionRuleCategory> readAllRuleCategories();
-	
-	public ShoppingCartPromotion maintainShoppingCartPromotion(ShoppingCartPromotion shoppingCartPromotion);
-	
-	public ShoppingCartPromotion readShoppingCartPromotionById(Long id);
 
-	public List<CouponCode> readAllCouponCodes();
-	
+	public ShoppingCartPromotion maintainShoppingCartPromotion(ShoppingCartPromotion shoppingCartPromotion);
+
 }
--- BroadleafCommerce/src-framework/org/broadleafcommerce/rules/domain/ShoppingCartPromotion.java
+++ BroadleafCommerce/src-framework/org/broadleafcommerce/rules/domain/ShoppingCartPromotion.java
@@ -1,7 +1,6 @@
 package org.broadleafcommerce.rules.domain;
 
 import java.io.Serializable;
-import java.util.Date;
 
 //@Entity
 //@Table(name = "BLC_PROMOTION_RULE_DEFINITION")
@@ -17,35 +16,11 @@ public class ShoppingCartPromotion implements Serializable {
 //	@Column(name = "NAME")
 	private String name;
 
-//	@Column(name = "DESCRIPTION")
-	private String description;
-
-//	@Column(name = "CATEGORY")
-	private String category;
-
-//	@Column(name = "ACTIVATION_DATE")
-	private Date activationDate;
-
-//	@Column(name = "EXPIRATION_DATE")
-	private Date expirationDate;
-
-//	@Column(name = "CREATED")
-	private Date created;
-
-//	@Column(name = "MODIFIED")
-	private Date modified;
-	
-//	@Column(name = "PRIORITY")
-	private int priority;
-	
 //	@Column(name = "COUPON_CODE")
 	private String couponCode;
-	
-//	@Column(name = "CUSTOMER_REDEMPTION_LIMIT")
-	private int customerRedemptionLimit;
-	
-//	@Column(name = "COUPON_REDEMPTION_LIMIT")
-	private int couponRedemptionLimit;
+
+//  @Column(name = "ORDER_TOTAL")
+	private double orderTotal;
 
 	public void setId(Long id) {
 		this.id = id;
@@ -63,62 +38,6 @@ public class ShoppingCartPromotion implements Serializable {
 		return name;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setActivationDate(Date activationDate) {
-		this.activationDate = activationDate;
-	}
-
-	public Date getActivationDate() {
-		return activationDate;
-	}
-
-	public void setExpirationDate(Date expirationDate) {
-		this.expirationDate = expirationDate;
-	}
-
-	public Date getExpirationDate() {
-		return expirationDate;
-	}
-
-	public void setCreated(Date created) {
-		this.created = created;
-	}
-
-	public Date getCreated() {
-		return created;
-	}
-
-	public void setModified(Date modified) {
-		this.modified = modified;
-	}
-
-	public Date getModified() {
-		return modified;
-	}
-
-	public void setPriority(int priority) {
-		this.priority = priority;
-	}
-
-	public int getPriority() {
-		return priority;
-	}
-
 	public void setCouponCode(String couponCode) {
 		this.couponCode = couponCode;
 	}
@@ -127,20 +46,12 @@ public class ShoppingCartPromotion implements Serializable {
 		return couponCode;
 	}
 
-	public void setCustomerRedemptionLimit(int customerRedemptionLimit) {
-		this.customerRedemptionLimit = customerRedemptionLimit;
+	public void setOrderTotal(double orderTotal) {
+		this.orderTotal = orderTotal;
 	}
 
-	public int getCustomerRedemptionLimit() {
-		return customerRedemptionLimit;
-	}
-
-	public void setCouponRedemptionLimit(int couponRedemptionLimit) {
-		this.couponRedemptionLimit = couponRedemptionLimit;
-	}
-
-	public int getCouponRedemptionLimit() {
-		return couponRedemptionLimit;
+	public double getOrderTotal() {
+		return orderTotal;
 	}
 
 }
