a912501d64f87a6d8a480783e5ceda249aa8b2ef
Brian Polster <bpolster@broadleafcommerce.org>
2009-03-07 02:28:29 +0000

    made first letter lowercase for category urls

--- src-framework/org/broadleafcommerce/util/UrlUtil.java
+++ src-framework/org/broadleafcommerce/util/UrlUtil.java
@@ -3,9 +3,15 @@ package org.broadleafcommerce.util;
 public class UrlUtil {
 	public static String generateUrlKey(String toConvert) {
 		StringBuffer newUrlKey = new StringBuffer();
+		boolean firstChar = true;
 		for (int i = 0; i < toConvert.length(); i++) {
 			if (Character.isLetter(toConvert.charAt(i)))
-				newUrlKey.append(toConvert.charAt(i));
+				if (firstChar) {
+					newUrlKey.append(toConvert.substring(i,i+1).toLowerCase());
+					firstChar = false;
+				} else {
+					newUrlKey.append(toConvert.charAt(i));
+				}
 		}
 		return newUrlKey.toString();
 	}
