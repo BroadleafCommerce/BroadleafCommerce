7b6e990402c1ae6b76bfdf3ecd9ee0ab30439eee
Jonathan Ball <jball@credera.com>
2009-03-12 05:56:37 +0000

    add table id generation for Customer

--- src-profile/org/broadleafcommerce/profile/domain/CustomerImpl.java
+++ src-profile/org/broadleafcommerce/profile/domain/CustomerImpl.java
@@ -5,10 +5,12 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
 
@@ -20,7 +22,8 @@ public class CustomerImpl implements Customer, Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "CustomerId", strategy = GenerationType.TABLE)
+    @TableGenerator(name = "CustomerId", table = "SEQUENCE_GENERATOR", pkColumnName = "ID_NAME", valueColumnName = "ID_VAL", pkColumnValue = "CustomerImpl", allocationSize = 1)
     @Column(name = "CUSTOMER_ID")
     private Long id;
 
