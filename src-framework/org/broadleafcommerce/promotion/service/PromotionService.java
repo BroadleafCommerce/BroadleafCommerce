1daa0ba6fe3e0a9367a3e42a2c7c989e8d620359
Bradford Taylor <btaylor@credera.com>
2009-03-12 19:06:45 +0000

    Refactor promotion to offer and promotion.workflow to pricing.workflow

--- src-framework/org/broadleafcommerce/promotion/service/PromotionService.java
+++
@@ -1,9 +0,0 @@
-package org.broadleafcommerce.promotion.service;
-
-import org.broadleafcommerce.order.domain.Order;
-import org.broadleafcommerce.promotion.domain.Promotion;
-
-public interface PromotionService {
-
-	Order applyPromotionToOrder(Order order, Promotion promotion);
-}
