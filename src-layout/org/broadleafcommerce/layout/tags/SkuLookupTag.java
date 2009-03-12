efc41d0ad5ce3a6a6d4468120d9bdeb0364ee231
Kenny Rosenberg <krosenberg@credera.com>
2009-03-12 14:43:44 +0000

    SkuLookupTag

---
+++ src-layout/org/broadleafcommerce/layout/tags/SkuLookupTag.java
@@ -0,0 +1,39 @@
+package org.broadleafcommerce.layout.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+import org.broadleafcommerce.catalog.service.CatalogService;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class SkuLookupTag extends BodyTagSupport {
+    // private Logger log = Logger.getLogger(this.getClass());
+    private static final long serialVersionUID = 1L;
+    private String var;
+    private long skuId;
+
+	@Override
+    public int doStartTag() throws JspException {
+        WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(pageContext.getServletContext());
+        CatalogService catalogService = (CatalogService) applicationContext.getBean("catalogService");
+        pageContext.setAttribute(var, catalogService.readSkuById(skuId));
+        return EVAL_PAGE;
+    }
+
+    public String getVar() {
+        return var;
+    }
+
+    public void setVar(String var) {
+        this.var = var;
+    }
+
+	public long getSkuId() {
+		return skuId;
+	}
+
+	public void setSkuId(long skuId) {
+		this.skuId = skuId;
+	}
+}
