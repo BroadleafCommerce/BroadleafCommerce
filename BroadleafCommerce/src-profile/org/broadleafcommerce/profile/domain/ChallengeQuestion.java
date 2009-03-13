fb5e211dbb83a5fb107e0abf2b1afddc5baaa975
Jonathan Ball <jball@credera.com>
2009-03-13 19:02:16 +0000

    store challenge questions in the db

---
+++ BroadleafCommerce/src-profile/org/broadleafcommerce/profile/domain/ChallengeQuestion.java
@@ -0,0 +1,12 @@
+package org.broadleafcommerce.profile.domain;
+
+public interface ChallengeQuestion {
+
+    public Long getId();
+
+    public void setId(Long id);
+
+    public String getChallengeQuestion();
+
+    public void setChallengeQuestion(String challengeQuestion);
+}
---
+++ BroadleafCommerce/src-profile/org/broadleafcommerce/profile/domain/ChallengeQuestionImpl.java
@@ -0,0 +1,46 @@
+package org.broadleafcommerce.profile.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "BLC_CHALLENGE_QUESTION")
+public class ChallengeQuestionImpl implements ChallengeQuestion, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "ChallengeQuestionId", strategy = GenerationType.TABLE)
+    @TableGenerator(name = "ChallengeQuestionId", table = "SEQUENCE_GENERATOR", pkColumnName = "ID_NAME", valueColumnName = "ID_VAL", pkColumnValue = "CustomerImpl", allocationSize = 1)
+    @Column(name = "QUESTION_ID")
+    private Long id;
+
+    @Column(name = "QUESTION")
+    private String challengeQuestion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getChallengeQuestion() {
+        return challengeQuestion;
+    }
+
+    public void setChallengeQuestion(String challengeQuestion) {
+        this.challengeQuestion = challengeQuestion;
+    }
+}
