ad80c34cf63a074775de84f3bc23d54e24fb8833
Jonathan Ball <jball@credera.com>
2009-03-30 18:20:53 +0000

    separate ContactInfo into Phone and emailAddress on Customer

--- src-extensibility/org/broadleafcommerce/extensibility/web/MergeXmlWebApplicationContext.java
+++ src-extensibility/org/broadleafcommerce/extensibility/web/MergeXmlWebApplicationContext.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
@@ -50,133 +49,133 @@ import org.springframework.web.context.support.XmlWebApplicationContext;
  */
 public class MergeXmlWebApplicationContext extends XmlWebApplicationContext {
 
-	private static final Log LOG = LogFactory.getLog(MergeXmlWebApplicationContext.class);
-	
-	private String patchLocation;
-	private String shutdownBean;
-	private String shutdownMethod;
+    private static final Log LOG = LogFactory.getLog(MergeXmlWebApplicationContext.class);
 
-	/**
-	 * Load the bean definitions with the given XmlBeanDefinitionReader.
-	 * <p>The lifecycle of the bean factory is handled by the refreshBeanFactory method;
-	 * therefore this method is just supposed to load and/or register bean definitions.
-	 * <p>Delegates to a ResourcePatternResolver for resolving location patterns
-	 * into Resource instances.
-	 * @throws org.springframework.beans.BeansException in case of bean registration errors
-	 * @throws java.io.IOException if the required XML document isn't found
-	 * @see #refreshBeanFactory
-	 * @see #getConfigLocations
-	 * @see #getResources
-	 * @see #getResourcePatternResolver
-	 */
-	protected void loadBeanDefinitions(XmlBeanDefinitionReader reader) throws BeansException, IOException {
-		String[] broadleafConfigLocations = getStandardConfigLocations();
-		
-		InputStream[] sources = new InputStream[broadleafConfigLocations.length];
-		for (int i = 0; i < broadleafConfigLocations.length; i++) {
-			sources[i] = MergeXmlWebApplicationContext.class.getClassLoader().getResourceAsStream(broadleafConfigLocations[i]);
-		}
-		String patchLocation = getPatchLocation();
-		String[] patchLocations = StringUtils.tokenizeToStringArray(patchLocation, CONFIG_LOCATION_DELIMITERS);
-		InputStream[] patches = new InputStream[patchLocations.length];
-		for (int i = 0; i < patchLocations.length; i++) {
-			if (patchLocations[i].startsWith("classpath")) {
-				patches[i] = MergeXmlWebApplicationContext.class.getClassLoader().getResourceAsStream(patchLocations[i].substring("classpath*:".length(), patchLocations[i].length()));
-			} else {
-				Resource resource = getResourceByPath(patchLocations[i]);
-				patches[i] = resource.getInputStream();
-			}
-		}
-		Resource[] resources = new MergeApplicationContextXmlConfigResource().getConfigResources(sources, patches);
+    private String patchLocation;
+    private String shutdownBean;
+    private String shutdownMethod;
 
-		reader.loadBeanDefinitions(resources);
-	}
-	
-	private String[] getStandardConfigLocations() throws IOException {
-		String[] response;
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new InputStreamReader(MergeXmlWebApplicationContext.class.getResourceAsStream("StandardConfigLocations.txt")));
-			ArrayList<String> items = new ArrayList<String>();
-			boolean eof = false;
-			while (!eof) {
-				String temp = reader.readLine();
-				if (temp == null) {
-					eof = true;
-				} else {
-					if (!temp.startsWith("#") && temp.trim().length() > 0) {
-						items.add(temp.trim());
-					}
-				}
-			}
-			response = new String[]{};
-			response = items.toArray(response);
-		} finally {
-			if (reader != null) {
-				try{ reader.close(); } catch (Throwable e) {}
-			}
-		}
-		
-		return response;
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.springframework.context.support.AbstractApplicationContext#doClose()
-	 */
-	@Override
-	protected void doClose() {
-		if (getShutdownBean() != null && getShutdownMethod() != null) {
-			try {
-				Object shutdownBean = getBean(getShutdownBean());
-				Method shutdownMethod = shutdownBean.getClass().getMethod(getShutdownMethod(), new Class[]{});
-				shutdownMethod.invoke(shutdownBean, new Object[]{});
-			} catch (Throwable e) {
-				LOG.error("Unable to execute custom shutdown call", e);
-			}
-		}
-		super.doClose();
-	}
+    /**
+     * Load the bean definitions with the given XmlBeanDefinitionReader.
+     * <p>The lifecycle of the bean factory is handled by the refreshBeanFactory method;
+     * therefore this method is just supposed to load and/or register bean definitions.
+     * <p>Delegates to a ResourcePatternResolver for resolving location patterns
+     * into Resource instances.
+     * @throws org.springframework.beans.BeansException in case of bean registration errors
+     * @throws java.io.IOException if the required XML document isn't found
+     * @see #refreshBeanFactory
+     * @see #getConfigLocations
+     * @see #getResources
+     * @see #getResourcePatternResolver
+     */
+    protected void loadBeanDefinitions(XmlBeanDefinitionReader reader) throws BeansException, IOException {
+        String[] broadleafConfigLocations = getStandardConfigLocations();
 
-	/**
-	 * @return the patchLocation
-	 */
-	public String getPatchLocation() {
-		return patchLocation;
-	}
+        InputStream[] sources = new InputStream[broadleafConfigLocations.length];
+        for (int i = 0; i < broadleafConfigLocations.length; i++) {
+            sources[i] = MergeXmlWebApplicationContext.class.getClassLoader().getResourceAsStream(broadleafConfigLocations[i]);
+        }
+        String patchLocation = getPatchLocation();
+        String[] patchLocations = StringUtils.tokenizeToStringArray(patchLocation, CONFIG_LOCATION_DELIMITERS);
+        InputStream[] patches = new InputStream[patchLocations.length];
+        for (int i = 0; i < patchLocations.length; i++) {
+            if (patchLocations[i].startsWith("classpath")) {
+                patches[i] = MergeXmlWebApplicationContext.class.getClassLoader().getResourceAsStream(patchLocations[i].substring("classpath*:".length(), patchLocations[i].length()));
+            } else {
+                Resource resource = getResourceByPath(patchLocations[i]);
+                patches[i] = resource.getInputStream();
+            }
+        }
+        Resource[] resources = new MergeApplicationContextXmlConfigResource().getConfigResources(sources, patches);
 
-	/**
-	 * @param patchLocation the patchLocation to set
-	 */
-	public void setPatchLocation(String patchLocation) {
-		this.patchLocation = patchLocation;
-	}
+        reader.loadBeanDefinitions(resources);
+    }
 
-	/**
-	 * @return the shutdownBean
-	 */
-	public String getShutdownBean() {
-		return shutdownBean;
-	}
+    private String[] getStandardConfigLocations() throws IOException {
+        String[] response;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(MergeXmlWebApplicationContext.class.getResourceAsStream("StandardConfigLocations.txt")));
+            ArrayList<String> items = new ArrayList<String>();
+            boolean eof = false;
+            while (!eof) {
+                String temp = reader.readLine();
+                if (temp == null) {
+                    eof = true;
+                } else {
+                    if (!temp.startsWith("#") && temp.trim().length() > 0) {
+                        items.add(temp.trim());
+                    }
+                }
+            }
+            response = new String[]{};
+            response = items.toArray(response);
+        } finally {
+            if (reader != null) {
+                try{ reader.close(); } catch (Throwable e) {}
+            }
+        }
 
-	/**
-	 * @param shutdownBean the shutdownBean to set
-	 */
-	public void setShutdownBean(String shutdownBean) {
-		this.shutdownBean = shutdownBean;
-	}
+        return response;
+    }
 
-	/**
-	 * @return the shutdownMethod
-	 */
-	public String getShutdownMethod() {
-		return shutdownMethod;
-	}
+    /* (non-Javadoc)
+     * @see org.springframework.context.support.AbstractApplicationContext#doClose()
+     */
+    @Override
+    protected void doClose() {
+        if (getShutdownBean() != null && getShutdownMethod() != null) {
+            try {
+                Object shutdownBean = getBean(getShutdownBean());
+                Method shutdownMethod = shutdownBean.getClass().getMethod(getShutdownMethod(), new Class[]{});
+                shutdownMethod.invoke(shutdownBean, new Object[]{});
+            } catch (Throwable e) {
+                LOG.error("Unable to execute custom shutdown call", e);
+            }
+        }
+        super.doClose();
+    }
 
-	/**
-	 * @param shutdownMethod the shutdownMethod to set
-	 */
-	public void setShutdownMethod(String shutdownMethod) {
-		this.shutdownMethod = shutdownMethod;
-	}
+    /**
+     * @return the patchLocation
+     */
+    public String getPatchLocation() {
+        return patchLocation;
+    }
+
+    /**
+     * @param patchLocation the patchLocation to set
+     */
+    public void setPatchLocation(String patchLocation) {
+        this.patchLocation = patchLocation;
+    }
+
+    /**
+     * @return the shutdownBean
+     */
+    public String getShutdownBean() {
+        return shutdownBean;
+    }
+
+    /**
+     * @param shutdownBean the shutdownBean to set
+     */
+    public void setShutdownBean(String shutdownBean) {
+        this.shutdownBean = shutdownBean;
+    }
+
+    /**
+     * @return the shutdownMethod
+     */
+    public String getShutdownMethod() {
+        return shutdownMethod;
+    }
+
+    /**
+     * @param shutdownMethod the shutdownMethod to set
+     */
+    public void setShutdownMethod(String shutdownMethod) {
+        this.shutdownMethod = shutdownMethod;
+    }
 
 }
