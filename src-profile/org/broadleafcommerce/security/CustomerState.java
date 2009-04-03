635e978de9ba472366392618d3843ff6e6feacb1
Jonathan Ball <jball@credera.com>
2009-04-03 04:29:50 +0000

    add getCustomerId to CustomerState for convenience

--- src-profile/org/broadleafcommerce/security/CustomerState.java
+++ src-profile/org/broadleafcommerce/security/CustomerState.java
@@ -7,21 +7,25 @@ import org.springframework.security.context.SecurityContextHolder;
 
 public class CustomerState {
 
-	private final static String CUSTOMER_SESSION_ATTR_NAME = "broadleaf_commerce.customer";
+    private final static String CUSTOMER_SESSION_ATTR_NAME = "broadleaf_commerce.customer";
 
     public static Customer getCustomer(HttpServletRequest request) {
-    	return (Customer) request.getSession().getAttribute(CUSTOMER_SESSION_ATTR_NAME);
+        return (Customer) request.getSession().getAttribute(CUSTOMER_SESSION_ATTR_NAME);
     }
 
     public static void setCustomer(Customer customer, HttpServletRequest request) {
         request.getSession().setAttribute(CUSTOMER_SESSION_ATTR_NAME, customer);
     }
 
+    public static Long getCustomerId(HttpServletRequest request) {
+        return getCustomer(request) == null ? null : getCustomer(request).getId();
+    }
+
     public static boolean isCustomerAuthenticated() {
-    	if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null) {
-    		return SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
-    	}
-    	return false;
+        if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null) {
+            return SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
+        }
+        return false;
     }
 
 }
