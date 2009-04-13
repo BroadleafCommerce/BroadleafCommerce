1529cfe0b264523849ae2ae474ff52c6ef11969b
Kenny Rosenberg <krosenberg@credera.com>
2009-04-13 17:07:48 +0000

    id generation for address and fulfillment group item

--- src-profile/org/broadleafcommerce/profile/domain/AddressImpl.java
+++ src-profile/org/broadleafcommerce/profile/domain/AddressImpl.java
@@ -5,12 +5,14 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 import org.broadleafcommerce.profile.domain.listener.TemporalTimestampListener;
 
@@ -21,7 +23,8 @@ import org.broadleafcommerce.profile.domain.listener.TemporalTimestampListener;
 public class AddressImpl implements Address {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "AddressId", strategy = GenerationType.TABLE)
+    @TableGenerator(name = "AddressId", table = "SEQUENCE_GENERATOR", pkColumnName = "ID_NAME", valueColumnName = "ID_VAL", pkColumnValue = "AddressImpl", allocationSize = 1)
     @Column(name = "ADDRESS_ID")
     private Long id;
 
@@ -37,8 +40,7 @@ public class AddressImpl implements Address {
     @Column(name = "POSTAL_CODE")
     private String postalCode;
 
-    @ManyToOne(cascade = CascadeType.ALL, targetEntity = StateImpl.class)
-    @JoinColumn(name = "COUNTY")
+    @Column(name = "COUNTY")
     private String county;
 
     @ManyToOne(cascade = CascadeType.ALL, targetEntity = StateImpl.class)
