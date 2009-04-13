2323f03fd775a1ea781b9ad23adcad2461723196
Kenny Rosenberg <krosenberg@credera.com>
2009-04-13 17:08:59 +0000

    null check the order items in an order before iterating

--- src-layout/org/broadleafcommerce/layout/tags/OrderLookupTag.java
+++ src-layout/org/broadleafcommerce/layout/tags/OrderLookupTag.java
@@ -14,89 +14,91 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class OrderLookupTag extends BodyTagSupport {
-	// private Logger log = Logger.getLogger(this.getClass());
-	private static final long serialVersionUID = 1L;
-	private Long customerId;
-	private Long orderId;
-	private String orderName;
-	private String orderVar;
-	private String totalQuantityVar;
+    // private Logger log = Logger.getLogger(this.getClass());
+    private static final long serialVersionUID = 1L;
+    private Long customerId;
+    private Long orderId;
+    private String orderName;
+    private String orderVar;
+    private String totalQuantityVar;
 
-	@Override
+    @Override
     public int doStartTag() throws JspException {
-		Customer customer = null;
-		if (customerId != null){
-	        WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(pageContext.getServletContext());
-	        CustomerService customerService = (CustomerService) applicationContext.getBean("customerService");
-	        customer = customerService.readCustomerById((customerId));
-		} else {
-			customer =  CustomerState.getCustomer((HttpServletRequest)pageContext.getRequest());
-		}
+        Customer customer = null;
+        if (customerId != null){
+            WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(pageContext.getServletContext());
+            CustomerService customerService = (CustomerService) applicationContext.getBean("customerService");
+            customer = customerService.readCustomerById((customerId));
+        } else {
+            customer =  CustomerState.getCustomer((HttpServletRequest)pageContext.getRequest());
+        }
         WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(pageContext.getServletContext());
         OrderService orderService = (OrderService) applicationContext.getBean("orderService");
         Order order = null;
         if (orderName != null && orderId != null) {
-        	throw new IllegalArgumentException("Only orderName or orderId attribute may be specified on orderLookup tag");
+            throw new IllegalArgumentException("Only orderName or orderId attribute may be specified on orderLookup tag");
         } else if (orderId != null) {
-        	order = orderService.findOrderById(orderId);
+            order = orderService.findOrderById(orderId);
         } else if (orderName != null) {
-        	order = orderService.findNamedOrderForCustomer(orderName, customer);
+            order = orderService.findNamedOrderForCustomer(orderName, customer);
         } else {
-        	order = orderService.findCartForCustomer(customer);
+            order = orderService.findCartForCustomer(customer);
         }
         if (orderVar != null) {
-        	pageContext.setAttribute(orderVar, order);
+            pageContext.setAttribute(orderVar, order);
         }
         if (totalQuantityVar != null) {
-        	int orderItemsCount = 0;
-        	for (OrderItem orderItem : order.getOrderItems()) {
-        		orderItemsCount += orderItem.getQuantity();
-        	}
-        	pageContext.setAttribute(totalQuantityVar, orderItemsCount);
+            int orderItemsCount = 0;
+            if (order.getOrderItems() != null) {
+                for (OrderItem orderItem : order.getOrderItems()) {
+                    orderItemsCount += orderItem.getQuantity();
+                }
+            }
+            pageContext.setAttribute(totalQuantityVar, orderItemsCount);
         } else if (totalQuantityVar != null) {
-        	pageContext.setAttribute(totalQuantityVar, 0);
+            pageContext.setAttribute(totalQuantityVar, 0);
         }
         return EVAL_PAGE;
     }
 
-	public Long getCustomerId() {
-		return customerId;
-	}
+    public Long getCustomerId() {
+        return customerId;
+    }
 
-	public void setCustomerId(Long customerId) {
-		this.customerId = customerId;
-	}
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
 
-	public String getOrderVar() {
-		return orderVar;
-	}
+    public String getOrderVar() {
+        return orderVar;
+    }
 
-	public void setOrderVar(String orderVar) {
-		this.orderVar = orderVar;
-	}
+    public void setOrderVar(String orderVar) {
+        this.orderVar = orderVar;
+    }
 
-	public String getTotalQuantityVar() {
-		return totalQuantityVar;
-	}
+    public String getTotalQuantityVar() {
+        return totalQuantityVar;
+    }
 
-	public void setTotalQuantityVar(String totalQuantityVar) {
-		this.totalQuantityVar = totalQuantityVar;
-	}
+    public void setTotalQuantityVar(String totalQuantityVar) {
+        this.totalQuantityVar = totalQuantityVar;
+    }
 
-	public String getOrderName() {
-		return orderName;
-	}
+    public String getOrderName() {
+        return orderName;
+    }
 
-	public void setOrderName(String orderName) {
-		this.orderName = orderName;
-	}
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
 
-	public Long getOrderId() {
-		return orderId;
-	}
+    public Long getOrderId() {
+        return orderId;
+    }
 
-	public void setOrderId(Long orderId) {
-		this.orderId = orderId;
-	}
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
 
 }
