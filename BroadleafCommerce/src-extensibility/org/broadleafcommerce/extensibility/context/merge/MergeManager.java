a17bb718a968560714757c6d618cca4dabac72ea
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-04-14 18:18:04 +0000

    Fixed the extensibility to better handle multiple persistence units

--- BroadleafCommerce/src-extensibility/org/broadleafcommerce/extensibility/context/merge/MergeManager.java
+++ BroadleafCommerce/src-extensibility/org/broadleafcommerce/extensibility/context/merge/MergeManager.java
@@ -33,7 +33,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 /**
- * This class manages all xml merge interactions with callers. It is responsible for 
+ * This class manages all xml merge interactions with callers. It is responsible for
  * not only loading the handler configurations, but also for cycling through the handlers
  * in a prioritized fashion and exporting the final merged document.
  * 
@@ -41,83 +41,83 @@ import org.w3c.dom.Node;
  *
  */
 public class MergeManager {
-	
-	public static void main(String[] items) {
-		try {
-			MergeManager manager = new MergeManager();
-			InputStream stream1 = MergeManager.class.getResourceAsStream("applicationContext_1.xml");
-			InputStream stream2 = MergeManager.class.getResourceAsStream("applicationContext_2.xml");
-			
-			InputStream result = manager.merge(stream1, stream2);
-			
-			BufferedReader reader = null;
-			try{
-				boolean eof = false;
-				reader = new BufferedReader(new InputStreamReader(result));
-				while (!eof) {
-					String temp = reader.readLine();
-					if (temp == null) {
-						eof = true;
-					} else {
-						System.out.println(temp);
-					}
-				}
-			} finally {
-				if (reader != null) {
-					try{ reader.close(); } catch (Throwable e) {}
-				}
-			}
-		} catch (MergeManagerSetupException e) {
-			e.printStackTrace();
-		} catch (MergeException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Additional merge points may be added by the caller. Also default merge points
-	 * may be overriden to change their current behavior. This is accomplished by
-	 * specifying the system property denoted by the key MergeManager.MERGE_DEFINITION_SYSTEM_PROPERTY
-	 * with a value stating the fully qualified path of user-created property file. Please refer
-	 * to the default properties file located at org/broadleafcommerce/extensibility/context/merge/default.properties
-	 * for more details.
-	 * 
-	 */
-	public static final String MERGE_DEFINITION_SYSTEM_PROPERTY = "org.broadleafcommerce.extensibility.context.merge.handlers.merge.properties";
-	
-	private static final Log LOG = LogFactory.getLog(MergeManager.class);
-	
-	private static DocumentBuilder builder;
-	
-	static {
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		try {
-			builder = dbf.newDocumentBuilder();
-		} catch (ParserConfigurationException e) {
-			LOG.error("Unable to create document builder", e);
-			throw new RuntimeException(e);
-		}
-	}
-    
-    private MergeHandler[] handlers;
-    
-    public MergeManager() throws MergeManagerSetupException {
-    	try {
-			Properties props = loadProperties();
-			setHandlers(props);
-		} catch (IOException e) {
-			throw new MergeManagerSetupException(e);
-		} catch (ClassNotFoundException e) {
-			throw new MergeManagerSetupException(e);
-		} catch (IllegalAccessException e) {
-			throw new MergeManagerSetupException(e);
-		} catch (InstantiationException e) {
-			throw new MergeManagerSetupException(e);
-		}
+
+    public static void main(String[] items) {
+        try {
+            MergeManager manager = new MergeManager();
+            InputStream stream1 = MergeManager.class.getResourceAsStream("applicationContext_1.xml");
+            InputStream stream2 = MergeManager.class.getResourceAsStream("applicationContext_2.xml");
+
+            InputStream result = manager.merge(stream1, stream2);
+
+            BufferedReader reader = null;
+            try{
+                boolean eof = false;
+                reader = new BufferedReader(new InputStreamReader(result));
+                while (!eof) {
+                    String temp = reader.readLine();
+                    if (temp == null) {
+                        eof = true;
+                    } else {
+                        System.out.println(temp);
+                    }
+                }
+            } finally {
+                if (reader != null) {
+                    try{ reader.close(); } catch (Throwable e) {}
+                }
+            }
+        } catch (MergeManagerSetupException e) {
+            e.printStackTrace();
+        } catch (MergeException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
+    /**
+     * Additional merge points may be added by the caller. Also default merge points
+     * may be overriden to change their current behavior. This is accomplished by
+     * specifying the system property denoted by the key MergeManager.MERGE_DEFINITION_SYSTEM_PROPERTY
+     * with a value stating the fully qualified path of user-created property file. Please refer
+     * to the default properties file located at org/broadleafcommerce/extensibility/context/merge/default.properties
+     * for more details.
+     * 
+     */
+    public static final String MERGE_DEFINITION_SYSTEM_PROPERTY = "org.broadleafcommerce.extensibility.context.merge.handlers.merge.properties";
+
+    private static final Log LOG = LogFactory.getLog(MergeManager.class);
+
+    private static DocumentBuilder builder;
+
+    static {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            builder = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            LOG.error("Unable to create document builder", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private MergeHandler[] handlers;
+
+    public MergeManager() throws MergeManagerSetupException {
+        try {
+            Properties props = loadProperties();
+            setHandlers(props);
+        } catch (IOException e) {
+            throw new MergeManagerSetupException(e);
+        } catch (ClassNotFoundException e) {
+            throw new MergeManagerSetupException(e);
+        } catch (IllegalAccessException e) {
+            throw new MergeManagerSetupException(e);
+        } catch (InstantiationException e) {
+            throw new MergeManagerSetupException(e);
+        }
+    }
+
     /**
      * Merge 2 xml document streams together into a final resulting stream. During
      * the merge, various merge business rules are followed based on configuration
@@ -129,115 +129,115 @@ public class MergeManager {
      * @throws MergeException
      */
     public InputStream merge(InputStream stream1, InputStream stream2) throws MergeException {
-    	try {
-			Document doc1 = builder.parse(stream1);
-			Document doc2 = builder.parse(stream2);
-			
-			List<Node> exhaustedNodes = new ArrayList<Node>();
-			
-			//process any defined handlers
-			for (int j=0;j<this.handlers.length;j++){
-				MergePoint point = new MergePoint(this.handlers[j], doc1, doc2);
-				Node[] temp = {};
-				temp = exhaustedNodes.toArray(temp);
-				Node[] list = point.merge(temp);
-				if (list != null) {
-					for (int x=0;x<list.length;x++){
-						exhaustedNodes.add(list[x]);
-					}
-				}
-			}
-			
-			TransformerFactory tFactory = TransformerFactory.newInstance();
-			Transformer xmlTransformer = tFactory.newTransformer();
-			xmlTransformer.setOutputProperty(OutputKeys.VERSION, "1.0");
-			xmlTransformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-			xmlTransformer.setOutputProperty(OutputKeys.METHOD, "xml");
-			xmlTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			
-			DOMSource source = new DOMSource(doc1);
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(baos));
-			StreamResult result = new StreamResult(writer);
-			xmlTransformer.transform(source, result);
-			
-			return new ByteArrayInputStream(baos.toByteArray());
-		} catch (Throwable e) {
-			throw new MergeException(e);
-		}
+        try {
+            Document doc1 = builder.parse(stream1);
+            Document doc2 = builder.parse(stream2);
+
+            List<Node> exhaustedNodes = new ArrayList<Node>();
+
+            //process any defined handlers
+            for (int j=0;j<this.handlers.length;j++){
+                MergePoint point = new MergePoint(this.handlers[j], doc1, doc2);
+                Node[] temp = {};
+                temp = exhaustedNodes.toArray(temp);
+                Node[] list = point.merge(temp);
+                if (list != null) {
+                    for (int x=0;x<list.length;x++){
+                        exhaustedNodes.add(list[x]);
+                    }
+                }
+            }
+
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+            Transformer xmlTransformer = tFactory.newTransformer();
+            xmlTransformer.setOutputProperty(OutputKeys.VERSION, "1.0");
+            xmlTransformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            xmlTransformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            xmlTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            DOMSource source = new DOMSource(doc1);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(baos));
+            StreamResult result = new StreamResult(writer);
+            xmlTransformer.transform(source, result);
+
+            return new ByteArrayInputStream(baos.toByteArray());
+        } catch (Throwable e) {
+            throw new MergeException(e);
+        }
     }
-    
+
     private void setHandlers(Properties props) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-    	ArrayList<MergeHandler> handlers = new ArrayList<MergeHandler>();
-    	String[] keys = {};
-    	keys = props.keySet().toArray(keys);
-    	for (int j=0;j<keys.length;j++){
-    		if (keys[j].startsWith("handler.")) {
-    			MergeHandler temp = (MergeHandler) Class.forName(props.getProperty(keys[j])).newInstance();
-    			String name = keys[j].substring(8, keys[j].length());
-    			temp.setName(name);
-    			String priority = props.getProperty("priority."+name);
-    			if (priority != null) {
-    				temp.setPriority(Integer.parseInt(priority));
-    			}
-    			String xpath = props.getProperty("xpath."+name);
-    			if (priority != null) {
-    				temp.setXPath(xpath);
-    			}
-    			handlers.add(temp);
-    		}
-    	}
-    	MergeHandler[] explodedView = {};
-    	explodedView = handlers.toArray(explodedView);
-    	Comparator<Object> nameCompare = new Comparator<Object>() {
-			@Override
-			public int compare(Object arg0, Object arg1) {
-				return ((MergeHandler) arg0).getName().compareTo(((MergeHandler) arg1).getName());
-			}
-    	};
-    	Arrays.sort(explodedView, nameCompare);
-    	ArrayList<MergeHandler> finalHandlers = new ArrayList<MergeHandler>();
-    	for (int j=0;j<explodedView.length;j++){
-    		MergeHandler temp = explodedView[j];
-    		if (temp.getName().indexOf(".") >= 0) {
-	    		final String parentName = temp.getName().substring(0, temp.getName().lastIndexOf("."));
-	    		int pos = Arrays.binarySearch(explodedView, new MergeHandlerAdapter() {
-					@Override
-					public String getName() {
-						return parentName;
-					}
-	    		}, nameCompare);
-	    		if (pos >= 0) {
-	    			MergeHandler[] parentHandlers = explodedView[pos].getChildren();
-	    			MergeHandler[] newHandlers = new MergeHandler[parentHandlers.length + 1];
-	    			System.arraycopy(parentHandlers, 0, newHandlers, 0, parentHandlers.length);
-	    			newHandlers[newHandlers.length - 1] = temp;
-	    			Arrays.sort(newHandlers);
-	    			explodedView[pos].setChildren(newHandlers);
-	    		}
-    		} else {
-    			finalHandlers.add(temp);
-    		}
-    	}
-    
-    	this.handlers = new MergeHandler[0];
-    	this.handlers = finalHandlers.toArray(this.handlers);
-    	Arrays.sort(this.handlers);
+        ArrayList<MergeHandler> handlers = new ArrayList<MergeHandler>();
+        String[] keys = {};
+        keys = props.keySet().toArray(keys);
+        for (int j=0;j<keys.length;j++){
+            if (keys[j].startsWith("handler.")) {
+                MergeHandler temp = (MergeHandler) Class.forName(props.getProperty(keys[j])).newInstance();
+                String name = keys[j].substring(8, keys[j].length());
+                temp.setName(name);
+                String priority = props.getProperty("priority."+name);
+                if (priority != null) {
+                    temp.setPriority(Integer.parseInt(priority));
+                }
+                String xpath = props.getProperty("xpath."+name);
+                if (priority != null) {
+                    temp.setXPath(xpath);
+                }
+                handlers.add(temp);
+            }
+        }
+        MergeHandler[] explodedView = {};
+        explodedView = handlers.toArray(explodedView);
+        Comparator<Object> nameCompare = new Comparator<Object>() {
+            @Override
+            public int compare(Object arg0, Object arg1) {
+                return ((MergeHandler) arg0).getName().compareTo(((MergeHandler) arg1).getName());
+            }
+        };
+        Arrays.sort(explodedView, nameCompare);
+        ArrayList<MergeHandler> finalHandlers = new ArrayList<MergeHandler>();
+        for (int j=0;j<explodedView.length;j++){
+            MergeHandler temp = explodedView[j];
+            if (temp.getName().indexOf(".") >= 0) {
+                final String parentName = temp.getName().substring(0, temp.getName().lastIndexOf("."));
+                int pos = Arrays.binarySearch(explodedView, new MergeHandlerAdapter() {
+                    @Override
+                    public String getName() {
+                        return parentName;
+                    }
+                }, nameCompare);
+                if (pos >= 0) {
+                    MergeHandler[] parentHandlers = explodedView[pos].getChildren();
+                    MergeHandler[] newHandlers = new MergeHandler[parentHandlers.length + 1];
+                    System.arraycopy(parentHandlers, 0, newHandlers, 0, parentHandlers.length);
+                    newHandlers[newHandlers.length - 1] = temp;
+                    Arrays.sort(newHandlers);
+                    explodedView[pos].setChildren(newHandlers);
+                }
+            } else {
+                finalHandlers.add(temp);
+            }
+        }
+
+        this.handlers = new MergeHandler[0];
+        this.handlers = finalHandlers.toArray(this.handlers);
+        Arrays.sort(this.handlers);
     }
-    
+
     private Properties loadProperties() throws IOException {
-    	Properties defaultProperties = new Properties();
-    	defaultProperties.load(MergeManager.class.getResourceAsStream("default.properties"));
-    	Properties props;
-    	String overrideFileClassPath = System.getProperty(MERGE_DEFINITION_SYSTEM_PROPERTY);
-    	if (overrideFileClassPath != null) {
-    		props = new Properties(defaultProperties);
-    		props.load(MergeManager.class.getClassLoader().getResourceAsStream(overrideFileClassPath));
-    	} else {
-    		props = defaultProperties;
-    	}
-    	
-    	return props;
+        Properties defaultProperties = new Properties();
+        defaultProperties.load(MergeManager.class.getResourceAsStream("default.properties"));
+        Properties props;
+        String overrideFileClassPath = System.getProperty(MERGE_DEFINITION_SYSTEM_PROPERTY);
+        if (overrideFileClassPath != null) {
+            props = new Properties(defaultProperties);
+            props.load(MergeManager.class.getClassLoader().getResourceAsStream(overrideFileClassPath));
+        } else {
+            props = defaultProperties;
+        }
+
+        return props;
     }
-    
+
 }
--- BroadleafCommerce/src-extensibility/org/broadleafcommerce/extensibility/jpa/MergePersistenceUnitManager.java
+++ BroadleafCommerce/src-extensibility/org/broadleafcommerce/extensibility/jpa/MergePersistenceUnitManager.java
@@ -2,41 +2,115 @@ package org.broadleafcommerce.extensibility.jpa;
 
 import java.net.URL;
 import java.util.List;
-import java.util.Properties;
+
+import javax.persistence.spi.PersistenceUnitInfo;
 
 import org.springframework.orm.jpa.persistenceunit.DefaultPersistenceUnitManager;
 import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
 
 /**
- * Merges properties from multiple persistence.xml files into a single PersistenceUnit.
- * For the post-processor to work properly all persistence.xml files must specify the same
- * persistence unit name.
- * @author jjacobs
+ * Merges jars, class names and mapping file names from several persistence.xml files. The
+ * MergePersistenceUnitManager will continue to keep track of individual persistence unit
+ * names (including individual data sources). When a specific PersistenceUnitInfo is requested
+ * by unit name, the appropriate PersistenceUnitInfo is returned with modified jar files
+ * urls, class names and mapping file names that include the comprehensive collection of these
+ * values from all persistence.xml files. Note, only persistence units belonging to the
+ * validPersistenceUnitNames list are included in the merge.
+ * 
+ * 
+ * @author jfischer, jjacobs
  */
 public class MergePersistenceUnitManager extends DefaultPersistenceUnitManager {
-	
+
+    private MutablePersistenceUnitInfo masterPU = new MutablePersistenceUnitInfo();
+    private List<String> validPersistenceUnitNames;
+
     @Override
     protected void postProcessPersistenceUnitInfo(MutablePersistenceUnitInfo newPU) {
         super.postProcessPersistenceUnitInfo(newPU);
-        final URL persistenceUnitRootUrl = newPU.getPersistenceUnitRootUrl();
-        newPU.addJarFileUrl(persistenceUnitRootUrl);
-        final String persistenceUnitName = newPU.getPersistenceUnitName();
-        final MutablePersistenceUnitInfo oldPU = getPersistenceUnitInfo(persistenceUnitName);
-        if (oldPU != null) {
-            List<URL> urls = oldPU.getJarFileUrls();
-            for (URL url : urls)
-                newPU.addJarFileUrl(url);
-            List<String> managedClassNames = oldPU.getManagedClassNames();
-            for (String managedClassName : managedClassNames)
-                newPU.addManagedClassName(managedClassName);
-            List<String> mappingFileNames = oldPU.getMappingFileNames();
-            for (String mappingFileName : mappingFileNames)
-                newPU.addMappingFileName(mappingFileName);
-            Properties oldProperties = oldPU.getProperties();
-            Properties newProperties = newPU.getProperties();
-            newProperties.putAll(oldProperties);
-            newPU.setProperties(newProperties);
+        if (validPersistenceUnitNames == null) {
+            throw new IllegalArgumentException("validPersistenceUnitNames must be set");
+        }
+        String persistenceUnitName = newPU.getPersistenceUnitName();
+        if (validPersistenceUnitNames.contains(persistenceUnitName)) {
+            final URL persistenceUnitRootUrl = newPU.getPersistenceUnitRootUrl();
+            if (!masterPU.getJarFileUrls().contains(persistenceUnitRootUrl)){
+                masterPU.addJarFileUrl(persistenceUnitRootUrl);
+            }
+            List<URL> urls = newPU.getJarFileUrls();
+            for (URL url : urls){
+                if (!masterPU.getJarFileUrls().contains(url)){
+                    masterPU.addJarFileUrl(url);
+                }
+            }
+            List<String> managedClassNames = newPU.getManagedClassNames();
+            for (String managedClassName : managedClassNames){
+                if (!masterPU.getManagedClassNames().contains(managedClassName)) {
+                    masterPU.addManagedClassName(managedClassName);
+                }
+            }
+            List<String> mappingFileNames = newPU.getMappingFileNames();
+            for (String mappingFileName : mappingFileNames) {
+                if (!masterPU.getMappingFileNames().contains(mappingFileName)) {
+                    masterPU.addMappingFileName(mappingFileName);
+                }
+            }
         }
     }
-    
+
+    /* (non-Javadoc)
+     * @see org.springframework.orm.jpa.persistenceunit.DefaultPersistenceUnitManager#obtainPersistenceUnitInfo(java.lang.String)
+     */
+    @Override
+    public PersistenceUnitInfo obtainPersistenceUnitInfo(String persistenceUnitName) {
+        MutablePersistenceUnitInfo pui = getPersistenceUnitInfo(persistenceUnitName);
+        if (pui == null) {
+            throw new IllegalArgumentException("No persistence unit with name '"
+                    + persistenceUnitName + "' found");
+        }
+
+        List<URL> jarUrls = pui.getJarFileUrls();
+        jarUrls.clear();
+        List<URL> urls = masterPU.getJarFileUrls();
+        for (URL url : urls) {
+            jarUrls.add(url);
+        }
+        List<String> classNames = pui.getManagedClassNames();
+        classNames.clear();
+        List<String> managedClassNames = masterPU.getManagedClassNames();
+        for (String managedClassName : managedClassNames) {
+            classNames.add(managedClassName);
+        }
+        List<String> mappingNames = pui.getMappingFileNames();
+        mappingNames.clear();
+        List<String> mappingFileNames = masterPU.getMappingFileNames();
+        for (String mappingFileName : mappingFileNames) {
+            mappingNames.add(mappingFileName);
+        }
+
+        return pui;
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.orm.jpa.persistenceunit.DefaultPersistenceUnitManager#obtainDefaultPersistenceUnitInfo()
+     */
+    @Override
+    public PersistenceUnitInfo obtainDefaultPersistenceUnitInfo() {
+        throw new IllegalStateException("Default Persistence Unit is not supported. The persistence unit name must be specified at the entity manager factory.");
+    }
+
+    /**
+     * @return the validPersistenceUnitNames
+     */
+    public List<String> getValidPersistenceUnitNames() {
+        return validPersistenceUnitNames;
+    }
+
+    /**
+     * @param validPersistenceUnitNames the validPersistenceUnitNames to set
+     */
+    public void setValidPersistenceUnitNames(List<String> validPersistenceUnitNames) {
+        this.validPersistenceUnitNames = validPersistenceUnitNames;
+    }
+
 }
