4550703ef9b8cba34f27191e0cde105e84162305
Jonathan Ball <jball@credera.com>
2009-04-14 01:34:43 +0000

    add merge cart response to allow for added/removed items as well as merged order returned

--- src-framework/org/broadleafcommerce/session/MergeCartProcessorImpl.java
+++ src-framework/org/broadleafcommerce/session/MergeCartProcessorImpl.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.broadleafcommerce.order.service.MergeCartResponse;
 import org.broadleafcommerce.order.service.OrderService;
 import org.broadleafcommerce.profile.domain.Customer;
 import org.broadleafcommerce.profile.service.CustomerService;
@@ -15,6 +16,10 @@ import org.springframework.stereotype.Service;
 @Service("mergeCartProcessor")
 public class MergeCartProcessorImpl implements MergeCartProcessor {
 
+    public final static String BC_MERGE_CART_ITEMS_ADDED_SESSION_KEY = "merge_cart_items_added";
+
+    public final static String BC_MERGE_CART_ITEMS_REMOVED_SESSION_KEY = "merge_cart_items_removed";
+
     @Resource
     private CustomerService customerService;
 
@@ -25,6 +30,12 @@ public class MergeCartProcessorImpl implements MergeCartProcessor {
         Customer loggedInCustomer = customerService.readCustomerByUsername((String) authResult.getPrincipal());
         Customer anonymousCustomer = CustomerState.getCustomer(request);
         Long anonymousCartId = orderService.findCartForCustomer(anonymousCustomer) == null ? null : orderService.findCartForCustomer(anonymousCustomer).getId();
-        orderService.mergeCart(loggedInCustomer, anonymousCartId);
+        MergeCartResponse mergeCartResponse = orderService.mergeCart(loggedInCustomer, anonymousCartId);
+        if (!mergeCartResponse.getAddedItems().isEmpty()) {
+            request.getSession().setAttribute(BC_MERGE_CART_ITEMS_ADDED_SESSION_KEY, mergeCartResponse.getAddedItems());
+        }
+        if (!mergeCartResponse.getRemovedItems().isEmpty()) {
+            request.getSession().setAttribute(BC_MERGE_CART_ITEMS_REMOVED_SESSION_KEY, mergeCartResponse.getRemovedItems());
+        }
     }
 }
