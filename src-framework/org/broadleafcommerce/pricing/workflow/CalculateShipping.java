5d5687857fb1a2df677827dadfd6cf59c2d17ff9
Erik Weibust <eweibust@credera.com>
2009-04-20 21:41:12 +0000

    added more logic for shipping and debug stmts.  --efw

--- src-framework/org/broadleafcommerce/pricing/workflow/CalculateShipping.java
+++ src-framework/org/broadleafcommerce/pricing/workflow/CalculateShipping.java
@@ -3,22 +3,35 @@ package org.broadleafcommerce.pricing.workflow;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.broadleafcommerce.order.domain.FulfillmentGroup;
 import org.broadleafcommerce.order.domain.Order;
 import org.broadleafcommerce.pricing.module.ShippingModule;
+import org.broadleafcommerce.pricing.service.ShippingService;
 import org.broadleafcommerce.workflow.BaseActivity;
 import org.broadleafcommerce.workflow.ProcessContext;
 
 public class CalculateShipping extends BaseActivity {
 
-    private ShippingModule shippingModule;
+    @Resource
+    private ShippingService shippingService;
+
+    private String shippingModuleName;
+
+    public void setShippingModuleName(String shippingModuleName) {
+        this.shippingModuleName = shippingModuleName;
+    }
 
     @Override
     public ProcessContext execute(ProcessContext context) throws Exception {
         Order order = ((OfferContext)context).getSeedData();
 
         System.out.println("*** in CalculateShipping.execute()");
+        System.out.println("*** order: " + order);
 
+        ShippingModule shippingModule = shippingService.getShippingModuleByName(
+                shippingModuleName);
         /*
          * 1. Get FGs from Order
          * 2. take each FG and call shipping module with the shipping svc
@@ -30,22 +43,19 @@ public class CalculateShipping extends BaseActivity {
 
         while ( itr.hasNext() ) {
             FulfillmentGroup fulfillmentGroup = itr.next();
+            System.out.println("**** FG b4 change: " + fulfillmentGroup.getMethod());
+            //fulfillmentGroup.getMethod();
             fulfillmentGroup =
                 shippingModule.calculateShippingForFulfillmentGroup(fulfillmentGroup);
+
+            System.out.println("**** FG after change: " + fulfillmentGroup.getMethod());
             order.setFulfillmentGroups(fulfillmentGroups);
+            System.out.println("**** order after change: " + order);
         }
 
         // TODO Add code to calculate shipping
         context.setSeedData(order);
         return context;
-    }
-
-    public ShippingModule getShippingModule() {
-        return shippingModule;
-    }
-
-    public void setShippingModule(ShippingModule shippingModule) {
-        this.shippingModule = shippingModule;
     }
 
 }
