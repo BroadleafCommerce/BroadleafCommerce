af49a7ac7536474685e49d33fecdba9f154de218
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-05-06 15:52:30 +0000

    refactored the pricing manager a bit

--- BroadleafCommerce/src-framework/org/broadleafcommerce/pricing/service/advice/PricingExecutionManagerImpl.java
+++ BroadleafCommerce/src-framework/org/broadleafcommerce/pricing/service/advice/PricingExecutionManagerImpl.java
@@ -1,16 +1,35 @@
 package org.broadleafcommerce.pricing.service.advice;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.broadleafcommerce.order.domain.Order;
+import org.broadleafcommerce.pricing.service.PricingService;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Component("pricingExecutionManager")
-public class PricingExecutionManagerImpl implements PricingExecutionManager, Compoundable<Order> {
+public class PricingExecutionManagerImpl implements PricingExecutionManager, Compoundable<Order>, Ordered {
 
     private static final Log LOG = LogFactory.getLog(PricingExecutionManagerImpl.class);
 
     private static final ThreadLocal <Order> uniqueOrder  = new ThreadLocal <Order>();
+
+    private int order;
+
+    @Resource
+    private PricingService pricingService;
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
 
     public void clearCache() {
         uniqueOrder.remove();
@@ -28,5 +47,22 @@ public class PricingExecutionManagerImpl implements PricingExecutionManager, Com
         }
 
         return order;
+    }
+
+    public Object priceOrder(ProceedingJoinPoint call) throws Throwable {
+        clearCache();
+        Object returnValue;
+        try {
+            returnValue = call.proceed();
+        } finally {
+            Order orderItem = getLatestItem();
+            clearCache();
+            if (orderItem != null) {
+                pricingService.executePricing(orderItem);
+                LOG.debug("Context order priced : order id " + orderItem.getId());
+            }
+        }
+
+        return returnValue;
     }
 }
