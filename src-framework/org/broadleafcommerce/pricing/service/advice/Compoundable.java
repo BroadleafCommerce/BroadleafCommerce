0d2b857a1deb70a7021ebe1907e22c7988ff99ec
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-05-06 15:32:49 +0000

    update the pricing advice for cleaner usage

--- src-framework/org/broadleafcommerce/pricing/service/advice/Compoundable.java
+++ src-framework/org/broadleafcommerce/pricing/service/advice/Compoundable.java
@@ -1,6 +1,8 @@
 package org.broadleafcommerce.pricing.service.advice;
 
-public interface Compileable<T> {
+public interface Compoundable<T> {
+
+    public void clearCache();
 
     public T getLatestItem();
 
