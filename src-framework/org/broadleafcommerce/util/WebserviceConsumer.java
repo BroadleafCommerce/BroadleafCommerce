38af65904d559fae7ad126418e0fd0edcfec49a7
Sean Conlon <sconlon@credera.com>
2009-05-07 16:39:01 +0000

    removed everything EmailAProductController related, and updated the location of pertinent jars

--- src-framework/org/broadleafcommerce/util/WebserviceConsumer.java
+++
@@ -1,9 +0,0 @@
-package org.broadleafcommerce.util;
-
-public interface WebserviceConsumer {
-    String getWebserviceUrl();
-    void setWebserviceUrl(String webserviceUrl);
-
-    int getWebserviceTimeout();
-    void setWebserviceTimeout(int webserviceTimeout);
-}
