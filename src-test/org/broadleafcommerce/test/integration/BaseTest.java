f32d60ab7c09f753c753e53dd1b7f392c672cc1f
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-05-13 22:34:22 +0000

    updated some persistence information and changed the payment type to an enumeration.

--- src-test/org/broadleafcommerce/test/integration/BaseTest.java
+++ src-test/org/broadleafcommerce/test/integration/BaseTest.java
@@ -13,6 +13,15 @@ import org.testng.annotations.BeforeClass;
 @ContextConfiguration(locations = { "classpath:/applicationContext.xml", "classpath:/applicationContext-test.xml"})
 public abstract class BaseTest extends AbstractTransactionalTestNGSpringContextTests {
 
+    /*
+     * TODO each extension of this BaseTest is getting a datasource set via autowiring. However, with the
+     * new blSecurePU persistence unit, we will need to specify different datasources for the 2 persistence
+     * units. Spring does not like this, since autowiring for this class expects a single DS to be defined. Therefore,
+     * we need to override in xml the datasource being set. Possibly, we need to find a convenient way to do this without
+     * specifying every test class in xml. Not a priority, but will come up later as we add test cases that involve
+     * any of the secure payment info classes (i.e. CreditCardPaymentInfo).
+     */
+
     /** Logger for this class and subclasses */
     protected final Log logger = LogFactory.getLog(getClass());
 
