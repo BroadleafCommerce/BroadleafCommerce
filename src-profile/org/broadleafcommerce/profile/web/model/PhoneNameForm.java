743e46469392886e29a7d61996c6f14a66d288d4
Sean Conlon <sconlon@credera.com>
2009-05-20 20:42:30 +0000

    comments

--- src-profile/org/broadleafcommerce/profile/web/model/PhoneNameForm.java
+++ src-profile/org/broadleafcommerce/profile/web/model/PhoneNameForm.java
@@ -2,6 +2,15 @@ package org.broadleafcommerce.profile.web.model;
 
 import org.broadleafcommerce.profile.domain.Phone;
 
+/**
+ * The Form Backing Bean used by the CustomerPhoneController.  This design was chosen instead
+ * of placing multiple values on the request.  This is a smaller scenario since there are not many
+ * properties for the CustomerPhoneController, but to be consistent, we did not put a large amount of
+ * unnecessary parameters on the request.
+ * 
+ * @author sconlon
+ *
+ */
 public class PhoneNameForm {
     private Phone phone;
     private String phoneName;
