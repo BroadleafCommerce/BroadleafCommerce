7b9363128e618e5b5d56b12275cdc2ca5141b2fb
Vikalp Jain <vjain@credera.com>
2009-05-21 22:32:06 +0000

    deprecated certain methods per Brian's spec

--- BroadleafCommerce/src-auxiliary/org/broadleafcommerce/email/service/EmailService.java
+++ BroadleafCommerce/src-auxiliary/org/broadleafcommerce/email/service/EmailService.java
@@ -2,6 +2,7 @@ package org.broadleafcommerce.email.service;
 
 import java.util.HashMap;
 
+import org.broadleafcommerce.email.domain.EmailTarget;
 import org.broadleafcommerce.email.service.info.EmailInfo;
 
 /**
@@ -13,10 +14,10 @@ public interface EmailService {
     @SuppressWarnings("unchecked")
     public boolean sendTemplateEmail(final HashMap props);
 
-	@SuppressWarnings("unchecked")
-    public boolean sendTemplateEmail(final EmailInfo emailInfo, final HashMap props);
+    @SuppressWarnings("unchecked")
+    public boolean sendTemplateEmail(EmailInfo emailInfo, EmailTarget emailTarget, HashMap props);
 
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     public boolean sendBasicEmail(HashMap props);
 
 }
