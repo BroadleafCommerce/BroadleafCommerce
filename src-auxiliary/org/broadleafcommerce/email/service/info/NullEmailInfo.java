1c1242505965aa9697e961f9e61d28aac98c749a
Vikalp Jain <vjain@credera.com>
2009-05-26 19:29:50 +0000

    added blNullEmailInfo class

---
+++ src-auxiliary/org/broadleafcommerce/email/service/info/NullEmailInfo.java
@@ -0,0 +1,12 @@
+package org.broadleafcommerce.email.service.info;
+
+import java.io.IOException;
+
+public class NullEmailInfo extends EmailInfo {
+    private static final long serialVersionUID = 1L;
+
+    public NullEmailInfo() throws IOException {
+        super();
+    }
+
+}
