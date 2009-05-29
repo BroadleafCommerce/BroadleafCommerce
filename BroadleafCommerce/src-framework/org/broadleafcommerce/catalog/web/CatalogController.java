f929b94e65e021e84d3840fb60e6c5352af89c20
Vikalp Jain <vjain@credera.com>
2009-05-29 15:02:58 +0000

    added feature to pass in the category name instead of category id

--- BroadleafCommerce/src-framework/org/broadleafcommerce/catalog/web/CatalogController.java
+++ BroadleafCommerce/src-framework/org/broadleafcommerce/catalog/web/CatalogController.java
@@ -18,194 +18,206 @@ import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.util.UrlPathHelper;
 
 public class CatalogController extends AbstractController {
-	protected final Log logger = LogFactory.getLog(getClass());
-	private final UrlPathHelper pathHelper = new UrlPathHelper();
-	private CatalogService catalogService;
-	private String defaultCategoryView;
-	private String defaultProductView;
-	private Long rootCategoryId;
-	private String categoryTemplatePrefix;
+    protected final Log logger = LogFactory.getLog(getClass());
+    private final UrlPathHelper pathHelper = new UrlPathHelper();
+    private CatalogService catalogService;
+    private String defaultCategoryView;
+    private String defaultProductView;
+    private Long rootCategoryId;
+    private String rootCategoryName;
+    private String categoryTemplatePrefix;
 
-	@Override
+    @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request,
             HttpServletResponse response) {
 
-    	HashMap<String,Object> model = new HashMap<String,Object>();
+        HashMap<String,Object> model = new HashMap<String,Object>();
 
-    	addCategoryToModel(request, model);
-    	boolean productFound = addProductsToModel(request, model);
+        addCategoryToModel(request, model);
+        boolean productFound = addProductsToModel(request, model);
 
-    	String view = defaultCategoryView;
-      	if (productFound) {
-      		// TODO: Nice to have: product logic similar to category below
-      		view = defaultProductView;
-      	} else {
-      		Category currentCategory = (Category) model.get("currentCategory");
-      		if (currentCategory.getUrl() != null) {
-      			return new ModelAndView("redirect:"+currentCategory.getUrl());
-      		} else if (currentCategory.getDisplayTemplate() != null) {
-      			view = categoryTemplatePrefix + currentCategory.getDisplayTemplate();
-      		} else {
-      			view = defaultCategoryView;
-      		}
-      	}
-          return new ModelAndView(view, model);
-	}
+        String view = defaultCategoryView;
+        if (productFound) {
+            // TODO: Nice to have: product logic similar to category below
+            view = defaultProductView;
+        } else {
+            Category currentCategory = (Category) model.get("currentCategory");
+            if (currentCategory.getUrl() != null && !"".equals(currentCategory.getUrl())) {
+                return new ModelAndView("redirect:"+currentCategory.getUrl());
+            } else if (currentCategory.getDisplayTemplate() != null && !"".equals(currentCategory.getUrl())) {
+                view = categoryTemplatePrefix + currentCategory.getDisplayTemplate();
+            } else {
+                view = defaultCategoryView;
+            }
+        }
+        return new ModelAndView(view, model);
+    }
 
-	protected void addCategoryToModel(HttpServletRequest request, Map<String,Object> model ) {
-    	Category rootCategory = null;
-    	if (getRootCategoryId() != null) {
-    		rootCategory = catalogService.findCategoryById(getRootCategoryId());
-    	}
-    	if (rootCategory == null) {
-    		throw new IllegalStateException("Catalog Controller configured incorrectly - root category not found: " + rootCategoryId);
-    	}
-
-    	String url = pathHelper.getRequestUri(request).substring(pathHelper.getContextPath(request).length());
-        String categoryId = request.getParameter("categoryId");
-        if (categoryId != null) {
-        	Category category = catalogService.findCategoryById(new Long(categoryId));
-        	if (category != null) {
-        		url = category.getUrl();
-        	}
+    protected void addCategoryToModel(HttpServletRequest request, Map<String,Object> model ) {
+        Category rootCategory = null;
+        if (getRootCategoryId() != null) {
+            rootCategory = catalogService.findCategoryById(getRootCategoryId());
+        } else if (getRootCategoryName() != null) {
+            rootCategory = catalogService.findCategoryByName(getRootCategoryName());
         }
 
-    	List<Category> categoryList = rootCategory.getChildCategoryURLMap().get(url);
-    	addCategoryListToModel(categoryList, rootCategory, url, model);
-    	model.put("rootCategory", rootCategory);
-	}
+        if (rootCategory == null) {
+            throw new IllegalStateException("Catalog Controller configured incorrectly - root category not found: " + rootCategoryId);
+        }
 
-	protected int findProductPositionInList(Product product, List<Product> products) {
-		for (int i=0; i < products.size(); i++) {
-			Product currentProduct = products.get(i);
-			if (product.getId().equals(currentProduct.getId())) {
-				return i+1;
-			}
-		}
-		return 0;
-	}
+        String url = pathHelper.getRequestUri(request).substring(pathHelper.getContextPath(request).length());
+        String categoryId = request.getParameter("categoryId");
+        if (categoryId != null) {
+            Category category = catalogService.findCategoryById(new Long(categoryId));
+            if (category != null) {
+                url = category.getUrl();
+            }
+        }
 
-	protected boolean addCategoryListToModel(List<Category> categoryList, Category rootCategory, String url, Map<String,Object> model) {
-		boolean categoryError = false;
+        List<Category> categoryList = rootCategory.getChildCategoryURLMap().get(url);
+        addCategoryListToModel(categoryList, rootCategory, url, model);
+        model.put("rootCategory", rootCategory);
+    }
 
-		while (categoryList == null) {
-    		categoryError = true;
+    protected int findProductPositionInList(Product product, List<Product> products) {
+        for (int i=0; i < products.size(); i++) {
+            Product currentProduct = products.get(i);
+            if (product.getId().equals(currentProduct.getId())) {
+                return i+1;
+            }
+        }
+        return 0;
+    }
 
-    		int pos = url.indexOf("/");
-    		if (pos == -1) {
-        		categoryList = new ArrayList<Category>();
-        		categoryList.add(rootCategory);
-    		} else {
-    			url = url.substring(0, url.lastIndexOf("/"));
-        		categoryList = rootCategory.getChildCategoryURLMap().get(url);
-    		}
-    	}
+    protected boolean addCategoryListToModel(List<Category> categoryList, Category rootCategory, String url, Map<String,Object> model) {
+        boolean categoryError = false;
 
-    	model.put("breadcrumbCategories", categoryList);
-		model.put("currentCategory", categoryList.get(categoryList.size()-1));
-		model.put("categoryError", categoryError);
-		return categoryError;
-	}
+        while (categoryList == null) {
+            categoryError = true;
 
-	protected boolean validateProductAndAddToModel(Product product, Map<String,Object> model) {
-		Category currentCategory = (Category) model.get("currentCategory");
-		Category rootCategory = (Category) model.get("rootCategory");
-		int productPosition=0;
+            int pos = url.indexOf("/");
+            if (pos == -1) {
+                categoryList = new ArrayList<Category>();
+                categoryList.add(rootCategory);
+            } else {
+                url = url.substring(0, url.lastIndexOf("/"));
+                categoryList = rootCategory.getChildCategoryURLMap().get(url);
+            }
+        }
 
-		List<Product> productList = catalogService.findActiveProductsByCategory(currentCategory);
-		if (productList != null) {
-			model.put("currentProducts", productList);
-		}
-		productPosition = findProductPositionInList(product, productList);
-		if (productPosition == 0) {
-			// look for product in its default category and override category from request URL
-			currentCategory = product.getDefaultCategory();
-			productList = catalogService.findActiveProductsByCategory(currentCategory);
-			if (productList != null) {
-				model.put("currentProducts", productList);
-			}
-			String url = currentCategory.getGeneratedUrl();
+        model.put("breadcrumbCategories", categoryList);
+        model.put("currentCategory", categoryList.get(categoryList.size()-1));
+        model.put("categoryError", categoryError);
+        return categoryError;
+    }
 
-			// override category list settings using this products default
-			List<Category> categoryList = rootCategory.getChildCategoryURLMap().get(url);
-	    	if (categoryList != null && ! addCategoryListToModel(categoryList, rootCategory, url, model)) {
-				productPosition = findProductPositionInList(product, productList);
-	    	}
-		}
+    protected boolean validateProductAndAddToModel(Product product, Map<String,Object> model) {
+        Category currentCategory = (Category) model.get("currentCategory");
+        Category rootCategory = (Category) model.get("rootCategory");
+        int productPosition=0;
 
-		if (productPosition != 0) {
-			model.put("productError", false);
-			model.put("currentProduct", product);
-			model.put("productPosition", productPosition);
-			if (productPosition != 1) {
-				model.put("previousProduct", productList.get(productPosition-2));
-			}
-			if (productPosition < productList.size()) {
-				model.put("nextProduct", productList.get(productPosition));
-			}
-			model.put("totalProducts", productList.size());
-		} else {
-			model.put("productError", true);
-		}
+        List<Product> productList = catalogService.findActiveProductsByCategory(currentCategory);
+        if (productList != null) {
+            model.put("currentProducts", productList);
+        }
+        productPosition = findProductPositionInList(product, productList);
+        if (productPosition == 0) {
+            // look for product in its default category and override category from request URL
+            currentCategory = product.getDefaultCategory();
+            productList = catalogService.findActiveProductsByCategory(currentCategory);
+            if (productList != null) {
+                model.put("currentProducts", productList);
+            }
+            String url = currentCategory.getGeneratedUrl();
 
-		return (productPosition !=0);
-	}
+            // override category list settings using this products default
+            List<Category> categoryList = rootCategory.getChildCategoryURLMap().get(url);
+            if (categoryList != null && ! addCategoryListToModel(categoryList, rootCategory, url, model)) {
+                productPosition = findProductPositionInList(product, productList);
+            }
+        }
 
-	protected boolean addProductsToModel(HttpServletRequest request, Map<String,Object> model ) {
+        if (productPosition != 0) {
+            model.put("productError", false);
+            model.put("currentProduct", product);
+            model.put("productPosition", productPosition);
+            if (productPosition != 1) {
+                model.put("previousProduct", productList.get(productPosition-2));
+            }
+            if (productPosition < productList.size()) {
+                model.put("nextProduct", productList.get(productPosition));
+            }
+            model.put("totalProducts", productList.size());
+        } else {
+            model.put("productError", true);
+        }
+
+        return (productPosition !=0);
+    }
+
+    protected boolean addProductsToModel(HttpServletRequest request, Map<String,Object> model ) {
         boolean productFound = false;
 
         String productId = request.getParameter("productId");
         if (productId != null) {
-        	Product product = catalogService.findProductById(new Long(productId));
-        	if (product != null) {
-        		productFound = validateProductAndAddToModel(product, model);
-        	}
+            Product product = catalogService.findProductById(new Long(productId));
+            if (product != null) {
+                productFound = validateProductAndAddToModel(product, model);
+            }
         } else {
-        	Category currentCategory = (Category) model.get("currentCategory");
-        	List<Product> productList = catalogService.findActiveProductsByCategory(currentCategory);
-        	model.put("currentProducts", productList);
+            Category currentCategory = (Category) model.get("currentCategory");
+            List<Product> productList = catalogService.findActiveProductsByCategory(currentCategory);
+            model.put("currentProducts", productList);
         }
 
         return productFound;
-	}
+    }
 
-	public Long getRootCategoryId() {
-		return rootCategoryId;
-	}
+    public Long getRootCategoryId() {
+        return rootCategoryId;
+    }
 
-	public void setRootCategoryId(Long rootCategoryId) {
-		this.rootCategoryId = rootCategoryId;
-	}
+    public String getRootCategoryName() {
+        return rootCategoryName;
+    }
 
-	public CatalogService getCatalogService() {
-		return catalogService;
-	}
+    public void setRootCategoryId(Long rootCategoryId) {
+        this.rootCategoryId = rootCategoryId;
+    }
 
-	public void setCatalogService(CatalogService catalogService) {
-		this.catalogService = catalogService;
-	}
+    public void setRootCategoryName(String rootCategoryName) {
+        this.rootCategoryName = rootCategoryName;
+    }
 
-	public String getDefaultCategoryView() {
-		return defaultCategoryView;
-	}
+    public CatalogService getCatalogService() {
+        return catalogService;
+    }
 
-	public void setDefaultCategoryView(String defaultCategoryView) {
-		this.defaultCategoryView = defaultCategoryView;
-	}
+    public void setCatalogService(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
 
-	public String getDefaultProductView() {
-		return defaultProductView;
-	}
+    public String getDefaultCategoryView() {
+        return defaultCategoryView;
+    }
 
-	public void setDefaultProductView(String defaultProductView) {
-		this.defaultProductView = defaultProductView;
-	}
+    public void setDefaultCategoryView(String defaultCategoryView) {
+        this.defaultCategoryView = defaultCategoryView;
+    }
 
-	public String getCategoryTemplatePrefix() {
-		return categoryTemplatePrefix;
-	}
+    public String getDefaultProductView() {
+        return defaultProductView;
+    }
 
-	public void setCategoryTemplatePrefix(String categoryTemplatePrefix) {
-		this.categoryTemplatePrefix = categoryTemplatePrefix;
-	}
+    public void setDefaultProductView(String defaultProductView) {
+        this.defaultProductView = defaultProductView;
+    }
+
+    public String getCategoryTemplatePrefix() {
+        return categoryTemplatePrefix;
+    }
+
+    public void setCategoryTemplatePrefix(String categoryTemplatePrefix) {
+        this.categoryTemplatePrefix = categoryTemplatePrefix;
+    }
 }
