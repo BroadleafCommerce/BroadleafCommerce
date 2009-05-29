d1110f91d21e86fad4f37718c41baf920b98117e
Vikalp Jain <vjain@credera.com>
2009-05-29 16:55:45 +0000

    Correctly implemented composite ids

--- src-framework/org/broadleafcommerce/catalog/domain/CategoryXref.java
+++ src-framework/org/broadleafcommerce/catalog/domain/CategoryXref.java
@@ -3,8 +3,8 @@ package org.broadleafcommerce.catalog.domain;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
@@ -22,34 +22,19 @@ public class CategoryXref implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /** The category id. */
-    @Id
-    @Column(name = "CATEGORY_ID")
-    private Long categoryId;
+    @EmbeddedId
+    CategoryXrefPK categoryXrefPK;
 
-    /** The sub-category id. */
-    @Id
-    @Column(name = "SUB_CATEGORY_ID")
-    private Long subCategoryId;
+    public CategoryXrefPK getCategoryXrefPK() {
+        return categoryXrefPK;
+    }
+
+    public void setCategoryXrefPK(CategoryXrefPK categoryXrefPK) {
+        this.categoryXrefPK = categoryXrefPK;
+    }
 
     @Column(name = "DISPLAY_ORDER")
     private int displayOrder;
-
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public Long getSubCategoryId() {
-        return subCategoryId;
-    }
-
-    public void setSubCategoryId(Long subCategoryId) {
-        this.subCategoryId = subCategoryId;
-    }
 
     public int getDisplayOrder() {
         return displayOrder;
@@ -57,6 +42,50 @@ public class CategoryXref implements Serializable {
 
     public void setDisplayOrder(int displayOrder) {
         this.displayOrder = displayOrder;
+    }
+
+    public static class CategoryXrefPK implements Serializable {
+        /** The Constant serialVersionUID. */
+        private static final long serialVersionUID = 1L;
+
+        @Column(name = "CATEGORY_ID", nullable = false)
+        private Long categoryId;
+
+        /** The sub-category id. */
+        @Column(name = "SUB_CATEGORY_ID", nullable = false)
+        private Long subCategoryId;
+
+        public Long getCategoryId() {
+            return categoryId;
+        }
+
+        public void setCategoryId(Long categoryId) {
+            this.categoryId = categoryId;
+        }
+
+        public Long getSubCategoryId() {
+            return subCategoryId;
+        }
+
+        public void setSubCategoryId(Long subCategoryId) {
+            this.subCategoryId = subCategoryId;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) return false;
+            else if (!(obj instanceof CategoryXrefPK)) return false;
+
+            return categoryId.equals(((CategoryXrefPK) obj).getCategoryId())
+            && subCategoryId.equals(((CategoryXrefPK) obj).getSubCategoryId());
+        }
+
+        @Override
+        public int hashCode() {
+            return categoryId.hashCode() + subCategoryId.hashCode();
+        }
+
+
     }
 
 }
