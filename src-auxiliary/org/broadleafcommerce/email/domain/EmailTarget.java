4edfe23cd56a972c3a7d50a1393d94597032e975
Brian Polster <bpolster@broadleafcommerce.org>
2009-06-01 22:26:38 +0000

    Added comment

--- src-auxiliary/org/broadleafcommerce/email/domain/EmailTarget.java
+++ src-auxiliary/org/broadleafcommerce/email/domain/EmailTarget.java
@@ -1,12 +1,18 @@
 package org.broadleafcommerce.email.domain;
 
+/**
+ * The EmailTarget interface is used to specify the recipients of the email.
+ *
+ * @see EmailTargetImpl
+ * @author bpolster
+ */
 public interface EmailTarget {
-	
-	public String getEmailAddress();
-	public void setEmailAddress(String emailAddress);
-	public String[] getCCAddresses();
-	public void setCCAddresses(String[] ccAddresses);
-	public String[] getBCCAddresses();
-	public void setBCCAddresses(String[] BCCAddresses);
+
+    public String getEmailAddress();
+    public void setEmailAddress(String emailAddress);
+    public String[] getCCAddresses();
+    public void setCCAddresses(String[] ccAddresses);
+    public String[] getBCCAddresses();
+    public void setBCCAddresses(String[] BCCAddresses);
 
 }
