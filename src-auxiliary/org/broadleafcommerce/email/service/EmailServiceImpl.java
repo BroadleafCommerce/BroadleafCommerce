a0d677f66f4ea84c092765de1d980a896e48b4bd
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-06-02 01:05:02 +0000

    added license headers

--- src-auxiliary/org/broadleafcommerce/email/service/EmailServiceImpl.java
+++ src-auxiliary/org/broadleafcommerce/email/service/EmailServiceImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.broadleafcommerce.email.service;
 
 import java.util.HashMap;
@@ -32,7 +47,12 @@ public class EmailServiceImpl implements EmailService {
     protected MessageCreator messageCreator;
 
     public boolean sendTemplateEmail(EmailTarget emailTarget, EmailInfo emailInfo, HashMap<String,Object> props) {
-        if (props == null) return false;
+        if (props == null) {
+            props = new HashMap<String, Object>();
+        }
+        if (emailInfo == null) {
+            emailInfo = new EmailInfo();
+        }
 
         props.put(EmailPropertyType.INFO.toString(), emailInfo);
         props.put(EmailPropertyType.USER.toString(), emailTarget);
@@ -48,7 +68,12 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public boolean sendBasicEmail(EmailInfo emailInfo, EmailTarget emailTarget, HashMap<String,Object> props) {
-        if (props == null) return false;
+        if (props == null) {
+            props = new HashMap<String, Object>();
+        }
+        if (emailInfo == null) {
+            emailInfo = new EmailInfo();
+        }
 
         props.put(EmailPropertyType.INFO.toString(), emailInfo);
         props.put(EmailPropertyType.USER.toString(), emailTarget);
