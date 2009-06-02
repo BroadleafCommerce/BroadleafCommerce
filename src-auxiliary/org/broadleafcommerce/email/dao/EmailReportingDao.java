feb215c8c39ac7ac794e2f5bf026cc08b112899c
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-06-02 04:11:24 +0000

    fixed dao construction of entity

--- src-auxiliary/org/broadleafcommerce/email/dao/EmailReportingDao.java
+++ src-auxiliary/org/broadleafcommerce/email/dao/EmailReportingDao.java
@@ -15,6 +15,7 @@
  */
 package org.broadleafcommerce.email.dao;
 
+import org.broadleafcommerce.email.domain.EmailTarget;
 import org.broadleafcommerce.email.domain.EmailTracking;
 import org.broadleafcommerce.profile.domain.Customer;
 
@@ -23,10 +24,11 @@ import org.broadleafcommerce.profile.domain.Customer;
  *
  */
 public interface EmailReportingDao {
-	
-	public Long createTracking(String emailAddress, String type, String extraValue) ;
-	public void recordOpen(Long emailId, String userAgent);
-	public void recordClick(Long emailId, Customer customer, String destinationUri, String queryString);
-	public EmailTracking retrieveTracking(Long emailId);
-	
+
+    public Long createTracking(String emailAddress, String type, String extraValue) ;
+    public void recordOpen(Long emailId, String userAgent);
+    public void recordClick(Long emailId, Customer customer, String destinationUri, String queryString);
+    public EmailTracking retrieveTracking(Long emailId);
+    public EmailTarget createTarget();
+
 }
