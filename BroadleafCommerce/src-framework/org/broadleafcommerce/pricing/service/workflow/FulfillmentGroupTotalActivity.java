90c68ab2250d86ed6e96b4412c16bcc150982dd8
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-06-03 17:16:30 +0000

    removed the subtotal activity

--- BroadleafCommerce/src-framework/org/broadleafcommerce/pricing/service/workflow/FulfillmentGroupTotalActivity.java
+++ BroadleafCommerce/src-framework/org/broadleafcommerce/pricing/service/workflow/FulfillmentGroupTotalActivity.java
@@ -15,8 +15,6 @@
  */
 package org.broadleafcommerce.pricing.service.workflow;
 
-import java.util.List;
-
 import org.broadleafcommerce.order.domain.FulfillmentGroup;
 import org.broadleafcommerce.order.domain.FulfillmentGroupItem;
 import org.broadleafcommerce.order.domain.Order;
@@ -25,17 +23,17 @@ import org.broadleafcommerce.util.money.Money;
 import org.broadleafcommerce.workflow.BaseActivity;
 import org.broadleafcommerce.workflow.ProcessContext;
 
-public class SubtotalActivity extends BaseActivity {
+public class FulfillmentGroupTotalActivity extends BaseActivity {
 
     @Override
     public ProcessContext execute(ProcessContext context) throws Exception {
         Order order = ((PricingContext) context).getSeedData();
-        Money subTotal = new Money(0D);
+        /*Money subTotal = new Money(0D);
         List<OrderItem> orderItems = order.getOrderItems();
         for (OrderItem orderItem : orderItems) {
             subTotal = subTotal.add(orderItem.getPrice().multiply(orderItem.getQuantity()));
         }
-        order.setSubTotal(subTotal);
+        order.setSubTotal(subTotal);*/
 
         for(FulfillmentGroup fulfillmentGroup : order.getFulfillmentGroups()) {
             Money merchandiseTotal = new Money(0D);
