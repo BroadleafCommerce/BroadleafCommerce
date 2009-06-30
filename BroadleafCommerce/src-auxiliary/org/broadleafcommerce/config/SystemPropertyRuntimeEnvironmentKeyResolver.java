0a35f4b1c6a7d1dea996256dc53b68dd79625dc2
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-06-30 04:20:03 +0000

    updated some code formatting

--- BroadleafCommerce/src-auxiliary/org/broadleafcommerce/config/SystemPropertyRuntimeEnvironmentKeyResolver.java
+++ BroadleafCommerce/src-auxiliary/org/broadleafcommerce/config/SystemPropertyRuntimeEnvironmentKeyResolver.java
@@ -1,30 +1,23 @@
 package org.broadleafcommerce.config;
 
 /**
- * Determines the current runtime environment by reading a system property (specified in
- * environmentKey); if no system property is specified, a (reasonable) default of "runtime.environment"
- * is used.
- *
+ * Determines the current runtime environment by reading a system property
+ * (specified in environmentKey); if no system property is specified, a
+ * (reasonable) default of "runtime.environment" is used.
  */
-/*
- * TODO this should be supported directly in Spring 3.0.
- */
-public class SystemPropertyRuntimeEnvironmentKeyResolver implements RuntimeEnvironmentKeyResolver
-{
-    private String m_environmentKey = "runtime.environment";
+public class SystemPropertyRuntimeEnvironmentKeyResolver implements RuntimeEnvironmentKeyResolver {
 
-    public SystemPropertyRuntimeEnvironmentKeyResolver()
-    {
+    private String environmentKey = "runtime.environment";
+
+    public SystemPropertyRuntimeEnvironmentKeyResolver() {
         // EMPTY
     }
 
-    public String resolveRuntimeEnvironmentKey()
-    {
-        return System.getProperty( m_environmentKey );
+    public String resolveRuntimeEnvironmentKey() {
+        return System.getProperty(environmentKey);
     }
 
-    public void setEnvironmentKey( String environmentKey )
-    {
-        m_environmentKey = environmentKey;
+    public void setEnvironmentKey(String environmentKey) {
+        this.environmentKey = environmentKey;
     }
 }
