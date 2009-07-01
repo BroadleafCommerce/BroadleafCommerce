2b0b3d7503c8e609633f2792d2388967c16bbf42
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-07-01 23:18:02 +0000

    updated tests

--- BroadleafCommerce/src-auxiliary/org/broadleafcommerce/vendor/usps/service/message/AddressStandardAbbreviations.java
+++ BroadleafCommerce/src-auxiliary/org/broadleafcommerce/vendor/usps/service/message/AddressStandardAbbreviations.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.broadleafcommerce.vendor.usps.service.module;
+package org.broadleafcommerce.vendor.usps.service.message;
 
 import java.io.IOException;
 import java.util.Collections;
