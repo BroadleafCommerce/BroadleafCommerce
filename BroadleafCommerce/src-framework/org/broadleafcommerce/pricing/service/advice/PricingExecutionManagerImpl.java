1ae79156ce462a453944d06cb1fb39c714f8e043
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-07-08 21:00:34 +0000

    updated the save behavior during pricing

--- BroadleafCommerce/src-framework/org/broadleafcommerce/pricing/service/advice/PricingExecutionManagerImpl.java
+++ BroadleafCommerce/src-framework/org/broadleafcommerce/pricing/service/advice/PricingExecutionManagerImpl.java
@@ -77,6 +77,12 @@ public class PricingExecutionManagerImpl implements PricingExecutionManager, Ord
             Order orderItem = getLatestItem();
             clearCache();
             if (orderItem != null) {
+                /*
+                 * We need to save the order before pricing, because it is sometimes
+                 * possible for transient items to exist in the order hierarchy that will
+                 * cause problems downstream during pricing.
+                 */
+                orderDao.save(orderItem);
                 orderItem = pricingService.executePricing(orderItem);
                 orderDao.save(orderItem);
                 LOG.debug("Context order priced : order id " + orderItem.getId());
