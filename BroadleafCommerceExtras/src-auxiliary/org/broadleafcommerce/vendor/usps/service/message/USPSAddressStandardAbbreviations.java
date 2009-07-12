7eaa5cdfbce749f36fdbdd20cb74998621922d28
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-07-12 23:42:13 +0000

    initial commit for broadleaf commerce extras project

---
+++ BroadleafCommerceExtras/src-auxiliary/org/broadleafcommerce/vendor/usps/service/message/USPSAddressStandardAbbreviations.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.broadleafcommerce.vendor.usps.service.message;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.lang.UnhandledException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.core.io.Resource;
+
+public class USPSAddressStandardAbbreviations {
+
+    private static final Log LOG = LogFactory.getLog(USPSAddressStandardAbbreviations.class);
+
+    private Map<Object,Object> abbreviationMap;
+
+    public Map<Object,Object> getAbbreviationsMap() {
+        return abbreviationMap;
+    }
+
+    public void setAbbreviationPropertyFile(Resource abbreviationPropertyFile) {
+        try {
+            Properties props = new Properties();
+            props.load(abbreviationPropertyFile.getInputStream());
+            abbreviationMap = Collections.unmodifiableMap(props);
+        } catch (IOException e) {
+            LOG.error("Error loading USPSAddressStandardAbbreviations properties file using Resource: " + abbreviationPropertyFile, e);
+            throw new UnhandledException(e);
+        }
+    }
+}
