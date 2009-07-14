9f749c99c896feabcad5d47ed71d7231dfcd2648
David McLain <dmclain@credera.com>
2009-07-14 16:50:47 +0000

    Making org.blc.search.web an acceptable package in DependencyTest

--- src-test/org/broadleafcommerce/dependency/test/DependencyTest.java
+++ src-test/org/broadleafcommerce/dependency/test/DependencyTest.java
@@ -76,6 +76,7 @@ public class DependencyTest extends BaseTest {
         acceptablePackages.add("org.broadleafcommerce.email.web");
         acceptablePackages.add("org.broadleafcommerce.order.web");
         acceptablePackages.add("org.broadleafcommerce.profile.web");
+        acceptablePackages.add("org.broadleafcommerce.search.web");
         acceptablePackages.add("org.broadleafcommerce.checkout.web");
         acceptablePackages.add("org.broadleafcommerce.util.web");
     }
