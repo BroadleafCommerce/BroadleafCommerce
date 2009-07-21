32f84b49309e42fb848031d2520f3f785ea1f3cb
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-07-21 22:03:14 +0000

    enable additional email producers

--- src-auxiliary/org/broadleafcommerce/email/service/jms/EmailServiceProducer.java
+++ src-auxiliary/org/broadleafcommerce/email/service/jms/EmailServiceProducer.java
@@ -18,64 +18,32 @@ package org.broadleafcommerce.email.service.jms;
 import java.util.HashMap;
 
 import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
-import javax.jms.Session;
 
-import org.broadleafcommerce.email.service.info.EmailInfo;
-import org.broadleafcommerce.email.service.message.EmailPropertyType;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 
-/**
- * @author jfischer
- *
- */
-public class EmailServiceProducer {
-
-    private JmsTemplate emailServiceTemplate;
-
-    private Destination emailServiceDestination;
+public interface EmailServiceProducer {
 
     @SuppressWarnings("unchecked")
-    public void send(final HashMap props) {
-        emailServiceTemplate.send(emailServiceDestination, new MessageCreator() {
-            public Message createMessage(Session session) throws JMSException {
-                ObjectMessage message = session.createObjectMessage(props);
-                EmailInfo info = (EmailInfo) props.get(EmailPropertyType.INFO.getType());
-                message.setJMSPriority(Integer.parseInt(info.getSendAsyncPriority()));
-                return message;
-            }
-        });
-    }
+    public void send(final HashMap props);
 
     /**
      * @return the emailServiceTemplate
      */
-    public JmsTemplate getEmailServiceTemplate() {
-        return emailServiceTemplate;
-    }
+    public JmsTemplate getEmailServiceTemplate();
 
     /**
      * @param emailServiceTemplate the emailServiceTemplate to set
      */
-    public void setEmailServiceTemplate(JmsTemplate emailServiceTemplate) {
-        this.emailServiceTemplate = emailServiceTemplate;
-    }
+    public void setEmailServiceTemplate(JmsTemplate emailServiceTemplate);
 
     /**
      * @return the emailServiceDestination
      */
-    public Destination getEmailServiceDestination() {
-        return emailServiceDestination;
-    }
+    public Destination getEmailServiceDestination();
 
     /**
      * @param emailServiceDestination the emailServiceDestination to set
      */
-    public void setEmailServiceDestination(Destination emailServiceDestination) {
-        this.emailServiceDestination = emailServiceDestination;
-    }
+    public void setEmailServiceDestination(Destination emailServiceDestination);
 
 }
