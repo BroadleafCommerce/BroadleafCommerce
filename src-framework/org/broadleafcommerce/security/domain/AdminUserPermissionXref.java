bac9a2b5d60fdb8dcbb1678c08d17c6eadaa3fa0
Vikalp Jain <vjain@credera.com>
2009-07-22 15:41:03 +0000

    added enabled flag

--- src-framework/org/broadleafcommerce/security/domain/AdminUserPermissionXref.java
+++ src-framework/org/broadleafcommerce/security/domain/AdminUserPermissionXref.java
@@ -33,6 +33,17 @@ public class AdminUserPermissionXref {
     @EmbeddedId
     AdminUserPermissionXrefPK adminUserPermissionXrefPK;
 
+    @Column(name = "ADMIN_PERMISSION_ENABLED", nullable = false)
+    private boolean enabled;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
     public AdminUserPermissionXrefPK getAdminUserPermissionXrefPK() {
         return adminUserPermissionXrefPK;
     }
