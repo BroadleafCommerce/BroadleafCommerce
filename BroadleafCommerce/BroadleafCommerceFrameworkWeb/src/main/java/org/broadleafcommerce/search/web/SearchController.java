8823f6e1438586983e5a61e42e96c33588b925f6
David McLain <dmclain@credera.com>
2009-09-11 19:59:48 +0000

    re-fixing search controller because the compass context post processor doesn't inject the Compass instance if we rely on the annotations

--- BroadleafCommerce/BroadleafCommerceFrameworkWeb/src/main/java/org/broadleafcommerce/search/web/SearchController.java
+++ BroadleafCommerce/BroadleafCommerceFrameworkWeb/src/main/java/org/broadleafcommerce/search/web/SearchController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller("compassSearchController")
+@Controller
 public class SearchController {
 
     @Resource(name="blSearchService")
