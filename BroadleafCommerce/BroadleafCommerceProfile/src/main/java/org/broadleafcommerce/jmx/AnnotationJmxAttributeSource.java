a2a750a780ce76996f16522b08d7bb2f6e7bf3d7
Jeff Fischer <jfischer@broadleafcommerce.org>
2009-10-19 17:08:30 +0000

    Fixes defect #72 - admin and demo can't run together Added config to allow cobertura to be skipped when tests are skipped

---
+++ BroadleafCommerce/BroadleafCommerceProfile/src/main/java/org/broadleafcommerce/jmx/AnnotationJmxAttributeSource.java
@@ -0,0 +1,26 @@
+package org.broadleafcommerce.jmx;
+
+import org.springframework.jmx.export.metadata.InvalidMetadataException;
+import org.springframework.jmx.export.metadata.ManagedResource;
+
+public class AnnotationJmxAttributeSource extends org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource {
+	
+	private final String appName;
+	
+	public AnnotationJmxAttributeSource(String appName) {
+		this.appName = appName;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public ManagedResource getManagedResource(Class beanClass) throws InvalidMetadataException {
+		ManagedResource resource = super.getManagedResource(beanClass);
+		if (resource != null && appName != null) {
+			String objectName = resource.getObjectName();
+			objectName += "." + appName;
+			resource.setObjectName(objectName);
+		}
+		return resource;
+	}
+	
+}
