5a11254567abec84ee866a4705f2007dba33ea64
Bradford Taylor <btaylor@credera.com>
2009-12-11 17:15:22 +0000

    Added renderer to Content service that applies a string stylesheet to xml returned from DB

--- BroadleafCommerceFramework/src/main/java/org/broadleafcommerce/content/service/ContentServiceImpl.java
+++ BroadleafCommerceFramework/src/main/java/org/broadleafcommerce/content/service/ContentServiceImpl.java
@@ -16,22 +16,33 @@
 package org.broadleafcommerce.content.service;
 
 import java.io.Serializable;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.collections.map.LRUMap;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.broadleafcommerce.content.dao.ContentDao;
 import org.broadleafcommerce.content.dao.ContentDetailsDao;
 import org.broadleafcommerce.content.domain.Content;
 import org.broadleafcommerce.content.domain.ContentDetails;
 import org.broadleafcommerce.util.DateUtil;
+import org.compass.core.util.reader.StringReader;
 import org.mvel2.MVEL;
 import org.mvel2.ParserContext;
 import org.springframework.stereotype.Service;
+import org.xml.sax.InputSource;
 
 /**
  * @author btaylor
@@ -39,9 +50,11 @@ import org.springframework.stereotype.Service;
  */
 @Service("blContentService")
 public class ContentServiceImpl implements ContentService {
+    private static final Log LOG = LogFactory.getLog(ContentServiceImpl.class);
     private static final LRUMap EXPRESSION_CACHE = new LRUMap(100);
+    
 
-	@Resource(name="blContentDao")
+    @Resource(name="blContentDao")
 	protected ContentDao contentDao;
 	
 	@Resource(name="blContentDetailsDao")
@@ -86,6 +99,27 @@ public class ContentServiceImpl implements ContentService {
 		
 	}
 		
+	public String renderedContentDetails(String styleSheetString, List<ContentDetails> contentDetails) throws Exception{
+		Source xmlSource;
+		Writer resultWriter = new StringWriter();
+	    StreamResult result = new StreamResult(resultWriter);
+	    Source styleSheetSource = getSource(styleSheetString);
+	    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	    Transformer transformer = transformerFactory.newTransformer(styleSheetSource);
+	    
+	    for(ContentDetails contentDetail : contentDetails){
+	    	xmlSource = getSource(contentDetail.getXmlContent());	    	
+	    	try{
+	    		transformer.transform(xmlSource, result);	    	
+	    	}catch (Exception e){
+	    		LOG.error("Error during transformation. ",e);
+	    		throw e;
+	    	}
+	    	
+	    }
+		return resultWriter.toString();
+	}
+	
     protected Boolean executeExpression(String expression, Map<String, Object> vars) {
         Serializable exp = (Serializable)EXPRESSION_CACHE.get(expression);
         if (exp == null) {
@@ -97,5 +131,12 @@ public class ContentServiceImpl implements ContentService {
         return (Boolean)MVEL.executeExpression(exp, vars);
 
     }
-
+    
+    private Source getSource(String sourceString){
+	    StringReader styleSheetSourceReader = new StringReader(sourceString);
+	    InputSource inputStyleSheetSource = new InputSource(styleSheetSourceReader);
+	    return new SAXSource(inputStyleSheetSource);
+    	
+    }
+    
 }
