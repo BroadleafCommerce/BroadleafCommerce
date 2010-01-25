ecbcf6310662d2c0317f1ad3151b84b4906e9a4e
Jeff Fischer <jfischer@broadleafcommerce.org>
2010-01-25 21:50:27 +0000

    adding CyberSource tax support

--- BroadleafCommerceCyberSource/src/main/java/org/broadleafcommerce/vendor/cybersource/service/message/CyberSourceRequest.java
+++ BroadleafCommerceCyberSource/src/main/java/org/broadleafcommerce/vendor/cybersource/service/message/CyberSourceRequest.java
@@ -1,24 +1,12 @@
 package org.broadleafcommerce.vendor.cybersource.service.message;
 
 import org.broadleafcommerce.vendor.cybersource.service.type.CyberSourceServiceType;
-import org.broadleafcommerce.vendor.cybersource.service.type.CyberSourceTransactionType;
-import org.broadleafcommerce.vendor.cybersource.service.type.CyberSourceMethodType;
 
 public abstract class CyberSourceRequest implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private CyberSourceTransactionType transactionType;
 	private CyberSourceServiceType serviceType;
-	private CyberSourceMethodType methodType;
-	
-	public CyberSourceTransactionType getTransactionType() {
-		return transactionType;
-	}
-	
-	public void setTransactionType(CyberSourceTransactionType transactionType) {
-		this.transactionType = transactionType;
-	}
 	
 	public CyberSourceServiceType getServiceType() {
 		return serviceType;
@@ -28,12 +16,4 @@ public abstract class CyberSourceRequest implements java.io.Serializable {
 		this.serviceType = serviceType;
 	}
 
-	public CyberSourceMethodType getMethodType() {
-		return methodType;
-	}
-
-	public void setMethodType(CyberSourceMethodType methodType) {
-		this.methodType = methodType;
-	}
-	
 }
