6a6da3277c4c8bd9deb857f37e1825744ca317cd
Jeff Fischer <jfischer@broadleafcommerce.org>
2010-06-11 04:33:45 +0000

    updated the demo launcher parameter requirements

--- BroadleafCommerce/BroadleafCommerceDemoLauncher/src/main/java/org/broadleafcommerce/demo/Launcher.java
+++ BroadleafCommerce/BroadleafCommerceDemoLauncher/src/main/java/org/broadleafcommerce/demo/Launcher.java
@@ -9,7 +9,7 @@ import org.apache.tools.ant.ProjectHelper;
 public class Launcher {
 	
 	public static void main(String[] items) {
-		if (items.length == 0 || items.length > 1 || (!items[0].equals("start") && !items[0].equals("stop"))) {
+		if (items.length > 1 || (items.length == 1 && !items[0].equals("start") && !items[0].equals("stop"))) {
 			System.out.println("Usage pattern: java -jar broadleaf-demo-launcher-[version].jar start|stop");
 			System.out.println("Must provide an operation parameter - either start or stop");
 			System.exit(1);
@@ -23,7 +23,7 @@ public class Launcher {
 		project.addBuildListener(antLogger);
 		URL url = Launcher.class.getClassLoader().getResource("launch.xml");
 		ProjectHelper.getProjectHelper().parse(project, url);
-		if (items[0].equals("start")) {
+		if (items.length == 0 || items[0].equals("start")) {
 			project.executeTarget("jetty-start");
 		} else {
 			project.executeTarget("jetty-stop");
