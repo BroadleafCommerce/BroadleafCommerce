ed497eb1554a34bffc9e8040c9d08a9b91647f21
Jonathan Ball <jball@credera.com>
2010-06-29 14:22:20 +0000

    Add ability to enforce contextRelative to LocalRedirectStrategy

--- src-profile/org/broadleafcommerce/profile/web/security/LocalRedirectStrategy.java
+++ src-profile/org/broadleafcommerce/profile/web/security/LocalRedirectStrategy.java
@@ -12,8 +12,10 @@ public class LocalRedirectStrategy implements RedirectStrategy {
 
     private Logger logger = Logger.getLogger(this.getClass());
 
+    private boolean contextRelative;
+
     public void sendRedirect(HttpServletRequest request, HttpServletResponse response, String url) throws IOException {
-        String redirectUrl = calculateRedirectUrl(request.getContextPath(), url, request.getServerName());
+        String redirectUrl = calculateRedirectUrl(request.getContextPath(), url, request.getServerName(), request.getServerPort());
         redirectUrl = response.encodeRedirectURL(redirectUrl);
 
         if (logger.isDebugEnabled()) {
@@ -23,8 +25,16 @@ public class LocalRedirectStrategy implements RedirectStrategy {
         response.sendRedirect(redirectUrl);
     }
 
-    private String calculateRedirectUrl(String contextPath, String url, String requestServerName) {
+    private String calculateRedirectUrl(String contextPath, String url, String requestServerName, int requestServerPort) {
         if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            if (contextRelative) {
+                return url;
+            } else {
+                return contextPath + url;
+            }
+        }
+
+        if (!contextRelative) {
             return url;
         }
 
@@ -33,17 +43,37 @@ public class LocalRedirectStrategy implements RedirectStrategy {
 
         // Calculate the relative URL from the fully qualifed URL, minus the
         // protocol and base context.
-        url = url.substring(url.indexOf("://") + 3); // strip off protocol
-        if (originalUrl.startsWith("https://") && url.indexOf(':') > -1 && requestServerName.equals(url.substring(0, url.indexOf(':')))) {
-            return originalUrl;
-        } else {
-            url = url.substring(url.indexOf(contextPath) + contextPath.length());
+        String channelServer = "https://" + requestServerName;
+        if (originalUrl.startsWith(channelServer)) {
+            String temp = originalUrl.substring(channelServer.length());
+            if (temp.startsWith(":") || temp.startsWith("/") && temp.indexOf(contextPath) > -1) {
+                return originalUrl;
+            }
         }
+
+        // Calculate the relative URL from the fully qualified URL, minus the
+        // protocol and base context.
+        channelServer = "://" + requestServerName;
+
+        // strip off protocol and server
+        url = url.substring(url.indexOf(channelServer) + channelServer.length());
+        if (url.startsWith(":") && url.indexOf('/') > -1) {
+            url = url.substring(url.indexOf('/'));
+        }
+        url = url.substring(url.indexOf(contextPath) + contextPath.length());
 
         if (url.length() > 1 && url.charAt(0) == '/') {
             url = url.substring(1);
         }
 
         return url;
+    }
+
+    /**
+     * If <tt>true</tt>, causes any redirection URLs to be calculated minus the
+     * protocol and context path (defaults to <tt>false</tt>).
+     */
+    public void setContextRelative(boolean useRelativeContext) {
+        this.contextRelative = useRelativeContext;
     }
 }
