5847fe42e9774bf03edf8701a51770ad4cf57e11
Jeff Fischer <jfischer@broadleafcommerce.org>
2010-09-29 15:37:58 +0000

    RESOLVED - issue BLC-27: Further optimize how often price order calls are made during cart modification events http://www.broadleafcommerce.org/jira/browse/BLC-27

--- BroadleafCommerce/core/BroadleafCommerceFrameworkWeb/src/main/java/org/broadleafcommerce/order/web/CartController.java
+++ BroadleafCommerce/core/BroadleafCommerceFrameworkWeb/src/main/java/org/broadleafcommerce/order/web/CartController.java
@@ -149,8 +149,12 @@ public class CartController {
                 }
             }
         }
-
-        updateFulfillmentGroups(cartSummary, cart);
+        
+        if ((cart.getFulfillmentGroups() != null) && (cart.getFulfillmentGroups().size() > 1)) {
+        	updateFulfillmentGroups(cartSummary, cart);
+        } else {
+        	cartSummary.setFulfillmentGroup(cart.getFulfillmentGroups().get(0));
+        }
         cartSummary.setOrderDiscounts(cart.getTotalAdjustmentsValue().getAmount());
         model.addAttribute("cartSummary", cartSummary);
         return cartViewRedirect ? "redirect:" + cartView : cartView;
