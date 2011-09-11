58021199d94d51668f1f2e587aed9f1e7e3bcb7e
Jeff Fischer <jfischer@broadleafcommerce.org>
2011-09-11 01:36:44 +0000

    support image previews

---
+++ admin/BroadleafCommerceOpenAdminPlatform/src/main/java/org/broadleafcommerce/openadmin/server/service/artifact/image/effects/chain/filter/FilterTypeEnum.java
@@ -0,0 +1,19 @@
+package org.broadleafcommerce.openadmin.server.service.artifact.image.effects.chain.filter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jfischer
+ * Date: 9/10/11
+ * Time: 1:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum FilterTypeEnum {
+    ALTERHSB,
+    ALTERRGB,
+    AUTOLEVELSRGB,
+    CROP,
+    GAUSSIANBLUR,
+    RESIZE,
+    ROTATE,
+    UNSHARPMASK
+}
