f5c52121a7be710d07c3e6d7d4d99e9b41c5f0ac
Brian Polster <bpolster@broadleafcommerce.org>
2012-01-07 22:13:51 -0600

    Removed unnecessary code to aid in refactoring of OperationType.   Was setting a value to its default.

--- admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/client/presenter/customer/CustomerPresenter.java
+++ admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/client/presenter/customer/CustomerPresenter.java
@@ -29,7 +29,6 @@ import org.broadleafcommerce.admin.client.view.customer.CustomerDisplay;
 import org.broadleafcommerce.openadmin.client.BLCMain;
 import org.broadleafcommerce.openadmin.client.datasource.dynamic.ListGridDataSource;
 import org.broadleafcommerce.openadmin.client.dto.Entity;
-import org.broadleafcommerce.openadmin.client.dto.OperationType;
 import org.broadleafcommerce.openadmin.client.dto.OperationTypes;
 import org.broadleafcommerce.openadmin.client.dto.PersistencePackage;
 import org.broadleafcommerce.openadmin.client.dto.PersistencePerspective;
@@ -73,7 +72,6 @@ public class CustomerPresenter extends DynamicEntityPresenter implements Instant
 								
 								PersistencePerspective tempPerspective = new PersistencePerspective();
 			            		OperationTypes opTypes = new OperationTypes();
-			            		opTypes.setUpdateType(OperationType.ENTITY);
 			            		tempPerspective.setOperationTypes(opTypes);
 			            		
 								final Entity entity = new Entity();
