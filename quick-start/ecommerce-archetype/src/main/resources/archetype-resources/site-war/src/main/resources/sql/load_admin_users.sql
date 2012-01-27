6a970ca68c41988d59fab0cd9afd98925875f93b
Brian Polster <bpolster@broadleafcommerce.org>
2012-01-27 16:38:45 -0600

    Break import.sql into several files and run from site instead of admin.

---
+++ quick-start/ecommerce-archetype/src/main/resources/archetype-resources/site-war/src/main/resources/sql/load_admin_users.sql
@@ -0,0 +1,31 @@
+--
+-- The Archetype is configured with "hibernate.hbm2ddl.auto" value="create-drop" in "persistence-mycompany.xml".
+--
+-- This will cause hibernate to populate the database when the application is started by processing the files that
+-- were configured in the hibernate.hbm2ddl.import_files property.
+--
+-- This file is responsible for loading the BLC out-of-box Admin users.   You need at least one
+-- admin setup in the system in order to use the BLC admin.   You can use this file initially and
+-- then setup your own admin users, but you should always disable these default accounts in a real,
+-- production system.
+--
+
+--
+-- Sample admin users
+--
+INSERT INTO BLC_ADMIN_USER (ADMIN_USER_ID, EMAIL, LOGIN, NAME, PASSWORD) VALUES (1,'admin@yourdomain.com','admin','Administrator','admin');
+INSERT INTO BLC_ADMIN_USER (ADMIN_USER_ID, EMAIL, LOGIN, NAME, PASSWORD) VALUES (2,'merchandise@yourdomain.com','merchandise','Merchandise Manager','admin');
+INSERT INTO BLC_ADMIN_USER (ADMIN_USER_ID, EMAIL, LOGIN, NAME, PASSWORD) VALUES (3,'promo@yourdomain.com','promo','Promotion Manager','admin');
+INSERT INTO BLC_ADMIN_USER (ADMIN_USER_ID, EMAIL, LOGIN, NAME, PASSWORD) VALUES (4,'csr@yourdomain.com','csr','CSR','admin');
+INSERT INTO BLC_ADMIN_USER (ADMIN_USER_ID, EMAIL, LOGIN, NAME, PASSWORD) VALUES (5,'cms_edit@yourdomain.com','cmsEditor','CMS Editor','admin');
+INSERT INTO BLC_ADMIN_USER (ADMIN_USER_ID, EMAIL, LOGIN, NAME, PASSWORD) VALUES (6,'cms_approver@yourdomain.com','cmsApprover','CMS Approver','admin');
+
+--
+-- Roles for sample admin users.
+--
+INSERT INTO BLC_ADMIN_USER_ROLE_XREF (ADMIN_ROLE_ID, ADMIN_USER_ID) VALUES (1,1);
+INSERT INTO BLC_ADMIN_USER_ROLE_XREF (ADMIN_ROLE_ID, ADMIN_USER_ID) VALUES (2,2);
+INSERT INTO BLC_ADMIN_USER_ROLE_XREF (ADMIN_ROLE_ID, ADMIN_USER_ID) VALUES (3,3);
+INSERT INTO BLC_ADMIN_USER_ROLE_XREF (ADMIN_ROLE_ID, ADMIN_USER_ID) VALUES (4,4);
+INSERT INTO BLC_ADMIN_USER_ROLE_XREF (ADMIN_ROLE_ID, ADMIN_USER_ID) VALUES (5,5);
+INSERT INTO BLC_ADMIN_USER_ROLE_XREF (ADMIN_ROLE_ID, ADMIN_USER_ID) VALUES (6,6);
