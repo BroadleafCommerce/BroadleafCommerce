dcf857e6519b7ebeea4f638cc4b4d6407c569dae
Jeff Fischer <jfischer@broadleafcommerce.org>
2012-02-11 18:47:58 -0600

    BLC-329 : If using mod_proxy through apache, the callback function in the admin is not executed after an asset upload(cherry picked from commit db969de)

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/artifact/upload/UploadCompletedView.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/artifact/upload/UploadCompletedView.java
@@ -17,12 +17,12 @@
 package org.broadleafcommerce.openadmin.server.service.artifact.upload;
 
 
-import org.springframework.web.servlet.View;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStreamWriter;
 import java.util.Map;
+
+import org.springframework.web.servlet.View;
 
 /**
  * Created by jfischer
@@ -37,7 +37,15 @@ public class UploadCompletedView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         StringBuffer sb = new StringBuffer();
-        sb.append("<html><head><script type=\"text/javascript\">");
+        sb.append("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        sb.append("<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">");
+        sb.append("<head>");
+        sb.append("<META HTTP-EQUIV=\"Pragma\" CONTENT=\"no-cache\">");
+        sb.append("<META HTTP -EQUIV=\"Expires\" CONTENT=\"-1\">");
+        sb.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />");
+        sb.append("<meta http-equiv=\"Content- Language\" content=\"en-us\" />");
+        sb.append("<meta http-equiv=\"Content-Author\" content=\"Broadleaf Commerce\" />");
+        sb.append("<script type=\"text/javascript\">");
         sb.append("window.top.");
         sb.append(model.get("callbackName"));
         sb.append("('(");
@@ -47,8 +55,14 @@ public class UploadCompletedView implements View {
             sb.append(model.get("result"));
         }
         sb.append(")');");
-        sb.append("</script></head><body>Upload Completed</body></html>");
+        sb.append("</script>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("Upload Completed");
+        sb.append("</body>");
+        sb.append("</html>");
 
+        response.setContentType("text/html");
         OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream());
         writer.write(sb.toString());
         writer.flush();
