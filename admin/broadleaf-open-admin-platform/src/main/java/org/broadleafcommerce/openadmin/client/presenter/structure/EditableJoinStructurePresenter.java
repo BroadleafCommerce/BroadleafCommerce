25595077b402659fd4a380b4630d20ce8acc29fe
Jeff Fischer <jfischer@broadleafcommerce.org>
2012-02-29 17:02:32 -0600

    BLC-349 : When adding a join structure member to a grid structure view, an NumberFormatException occurs on the server if you try to delete it immediately(cherry picked from commit d92003b)

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/client/presenter/structure/EditableJoinStructurePresenter.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/client/presenter/structure/EditableJoinStructurePresenter.java
@@ -16,10 +16,15 @@
 
 package org.broadleafcommerce.openadmin.client.presenter.structure;
 
+import java.util.Map;
+
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.smartgwt.client.data.Criteria;
 import com.smartgwt.client.data.DSCallback;
 import com.smartgwt.client.data.DSRequest;
 import com.smartgwt.client.data.DSResponse;
+import com.smartgwt.client.data.ResultSet;
+import com.smartgwt.client.types.DSOperationType;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.grid.ListGrid;
@@ -34,16 +39,17 @@ import com.smartgwt.client.widgets.grid.events.SelectionChangedHandler;
 import com.smartgwt.client.widgets.grid.events.SelectionEvent;
 import com.smartgwt.client.widgets.tree.TreeNode;
 import org.broadleafcommerce.openadmin.client.BLCMain;
+import org.broadleafcommerce.openadmin.client.callback.ItemEdited;
+import org.broadleafcommerce.openadmin.client.callback.ItemEditedHandler;
 import org.broadleafcommerce.openadmin.client.callback.SearchItemSelected;
 import org.broadleafcommerce.openadmin.client.callback.SearchItemSelectedHandler;
+import org.broadleafcommerce.openadmin.client.datasource.dynamic.AbstractDynamicDataSource;
 import org.broadleafcommerce.openadmin.client.datasource.dynamic.DynamicEntityDataSource;
 import org.broadleafcommerce.openadmin.client.dto.JoinStructure;
 import org.broadleafcommerce.openadmin.client.dto.PersistencePerspectiveItemType;
 import org.broadleafcommerce.openadmin.client.presenter.entity.AbstractSubPresentable;
 import org.broadleafcommerce.openadmin.client.view.dynamic.dialog.EntitySearchDialog;
 import org.broadleafcommerce.openadmin.client.view.dynamic.grid.GridStructureDisplay;
-
-import java.util.Map;
 
 /**
  * 
@@ -84,7 +90,35 @@ public class EditableJoinStructurePresenter extends AbstractSubPresentable {
 						public void onSearchItemSelected(SearchItemSelected event) {
 							Map initialValues = ((DynamicEntityDataSource) display.getGrid().getDataSource()).extractRecordValues((TreeNode) event.getRecord());
 							initialValues.put("backup_id", ((DynamicEntityDataSource) display.getGrid().getDataSource()).getPrimaryKeyValue(event.getRecord()));
-							BLCMain.ENTITY_ADD.editNewRecord(joinStructureEditTitle, (DynamicEntityDataSource) display.getGrid().getDataSource(), initialValues, null, joinStructureFields, null);
+							BLCMain.ENTITY_ADD.editNewRecord(joinStructureEditTitle, (DynamicEntityDataSource) display.getGrid().getDataSource(), initialValues, new ItemEditedHandler() {
+                                public void onItemEdited(ItemEdited event) {
+                                    ListGridRecord[] recordList = new ListGridRecord[]{(ListGridRecord) event.getRecord()};
+                                    DSResponse updateResponse = new DSResponse();
+                                    updateResponse.setData(recordList);
+                                    DSRequest updateRequest = new DSRequest();
+                                    updateRequest.setOperationType(DSOperationType.UPDATE);
+                                    display.getGrid().getDataSource().updateCaches(updateResponse, updateRequest);
+                                    display.getGrid().deselectAllRecords();
+                                    display.getGrid().selectRecord(display.getGrid().getRecordIndex(event.getRecord()));
+                                    String primaryKey = display.getGrid().getDataSource().getPrimaryKeyFieldName();
+                                    ResultSet results = display.getGrid().getResultSet();
+                                    boolean foundRecord = false;
+                                    if (results != null) {
+                                        foundRecord = display.getGrid().getResultSet().find(primaryKey, event.getRecord().getAttribute(primaryKey)) != null;
+                                    }
+                                    if (!foundRecord) {
+                                        ((AbstractDynamicDataSource) display.getGrid().getDataSource()).setAddedRecord(event.getRecord());
+                                        display.getGrid().getDataSource().
+                                            fetchData(new Criteria("blc.fetch.from.cache", event.getRecord().getAttribute(primaryKey)), new DSCallback() {
+                                                @Override
+                                                public void execute(DSResponse response, Object rawData, DSRequest request) {
+                                                    display.getGrid().setData(response.getData());
+                                                    display.getGrid().selectRecord(0);
+                                                }
+                                            });
+                                    }
+                                }
+                            }, joinStructureFields, null);
 						}
 					});
 				}
@@ -125,7 +159,7 @@ public class EditableJoinStructurePresenter extends AbstractSubPresentable {
 		removedClickedHandlerRegistration = display.getRemoveButton().addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				if (event.isLeftButtonDown()) {
-					display.getGrid().removeData(display.getGrid().getSelectedRecord(), new DSCallback() {
+                    display.getGrid().removeData(display.getGrid().getSelectedRecord(), new DSCallback() {
 						public void execute(DSResponse response, Object rawData, DSRequest request) {
 							display.getRemoveButton().disable();
 						}
@@ -136,7 +170,12 @@ public class EditableJoinStructurePresenter extends AbstractSubPresentable {
         rowDoubleClickedHandlerRegistration = display.getGrid().addCellDoubleClickHandler(new CellDoubleClickHandler() {
             @Override
             public void onCellDoubleClick(CellDoubleClickEvent cellDoubleClickEvent) {
-                BLCMain.ENTITY_ADD.editRecord(joinStructureEditTitle, (DynamicEntityDataSource) display.getGrid().getDataSource(), display.getGrid().getSelectedRecord(), null, joinStructureFields, null, readOnly);
+                BLCMain.ENTITY_ADD.editRecord(joinStructureEditTitle, (DynamicEntityDataSource) display.getGrid().getDataSource(), display.getGrid().getSelectedRecord(), new ItemEditedHandler() {
+                    @Override
+                    public void onItemEdited(ItemEdited event) {
+                        display.getRemoveButton().disable();
+                    }
+                }, joinStructureFields, null, readOnly);
             }
         });
 	}
