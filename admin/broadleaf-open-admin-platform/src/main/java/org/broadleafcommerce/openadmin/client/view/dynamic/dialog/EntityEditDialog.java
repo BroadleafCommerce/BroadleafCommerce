56bca012d4f4d04347187977668281ec4d813a7b
Jeff Fischer <jfischer@broadleafcommerce.org>
2012-03-26 16:17:00 -0500

    BLC-371 - Add api support for server side validation for the admin

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/client/view/dynamic/dialog/EntityEditDialog.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/client/view/dynamic/dialog/EntityEditDialog.java
@@ -114,6 +114,7 @@ public class EntityEditDialog extends Window {
                                 hide();
                                 isHidden = true;
                             }
+                            saveButton.enable();
                             cancelButton.enable();
 						}
             		});
