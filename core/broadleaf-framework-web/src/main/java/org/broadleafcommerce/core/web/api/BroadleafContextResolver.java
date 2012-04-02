e67172f862b712befdb592019b14f66f28e57682
Phillip Verheyden <pverheyden@gmail.com>
2012-04-02 12:47:18 -0500

    Added a custom JAXBContextResolver to determine the correct serialization context with respect to custom entity overrides. This also required a change in the EntityConfiguration to return back the bean names from the entity applicationcontexts.

---
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/api/BroadleafContextResolver.java
@@ -0,0 +1,90 @@
+package org.broadleafcommerce.core.web.api;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import javax.annotation.Resource;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.broadleafcommerce.common.persistence.EntityConfiguration;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * JAXB has trouble mapping interfaces, therefore this allows us to find the correct implementation
+ * for interfaces and set up the appropriate JAXBContext.
+ * 
+ * For example, a ProductSkuImpl is related to SkuImpl. If SkuImpl is subclassed with a custom implementation
+ * (e.g. MyCompanySkuImpl) then MyCompanySkuImpl will be added to the JAXBContext for serialization instead
+ * of SkuImpl.
+ * 
+ * @author phillipverheyden
+ *
+ */
+@Component
+@Provider
+@Produces(value={MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public class BroadleafContextResolver implements ContextResolver<JAXBContext>, InitializingBean {
+	
+	@Resource(name="blEntityConfiguration")
+	private EntityConfiguration ec;
+	
+	private JAXBContext context;
+	
+	/**
+	 * getContext can pass in an implementation. Store the implementation details from the EntityConfiguration
+	 */
+	private HashSet<String> domainImplementationNames = new HashSet<String>();
+	
+	@Override
+	public JAXBContext getContext(Class<?> type) {
+		if (domainImplementationNames.contains(type.getName())) {
+			return context;
+		}
+		
+		try {
+			if (ec.lookupEntityClass(type.getName()) != null){
+				return context;
+			}
+			
+			return null;
+			
+		} catch (BeansException e) {
+			//bean not found in the context
+			return null;
+		}
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		String[] entityBeans = ec.getEntityBeanNames();
+		Class<?>[] classes = new Class<?>[entityBeans.length];
+		for (int i = 0; i < classes.length; i++) {
+			classes[i] = ec.lookupEntityClass(entityBeans[i]);
+		}
+		
+		//Go through the declared beans and only add the ones declared via JAXB annotations to the JAXB context
+		ArrayList<Class<?>> xmlClasses = new ArrayList<Class<?>>();
+		for (Class<?> clazz : classes) {
+			if (clazz.isAnnotationPresent(XmlRootElement.class) || clazz.isAnnotationPresent(XmlType.class)) {
+				xmlClasses.add(clazz);
+				domainImplementationNames.add(clazz.getName());
+			}
+		}
+		
+		try {
+			context = JAXBContext.newInstance(xmlClasses.toArray(new Class<?>[xmlClasses.size()]));
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}		
+	}
+
+}
