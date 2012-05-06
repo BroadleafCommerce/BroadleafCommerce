5342573aaea1110baf64953aea9de46a3e5ec6fa
Phillip Verheyden <pverheyden@gmail.com>
2012-05-06 02:15:48 -0500

    Updated docs for CurrencyConversionService

--- common/src/main/java/org/broadleafcommerce/common/money/CurrencyConversionService.java
+++ common/src/main/java/org/broadleafcommerce/common/money/CurrencyConversionService.java
@@ -27,7 +27,8 @@ public interface CurrencyConversionService {
 	 * @param destinationCurrency - which Currency to convert to
 	 * @param destinationScale - the scale that the result will be in. If zero, this defaults to the scale of <b>source</b> 
 	 * and if that is zero, defaults to {@code BankersRounding.DEFAULT_SCALE}
-	 * @return a new Money in <b>destinationCurrency</b>
+	 * @return a new Money in <b>destinationCurrency</b>. If the source and destination are the same currency, the original
+	 * source is returned unchanged
 	 */
 	public Money convertCurrency(Money source, Currency destinationCurrency, int destinationScale);
 	
