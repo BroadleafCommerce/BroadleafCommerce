629471a1df45a288b40d7e9aedea09805d77f3a3
Brian Polster <bpolster@broadleafcommerce.org>
2012-05-24 20:37:27 -0500

    BLC-505 - Change IDGeneration to use intelligent defaults

--- common/src/main/java/org/broadleafcommerce/common/persistence/IdOverrideTableGenerator.java
+++ common/src/main/java/org/broadleafcommerce/common/persistence/IdOverrideTableGenerator.java
@@ -49,6 +49,21 @@ public class IdOverrideTableGenerator extends TableGenerator {
 
     @Override
     public void configure(Type type, Properties params, Dialect dialect) throws MappingException {
+        if (params.get("table_name") == null) {
+            params.put("table_name", "SEQUENCE_GENERATOR");
+        }
+
+        if (params.get("segment_column_name") == null) {
+            params.put("segment_column_name", "ID_NAME");
+        }
+
+        if (params.get("value_column_name") == null) {
+            params.put("value_column_name", "ID_VAL");
+        }
+
+        if (params.get("increment_size") == null) {
+            params.put("increment_size", 50);
+        }
         super.configure(type, params, dialect);
         entityName = (String) params.get(ENTITY_NAME_PARAM);
     }
