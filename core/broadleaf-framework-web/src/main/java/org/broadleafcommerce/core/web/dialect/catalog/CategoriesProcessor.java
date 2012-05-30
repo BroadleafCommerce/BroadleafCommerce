915fe5ea7be71403117871c70f50c67ed249deb1
Andre Azzolini <apazzolini@gmail.com>
2012-05-29 22:59:46 -0500

    Add "maxResults" to the CategoriesProcessor

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/dialect/catalog/CategoriesProcessor.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/dialect/catalog/CategoriesProcessor.java
@@ -2,6 +2,7 @@ package org.broadleafcommerce.core.web.dialect.catalog;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.broadleafcommerce.core.catalog.domain.Category;
 import org.broadleafcommerce.core.catalog.service.CatalogService;
 import org.broadleafcommerce.core.web.dialect.AbstractModelVariableModifierProcessor;
@@ -31,14 +32,21 @@ public class CategoriesProcessor extends AbstractModelVariableModifierProcessor 
 	protected void modifyModelAttributes(Arguments arguments, Element element) {
 		CatalogService catalogService = ProcessorUtils.getCatalogService(arguments);
 		
-		String resultVar = element.getAttributeValueFromNormalizedName("resultvar");
-		String parentCategory = element.getAttributeValueFromNormalizedName("parentcategory");
+		String resultVar = element.getAttributeValue("resultVar");
+		String parentCategory = element.getAttributeValue("parentCategory");
+		String unparsedMaxResults = element.getAttributeValue("maxResults");
 		
 		// TODO: Potentially write an algorithm that will pick the minimum depth category
 		// instead of the first category in the list
 		List<Category> categories = catalogService.findCategoriesByName(parentCategory);
 		if (categories != null && categories.size() > 0) {
-			addToModel(resultVar, catalogService.findAllSubCategories(categories.get(0)));
+			List<Category> subcategories = catalogService.findAllSubCategories(categories.get(0));
+			if (StringUtils.isNotEmpty(unparsedMaxResults)) {
+				int maxResults = Integer.parseInt(unparsedMaxResults);
+				subcategories = subcategories.subList(0, maxResults);
+			}
+			
+			addToModel(resultVar, subcategories);
 		}
 	}
 }
