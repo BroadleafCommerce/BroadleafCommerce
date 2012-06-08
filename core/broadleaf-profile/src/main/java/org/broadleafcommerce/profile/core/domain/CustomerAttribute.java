1cf633070ab5b6300b0fc6055d957b32f219c9a6
Brian Polster <bpolster@broadleafcommerce.org>
2012-06-08 02:57:08 -0500

    BLC-526 - Add Customer attribute domain classes.

---
+++ core/broadleaf-profile/src/main/java/org/broadleafcommerce/profile/core/domain/CustomerAttribute.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.profile.core.domain;
+
+import java.io.Serializable;
+
+
+/**
+ * Implementations of this interface are used to hold data about a Customers Attributes.
+ * <br>
+ * For high volume sites, you should consider extending the BLC Customer entity instead of
+ * relying on custom attributes as the extension mechanism is more performant under load.
+ *
+ * @see {@link CustomerAttributeImpl}, {@link Customer}
+ * @author bpolster
+ *
+ */
+public interface CustomerAttribute extends Serializable {
+
+    /**
+     * Gets the id.
+     *
+     * @return the id
+     */
+    public Long getId();
+
+    /**
+     * Sets the id.
+     *
+     * @param id the new id
+     */
+    public void setId(Long id);
+    
+    /**
+     * Gets the name.  (e.g. shoeSize)
+     *
+     * @return the name
+     */
+    public String getName();
+
+    /**
+     * Sets the name.
+     *
+     * @param name the new name
+     */
+    public void setName(String name);
+
+    /**
+     * Gets the value.   (e.g. 9.5)
+     *
+     * @return the value
+     */
+    public String getValue();
+
+    /**
+     * Sets the value.
+     *
+     * @param value the new value
+     */
+    public void setValue(String value);   
+
+    /**
+     * Gets the associated customer.
+     *
+     * @return the customer
+     */
+    public Customer getCustomer();
+
+    /**
+     * Sets the associated customer.
+     *
+     * @param customer
+     */
+    public void setCustomer(Customer customer);
+}
---
+++ core/broadleaf-profile/src/main/java/org/broadleafcommerce/profile/core/domain/CustomerAttributeImpl.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.profile.core.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.*;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name="BLC_CUSTOMER_ATTRIBUTE")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region="blStandardElements")
+public class CustomerAttributeImpl implements CustomerAttribute {
+
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 1L;
+
+    /** The id. */
+    @Id
+    @GeneratedValue(generator= "CustomerAttributeId")
+    @GenericGenerator(
+        name="CustomerAttributeId",
+        strategy="org.broadleafcommerce.common.persistence.IdOverrideTableGenerator",
+        parameters = {
+            @Parameter(name="segment_value", value="CustomerAttributeImpl"),
+            @Parameter(name="entity_name", value="org.broadleafcommerce.profile.core.domain.CustomerAttributeImpl")
+        }
+    )
+    @Column(name = "CUSTOMER_ATTR_ID")
+    protected Long id;
+    
+    /** The name. */
+    @Column(name = "NAME", nullable=false)
+    protected String name;
+
+    /** The value. */
+    @Column(name = "VALUE", nullable=false)
+    protected String value;
+  
+    /** The customer. */
+    @ManyToOne(targetEntity = CustomerImpl.class, optional=false)
+    @JoinColumn(name = "CUSTOMER_ID")
+    protected Customer customer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CustomerAttributeImpl other = (CustomerAttributeImpl) obj;
+
+        if (id != null && other.id != null) {
+            return id.equals(other.id);
+        }
+
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (customer == null) {
+            if (other.customer != null)
+                return false;
+        } else if (!customer.equals(other.customer))
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
+}
