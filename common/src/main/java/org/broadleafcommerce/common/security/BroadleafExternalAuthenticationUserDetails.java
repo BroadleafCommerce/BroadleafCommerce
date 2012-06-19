3f09b37cc00520a9a4545c3c37afc1ef7ead527f
Kelly Tisdell <ktisdell@broadleafcommerce.org>
2012-06-19 16:53:21 -0500

    Refactored code for use in Active Directory situation

---
+++ common/src/main/java/org/broadleafcommerce/common/security/BroadleafExternalAuthenticationUserDetails.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.common.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+/**
+ * This is an extension of Spring's User class to provide additional data to the UserDetails interface.  This should be used by derivitave
+ * authentication providers to return an instance of UserDetails when authenticating against a system other than the Broadleaf tables (e.g. LDAP)
+ * <p/>
+ * User: Kelly Tisdell
+ * Date: 6/19/12
+ */
+public class BroadleafExternalAuthenticationUserDetails extends User {
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    private String email;
+    
+    public BroadleafExternalAuthenticationUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
