11a0cde835de2356f234068bb27770e1df7187e5
Kelly Tisdell <ktisdell@broadleafcommerce.org>
2012-06-25 14:49:55 -0500

    Added functionality to support DB-driven system properties

---
+++ common/src/main/java/org/broadleafcommerce/common/config/service/SystemPropertiesService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.common.config.service;
+
+import org.broadleafcommerce.common.config.domain.SystemProperty;
+
+import java.util.List;
+
+/**
+ * To change this template use File | Settings | File Templates.
+ * <p/>
+ * User: Kelly Tisdell
+ * Date: 6/25/12
+ */
+public interface SystemPropertiesService {
+
+    public SystemProperty saveSystemProperty(SystemProperty systemProperty);
+
+    public void deleteSystemProperty(SystemProperty systemProperty);
+
+    public List<SystemProperty> findAllSystemProperties();
+
+    public SystemProperty findSystemPropertyByName(String name);
+
+    /**
+     * This method should not persist anything to the database. It should simply return the correct implementation of
+     * the SystemProperty interface.
+     * @return
+     */
+    public SystemProperty createNewSystemProperty();
+}
---
+++ common/src/main/java/org/broadleafcommerce/common/config/service/SystemPropertiesServiceImpl.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.common.config.service;
+
+import org.broadleafcommerce.common.config.dao.SystemPropertiesDao;
+import org.broadleafcommerce.common.config.domain.SystemProperty;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * To change this template use File | Settings | File Templates.
+ * <p/>
+ * User: Kelly Tisdell
+ * Date: 6/25/12
+ */
+@Service("blSystemPropertiesService")
+public class SystemPropertiesServiceImpl implements SystemPropertiesService{
+
+    @Resource(name="blSystemPropertiesDao")
+    protected SystemPropertiesDao systemPropertiesDao;
+
+    @Override
+    @Transactional("blTransactionManager")
+    public SystemProperty saveSystemProperty(SystemProperty systemProperty) {
+        return systemPropertiesDao.saveSystemProperty(systemProperty);
+    }
+
+    @Override
+    @Transactional("blTransactionManager")
+    public void deleteSystemProperty(SystemProperty systemProperty) {
+        systemPropertiesDao.deleteSystemProperty(systemProperty);
+    }
+
+    @Override
+    @Transactional("blTransactionManager")
+    public List<SystemProperty> findAllSystemProperties() {
+        return systemPropertiesDao.readAllSystemProperties();
+    }
+
+    @Override
+    @Transactional("blTransactionManager")
+    public SystemProperty findSystemPropertyByName(String name) {
+        return systemPropertiesDao.readSystemPropertyByName(name);
+    }
+
+    @Override
+    @Transactional("blTransactionManager")
+    public SystemProperty createNewSystemProperty() {
+        return systemPropertiesDao.createNewSystemProperty();
+    }
+}
