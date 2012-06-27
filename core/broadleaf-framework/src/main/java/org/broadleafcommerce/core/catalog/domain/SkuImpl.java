54185ca8b41b6fe844366f3b5db50753e930d6b5
Andre Azzolini <apazzolini@gmail.com>
2012-06-27 12:29:35 -0500

    Fix missing import. Got dropped during last merge from develop.

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/SkuImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/SkuImpl.java
@@ -45,6 +45,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
