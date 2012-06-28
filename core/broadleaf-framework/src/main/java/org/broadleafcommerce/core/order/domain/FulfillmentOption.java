691680e0a6556c9fdd6ffb904b93b5774e8f85cb
Phillip Verheyden <pverheyden@gmail.com>
2012-06-28 17:35:10 -0500

    Added documentation to FulfillmentGroup and FulfillmentOption

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/domain/FulfillmentOption.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/domain/FulfillmentOption.java
@@ -20,14 +20,35 @@
 package org.broadleafcommerce.core.order.domain;
 
 import org.broadleafcommerce.core.order.service.type.FulfillmentType;
+import org.broadleafcommerce.core.pricing.service.fulfillment.processor.FulfillmentProcessor;
 
 import java.io.Serializable;
 
 /**
+ * A FulfillmentOption is used to hold information about a particular type of Fulfillment implementation.
+ * Third-party fulfillment implementations should extend this to provide their own configuration options
+ * particular to that implementation. For instance, a UPS shipping calculator might want an admin user to be
+ * able to specify which type of UPS shipping this FulfillmentOption represents.
+ * <br />
+ * <br />
+ * This entity will be presented to the user to allow them to specify which shipping they want. A possible
+ * scenario is that say a site can ship with both UPS and Fedex. They will import both the Fedex and UPS
+ * third-party modules, each of which will have a unique definition of FulfillmentOption (for instance,
+ * FedexFulfillmentOption and UPSFulfillmentOption). Let's say the site can do 2-day shipping with UPS,
+ * and next-day shipping with Fedex. What they would do in the admin is create an instance of FedexFulfillmentOption
+ * entity and give it the name "Overnight" (along with any needed Fedex configuration properties), then create an instance of
+ * UPSFulfillmentOption and give it the name "2 Day". When the user goes to check out, they will then see a list
+ * with "Overnight" and "2 day" in it. A FulfillmentProcessor can then be used to estimate the fulfillment cost
+ * (and calculate the fulfillment cost) for that particular option.
+ * <br />
+ * <br />
+ * FulfillmentOptions are also inherently related to FulfillmentProcessors, in that specific types of FulfillmentOption
+ * implementations should also have a FulfillmentProcessor that can handle operations (estimation and calculation) for
+ * pricing a FulfillmentGroup. Typical third-party implementations of this paradigm would have a 1 FulfillmentOption
+ * entity implementation and 1 FulfillmentProcessor implementation for that particular service.
  * 
- * TODO: document
  * @author Phillip Verheyden
- *
+ * @see {@link FulfillmentProcessor}, {@link FulfillmentGroup}
  */
 public interface FulfillmentOption extends Serializable {
     
@@ -35,12 +56,38 @@ public interface FulfillmentOption extends Serializable {
     
     public void setId(Long id);
     
+    /**
+     * Gets the name displayed to the user when they selected the FulfillmentOption for
+     * their order. This might be "2-day" or "Super-saver shipping"
+     * 
+     * @return the display name for this option
+     */
     public String getName();
 
+    /**
+     * Set the display name for this option that will be shown to the user to select from
+     * such as "2-day" or "Express" or "Super-saver shipping"
+     * 
+     * @param name - the display name for this option
+     */
     public void setName(String name);
 
+    /**
+     * Gets the long description for this option which can be shown to the user
+     * to provide more information about the option they are selecting. An example
+     * might be that this is shipped the next business day or that it requires additional
+     * processing time
+     * 
+     * @return the description to display to the user
+     */
     public String getLongDescription();
 
+    /**
+     * Sets the long description for this option to show to the user when they select an option
+     * for fulfilling their order 
+     * 
+     * @param longDescription - the description to show to the user
+     */
     public void setLongDescription(String longDescription);
 
     public Boolean getUseFlatRates();
@@ -50,9 +97,19 @@ public interface FulfillmentOption extends Serializable {
     public Boolean getAddFulfillmentFees();
     
     public void setAddFulfillmentFees(Boolean addFulfillmentFees);
-    
+
+    /**
+     * Gets the type of fulfillment that this option supports
+     * 
+     * @return the type of this option
+     */
     public FulfillmentType getFulfillmentType();
 
+    /**
+     * Sets the type of fulfillment that this option supports
+     * 
+     * @param fulfillmentType
+     */
     public void setFulfillmentType(FulfillmentType fulfillmentType);
     
 }
