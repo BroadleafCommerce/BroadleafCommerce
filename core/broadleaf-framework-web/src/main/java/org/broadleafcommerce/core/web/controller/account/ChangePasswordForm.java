46d4d981121a94e7a7f5ec53c74d3f02318d19e6
Joseph Fridye <jfridye@gmail.com>
2012-07-13 09:34:08 -0500

    Added Manage Account/Change Password controller

---
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/controller/account/ChangePasswordForm.java
@@ -0,0 +1,39 @@
+package org.broadleafcommerce.core.web.controller.account;
+
+import org.broadleafcommerce.common.security.util.PasswordChange;
+
+import java.io.Serializable;
+
+public class ChangePasswordForm implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String currentPassword;
+    private String newPassword;
+    private String newPasswordConfirm;
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getNewPasswordConfirm() {
+        return newPasswordConfirm;
+    }
+
+    public void setNewPasswordConfirm(String newPasswordConfirm) {
+        this.newPasswordConfirm = newPasswordConfirm;
+    }
+
+}
