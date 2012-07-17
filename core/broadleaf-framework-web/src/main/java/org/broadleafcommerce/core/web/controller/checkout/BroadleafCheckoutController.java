5e7290cca3bb5ad52abeb85a10c4430843279f9f
Elbert Bautista <ebautista@broadleafcommerce.org>
2012-07-17 17:07:20 -0500

    fixing bugs when there are payment exceptions in checkout

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/controller/checkout/BroadleafCheckoutController.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/controller/checkout/BroadleafCheckoutController.java
@@ -22,6 +22,7 @@ import org.broadleafcommerce.profile.core.domain.Customer;
 import org.broadleafcommerce.profile.core.domain.CustomerAddress;
 import org.broadleafcommerce.profile.core.domain.State;
 import org.broadleafcommerce.profile.web.core.CustomerState;
+import org.joda.time.DateTime;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.ServletRequestDataBinder;
@@ -30,10 +31,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.beans.PropertyEditorSupport;
+import java.text.DateFormatSymbols;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -67,6 +69,8 @@ public class BroadleafCheckoutController extends AbstractCheckoutController {
         model.addAttribute("validShipping", hasValidShippingAddresses(cart));
     	model.addAttribute("states", stateService.findStates());
         model.addAttribute("countries", countryService.findCountries());
+        model.addAttribute("expirationMonths", populateExpirationMonths());
+        model.addAttribute("expirationYears", populateExpirationYears());
         return getCheckoutView();
     }
 
@@ -194,10 +198,16 @@ public class BroadleafCheckoutController extends AbstractCheckoutController {
     }
 
     /**
-     * Processes the request to complete checkout
+     * Processes the request to complete checkout using a Credit Card
      *
-     * This method allows a checkout and assumes that a credit card payment info
+     * This method assumes that a credit card payment info
      * will be either sent to a third party gateway or saved in a secure schema.
+     * If the transaction is successful, the order will be assigned an order number,
+     * its status change to SUBMITTED, and given a submit date. The method then
+     * returns the default confirmation path "/confirmation/{orderNumber}"
+     *
+     * If the transaction is unsuccessful, (e.g. the gateway declines payment)
+     * processFailedOrderCheckout() is called and reverses the state of the order.
      *
      * Note: this method removes any existing payment infos of type CREDIT_CARD
      * and re-creates it with the information from the BillingInfoForm
@@ -209,7 +219,7 @@ public class BroadleafCheckoutController extends AbstractCheckoutController {
      * @return the return path
      */
     public String completeSecureCreditCardCheckout(HttpServletRequest request, HttpServletResponse response, Model model,
-            BillingInfoForm billingForm, BindingResult result) throws CheckoutException {
+            BillingInfoForm billingForm, BindingResult result) throws CheckoutException, PricingException {
 
         Order cart = CartState.getCart();
         Map<PaymentInfo, Referenced> payments = new HashMap<PaymentInfo, Referenced>();
@@ -241,13 +251,31 @@ public class BroadleafCheckoutController extends AbstractCheckoutController {
         cart.setSubmitDate(Calendar.getInstance().getTime());
 
         CheckoutResponse checkoutResponse = checkoutService.performCheckout(cart, payments);
+
         if (!checkoutResponse.getPaymentResponse().getResponseItems().get(ccInfo).getTransactionSuccess()){
+            processFailedOrderCheckout(cart);
             checkout(request, response, model);
-            model.addAttribute("paymentException", true);
+            result.rejectValue("creditCardNumber", "payment.exception", null, null);
             return getCheckoutView();
         }
 
         return getConfirmationView(cart.getOrderNumber());
+    }
+
+    /**
+     * This method dictates what actions need to be taken if there is a failure during the checkout process.
+     * Normally called when either the transaction success is false (e.g. payment declined by gateway)
+     * or an unknown error occurs during the Checkout Workflow (e.g. a CheckoutException is thrown)
+     *
+     * The default behavior is to reverse the status of the order and set the submit date and order number to null.
+     *
+     * @param order
+     */
+    public void processFailedOrderCheckout(Order order) throws PricingException {
+        order.setOrderNumber(null);
+        order.setStatus(OrderStatus.IN_PROCESS);
+        order.setSubmitDate(null);
+        orderService.save(order, false);
     }
 
     /**
@@ -267,6 +295,37 @@ public class BroadleafCheckoutController extends AbstractCheckoutController {
             }
         }
         return true;
+    }
+
+    /**
+     * A helper method used to construct a list of Credit Card Expiration Months
+     * Useful for expiration dropdown menus.
+     *
+     * @return List containing expiration months of the form "01 - January"
+     */
+    public List<String> populateExpirationMonths() {
+        List<String> expirationMonths = new ArrayList<String>();
+        NumberFormat formatter = new DecimalFormat("00");
+        String[] months = new DateFormatSymbols().getMonths();
+        for (int i=1; i<months.length; i++) {
+            expirationMonths.add(formatter.format(i) + " - " + months[i-1]);
+        }
+        return expirationMonths;
+    }
+
+    /**
+     * A helper method used to construct a list of Credit Card Expiration Years
+     * Useful for expiration dropdown menus.
+     *
+     * @return List of the next ten years starting with the current year.
+     */
+    public List<String> populateExpirationYears() {
+        List<String> expirationYears = new ArrayList<String>();
+        DateTime dateTime = new DateTime();
+        for (int i=0; i<10; i++){
+            expirationYears.add(dateTime.plusYears(i).getYear()+"");
+        }
+        return expirationYears;
     }
     
     /**
