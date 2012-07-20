9ba9ad429ba784a904dcc52490690226e2aa0a4b
Joseph Fridye <jfridye@gmail.com>
2012-07-19 22:22:51 -0500

    Manage Customer Addresses

---
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/controller/account/CustomerAddressForm.java
@@ -0,0 +1,35 @@
+package org.broadleafcommerce.core.web.controller.account;
+
+import java.io.Serializable;
+
+import org.broadleafcommerce.profile.core.domain.Address;
+import org.broadleafcommerce.profile.core.domain.AddressImpl;
+
+public class CustomerAddressForm implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	protected Address address = new AddressImpl();
+	protected String addressName;
+	protected Long customerAddressId;
+	
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getAddressName() {
+		return addressName;
+	}
+	public void setAddressName(String addressName) {
+		this.addressName = addressName;
+	}
+	public Long getCustomerAddressId() {
+		return customerAddressId;
+	}
+	public void setCustomerAddressId(Long customerAddressId) {
+		this.customerAddressId = customerAddressId;
+	}
+	
+}
