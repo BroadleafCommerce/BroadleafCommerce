bab1aac5bffa8efe9ac9f3ff8f84808b013878eb
Phillip Verheyden <pverheyden@gmail.com>
2012-08-10 16:54:46 -0500

    BLC-616 - for banded price shipping, this should be the sum of the getPrice() not getRetailPrice()

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/pricing/service/fulfillment/provider/BandedFulfillmentPricingProvider.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/pricing/service/fulfillment/provider/BandedFulfillmentPricingProvider.java
@@ -137,9 +137,9 @@ public class BandedFulfillmentPricingProvider implements FulfillmentPricingProvi
                     }
                     
                     if (addToTotal) {
-                        BigDecimal price = (fulfillmentGroupItem.getRetailPrice() != null) ? fulfillmentGroupItem.getRetailPrice().getAmount().multiply(BigDecimal.valueOf(fulfillmentGroupItem.getQuantity())) : null;
+                        BigDecimal price = (fulfillmentGroupItem.getPrice() != null) ? fulfillmentGroupItem.getPrice().getAmount().multiply(BigDecimal.valueOf(fulfillmentGroupItem.getQuantity())) : null;
                         if (price == null) {
-                            price = fulfillmentGroupItem.getOrderItem().getRetailPrice().getAmount().multiply(BigDecimal.valueOf(fulfillmentGroupItem.getQuantity()));
+                            price = fulfillmentGroupItem.getOrderItem().getPrice().getAmount().multiply(BigDecimal.valueOf(fulfillmentGroupItem.getQuantity()));
                         }
                         retailTotal = retailTotal.add(price);
                         
