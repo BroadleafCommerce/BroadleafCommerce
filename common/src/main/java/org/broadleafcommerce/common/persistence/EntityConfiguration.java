5a2f621f1f314e86696d2629344762f5735c648f
jefffischer <jfischer@broadleafcommerce.org>
2012-08-11 14:30:17 -0500

    simplify entity component configuration

--- common/src/main/java/org/broadleafcommerce/common/persistence/EntityConfiguration.java
+++ common/src/main/java/org/broadleafcommerce/common/persistence/EntityConfiguration.java
@@ -18,21 +18,52 @@ package org.broadleafcommerce.common.persistence;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component("blEntityConfiguration")
-public class EntityConfiguration {
+public class EntityConfiguration implements ApplicationContextAware {
 
     private static final Log LOG = LogFactory.getLog(EntityConfiguration.class);
 
+    private ApplicationContext webApplicationContext;
     private final HashMap<String, Class<?>> entityMap = new HashMap<String, Class<?>>(50);
     private ApplicationContext applicationcontext;
     private Resource[] entityContexts;
+
+    @javax.annotation.Resource(name="blMergedEntityContexts")
+    protected Set<String> mergedEntityContexts;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.webApplicationContext = applicationContext;
+    }
+
+    @PostConstruct
+    public void configureMergedItems() {
+        Set<Resource> temp = new HashSet<Resource>();
+        if (mergedEntityContexts != null && !mergedEntityContexts.isEmpty()) {
+            for (String location : mergedEntityContexts) {
+                temp.add(webApplicationContext.getResource(location));
+            }
+        }
+        if (entityContexts != null) {
+            for (Resource resource : entityContexts) {
+                temp.add(resource);
+            }
+        }
+        entityContexts = temp.toArray(new Resource[temp.size()]);
+        applicationcontext = new GenericXmlApplicationContext(entityContexts);
+    }
 
     public Class<?> lookupEntityClass(String beanId) {
         Class<?> clazz;
@@ -90,6 +121,5 @@ public class EntityConfiguration {
 
 	public void setEntityContexts(Resource[] entityContexts) {
 		this.entityContexts = entityContexts;
-		applicationcontext = new GenericXmlApplicationContext(entityContexts);
 	}
 }
