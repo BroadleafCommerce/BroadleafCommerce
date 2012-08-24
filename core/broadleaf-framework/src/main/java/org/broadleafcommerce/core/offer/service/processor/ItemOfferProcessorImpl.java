1f7fe2c8b46c640bc2e947210d0094f04419c030
jefffischer <jfischer@broadleafcommerce.org>
2012-08-24 14:40:05 -0500

    BLC-624 - The max uses per order setting for promotions does not work correctly (cherry picked from commit 5ee883d)

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/service/processor/ItemOfferProcessorImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/service/processor/ItemOfferProcessorImpl.java
@@ -16,10 +16,6 @@
 
 package org.broadleafcommerce.core.offer.service.processor;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.apache.commons.beanutils.BeanComparator;
 import org.broadleafcommerce.common.money.Money;
 import org.broadleafcommerce.core.offer.domain.CandidateItemOffer;
@@ -39,6 +35,10 @@ import org.broadleafcommerce.core.offer.service.discount.domain.PromotableOrderI
 import org.broadleafcommerce.core.offer.service.type.OfferType;
 import org.compass.core.util.CollectionUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Filter and apply order item offers.
@@ -372,7 +372,7 @@ public class ItemOfferProcessorImpl extends OrderOfferProcessorImpl implements I
 						if (receiveQtyNeeded > 0) {
 							int itemQtyAvailableToBeUsedAsTarget = chargeableItem.getQuantityAvailableToBeUsedAsTarget(promotion);
 							if (itemQtyAvailableToBeUsedAsTarget > 0) {
-                                if (promotion.getMaxUses() == 0 || itemOffer.getUses() < promotion.getMaxUses()) {
+                                if (promotion.getMaxUses() == 0 || itemOffer.getUses() <= promotion.getMaxUses()) {
 								    int qtyToMarkAsTarget = Math.min(receiveQtyNeeded, itemQtyAvailableToBeUsedAsTarget);
 								    receiveQtyNeeded -= qtyToMarkAsTarget;
 								    //atLeastOneCriteriaMatched = true;
