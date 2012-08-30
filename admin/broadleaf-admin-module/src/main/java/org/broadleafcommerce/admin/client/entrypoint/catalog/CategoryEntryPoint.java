c2779cd310339b84012c0344fd51dd8b616e86fd
Elbert Bautista <ebautista@broadleafcommerce.org>
2012-08-30 10:40:27 -0500

    updating category entry point

--- admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/client/entrypoint/catalog/CategoryEntryPoint.java
+++ admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/client/entrypoint/catalog/CategoryEntryPoint.java
@@ -16,7 +16,14 @@
 
 package org.broadleafcommerce.admin.client.entrypoint.catalog;
 
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
 import org.broadleafcommerce.admin.client.presenter.catalog.category.CategoryPresenter;
 import org.broadleafcommerce.admin.client.view.catalog.category.CategoryView;
@@ -30,11 +37,22 @@ public class CategoryEntryPoint extends BroadleafAbstractEntryPoint {
 
     @Override
     public void onModuleLoad() {
-        Canvas canvas = new HLayout();
-        canvas.setWidth100();
-        canvas.setHeight100();
+        HLayout container = new HLayout();
 
-        initializeEntryPoint(canvas, new CategoryPresenter(), new CategoryView());
+
+        IButton button = new IButton("&quot;Hello World TEST &quot;");
+        button.addClickHandler(new ClickHandler()
+        {
+            public void onClick(ClickEvent event)
+            {
+                SC.say("&quot;Hello World from SmartGWT&quot;");
+            }
+        });
+        container.addChild(button);
+
+        RootPanel.get("gwt-component").add(container);
+
+        initializeEntryPoint(container, new CategoryPresenter(), new CategoryView());
 
         List<String> categoryPermissions = new ArrayList<String>();
         categoryPermissions.add("PERMISSION_CREATE_CATEGORY");
@@ -47,7 +65,7 @@ public class CategoryEntryPoint extends BroadleafAbstractEntryPoint {
         moduleFactory.put("categoryPresenter", "org.broadleafcommerce.admin.client.presenter.catalog.category.CategoryPresenter");
         org.broadleafcommerce.openadmin.client.security.SecurityManager.getInstance().registerSection("BLCMerchandising", "category", categoryPermissions);
 
-        canvas.draw();
     }
+
 
 }
