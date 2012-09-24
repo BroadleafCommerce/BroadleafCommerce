dcc4e7f4aed6be7ba302b6d061aaf7e92d5192e4
Andre Azzolini <apazzolini@gmail.com>
2012-09-24 10:26:00 -0500

    Fix NPE in CSRF Filter

--- common/src/main/java/org/broadleafcommerce/common/security/handler/CsrfFilter.java
+++ common/src/main/java/org/broadleafcommerce/common/security/handler/CsrfFilter.java
@@ -31,6 +31,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -59,11 +60,13 @@ public class CsrfFilter extends GenericFilterBean {
 		HttpServletResponse response = (HttpServletResponse) baseResponse;
 
         boolean excludedRequestFound = false;
-        for (String pattern : excludedRequestPatterns) {
-            RequestMatcher matcher = new AntPathRequestMatcher(pattern);
-            if (matcher.matches(request)){
-                excludedRequestFound = true;
-                break;
+        if (excludedRequestPatterns != null && excludedRequestPatterns.size() > 0) {
+            for (String pattern : excludedRequestPatterns) {
+                RequestMatcher matcher = new AntPathRequestMatcher(pattern);
+                if (matcher.matches(request)){
+                    excludedRequestFound = true;
+                    break;
+                }
             }
         }
 
