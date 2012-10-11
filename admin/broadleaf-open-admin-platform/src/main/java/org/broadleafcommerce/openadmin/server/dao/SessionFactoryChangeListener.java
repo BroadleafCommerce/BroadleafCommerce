58e1383c1dd61ba085e790760b076911c4fc80ac
jefffischer <jfischer@broadleafcommerce.org>
2012-10-11 17:01:06 -0500

    enable flush of metadata cache upon entity manager recycle for debugging purposes

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/dao/SessionFactoryChangeListener.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/dao/SessionFactoryChangeListener.java
@@ -16,8 +16,12 @@
 
 package org.broadleafcommerce.openadmin.server.dao;
 
+import org.broadleafcommerce.openadmin.server.service.DynamicEntityRemoteService;
 import org.hibernate.SessionFactory;
 import org.hibernate.SessionFactoryObserver;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 /**
  * Clear the static entity metadata caches from {@code DynamicEntityDao}
@@ -37,6 +41,13 @@ public class SessionFactoryChangeListener implements SessionFactoryObserver {
         synchronized (DynamicEntityDaoImpl.LOCK_OBJECT) {
             DynamicEntityDaoImpl.METADATA_CACHE.clear();
             DynamicEntityDaoImpl.POLYMORPHIC_ENTITY_CACHE.clear();
+            try {
+                Field metadataCache = DynamicEntityRemoteService.class.getDeclaredField("METADATA_CACHE");
+                metadataCache.setAccessible(true);
+                ((Map) metadataCache.get(null)).clear();
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
