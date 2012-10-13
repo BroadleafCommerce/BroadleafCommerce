f3ae100dd820ff9b23ac7ace84c08905d368aafa
jefffischer <jfischer@broadleafcommerce.org>
2012-10-13 16:43:32 -0500

    BLC-696 - The AdminPresentationAdornedTargetCollection results in a grid that shows field from the parent object instead of the target object (cherry picked from commit fb98ef21ae7e0754c7bec1e195d2bcd4429a409b)
    Conflicts:
    	admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/dao/DynamicEntityDaoImpl.java (cherry picked from commit c6aff65)

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/dao/DynamicEntityDaoImpl.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/dao/DynamicEntityDaoImpl.java
@@ -886,7 +886,7 @@ public class DynamicEntityDaoImpl extends BaseHibernateCriteriaDao<Serializable>
 	) {
 		int j = 0;
 		for (String propertyName : propertyNames) {
-			Type type = propertyTypes.get(j);
+			final Type type = propertyTypes.get(j);
 			boolean isPropertyForeignKey = testForeignProperty(foreignField, prefix, propertyName);
 			int additionalForeignKeyIndexPosition = findAdditionalForeignKeyIndex(additionalForeignFields, prefix, propertyName);
 			j++;
@@ -902,18 +902,18 @@ public class DynamicEntityDaoImpl extends BaseHibernateCriteriaDao<Serializable>
                         myField.getAnnotation(AdminPresentationAdornedTargetCollection.class) != null ||
                         myField.getAnnotation(AdminPresentationMap.class) != null)
                 ) {
-                    CollectionMetadata fieldMetadata = (CollectionMetadata) presentationAttributes.get(propertyName);
-                    if (StringUtils.isEmpty(fieldMetadata.getCollectionCeilingEntity())) {
-                        fieldMetadata.setCollectionCeilingEntity(type.getReturnedClass().getName());
-                    }
+                    final CollectionMetadata fieldMetadata = (CollectionMetadata) presentationAttributes.get(propertyName);
                     fieldMetadata.setInheritedFromType(targetClass.getName());
                     fieldMetadata.setAvailableToTypes(new String[]{targetClass.getName()});
                     fields.put(propertyName, fieldMetadata);
                     fieldMetadata.accept(new MetadataVisitorAdapter() {
                         @Override
                         public void visit(AdornedTargetCollectionMetadata metadata) {
-                            AdornedTargetList targetList = ((AdornedTargetList) metadata.getPersistencePerspective().getPersistencePerspectiveItems().get(PersistencePerspectiveItemType.ADORNEDTARGETLIST));
-                            targetList.setAdornedTargetEntityClassname(metadata.getCollectionCeilingEntity());
+                            if (StringUtils.isEmpty(fieldMetadata.getCollectionCeilingEntity())) {
+                                fieldMetadata.setCollectionCeilingEntity(type.getReturnedClass().getName());
+                                AdornedTargetList targetList = ((AdornedTargetList) metadata.getPersistencePerspective().getPersistencePerspectiveItems().get(PersistencePerspectiveItemType.ADORNEDTARGETLIST));
+                                targetList.setAdornedTargetEntityClassname(metadata.getCollectionCeilingEntity());
+                            }
                         }
 
                         @Override
