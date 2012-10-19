1786c4672734555c9bf93f5131b7e3f3d552fe7d
Phillip Verheyden <pverheyden@gmail.com>
2012-10-19 14:31:17 -0500

    BLC-700 - when estimating Fulfillment costs, you should be able to attempt estimation for all of the providers even if there are exceptions when attempting calculations for other providers

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/pricing/service/FulfillmentPricingServiceImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/pricing/service/FulfillmentPricingServiceImpl.java
@@ -16,6 +16,8 @@
 
 package org.broadleafcommerce.core.pricing.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.broadleafcommerce.common.money.Money;
 import org.broadleafcommerce.common.vendor.service.exception.FulfillmentPriceException;
 import org.broadleafcommerce.core.order.domain.FulfillmentGroup;
@@ -33,6 +35,8 @@ import java.util.Set;
 
 @Service("blFulfillmentPricingService")
 public class FulfillmentPricingServiceImpl implements FulfillmentPricingService {
+    
+    protected static final Log LOG  = LogFactory.getLog(FulfillmentPricingServiceImpl.class);
 
     @Resource(name = "blFulfillmentPricingProviders")
     protected List<FulfillmentPricingProvider> providers;
@@ -51,9 +55,9 @@ public class FulfillmentPricingServiceImpl implements FulfillmentPricingService 
             return fulfillmentGroup;
         }
         
-        for (FulfillmentPricingProvider processor : providers) {
-            if (processor.canCalculateCostForFulfillmentGroup(fulfillmentGroup, fulfillmentGroup.getFulfillmentOption())) {
-                return processor.calculateCostForFulfillmentGroup(fulfillmentGroup);
+        for (FulfillmentPricingProvider provider : providers) {
+            if (provider.canCalculateCostForFulfillmentGroup(fulfillmentGroup, fulfillmentGroup.getFulfillmentOption())) {
+                return provider.calculateCostForFulfillmentGroup(fulfillmentGroup);
             }
         }
 
@@ -67,14 +71,23 @@ public class FulfillmentPricingServiceImpl implements FulfillmentPricingService 
         FulfillmentEstimationResponse response = new FulfillmentEstimationResponse();
         HashMap<FulfillmentOption, Money> prices = new HashMap<FulfillmentOption, Money>();
         response.setFulfillmentOptionPrices(prices);
-        for (FulfillmentPricingProvider processor : providers) {
+        for (FulfillmentPricingProvider provider : providers) {
             //Leave it up to the providers to determine if they can respond to a pricing estimate.  If they can't, or if one or more of the options that are passed in can't be responded
             //to, then the response from the pricing provider should not include the options that it could not respond to.
-            FulfillmentEstimationResponse processorResponse = processor.estimateCostForFulfillmentGroup(fulfillmentGroup, options);
-            if (processorResponse != null
-                    && processorResponse.getFulfillmentOptionPrices() != null
-                    && processorResponse.getFulfillmentOptionPrices().size() > 0) {
-                prices.putAll(processorResponse.getFulfillmentOptionPrices());
+            try {
+                FulfillmentEstimationResponse processorResponse = provider.estimateCostForFulfillmentGroup(fulfillmentGroup, options);
+                if (processorResponse != null
+                        && processorResponse.getFulfillmentOptionPrices() != null
+                        && processorResponse.getFulfillmentOptionPrices().size() > 0) {
+                    prices.putAll(processorResponse.getFulfillmentOptionPrices());
+                }
+            } catch (FulfillmentPriceException e) {
+                //Shouldn't completely fail the rest of the estimation on a pricing exception. Another provider might still
+                //be able to respond
+                String errorMessage = "FulfillmentPriceException thrown when trying to estimate fulfillment costs from ";
+                errorMessage += provider.getClass().getName();
+                errorMessage += ". Underlying message was: " + e.getMessage();
+                LOG.error(errorMessage);
             }
         }
 
