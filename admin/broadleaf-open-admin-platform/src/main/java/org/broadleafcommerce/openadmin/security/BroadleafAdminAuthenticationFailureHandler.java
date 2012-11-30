c7920b5fc9f578eb50816338917926a420c7e34d
jefffischer <jfischer@broadleafcommerce.org>
2012-11-29 18:29:21 -0600

    handle url bookmarking for all browsers, including those that don't maintain the anchor portion of the url (like IE)

---
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/security/BroadleafAdminAuthenticationFailureHandler.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2008-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.openadmin.security;
+
+import org.apache.commons.lang.StringUtils;
+import org.broadleafcommerce.common.util.StringUtil;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BroadleafAdminAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private String defaultFailureUrl;
+
+    public BroadleafAdminAuthenticationFailureHandler() {
+        super();
+    }
+
+    public BroadleafAdminAuthenticationFailureHandler(String defaultFailureUrl) {
+        super(defaultFailureUrl);
+        this.defaultFailureUrl = defaultFailureUrl;
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String failureUrlParam = StringUtil.cleanseUrlString(request.getParameter("failureUrl"));
+        String successUrlParam = StringUtil.cleanseUrlString(request.getParameter("successUrl"));
+        String failureUrl = failureUrlParam==null?null:failureUrlParam.trim();
+        if (StringUtils.isEmpty(failureUrl)) {
+            failureUrl = defaultFailureUrl;
+        }
+        if (failureUrl != null) {
+            if (!StringUtils.isEmpty(successUrlParam)) {
+                if (!failureUrl.contains("?")) {
+                    failureUrl += "?successUrl=" + successUrlParam;
+                } else {
+                    failureUrl += "&successUrl=" + successUrlParam;
+                }
+            }
+            String moduleKey = request.getParameter("moduleKey");
+            String pageKey = request.getParameter("pageKey");
+            if (moduleKey != null && pageKey != null) {
+                failureUrl += "#" + "moduleKey=" + moduleKey + "&pageKey=" + pageKey;
+            }
+            saveException(request, exception);
+            getRedirectStrategy().sendRedirect(request, response, failureUrl);
+        } else {
+            super.onAuthenticationFailure(request, response, exception);
+        }
+    }
+
+}
