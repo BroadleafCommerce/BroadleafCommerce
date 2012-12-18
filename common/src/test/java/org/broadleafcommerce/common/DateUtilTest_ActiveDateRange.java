d80f3b184d6e45ac225ac126d900293b59cef1c6
Ivan Sim <ihcsim@gmail.com>
2012-12-18 13:15:01 -0800

    Replace 2-spaces indentation with 4-spaces to match project standard.

--- common/src/test/java/org/broadleafcommerce/common/DateUtilTest_ActiveDateRange.java
+++ common/src/test/java/org/broadleafcommerce/common/DateUtilTest_ActiveDateRange.java
@@ -9,80 +9,79 @@ import junit.framework.TestCase;
 import org.broadleafcommerce.common.util.DateUtil;
 
 public class DateUtilTest_ActiveDateRange extends TestCase {
-  public void testDateRangeWithNullDates_ExpectInactive(){
-    Date startDate = null;
-    Date endDate = null;
-    boolean includeTime = false;
-    boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
-    boolean expected = false;
-    Assert.assertEquals(expected, actual);
-  }
-  
-  public void testDateRangeWithNullStartDate_ExpectInactive(){
-    Date startDate = null;
-    Date endDate = dateInGregorianCalendar(2012, 11, 31);
-    boolean includeTime = false;
-    boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
-    boolean expected = false;
-    Assert.assertEquals(expected, actual);
-  }
-  
-  public void testDateRangeWithTodayStartDateAndCurrentTime_ExpectInactive(){
-    Date startDate = new Date();
-    Date endDate = null;
-    boolean includeTime = false;
-    boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
-    boolean expected = false;
-    Assert.assertEquals(expected, actual);
-  }
-  
-  public void testDateRangeWithFutureStartDate_ExpectInactive(){
-    Date startDate = dateInGregorianCalendar(2012, 11, 31);
-    Date endDate = null;
-    boolean includeTime = false;
-    boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
-    boolean expected = false;
-    Assert.assertEquals(expected, actual);
-  }
-  
-  public void testDateRangeWithNullEndDate_ExpectActive(){
-    Date startDate = dateInGregorianCalendar(2012, 9, 31);
-    Date endDate = null;
-    boolean includeTime = false;
-    boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
-    boolean expected = true;
-    Assert.assertEquals(expected, actual);
-  }
-  
-  public void testDateRangeWithPastEndDate_ExpectInactive(){
-    Date startDate = dateInGregorianCalendar(2012, 8, 31);
-    Date endDate = dateInGregorianCalendar(2012, 9, 31);
-    boolean includeTime = false;
-    boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
-    boolean expected = false;
-    Assert.assertEquals(expected, actual);
-  }
-  
-  public void testDateRangeWithTodayEndDateAndCurrentTime_ExpectActive(){
-    Date startDate = dateInGregorianCalendar(2012, 9, 31);
-    Date endDate = new Date();
-    boolean includeTime = false;
-    boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
-    boolean expected = true;
-    Assert.assertEquals(expected, actual);
-  }
-  
-  public void testDateRangeWithFutureEndDate_ExpectActive(){
-    Date startDate = dateInGregorianCalendar(2012, 9, 31);
-    Date endDate = dateInGregorianCalendar(2012, 11, 31);
-    boolean includeTime = false;
-    boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
-    boolean expected = true;
-    Assert.assertEquals(expected, actual);
-  }
-  
-  private Date dateInGregorianCalendar(int year, int month, int day){
-    return new GregorianCalendar(year, month, day).getTime();
-  }
-
+    public void testDateRangeWithNullDates_ExpectInactive(){
+        Date startDate = null;
+        Date endDate = null;
+        boolean includeTime = false;
+        boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
+        boolean expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+    
+    public void testDateRangeWithNullStartDate_ExpectInactive(){
+        Date startDate = null;
+        Date endDate = dateInGregorianCalendar(2012, 11, 31);
+        boolean includeTime = false;
+        boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
+        boolean expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+    
+    public void testDateRangeWithTodayStartDateAndCurrentTime_ExpectInactive(){
+        Date startDate = new Date();
+        Date endDate = null;
+        boolean includeTime = false;
+        boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
+        boolean expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+    
+    public void testDateRangeWithFutureStartDate_ExpectInactive(){
+        Date startDate = dateInGregorianCalendar(2012, 11, 31);
+        Date endDate = null;
+        boolean includeTime = false;
+        boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
+        boolean expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+    
+    public void testDateRangeWithNullEndDate_ExpectActive(){
+        Date startDate = dateInGregorianCalendar(2012, 9, 31);
+        Date endDate = null;
+        boolean includeTime = false;
+        boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
+        boolean expected = true;
+        Assert.assertEquals(expected, actual);
+    }
+    
+    public void testDateRangeWithPastEndDate_ExpectInactive(){
+        Date startDate = dateInGregorianCalendar(2012, 8, 31);
+        Date endDate = dateInGregorianCalendar(2012, 9, 31);
+        boolean includeTime = false;
+        boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
+        boolean expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+    
+    public void testDateRangeWithTodayEndDateAndCurrentTime_ExpectActive(){
+        Date startDate = dateInGregorianCalendar(2012, 9, 31);
+        Date endDate = new Date();
+        boolean includeTime = false;
+        boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
+        boolean expected = true;
+        Assert.assertEquals(expected, actual);
+    }
+    
+    public void testDateRangeWithFutureEndDate_ExpectActive(){
+        Date startDate = dateInGregorianCalendar(2012, 9, 31);
+        Date endDate = dateInGregorianCalendar(2012, 11, 31);
+        boolean includeTime = false;
+        boolean actual = DateUtil.isActive(startDate, endDate, includeTime);
+        boolean expected = true;
+        Assert.assertEquals(expected, actual);
+    }
+    
+    private Date dateInGregorianCalendar(int year, int month, int day){
+        return new GregorianCalendar(year, month, day).getTime();
+    }
 }
