46d4923a06217dbeb54c770aeecd3a4fc00b20e8
ivan.sim <ivan.sim@aeroinfo.com>
2012-12-20 12:29:11 -0800

    Rename Money_PrecisionTest.java to MoneyTest_AmountPrecision.java

--- common/src/test/java/org/broadleafcommerce/common/money/MoneyTest_AmountPrecision.java
+++ common/src/test/java/org/broadleafcommerce/common/money/MoneyTest_AmountPrecision.java
@@ -5,12 +5,17 @@ import java.math.BigDecimal;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-public class Money_PrecisionTest extends TestCase {
-
+public class MoneyTest_AmountPrecision extends TestCase {
     public void testCreateMoney_FromInteger_ZeroDollar(){
         Money money = new Money(0);
         BigDecimal expected = new BigDecimal(0).setScale(2); 
         Assert.assertEquals(expected, money.getAmount());
+    }
+    
+    public void testCreateMoney_FromInteger_NegativeDollar(){
+        Money money = new Money(-1);
+        BigDecimal expected = new BigDecimal(-1).setScale(2);
+        Assert.assertEquals(expected.doubleValue(), money.getAmount().doubleValue());
     }
   
     public void testCreateMoney_FromInteger_OneDollar(){
