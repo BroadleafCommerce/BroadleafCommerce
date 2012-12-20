ffa5bf1648cd639920b6f0a8222f5eeda9397644
ivan.sim <ivan.sim@aeroinfo.com>
2012-12-20 10:56:52 -0800

    Add unit tests to test money precisions using the integer-argument constructor.

---
+++ common/src/test/java/org/broadleafcommerce/common/money/Money_PrecisionTest.java
@@ -0,0 +1,27 @@
+package org.broadleafcommerce.common.money;
+
+import java.math.BigDecimal;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class Money_PrecisionTest extends TestCase {
+
+    public void testCreateMoney_FromInteger_ZeroDollar(){
+        Money money = new Money(0);
+        BigDecimal expected = new BigDecimal(0).setScale(2); 
+        Assert.assertEquals(expected, money.getAmount());
+    }
+  
+    public void testCreateMoney_FromInteger_OneDollar(){
+        Money money = new Money(1);
+        BigDecimal expected = new BigDecimal(1).setScale(2);
+        Assert.assertEquals(expected, money.getAmount());
+    }
+  
+    public void testCreateMoney_FromInteger_TenDollar(){
+        Money money = new Money(10);
+        BigDecimal expected = new BigDecimal(10).setScale(2);
+        Assert.assertEquals(expected.doubleValue(), money.getAmount().doubleValue());
+    }
+}
