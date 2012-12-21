1b84a17b1011e3b43cd665ba5eec9846d5e8f1ef
ivan.sim <ivan.sim@aeroinfo.com>
2012-12-20 17:35:35 -0800

    Rename test fixtures for clarity.

--- common/src/test/java/org/broadleafcommerce/common/money/MoneyTest_IntegerAmountPrecision.java
+++ common/src/test/java/org/broadleafcommerce/common/money/MoneyTest_IntegerAmountPrecision.java
@@ -7,44 +7,44 @@ import junit.framework.TestCase;
 
 public class MoneyTest_IntegerAmountPrecision extends TestCase {
     public void testCreateMoney_FromInteger_ZeroDollar(){
-        Money money = new Money(0);
+        Money actual = new Money(0);
         BigDecimal expected = new BigDecimal(0).setScale(2); 
-        Assert.assertEquals(expected, money.getAmount());
+        Assert.assertEquals(expected, actual.getAmount());
     }
     
     public void testCreateMoney_FromInteger_OneDollar(){
-        Money money = new Money(1);
+        Money actual = new Money(1);
         BigDecimal expected = new BigDecimal(1).setScale(2);
-        Assert.assertEquals(expected, money.getAmount());
+        Assert.assertEquals(expected, actual.getAmount());
     }
     
     public void testCreateMoney_FromNegativeInteger_OneDollar(){
-        Money money = new Money(-1);
+        Money actual = new Money(-1);
         BigDecimal expected = new BigDecimal(-1).setScale(2);
-        Assert.assertEquals(expected, money.getAmount());
+        Assert.assertEquals(expected, actual.getAmount());
     }
   
     public void testCreateMoney_FromInteger_TenDollar(){
-        Money money = new Money(10);
+        Money actual = new Money(10);
         BigDecimal expected = new BigDecimal(10).setScale(2);
-        Assert.assertEquals(expected, money.getAmount());
+        Assert.assertEquals(expected, actual.getAmount());
     }
     
     public void testCreateMoney_FromInteger_HundredDollar(){
-        Money money = new Money(100);
+        Money actual = new Money(100);
         BigDecimal expected = new BigDecimal(100).setScale(2);
-        Assert.assertEquals(expected, money.getAmount());
+        Assert.assertEquals(expected, actual.getAmount());
     }
     
     public void testCreateMoney_FromInteger_TenThousandsDollar(){
-        Money money = new Money(10000);
+        Money actual = new Money(10000);
         BigDecimal expected = new BigDecimal(10000).setScale(2);
-        Assert.assertEquals(expected, money.getAmount());
+        Assert.assertEquals(expected, actual.getAmount());
     }
     
     public void testCreateMoney_FromInteger_TenMillionsDollar(){
-        Money money = new Money(10000000);
+        Money actual = new Money(10000000);
         BigDecimal expected = new BigDecimal(10000000).setScale(2);
-        Assert.assertEquals(expected, money.getAmount());
+        Assert.assertEquals(expected, actual.getAmount());
     }
 }
