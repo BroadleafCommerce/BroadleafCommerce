a8b8c468a93f1140d58c8926bba2052aa11f63b4
jefffischer <jfischer@broadleafcommerce.org>
2013-01-02 13:21:42 -0600

    Merge remote-tracking branch 'remotes/origin/BroadleafCommerce-2.0.x' into BroadleafCommerce-2.1.x
    Conflicts:
    	admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/client/MerchandisingMessages.java
    	admin/broadleaf-admin-module/src/main/resources/org/broadleafcommerce/admin/client/MerchandisingMessages.properties

--- admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/client/MerchandisingMessages.java
+++ admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/client/MerchandisingMessages.java
@@ -150,6 +150,7 @@ public interface MerchandisingMessages extends ConstantsWithLookup {
     public String SkuImpl_Sku_Media_Key();
     public String skuGenerationInvalid();
 
+    public String Seo_Group();
     public String cloneErrorMessage();
     public String cloneSuccessMessage();
 }
