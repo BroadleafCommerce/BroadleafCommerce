e737aa33b75d2d7d1c24a316d212910c83fd9438
Jerry Ocanas <jerry77oz@gmail.com>
2013-01-09 16:02:59 -0600

    Created pattern to introduce boilerplate code needed on all payment module methods. Users will now extend the AbstractModule and/or DefaultModule and override the 'process' methods to add custom logic.

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/payment/service/module/AbstractModule.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/payment/service/module/AbstractModule.java
@@ -16,17 +16,176 @@
 
 package org.broadleafcommerce.core.payment.service.module;
 
-import javax.annotation.Resource;
-
-import org.broadleafcommerce.core.payment.domain.PaymentResponseItem;
+import org.broadleafcommerce.common.money.Money;
+import org.broadleafcommerce.common.time.SystemTime;
+import org.broadleafcommerce.core.payment.domain.*;
+import org.broadleafcommerce.core.payment.service.PaymentContext;
 import org.broadleafcommerce.core.payment.service.PaymentInfoService;
+import org.broadleafcommerce.core.payment.service.exception.PaymentException;
+
+import javax.annotation.Resource;
 
 public abstract class AbstractModule implements PaymentModule {
 
     @Resource(name="blPaymentInfoService")
     private PaymentInfoService paymentInfoService;
 
+    @Override
+    public PaymentResponseItem authorize(PaymentContext paymentContext) throws PaymentException {
+        PaymentResponseItem responseItem = getNewResponseItem();
+        return processAuthorize(paymentContext, responseItem);
+    }
+
+    /**
+     * Typically payment module implementors would not override this method.   They would instead override the
+     * processReverseAuthorize() method.
+     *
+     * @param paymentContext
+     * @return
+     * @throws PaymentException
+     */
+    @Override
+    public PaymentResponseItem reverseAuthorize(PaymentContext paymentContext) throws PaymentException {
+        Money amountAvailableToReverseAuthorize = getAmountAvailableToReverseAuthorize(paymentContext);
+        PaymentInfo paymentInfo = findPaymentInfoFromContext(paymentContext);
+
+        PaymentResponseItem responseItem = getNewResponseItem();
+        responseItem.setAmountPaid(amountAvailableToReverseAuthorize);
+        responseItem.setCurrency(paymentInfo.getCurrency());
+
+        //Add PaymentInfoDetail - ReverseAuth
+        paymentInfo.getPaymentInfoDetails().add(getNewReverseAuthPaymentInfoDetail(paymentInfo, amountAvailableToReverseAuthorize));
+        return processReverseAuthorize(paymentContext, amountAvailableToReverseAuthorize, responseItem);
+    }
+
+    @Override
+    public PaymentResponseItem debit(PaymentContext paymentContext) throws PaymentException {
+        Money amountAvailableToDebit = getAmountAvailableToDebit(paymentContext);
+        PaymentInfo paymentInfo = findPaymentInfoFromContext(paymentContext);
+
+        PaymentResponseItem responseItem = getNewResponseItem();
+        responseItem.setTransactionTimestamp(SystemTime.asDate());
+        responseItem.setAmountPaid(amountAvailableToDebit);
+        responseItem.setCurrency(paymentInfo.getCurrency());
+        //Add PaymentInfoDetail - Capture
+        paymentInfo.getPaymentInfoDetails().add(getNewCapturePaymentInfoDetail(paymentInfo, amountAvailableToDebit));
+
+        return processDebit(paymentContext, amountAvailableToDebit, responseItem);
+    }
+
+    @Override
+    public PaymentResponseItem authorizeAndDebit(PaymentContext paymentContext) throws PaymentException {
+        return processAuthorizeAndDebit(paymentContext, getAmountAvailableToDebit(paymentContext), getNewResponseItem());
+    }
+
+    @Override
+    public PaymentResponseItem credit(PaymentContext paymentContext) throws PaymentException {
+        Money amountAvailableToCredit = getAmountAvailableToCredit(paymentContext);
+        PaymentInfo paymentInfo = findPaymentInfoFromContext(paymentContext);
+
+        PaymentResponseItem responseItem = getNewResponseItem();
+        responseItem.setTransactionTimestamp(SystemTime.asDate());
+        responseItem.setAmountPaid(amountAvailableToCredit);
+        responseItem.setCurrency(paymentInfo.getCurrency());
+        //Add PaymentInfoDetail - Refund
+        paymentInfo.getPaymentInfoDetails().add(getNewRefundPaymentInfoDetail(paymentInfo, amountAvailableToCredit));
+
+        return processCredit(paymentContext, amountAvailableToCredit, responseItem);
+    }
+
+    @Override
+    public PaymentResponseItem voidPayment(PaymentContext paymentContext) throws PaymentException {
+        Money amountAlreadyCaptured = findPaymentInfoFromContext(paymentContext).getPaymentCapturedAmount();
+        return processVoidPayment(paymentContext, amountAlreadyCaptured, getNewResponseItem());
+    }
+
+    @Override
+    public PaymentResponseItem balance(PaymentContext paymentContext) throws PaymentException {
+        return processBalance(paymentContext, getNewResponseItem());
+    }
+
     protected PaymentResponseItem getNewResponseItem() {
         return paymentInfoService.createResponseItem();
+    }
+    
+    protected PaymentInfoDetail getNewPaymentInfoDetail(PaymentInfo paymentInfo, PaymentInfoDetailType type, Money amount){
+        PaymentInfoDetail paymentInfoDetail = new PaymentInfoDetailImpl();
+        paymentInfoDetail.setPaymentInfo(paymentInfo);
+        paymentInfoDetail.setType(type);
+        paymentInfoDetail.setDate(SystemTime.asDate());
+        paymentInfoDetail.setAmount(amount);
+        return paymentInfoDetail;
+    }
+    
+    protected PaymentInfoDetail getNewCapturePaymentInfoDetail(PaymentInfo paymentInfo, Money amount){
+        return getNewPaymentInfoDetail(paymentInfo, PaymentInfoDetailType.CAPTURE, amount);
+    }
+
+    protected PaymentInfoDetail getNewRefundPaymentInfoDetail(PaymentInfo paymentInfo, Money amount){
+        return getNewPaymentInfoDetail(paymentInfo, PaymentInfoDetailType.REFUND, amount);
+    }
+
+    protected PaymentInfoDetail getNewReverseAuthPaymentInfoDetail(PaymentInfo paymentInfo, Money amount){
+        return getNewPaymentInfoDetail(paymentInfo, PaymentInfoDetailType.REVERSE_AUTH, amount);
+    }
+
+    protected PaymentInfo findPaymentInfoFromContext(PaymentContext paymentContext){
+        PaymentInfo paymentInfo = paymentContext.getPaymentInfo();
+        for (PaymentInfo pi : paymentContext.getPaymentInfo().getOrder().getPaymentInfos()) {
+            if (paymentInfo.equals(pi)) {
+                return pi;
+            }
+        }
+        return null;
+    }
+    
+    protected Money getAmountAvailableToDebit(PaymentContext paymentContext){
+        // Get the remaining amount to debit for the current transaction
+        Money amountToDebit = paymentContext.getRemainingTransactionAmount();
+        PaymentInfo paymentInfo = findPaymentInfoFromContext(paymentContext);
+        // Get the amount available to debit = [(amount) - ((amount already captured) - (amount already reversed))]
+        Money amountCaptured = paymentInfo.getPaymentCapturedAmount();
+        Money amountAlreadyReversed = paymentInfo.getReverseAuthAmount();
+        Money amount = paymentInfo.getAmount();
+        Money amountAvailableToDebit = amount.subtract(amountCaptured).subtract(amountAlreadyReversed);
+        // Return the minimum of (amountToDebit, amountAvailableToDebit)
+        if (amountAvailableToDebit.lessThan(amountToDebit)){
+            return amountAvailableToDebit;
+        } else {
+            return amountToDebit;
+        }    
+    }
+
+    protected Money getAmountAvailableToCredit(PaymentContext paymentContext){
+        // Get the remaining amount to credit for the current transaction
+        Money amountToCredit = paymentContext.getRemainingTransactionAmount();
+        PaymentInfo paymentInfo = findPaymentInfoFromContext(paymentContext);
+        // Get the amount available to credit = [(amountCaptured) - (amount already refunded)]
+        Money amountCaptured = paymentInfo.getPaymentCapturedAmount();
+        Money amountAlreadyCredited = paymentInfo.getPaymentCreditedAmount();
+        Money amountAvailableToCredit = amountCaptured.subtract(amountAlreadyCredited);
+        // Return the minimum of (amountToCredit, amountAvailableToCredit)
+        if (amountAvailableToCredit.lessThan(amountToCredit)){
+            return amountAvailableToCredit;
+        } else {
+            return amountToCredit;
+        }
+    }
+
+    protected Money getAmountAvailableToReverseAuthorize(PaymentContext paymentContext){
+        // Get the remaining amount to reverse for the current transaction
+        Money amountToReverse = paymentContext.getRemainingTransactionAmount();
+        PaymentInfo paymentInfo = findPaymentInfoFromContext(paymentContext);
+        // Get the amount available to reverse = [(amount) - ((amount already captured) - (amount already reversed))]
+        Money amountCaptured = paymentInfo.getPaymentCapturedAmount();
+        Money amountAlreadyReversed = paymentInfo.getReverseAuthAmount();
+        Money amount = paymentInfo.getAmount();
+        Money amountAvailableToReverse = amount.subtract(amountCaptured).subtract(amountAlreadyReversed);
+        // Return the minimum of (amountToReverse, amountAvailableToReverse)
+        if (amountAvailableToReverse.lessThan(amountToReverse)){
+            return amountAvailableToReverse;
+        } else {
+            return amountToReverse;
+        }
     }
 }
