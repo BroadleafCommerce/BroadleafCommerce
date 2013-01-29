9095d4d9ad137f2be5767b134150755b9d8426c5
jefffischer <jfischer@broadleafcommerce.org>
2013-01-29 11:02:17 -0600

    Fix a merge problem related to product bundle pricing (cherry picked from commit 4a9cd33e59aafdf3e29a3b33a6612fa76eb69588)
    Conflicts:
    	core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/ProductBundleImpl.java

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/ProductBundleImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/ProductBundleImpl.java
@@ -15,6 +15,19 @@
  */
 package org.broadleafcommerce.core.catalog.domain;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.broadleafcommerce.common.money.BankersRounding;
 import org.broadleafcommerce.common.money.Money;
 import org.broadleafcommerce.common.presentation.AdminPresentation;
 import org.broadleafcommerce.common.presentation.AdminPresentationClass;
@@ -26,18 +39,6 @@ import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.Cascade;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -108,7 +109,8 @@ public class ProductBundleImpl extends ProductImpl implements ProductBundle {
         }
         return null;
     }
-    
+
+    @Override
     public Money getBundleItemsRetailPrice() {
         Money price = new Money(BigDecimal.ZERO);
         for (SkuBundleItem item : getSkuBundleItems()) {
@@ -116,11 +118,12 @@ public class ProductBundleImpl extends ProductImpl implements ProductBundle {
         }
         return price;
     }
-    
+
+    @Override
     public Money getBundleItemsSalePrice() {
         Money price = new Money(BigDecimal.ZERO);
         for (SkuBundleItem item : getSkuBundleItems()){
-            if (item.getSalePrice() != null) { 
+            if (item.getSalePrice() != null) {
                 price = price.add(item.getSalePrice().multiply(item.getQuantity()));
             } else {
                 price = price.add(item.getRetailPrice().multiply(item.getQuantity()));
@@ -129,46 +132,57 @@ public class ProductBundleImpl extends ProductImpl implements ProductBundle {
         return price;
     }
 
+    @Override
     public Boolean getAutoBundle() {
         return autoBundle == null ? false : autoBundle;
     }
 
+    @Override
     public void setAutoBundle(Boolean autoBundle) {
         this.autoBundle = autoBundle;
     }
 
+    @Override
     public Boolean getItemsPromotable() {
         return itemsPromotable == null ? false : itemsPromotable;
     }
 
+    @Override
     public void setItemsPromotable(Boolean itemsPromotable) {
         this.itemsPromotable = itemsPromotable;
     }
 
+    @Override
     public Boolean getBundlePromotable() {
         return bundlePromotable == null ? false : bundlePromotable;
     }
 
+    @Override
     public void setBundlePromotable(Boolean bundlePromotable) {
         this.bundlePromotable = bundlePromotable;
     }
 
+    @Override
     public List<SkuBundleItem> getSkuBundleItems() {
         return skuBundleItems;
     }
 
+    @Override
     public void setSkuBundleItems(List<SkuBundleItem> skuBundleItems) {
         this.skuBundleItems = skuBundleItems;
     }
 
+    @Override
     public Integer getPriority() {
         return priority;
     }
 
+    @Override
     public void setPriority(Integer priority) {
         this.priority = priority;
     }
 
+    @Override
     public BigDecimal getPotentialSavings() {
         if (skuBundleItems != null) {
             Money totalNormalPrice = new Money();
