fa9e8466509f3b3d67050bfa3604ddd4c5015e5f
Jerry Ocanas <jerry77oz@gmail.com>
2013-03-21 16:09:53 -0500

    Implementation and override for BroadleafPaymentModuleService.

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/payment/service/BroadleafPaymentModuleServiceImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/payment/service/BroadleafPaymentModuleServiceImpl.java
@@ -1,5 +1,7 @@
 package org.broadleafcommerce.core.payment.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.broadleafcommerce.core.payment.service.workflow.PaymentSeed;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +11,16 @@ import org.springframework.stereotype.Service;
 @Service("blPaymentModuleService")
 public class BroadleafPaymentModuleServiceImpl implements BroadleafPaymentModuleService {
 
+    private static final Log LOG = LogFactory.getLog(BroadleafPaymentModuleServiceImpl.class);
+
     @Override
     public void validateResponse(PaymentSeed paymentSeed) throws Exception {
+        LOG.warn("Validate response has not been implemented.");
     }
 
     @Override
     public void manualPayment(String transactionID) {
+        LOG.warn("Manual payment has not been implemented.");
     }
 
 }
