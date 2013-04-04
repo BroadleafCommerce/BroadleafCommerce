0c63760bccce4de82551c8536adfb6befef96595
Priyesh Patel <ppatel@broadleafcommerce.org>
2013-04-04 17:09:01 -0500

    add sku form field  service

---
+++ admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/web/rulebuilder/service/SkuFieldServiceImpl.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2008-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.admin.web.rulebuilder.service;
+
+import org.broadleafcommerce.common.presentation.client.SupportedFieldType;
+import org.broadleafcommerce.openadmin.web.rulebuilder.dto.FieldData;
+import org.broadleafcommerce.openadmin.web.rulebuilder.service.AbstractRuleBuilderFieldService;
+import org.springframework.stereotype.Service;
+
+/**
+ * An implementation of a RuleBuilderFieldService
+ * that constructs metadata necessary
+ * to build the supported fields for a Sku entity
+ *
+ * @author Priyesh Patel
+ */
+@Service("blSkuFieldService")
+public class SkuFieldServiceImpl  extends AbstractRuleBuilderFieldService {
+
+
+    {
+        fields.add(new FieldData.Builder()
+                .label("Sku - Name")
+                .name("name")
+                .operators("blcOperators_Text")
+                .options("[]")
+                .type(SupportedFieldType.STRING)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Sku - Description")
+                .name("description")
+                .operators("blcOperators_Text")
+                .options("[]")
+                .type(SupportedFieldType.STRING)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Sku - Long Description")
+                .name("longDescription")
+                .operators("blcOperators_Text")
+                .options("[]")
+                .type(SupportedFieldType.STRING)
+                .build());
+
+        fields.add(new FieldData.Builder()
+                .label("Sku - Sale Price")
+                .name("salePrice")
+                .operators("blcOperators_Numeric")
+                .options("[]")
+                .type(SupportedFieldType.MONEY)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Sku - Retail Price")
+                .name("retailPrice")
+                .operators("blcOperators_Numeric")
+                .options("[]")
+                .type(SupportedFieldType.MONEY)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Product - URL")
+                .name("product.url")
+                .operators("blcOperators_Text")
+                .options("[]")
+                .type(SupportedFieldType.STRING)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Product - Is Featured Product")
+                .name("product.isFeaturedProduct")
+                .operators("blcOperators_Boolean")
+                .options("[]")
+                .type(SupportedFieldType.BOOLEAN)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Product - Manufacturer")
+                .name("product.manufacturer")
+                .operators("blcOperators_Text")
+                .options("[]")
+                .type(SupportedFieldType.STRING)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Product - Model")
+                .name("product.model")
+                .operators("blcOperators_Text")
+                .options("[]")
+                .type(SupportedFieldType.STRING)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Sku - Fulfillment Type")
+                .name("fulfillmentType")
+                .operators("blcOperators_Enumeration")
+                .options("blcOptions_FulfillmentType")
+                .type(SupportedFieldType.BROADLEAF_ENUMERATION)
+                .build());
+        fields.add(new FieldData.Builder()
+                .label("Sku - Inventory Type")
+                .name("inventoryType")
+                .operators("blcOperators_Enumeration")
+                .options("blcOptions_InventoryType")
+                .type(SupportedFieldType.BROADLEAF_ENUMERATION)
+                .build());
+
+    }
+
+    @Override
+    public String getName() {
+        return "SKU_FIELDS";
+    }
+
+}
--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/type/RuleIdentifier.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/type/RuleIdentifier.java
@@ -13,12 +13,14 @@ public class RuleIdentifier {
     public static final String LOCALE  = "LOCALE_FIELDS";
     public static final String ORDER  = "ORDER_FIELDS";
     public static final String ORDERITEM  = "ORDER_ITEM_FIELDS";
+    public static final String SKU = "SKU_FIELDS";
 
     public static final String CUSTOMER_ENTITY_KEY = "customer";
     public static final String FULFILLMENTGROUP_ENTITY_KEY = "fulfillmentGroup";
     public static final String LOCALE_ENTITY_KEY = "locale";
     public static final String ORDER_ENTITY_KEY = "order";
     public static final String ORDERITEM_ENTITY_KEY = "orderItem";
+    public static final String SKU_ENTITY_KEY = "sku";
 
     public static final String CUSTOMER_FIELD_KEY = "CUSTOMER";
     public static final String LOCALE_FIELD_KEY = "LOCALE";
@@ -30,5 +32,6 @@ public class RuleIdentifier {
         ENTITY_KEY_MAP.put(LOCALE, LOCALE_ENTITY_KEY);
         ENTITY_KEY_MAP.put(ORDER, ORDER_ENTITY_KEY);
         ENTITY_KEY_MAP.put(ORDERITEM, ORDERITEM_ENTITY_KEY);
+        ENTITY_KEY_MAP.put(SKU, SKU_ENTITY_KEY);
     }
 }
