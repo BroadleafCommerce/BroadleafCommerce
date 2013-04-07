25c1e385c2558af6fa1c488fd63701eadbc5fd7e
Phillip Verheyden <pverheyden@gmail.com>
2013-04-07 00:47:01 -0500

    BLC-860 - Update docs for module activities

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/workflow/ModuleActivity.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/workflow/ModuleActivity.java
@@ -18,11 +18,15 @@ package org.broadleafcommerce.core.workflow;
 
 
 /**
+ * <p>
  * Marker interface that all modules should use when adding activities to Broadleaf workflows. This is used for logging to
  * the user on startup that a module has modified a particular workflow and the final ordering of the configured workflow.
  * This logging is necessary for users that might be unaware of all of the activities that different modules could be
  * injecting into their workflows, since it's possible they they might want to change the ordering of their particular
- * activities as well.
+ * activities as well.</p>
+ * 
+ * <p>When writing a module activity, the ordering should be configured in the 100 range (3100, 3200, etc) so that users
+ * who use your module can configure custom activities in-between framework <b>and</b> module activities.</p>
  * 
  * @author Phillip Verheyden (phillipuniverse)
  */
