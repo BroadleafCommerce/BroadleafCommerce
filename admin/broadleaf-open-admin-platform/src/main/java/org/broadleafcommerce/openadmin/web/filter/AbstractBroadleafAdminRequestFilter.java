47c62ade4c7cfc83117a7ac5cecdf8ac4195a889
jefffischer <jfischer@broadleafcommerce.org>
2013-05-20 13:27:04 -0500

    fix some admin request filter issues

---
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/filter/AbstractBroadleafAdminRequestFilter.java
@@ -0,0 +1,65 @@
+package org.broadleafcommerce.openadmin.web.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Jeff Fischer
+ */
+public abstract class AbstractBroadleafAdminRequestFilter extends OncePerRequestFilter {
+
+    private final Log LOG = LogFactory.getLog(AbstractBroadleafAdminRequestFilter.class);
+
+    private Set<String> ignoreSuffixes;
+
+    /**
+     * Determines if the passed in URL should be processed by the content management system.
+     * <p/>
+     * By default, this method returns false for any BLC-Admin URLs and service calls and for all
+     * common image/digital mime-types (as determined by an internal call to {@code getIgnoreSuffixes}.
+     * <p/>
+     * This check is called with the {@code doFilterInternal} method to short-circuit the content
+     * processing which can be expensive for requests that do not require it.
+     *
+     * @param requestURI - the HttpServletRequest.getRequestURI
+     * @return true if the {@code HttpServletRequest} should be processed
+     */
+    protected boolean shouldProcessURL(HttpServletRequest request, String requestURI) {
+        int pos = requestURI.lastIndexOf(".");
+        if (pos > 0) {
+            String suffix = requestURI.substring(pos);
+            if (getIgnoreSuffixes().contains(suffix.toLowerCase())) {
+                if (LOG.isTraceEnabled()) {
+                    LOG.trace("BroadleafProcessURLFilter ignoring request due to suffix " + requestURI);
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns a set of suffixes that can be ignored by content processing.   The following
+     * are returned:
+     * <p/>
+     * <B>List of suffixes ignored:</B>
+     *
+     * ".aif", ".aiff", ".asf", ".avi", ".bin", ".bmp", ".doc", ".eps", ".gif", ".hqx", ".jpg", ".jpeg", ".mid", ".midi", ".mov", ".mp3", ".mpg", ".mpeg", ".p65", ".pdf", ".pic", ".pict", ".png", ".ppt", ".psd", ".qxd", ".ram", ".ra", ".rm", ".sea", ".sit", ".stk", ".swf", ".tif", ".tiff", ".txt", ".rtf", ".vob", ".wav", ".wmf", ".xls", ".zip";
+     *
+     * @return set of suffixes to ignore.
+     */
+    @SuppressWarnings("rawtypes")
+    protected Set getIgnoreSuffixes() {
+        if (ignoreSuffixes == null || ignoreSuffixes.isEmpty()) {
+            String[] ignoreSuffixList = {".aif", ".aiff", ".asf", ".avi", ".bin", ".bmp", ".css", ".doc", ".eps", ".gif", ".hqx", ".js", ".jpg", ".jpeg", ".mid", ".midi", ".mov", ".mp3", ".mpg", ".mpeg", ".p65", ".pdf", ".pic", ".pict", ".png", ".ppt", ".psd", ".qxd", ".ram", ".ra", ".rm", ".sea", ".sit", ".stk", ".swf", ".tif", ".tiff", ".txt", ".rtf", ".vob", ".wav", ".wmf", ".xls", ".zip"};
+            ignoreSuffixes = new HashSet<String>(Arrays.asList(ignoreSuffixList));
+        }
+        return ignoreSuffixes;
+    }
+}
--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/filter/BroadleafAdminRequestFilter.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/filter/BroadleafAdminRequestFilter.java
@@ -22,18 +22,13 @@ import org.broadleafcommerce.common.exception.SiteNotFoundException;
 import org.broadleafcommerce.common.web.BroadleafWebRequestProcessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.ServletWebRequest;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Responsible for setting the necessary attributes on the BroadleafRequestContext
@@ -41,10 +36,9 @@ import javax.servlet.http.HttpServletResponse;
  * @author Andre Azzolini (apazzolini)
  */
 @Component("blAdminRequestFilter")
-public class BroadleafAdminRequestFilter extends OncePerRequestFilter {
-    private final Log LOG = LogFactory.getLog(BroadleafAdminRequestFilter.class);
+public class BroadleafAdminRequestFilter extends AbstractBroadleafAdminRequestFilter {
 
-    private Set<String> ignoreSuffixes;
+    private final Log LOG = LogFactory.getLog(BroadleafAdminRequestFilter.class);
 
     @Resource(name = "blAdminRequestProcessor")
     protected BroadleafWebRequestProcessor requestProcessor;
@@ -66,51 +60,5 @@ public class BroadleafAdminRequestFilter extends OncePerRequestFilter {
         } catch (SiteNotFoundException e) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
-    }
-   
-
-    /**
-     * Determines if the passed in URL should be processed by the content management system.
-     * <p/>
-     * By default, this method returns false for any BLC-Admin URLs and service calls and for all
-     * common image/digital mime-types (as determined by an internal call to {@code getIgnoreSuffixes}.
-     * <p/>
-     * This check is called with the {@code doFilterInternal} method to short-circuit the content
-     * processing which can be expensive for requests that do not require it.
-     *
-     * @param requestURI - the HttpServletRequest.getRequestURI
-     * @return true if the {@code HttpServletRequest} should be processed
-     */
-    protected boolean shouldProcessURL(HttpServletRequest request, String requestURI) {
-        int pos = requestURI.lastIndexOf(".");
-        if (pos > 0) {
-            String suffix = requestURI.substring(pos);
-            if (getIgnoreSuffixes().contains(suffix.toLowerCase())) {
-                if (LOG.isTraceEnabled()) {
-                    LOG.trace("BroadleafProcessURLFilter ignoring request due to suffix " + requestURI);
-                }
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Returns a set of suffixes that can be ignored by content processing.   The following
-     * are returned:
-     * <p/>
-     * <B>List of suffixes ignored:</B>
-     *
-     * ".aif", ".aiff", ".asf", ".avi", ".bin", ".bmp", ".doc", ".eps", ".gif", ".hqx", ".jpg", ".jpeg", ".mid", ".midi", ".mov", ".mp3", ".mpg", ".mpeg", ".p65", ".pdf", ".pic", ".pict", ".png", ".ppt", ".psd", ".qxd", ".ram", ".ra", ".rm", ".sea", ".sit", ".stk", ".swf", ".tif", ".tiff", ".txt", ".rtf", ".vob", ".wav", ".wmf", ".xls", ".zip";
-     *
-     * @return set of suffixes to ignore.
-     */
-    @SuppressWarnings("rawtypes")
-    protected Set getIgnoreSuffixes() {
-        if (ignoreSuffixes == null || ignoreSuffixes.isEmpty()) {
-            String[] ignoreSuffixList = {".aif", ".aiff", ".asf", ".avi", ".bin", ".bmp", ".css", ".doc", ".eps", ".gif", ".hqx", ".js", ".jpg", ".jpeg", ".mid", ".midi", ".mov", ".mp3", ".mpg", ".mpeg", ".p65", ".pdf", ".pic", ".pict", ".png", ".ppt", ".psd", ".qxd", ".ram", ".ra", ".rm", ".sea", ".sit", ".stk", ".swf", ".tif", ".tiff", ".txt", ".rtf", ".vob", ".wav", ".wmf", ".xls", ".zip"};
-            ignoreSuffixes = new HashSet<String>(Arrays.asList(ignoreSuffixList));
-        }
-        return ignoreSuffixes;
     }
 }
