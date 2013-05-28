718ea57bb15642906fe67d12af57a0fb47bba8dc
Andre Azzolini <apazzolini@gmail.com>
2013-05-28 17:27:26 -0500

    Move helper methods to the admin abstract controller

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/controller/entity/AdminBasicOperationsController.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/controller/entity/AdminBasicOperationsController.java
@@ -22,10 +22,8 @@ import org.broadleafcommerce.openadmin.dto.DynamicResultSet;
 import org.broadleafcommerce.openadmin.dto.FieldMetadata;
 import org.broadleafcommerce.openadmin.dto.Property;
 import org.broadleafcommerce.openadmin.server.domain.PersistencePackageRequest;
-import org.broadleafcommerce.openadmin.server.service.AdminEntityService;
 import org.broadleafcommerce.openadmin.web.controller.AdminAbstractController;
 import org.broadleafcommerce.openadmin.web.form.component.ListGrid;
-import org.broadleafcommerce.openadmin.web.service.FormBuilderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
@@ -36,7 +34,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -48,20 +45,6 @@ import javax.servlet.http.HttpServletResponse;
 @Controller("blAdminBasicOperationsController")
 public class AdminBasicOperationsController extends AdminAbstractController {
     
-    // ***********************
-    // RESOURCE DECLARATIONS *
-    // ***********************
-
-    @Resource(name = "blAdminEntityService")
-    protected AdminEntityService service;
-
-    @Resource(name = "blFormBuilderService")
-    protected FormBuilderService formService;
-    
-    // We will rely on some of the protected helper methods from the basic entity controller
-    @Resource(name = "blAdminBasicEntityController")
-    protected AdminBasicEntityController basicEntityController;
-
     /**
      * Shows the modal dialog that is used to select a "to-one" collection item. For example, this could be used to show
      * a list of categories for the ManyToOne field "defaultCategory" in Product.
@@ -81,16 +64,16 @@ public class AdminBasicOperationsController extends AdminAbstractController {
             @PathVariable(value = "owningClass") String owningClass,
             @PathVariable(value="collectionField") String collectionField,
             @RequestParam  MultiValueMap<String, String> requestParams) throws Exception {
-        PersistencePackageRequest ppr = basicEntityController.getSectionPersistencePackageRequest(owningClass, requestParams);
+        PersistencePackageRequest ppr = getSectionPersistencePackageRequest(owningClass, requestParams);
         ClassMetadata mainMetadata = service.getClassMetadata(ppr);
         Property collectionProperty = mainMetadata.getPMap().get(collectionField);
         FieldMetadata md = collectionProperty.getMetadata();
 
         ppr = PersistencePackageRequest.fromMetadata(md);
         
-        ppr.addFilterAndSortCriteria(basicEntityController.getCriteria(requestParams));
-        ppr.setStartIndex(basicEntityController.getStartIndex(requestParams));
-        ppr.setMaxIndex(basicEntityController.getMaxIndex(requestParams));
+        ppr.addFilterAndSortCriteria(getCriteria(requestParams));
+        ppr.setStartIndex(getStartIndex(requestParams));
+        ppr.setMaxIndex(getMaxIndex(requestParams));
         
         if (md instanceof BasicFieldMetadata) {
             DynamicResultSet drs = service.getRecords(ppr);
@@ -103,7 +86,7 @@ public class AdminBasicOperationsController extends AdminAbstractController {
         model.addAttribute("currentUrl", request.getRequestURL().toString());
         model.addAttribute("modalHeaderType", "selectCollectionItem");
         model.addAttribute("collectionProperty", collectionProperty);
-        basicEntityController.setModelAttributes(model, owningClass);
+        setModelAttributes(model, owningClass);
         return "modules/modalContainer";
     }
     
