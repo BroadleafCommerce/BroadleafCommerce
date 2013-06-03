98cf08f4787ac3e656e62f6fb608c6892993e0b2
Andre Azzolini <apazzolini@gmail.com>
2013-06-03 17:34:29 -0500

    Fix NPE

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/interceptor/JSFieldNameCompatibilityInterceptor.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/interceptor/JSFieldNameCompatibilityInterceptor.java
@@ -9,9 +9,10 @@ import org.broadleafcommerce.openadmin.web.form.entity.Field;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 /**
  * @author Jeff Fischer
@@ -21,28 +22,30 @@ public class JSFieldNameCompatibilityInterceptor extends HandlerInterceptorAdapt
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView
             modelAndView) throws Exception {
-        Entity entity = (Entity) modelAndView.getModelMap().get("entity");
-        EntityForm entityForm = (EntityForm) modelAndView.getModelMap().get("entityForm");
-
-        if (entity != null) {
-            for (Property property : entity.getProperties()) {
-                if (property.getName().contains(".")) {
-                    property.setName(JSCompatibilityHelper.encodeFieldName(property.getName()));
-                }
-            }
-        }
-
-        if (entityForm != null) {
-            entityForm.clearFieldsMap();
-            for (Map.Entry<String, Field> field : entityForm.getFields().entrySet()) {
-                if (field.getKey().contains(".")) {
-                    field.getValue().setName(JSCompatibilityHelper.encodeFieldName(field.getValue().getName()));
-                    if (field.getValue() instanceof RuleBuilderField) {
-                        ((RuleBuilderField) field.getValue()).setJsonFieldName(JSCompatibilityHelper.encodeFieldName(((RuleBuilderField) field.getValue()).getJsonFieldName()));
+        if (modelAndView != null) {
+            Entity entity = (Entity) modelAndView.getModelMap().get("entity");
+            EntityForm entityForm = (EntityForm) modelAndView.getModelMap().get("entityForm");
+    
+            if (entity != null) {
+                for (Property property : entity.getProperties()) {
+                    if (property.getName().contains(".")) {
+                        property.setName(JSCompatibilityHelper.encodeFieldName(property.getName()));
                     }
                 }
             }
-            entityForm.clearFieldsMap();
+    
+            if (entityForm != null) {
+                entityForm.clearFieldsMap();
+                for (Map.Entry<String, Field> field : entityForm.getFields().entrySet()) {
+                    if (field.getKey().contains(".")) {
+                        field.getValue().setName(JSCompatibilityHelper.encodeFieldName(field.getValue().getName()));
+                        if (field.getValue() instanceof RuleBuilderField) {
+                            ((RuleBuilderField) field.getValue()).setJsonFieldName(JSCompatibilityHelper.encodeFieldName(((RuleBuilderField) field.getValue()).getJsonFieldName()));
+                        }
+                    }
+                }
+                entityForm.clearFieldsMap();
+            }
         }
     }
 }
