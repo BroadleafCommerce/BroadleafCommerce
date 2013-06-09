d7011f7fef3cdf04f5d992eb4e6a2c067c99cb1e
Phillip Verheyden <pverheyden@gmail.com>
2013-06-09 17:37:23 -0500

    After validation, fix a problem where checkbox and radio fields weren't being sent back to the server and wouldn't be displayed

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/controller/entity/AdminBasicEntityController.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/controller/entity/AdminBasicEntityController.java
@@ -236,8 +236,7 @@ public class AdminBasicEntityController extends AdminAbstractController {
         if (result.hasErrors()) {
             ClassMetadata cmd = service.getClassMetadata(getSectionPersistencePackageRequest(entityForm.getEntityType()));
             entityForm.clearFieldsMap();
-            //TODO: this is currently removing valid properties (ones that came across the wire as null)
-            formService.populateEntityForm(cmd, entity, entityForm, false);
+            formService.populateEntityForm(cmd, entity, entityForm, true);
 
             formService.removeNonApplicableFields(cmd, entityForm, entityForm.getEntityType());
 
@@ -386,7 +385,7 @@ public class AdminBasicEntityController extends AdminAbstractController {
             Map<String, DynamicResultSet> subRecordsMap = service.getRecordsForAllSubCollections(ppr, entity);
             ClassMetadata cmd = service.getClassMetadata(ppr);
             entityForm.clearFieldsMap();
-            formService.populateEntityForm(cmd, entity, subRecordsMap, entityForm);
+            formService.populateEntityForm(cmd, entity, subRecordsMap, entityForm, true);
             
             model.addAttribute("entity", entity);
             model.addAttribute("currentUrl", request.getRequestURL().toString());
