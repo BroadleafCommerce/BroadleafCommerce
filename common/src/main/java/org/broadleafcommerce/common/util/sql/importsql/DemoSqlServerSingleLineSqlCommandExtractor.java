98addd5ae31d818d1bd99482f2714a304d100179
jefffischer <jfischer@broadleafcommerce.org>
2013-06-14 15:22:58 -0500

    Allow dynamic conversion of import sql for SQL Server compatibility

---
+++ common/src/main/java/org/broadleafcommerce/common/util/sql/importsql/DemoSqlServerSingleLineSqlCommandExtractor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2008-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.common.util.sql.importsql;
+
+import org.hibernate.tool.hbm2ddl.SingleLineSqlCommandExtractor;
+
+import java.io.Reader;
+
+/**
+ * This is a utility class that is only meant to be used for testing the BLC demo on SQL Server. In our current
+ * import sql files, there are a number of value declarations that are incompatible with Sql Server. This
+ * custom extractor takes care of transforming those values into something SQL Server understands.
+ *
+ * @author Jeff Fischer
+ */
+public class DemoSqlServerSingleLineSqlCommandExtractor extends SingleLineSqlCommandExtractor {
+
+    private static final String BOOLEANTRUEMATCH = "(?i)(true)";
+    private static final String BOOLEANFALSEMATCH = "(?i)(false)";
+    private static final String TIMESTAMPMATCH = "(?i)(current_date)";
+    public static final String TRUE = "'TRUE'";
+    public static final String FALSE = "'FALSE'";
+    public static final String CURRENT_TIMESTAMP = "CURRENT_TIMESTAMP";
+
+    @Override
+    public String[] extractCommands(Reader reader) {
+        String[] statements = super.extractCommands(reader);
+        for (int j=0; j<statements.length; j++) {
+            //try start matches
+            statements[j] = statements[j].replaceAll(BOOLEANTRUEMATCH + "\\s*[,]", TRUE + ",");
+            statements[j] = statements[j].replaceAll(BOOLEANFALSEMATCH + "\\s*[,]", FALSE + ",");
+            statements[j] = statements[j].replaceAll(TIMESTAMPMATCH + "\\s*[,]", CURRENT_TIMESTAMP + ",");
+
+            //try middle matches
+            statements[j] = statements[j].replaceAll("[,]\\s*" + BOOLEANTRUEMATCH + "\\s*[,]", "," + TRUE + ",");
+            statements[j] = statements[j].replaceAll("[,]\\s*" + BOOLEANFALSEMATCH + "\\s*[,]", "," + FALSE + ",");
+            statements[j] = statements[j].replaceAll("[,]\\s*" + TIMESTAMPMATCH + "\\s*[,]", "," + CURRENT_TIMESTAMP + ",");
+
+            //try end matches
+            statements[j] = statements[j].replaceAll("[,]\\s*" + BOOLEANTRUEMATCH, "," + TRUE);
+            statements[j] = statements[j].replaceAll("[,]\\s*" + BOOLEANFALSEMATCH, "," + FALSE);
+            statements[j] = statements[j].replaceAll("[,]\\s*" + TIMESTAMPMATCH, "," + CURRENT_TIMESTAMP);
+        }
+
+        return statements;
+    }
+}
