974a07844397597e394d2314c438e2290cce50be
Andre Azzolini <apazzolini@gmail.com>
2013-06-25 16:37:46 -0500

    Add init method to the SiteConfigProvider interface

--- common/src/main/java/org/broadleafcommerce/common/site/service/provider/SiteConfigProvider.java
+++ common/src/main/java/org/broadleafcommerce/common/site/service/provider/SiteConfigProvider.java
@@ -18,11 +18,15 @@ package org.broadleafcommerce.common.site.service.provider;
 
 import org.broadleafcommerce.common.site.domain.Site;
 
+import java.util.Map;
+
 /**
  * @author Jeff Fischer
  */
 public interface SiteConfigProvider {
 
     public void configSite(Site site);
+    
+    public void init(Map<String, Object> map);
 
 }
