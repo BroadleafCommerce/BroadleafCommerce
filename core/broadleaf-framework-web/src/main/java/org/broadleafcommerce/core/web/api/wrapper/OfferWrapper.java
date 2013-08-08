7eedd184b825dbb9d0261bd357d2711e08cc06dd
Priyesh Patel <ppatel@broadleafcommerce.org>
2013-08-08 10:35:53 -0500

    separate out details and summary info for offer wrapper

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/api/wrapper/OfferWrapper.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/api/wrapper/OfferWrapper.java
@@ -35,10 +35,18 @@ public class OfferWrapper extends BaseWrapper implements APIWrapper<Offer> {
 
     @XmlElement
     protected Long offerId;
-
+    @XmlElement
+    protected String startDate;
+    @XmlElement
+    protected String endDate;
     @XmlElement
     protected String marketingMessage;;
-
+    @XmlElement
+    protected String description;
+    @XmlElement
+    protected String name;
+    @XmlElement
+    protected Boolean automatic;
     @XmlElement
     protected BroadleafEnumerationTypeWrapper offerType;;
 
@@ -48,6 +56,11 @@ public class OfferWrapper extends BaseWrapper implements APIWrapper<Offer> {
     @Override
     public void wrapDetails(Offer model, HttpServletRequest request) {
         this.marketingMessage = model.getMarketingMessage();
+        this.name = model.getName();
+        this.startDate = model.getStartDate().toString();
+        this.endDate = model.getStartDate().toString();
+        this.description = model.getDescription();
+        this.automatic = model.isAutomaticallyAdded();
         this.offerType = (BroadleafEnumerationTypeWrapper) context.getBean(BroadleafEnumerationTypeWrapper.class.getName());
         this.offerType.wrapDetails(model.getType(), request);
         this.discountType = (BroadleafEnumerationTypeWrapper) context.getBean(BroadleafEnumerationTypeWrapper.class.getName());
@@ -57,6 +70,13 @@ public class OfferWrapper extends BaseWrapper implements APIWrapper<Offer> {
 
     @Override
     public void wrapSummary(Offer model, HttpServletRequest request) {
-        wrapDetails(model, request);
+        this.automatic = model.isAutomaticallyAdded();
+        this.offerType = (BroadleafEnumerationTypeWrapper) context.getBean(BroadleafEnumerationTypeWrapper.class.getName());
+        this.offerType.wrapDetails(model.getType(), request);
+        this.discountType = (BroadleafEnumerationTypeWrapper) context.getBean(BroadleafEnumerationTypeWrapper.class.getName());
+        this.discountType.wrapDetails(model.getDiscountType(), request);
+        this.offerId = model.getId();
+        this.marketingMessage = model.getMarketingMessage();
+        this.name = model.getName();
     }
 }
