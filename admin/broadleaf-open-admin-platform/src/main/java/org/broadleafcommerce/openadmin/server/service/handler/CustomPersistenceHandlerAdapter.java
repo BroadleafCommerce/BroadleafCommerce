f8bc03100c96b19bced31c9fc2ce2cd670e99fed
Andre Azzolini <apazzolini@gmail.com>
2013-08-14 14:02:30 -0500

    Provide some helper methods to facilitate custom inspects

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/handler/CustomPersistenceHandlerAdapter.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/handler/CustomPersistenceHandlerAdapter.java
@@ -17,13 +17,21 @@
 package org.broadleafcommerce.openadmin.server.service.handler;
 
 import org.broadleafcommerce.common.exception.ServiceException;
+import org.broadleafcommerce.openadmin.dto.ClassMetadata;
 import org.broadleafcommerce.openadmin.dto.CriteriaTransferObject;
 import org.broadleafcommerce.openadmin.dto.DynamicResultSet;
 import org.broadleafcommerce.openadmin.dto.Entity;
+import org.broadleafcommerce.openadmin.dto.FieldMetadata;
+import org.broadleafcommerce.openadmin.dto.MergedPropertyType;
 import org.broadleafcommerce.openadmin.dto.PersistencePackage;
+import org.broadleafcommerce.openadmin.dto.PersistencePerspective;
 import org.broadleafcommerce.openadmin.server.dao.DynamicEntityDao;
+import org.broadleafcommerce.openadmin.server.service.persistence.PersistenceManager;
 import org.broadleafcommerce.openadmin.server.service.persistence.module.InspectHelper;
 import org.broadleafcommerce.openadmin.server.service.persistence.module.RecordHelper;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -89,5 +97,38 @@ public class CustomPersistenceHandlerAdapter implements CustomPersistenceHandler
     @Override
     public int getOrder() {
         return CustomPersistenceHandler.DEFAULT_ORDER;
+    }
+    
+    /**
+     * This is a helper method that can be invoked as a first step in a custom inspect phase
+     */
+    protected Map<String, FieldMetadata> getMetadata(PersistencePackage persistencePackage, InspectHelper helper) 
+            throws ServiceException {
+        String entityName = persistencePackage.getCeilingEntityFullyQualifiedClassname();
+        PersistencePerspective perspective = persistencePackage.getPersistencePerspective();
+        return helper.getSimpleMergedProperties(entityName, perspective);
+    }
+    
+    /**
+     * This is a helper method that can be invoked as the last step in a custom inspect phase. It will assemble the
+     * appropriate DynamicResultSet from the given parameters.
+     */
+    protected DynamicResultSet getResultSet(PersistencePackage persistencePackage, InspectHelper helper,
+            Map<String, FieldMetadata> metadata) throws ServiceException {
+        String entityName = persistencePackage.getCeilingEntityFullyQualifiedClassname();
+        try {
+            if (helper instanceof PersistenceManager) {
+                Class<?>[] entities = ((PersistenceManager) helper).getPolymorphicEntities(entityName);
+                Map<MergedPropertyType, Map<String, FieldMetadata>> allMergedProperties = 
+                        new HashMap<MergedPropertyType, Map<String, FieldMetadata>>();
+                allMergedProperties.put(MergedPropertyType.PRIMARY, metadata);
+                ClassMetadata mergedMetadata = helper.getMergedClassMetadata(entities, allMergedProperties);
+                DynamicResultSet results = new DynamicResultSet(mergedMetadata);
+                return results;
+            }
+        } catch (ClassNotFoundException e) {
+            throw new ServiceException(e);
+        }
+        return new DynamicResultSet();
     }
 }
