dcb12df126ff2bd49655b95408ff589b9d0763ba
Phillip Verheyden <pverheyden@gmail.com>
2013-08-15 09:27:38 -0500

    Enhance regex validation to gracefully handle wrong regex configuration as well as optional null value failure

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/persistence/validation/RegexPropertyValidator.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/persistence/validation/RegexPropertyValidator.java
@@ -16,6 +16,9 @@
 
 package org.broadleafcommerce.openadmin.server.service.persistence.validation;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.broadleafcommerce.common.presentation.ConfigurationItem;
 import org.broadleafcommerce.openadmin.dto.BasicFieldMetadata;
 import org.broadleafcommerce.openadmin.dto.Entity;
 import org.broadleafcommerce.openadmin.dto.FieldMetadata;
@@ -23,6 +26,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.regex.PatternSyntaxException;
 
 
 /**
@@ -33,8 +37,24 @@ import java.util.Map;
 @Component("blRegexPropertyValidator")
 public class RegexPropertyValidator extends ValidationConfigurationBasedPropertyValidator {
 
+    protected static final Log LOG = LogFactory.getLog(RegexPropertyValidator.class);
+
+    /**
+     * Denotes what should occur when this validator encounters a null value to validate against. Default behavior is to
+     * allow them, which means that this validator will always return true with null values
+     */
+    protected boolean succeedForNullValues = true;
+    
+    /**
+     * Whether or not this validator should succeed (and thus pass validation) even when the configured regular expression
+     * pattern is invalid. While this value defaults to true, it might be beneficial to set this to false in a development
+     * environment to debug problems in regular expressions. In either case, this validator will log that there is an invalid
+     * regular expression
+     */
+    protected boolean suceedForInvalidRegex = true;
+    
     @Override
-    public boolean validateInternal(Entity entity,
+    public PropertyValidationResult validate(Entity entity,
             Serializable instance,
             Map<String, FieldMetadata> entityFieldMetadata,
             Map<String, String> validationConfiguration,
@@ -42,7 +62,34 @@ public class RegexPropertyValidator extends ValidationConfigurationBasedProperty
             String propertyName,
             String value) {
         String expression = validationConfiguration.get("regularExpression");
-        return (value == null) ? true : value.matches(expression);
+        
+        if (value == null) {
+            return new PropertyValidationResult(succeedForNullValues, validationConfiguration.get(ConfigurationItem.ERROR_MESSAGE));
+        }
+        
+        try {
+            return new PropertyValidationResult(value.matches(expression), validationConfiguration.get(ConfigurationItem.ERROR_MESSAGE));
+        } catch (PatternSyntaxException e) {
+            String message = "Invalid regular expression pattern '" + expression + "' for " + propertyName;
+            LOG.error(message, e);
+            return new PropertyValidationResult(suceedForInvalidRegex, "Invalid regular expression pattern for " + propertyName);
+        }
+    }
+    
+    public boolean isSucceedForNullValues() {
+        return succeedForNullValues;
+    }
+
+    public void setSucceedForNullValues(boolean succeedForNullValues) {
+        this.succeedForNullValues = succeedForNullValues;
+    }
+
+    public boolean isSuceedForInvalidRegex() {
+        return suceedForInvalidRegex;
+    }
+
+    public void setSuceedForInvalidRegex(boolean suceedForInvalidRegex) {
+        this.suceedForInvalidRegex = suceedForInvalidRegex;
     }
 
 }
