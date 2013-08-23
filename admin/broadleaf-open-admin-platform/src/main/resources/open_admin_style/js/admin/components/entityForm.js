abaa4f771a1c34665c64b270de28dcf0a35d5387
Andre Azzolini <apazzolini@gmail.com>
2013-08-23 11:07:30 -0500

    #296 - Slight bug fix(cherry picked from commit c3f92d9b3c9028a430f20bee86aba1d58fa2aa56)

--- admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/entityForm.js
+++ admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/entityForm.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	// When the delete button is clicked, we can change the desired action for the
 	// form and submit it normally (not via AJAX).
 	$('body').on('click', 'button.delete-button', function(event) {
-	    var $form = BLCAdmin.getForm();
+	    var $form = BLCAdmin.getForm($(this));
 		var currentAction = $form.attr('action');
 		
 		$form.attr('action', currentAction + '/delete');
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	});
 	
 	$('body').on('click', 'button.submit-button', function(event) {
-	    var $form = BLCAdmin.getForm();
+	    var $form = BLCAdmin.getForm($(this));
 	    $form.submit();
 		event.preventDefault();
 	});
