f96f207aa63800b8976097bb0abed918c297ec68
jefffischer <jfischer@broadleafcommerce.org>
2013-09-03 18:09:42 -0500

    Resolve #321 - Saving an entity form that contains a dynamic forms lead to partial saves Resolve #312 - Structured Content and Pages do not save properly when changing the type or template

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/persistence/PersistenceManagerImpl.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/persistence/PersistenceManagerImpl.java
@@ -326,7 +326,7 @@ public class PersistenceManagerImpl implements InspectHelper, PersistenceManager
                             if (!handler.willHandleSecurity(subPackage.getValue())) {
                                 adminRemoteSecurityService.securityCheck(subPackage.getValue().getCeilingEntityFullyQualifiedClassname(), EntityOperationType.ADD);
                             }
-                            subResponse = handler.add(subPackage.getValue(), dynamicEntityDao, (RecordHelper) getCompatibleModule(OperationType.BASIC));
+                            subResponse = handler.update(subPackage.getValue(), dynamicEntityDao, (RecordHelper) getCompatibleModule(OperationType.BASIC));
                             subPackage.getValue().setEntity(subResponse);
 
                             break checkHandler;
@@ -334,7 +334,7 @@ public class PersistenceManagerImpl implements InspectHelper, PersistenceManager
                     }
                     adminRemoteSecurityService.securityCheck(subPackage.getValue().getCeilingEntityFullyQualifiedClassname(), EntityOperationType.ADD);
                     PersistenceModule subModule = getCompatibleModule(subPackage.getValue().getPersistencePerspective().getOperationTypes().getAddType());
-                    subResponse = subModule.add(persistencePackage);
+                    subResponse = subModule.update(persistencePackage);
                     subPackage.getValue().setEntity(subResponse);
                 }
             } catch (ValidationException e) {
