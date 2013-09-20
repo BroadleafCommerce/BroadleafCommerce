390cdbc1a19607e0d4cb4d2688812c12ac3ae57b
Phillip Verheyden <pverheyden@gmail.com>
2013-09-20 16:34:29 -0500

    #388 - Add a rollback handler for the CompleteOrderActivity and make the CommitTaxRollbackHandler an overridable bean

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/checkout/service/workflow/CompleteOrderRollbackHandler.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/checkout/service/workflow/CompleteOrderRollbackHandler.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,9 +16,7 @@
 
 package org.broadleafcommerce.core.checkout.service.workflow;
 
-import org.broadleafcommerce.core.order.domain.Order;
-import org.broadleafcommerce.core.pricing.service.TaxService;
-import org.broadleafcommerce.core.pricing.service.exception.TaxException;
+import org.broadleafcommerce.core.order.service.type.OrderStatus;
 import org.broadleafcommerce.core.workflow.Activity;
 import org.broadleafcommerce.core.workflow.ProcessContext;
 import org.broadleafcommerce.core.workflow.state.RollbackFailureException;
@@ -27,24 +25,25 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-import javax.annotation.Resource;
 
-@Component("blCommitTaxRollbackHandler")
-public class CommitTaxRollbackHandler implements RollbackHandler<CheckoutSeed> {
-
-    @Resource(name = "blTaxService")
-    protected TaxService taxService;
+/**
+ * Rollback handler to execute after an order has been marked as 'completed' and there is an exception.
+ * 
+ *  1. Change the status back to IN_PROCESS
+ *  2. Change the order number back to null
+ *  3. Change the submit date back to null
+ *
+ * @author Phillip Verheyden (phillipuniverse)
+ */
+@Component("blCompleteOrderRollbackHandler")
+public class CompleteOrderRollbackHandler implements RollbackHandler<CheckoutSeed> {
 
     @Override
     public void rollbackState(Activity<? extends ProcessContext<CheckoutSeed>> activity, ProcessContext<CheckoutSeed> processContext, Map<String, Object> stateConfiguration) throws RollbackFailureException {
-        ProcessContext<CheckoutSeed> ctx = processContext;
-        Order order = ctx.getSeedData().getOrder();
-        try {
-            taxService.cancelTax(order);
-        } catch (TaxException e) {
-            throw new RollbackFailureException("An exception occured cancelling taxes for order id: " + order.getId(), e);
-        }
-
+        CheckoutSeed seed = processContext.getSeedData();
+        seed.getOrder().setStatus(OrderStatus.IN_PROCESS);
+        seed.getOrder().setOrderNumber(null);
+        seed.getOrder().setSubmitDate(null);
     }
 
 }
