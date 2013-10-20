baca572b53f76d26bd52754eef32a830dc78b486
Phillip Verheyden <pverheyden@gmail.com>
2013-10-19 23:49:55 -0500

    #373 - Fix remaining issues with refreshing CartState when an Order is persisted

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/order/CartStateRefresher.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/order/CartStateRefresher.java
@@ -16,11 +16,17 @@
 
 package org.broadleafcommerce.core.web.order;
 
+import org.broadleafcommerce.common.web.BroadleafRequestContext;
+import org.broadleafcommerce.core.order.domain.NullOrderImpl;
 import org.broadleafcommerce.core.order.domain.Order;
 import org.broadleafcommerce.core.order.domain.OrderPersistedEntityListener;
 import org.broadleafcommerce.core.order.domain.OrderPersistedEvent;
+import org.broadleafcommerce.core.order.service.type.OrderStatus;
+import org.broadleafcommerce.profile.core.domain.Customer;
+import org.broadleafcommerce.profile.web.core.CustomerState;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
 
 
 /**
@@ -35,16 +41,30 @@ import org.springframework.stereotype.Component;
 public class CartStateRefresher implements ApplicationListener<OrderPersistedEvent> {
 
     /**
-     * Resets {@link CartState} with the newly persisted Order. This ensures that whatever is returned from
-     * {@link CartState#getCart()} will always be the most up-to-date database version (meaning, safe to write to the DB).
+     * <p>Resets {@link CartState} with the newly persisted Order. If {@link CartState} was empty, this will only update it if
+     * the {@link Order} that has been persisted is the {@link OrderStatus#IN_PROCESS} {@link Order} for the active
+     * {@link Customer} (as determined by {@link CustomerState#getCustomer()}. If {@link CartState} was <b>not</b> empty,
+     * then it will be replaced only if this newly persisted {@link Order} has the same id.</p>
+     * 
+     * <p>This ensures that whatever is returned from {@link CartState#getCart()} will always be the most up-to-date
+     * database version (meaning, safe to write to the DB).</p>
      */
     @Override
     public void onApplicationEvent(final OrderPersistedEvent event) {
-        Order dbOrder = event.getOrder();
-        //Update the cart state ONLY IF the IDs of the newly persisted order and whatever is already in CartState match
-        //TODO: what if CartState.getCart() is null? Need to know if the order can become CartState
-        if (CartState.getCart() != null && CartState.getCart().getId().equals(dbOrder.getId())) {
-            CartState.setCart(dbOrder);
+        WebRequest request = BroadleafRequestContext.getBroadleafRequestContext().getWebRequest();
+        if (request != null) {
+             Order dbOrder = event.getOrder();
+            //Update the cart state ONLY IF the IDs of the newly persisted order and whatever is already in CartState match
+            boolean emptyCartState = CartState.getCart() == null || CartState.getCart() instanceof NullOrderImpl;
+            if (emptyCartState) {
+                //If cart state is empty, set it to this newly persisted order if it's the active Customer's cart
+                if (CustomerState.getCustomer() != null && CustomerState.getCustomer().getId().equals(dbOrder.getCustomer().getId())
+                        && OrderStatus.IN_PROCESS.equals(dbOrder.getStatus())) {
+                    CartState.setCart(dbOrder);
+                }
+            } else if (CartState.getCart().getId().equals(dbOrder.getId())) {
+                CartState.setCart(dbOrder);
+            }
         }
     }
 
