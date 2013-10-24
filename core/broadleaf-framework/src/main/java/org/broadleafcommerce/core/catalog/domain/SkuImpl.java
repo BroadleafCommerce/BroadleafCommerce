0d0308828613585ace2895d983cf502ba7f967e0
Kelly Tisdell <ktisdell@broadleafcommerce.org>
2013-10-24 14:56:20 -0500

    #462(cherry picked from commit d9f66c86517d8ba396fcc76707c6097208f95f01)

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/SkuImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/SkuImpl.java
@@ -826,6 +826,13 @@ public class SkuImpl implements Sku {
 
     @Override
     public FulfillmentType getFulfillmentType() {
+        if (StringUtils.isEmpty(this.fulfillmentType)) {
+            if (hasDefaultSku() && !StringUtils.isEmpty(lookupDefaultSku().getTaxCode())) {
+                return lookupDefaultSku().getFulfillmentType();
+            } else if (getProduct() != null && getProduct().getDefaultCategory() != null) {
+                return getProduct().getDefaultCategory().getFulfillmentType();
+            }
+        }
         return FulfillmentType.getInstance(this.fulfillmentType);
     }
 
