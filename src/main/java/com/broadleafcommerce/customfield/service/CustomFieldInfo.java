b0aeea7c9f3ac242f0169811d0a3d31a2fb798dc
jefffischer <jfischer@broadleafcommerce.org>
2013-11-05 11:31:43 -0600

    Temporary demo changes

--- src/main/java/com/broadleafcommerce/customfield/service/CustomFieldInfo.java
+++ src/main/java/com/broadleafcommerce/customfield/service/CustomFieldInfo.java
@@ -18,13 +18,13 @@
 
 package com.broadleafcommerce.customfield.service;
 
-import org.broadleafcommerce.openadmin.server.service.type.RuleIdentifier;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.broadleafcommerce.common.presentation.RuleIdentifier;
 
 import com.broadleafcommerce.customfield.service.type.CustomFieldTargetType;
 import com.broadleafcommerce.customfield.service.type.CustomFieldType;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Useful constants and relationship maps for custom fields.
