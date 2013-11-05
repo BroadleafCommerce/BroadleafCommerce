936f35d3ed51123db35d951677519e3ee77c2d44
Joshua Skorton <jskorton@broadleafcommerce.com>
2013-11-05 14:10:56 -0600

    Added a utility method to create a proxy using the InvocationHandlerForUnitTestingByteWeavedClasses.

--- common/src/main/java/org/broadleafcommerce/common/util/InvocationHandlerForUnitTestingByteWeavedClasses.java
+++ common/src/main/java/org/broadleafcommerce/common/util/InvocationHandlerForUnitTestingByteWeavedClasses.java
@@ -1,29 +1,55 @@
+/*
+ * Copyright 2008-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.broadleafcommerce.common.util;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Proxy;
 
 /**
- * Invocation handler for unit testing byte-weaved classes.  When Spring is unavailable to complete byte-weaving during
- * unit testing, this invocation handler will invoke a chosen method against an array of Objects that are meant to be
- * byte-weaved together.  The invocation handler will return when the first object is found that can be successfully used
- * with the chosen method.
+ * Invocation handler for unit testing byte-weaved classes. Use this InvocationHandler and utility method when Spring is unavailable
+ * to complete byte-weaving.
  * 
  * @author Joshua Skorton (jskorton)
  */
 public class InvocationHandlerForUnitTestingByteWeavedClasses implements InvocationHandler {
 
-    protected List<Object> objectsForByteWeaving = new ArrayList<Object>();
+    /**
+     * This utility method will return a Proxy of a chosen type that response to an array of chose Interfaces and uses a
+     * InvocationHandlerForUnitTestingByteWeavedClasses that is backed by an array of chosen Objects.
+     * 
+     * @param proxyType
+     * @param interfaces
+     * @param objectsForByteWeaving
+     * @return
+     */
+    public static <T> T createProxy(Class<T> proxyType, Class<?>[] interfaces, Object[] objectsForByteWeaving) {
+        InvocationHandler handler = new InvocationHandlerForUnitTestingByteWeavedClasses(objectsForByteWeaving);
+        return (T) Proxy.newProxyInstance(handler.getClass().getClassLoader(), interfaces, handler);
+    }
 
-    public InvocationHandlerForUnitTestingByteWeavedClasses(List<Object> objectsForByteWeaving) {
+    protected Object[] objectsForByteWeaving;
+
+    public InvocationHandlerForUnitTestingByteWeavedClasses(Object[] objectsForByteWeaving) {
         this.objectsForByteWeaving = objectsForByteWeaving;
     }
 
     /**
-     * When Spring is unavailable to complete byte-weaving during unit testing, this invocation handler will invoke a chosen
-     * method against an array of Objects that are meant to be byte-weaved together.  The invocation handler will return when
+     * Will invoke a chosen method against an array of Objects that are meant to be byte-weaved together.  Invoke will return when
      * the first object is found that can be successfully used with the chosen method.  If no objects are found to work with
      * the chosen method, null will be returned.
      */
@@ -42,20 +68,20 @@ public class InvocationHandlerForUnitTestingByteWeavedClasses implements Invocat
     }
     
     /**
-     * Returns a list of Objects that are meant to be byte-weaved.  The invoke method will attempt to run against this list.
+     * Returns an array of Objects that are meant to be byte-weaved.
      * 
      * @return
      */
-    public List<Object> getObjectsForByteWeaving() {
+    public Object[] getObjectsForByteWeaving() {
         return objectsForByteWeaving;
     }
 
     /**
-     * Sets a list of Objects that are meant to be byte-weaved.  The invoke method will attempt to run against this list.
+     * Sets an array of Objects that are meant to be byte-weaved.
      * 
      * @param objects
      */
-    public void setObjectsForByteWeaving(List<Object> objects) {
+    public void setObjectsForByteWeaving(Object[] objects) {
         this.objectsForByteWeaving = objects;
     }
 
