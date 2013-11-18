7bc7bc7dede812252a87421be7bb4f0c4449007a
jefffischer <jfischer@broadleafcommerce.org>
2013-11-18 12:29:29 -0600

    replace license

--- admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/lib/plugins/jquery.cookie.js
+++ admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/lib/plugins/jquery.cookie.js
@@ -1,13 +1,5 @@
 /*jshint eqnull:true */
-/*!
- * jQuery Cookie Plugin v1.2
- * https://github.com/carhartl/jquery-cookie
- *
- * Copyright 2011, Klaus Hartl
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.opensource.org/licenses/GPL-2.0
- */
+
 (function ($, document, undefined) {
 
   var pluses = /\+/g;
