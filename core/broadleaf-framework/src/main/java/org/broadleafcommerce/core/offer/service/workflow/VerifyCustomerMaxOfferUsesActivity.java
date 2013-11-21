b668ecc7a550e1a4a3958d50ced3b5014a5e0a8c
Phillip Verheyden <pverheyden@gmail.com>
2013-11-21 10:58:39 -0600

    #195 - Utilize the offer audit service rather than the dao directly in verifying max offer usage

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/service/workflow/VerifyCustomerMaxOfferUsesActivity.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/service/workflow/VerifyCustomerMaxOfferUsesActivity.java
@@ -17,9 +17,9 @@
 package org.broadleafcommerce.core.offer.service.workflow;
 
 import org.broadleafcommerce.core.checkout.service.workflow.CheckoutContext;
-import org.broadleafcommerce.core.offer.dao.OfferAuditDao;
 import org.broadleafcommerce.core.offer.domain.Offer;
 import org.broadleafcommerce.core.offer.domain.OfferCode;
+import org.broadleafcommerce.core.offer.service.OfferAuditService;
 import org.broadleafcommerce.core.offer.service.OfferService;
 import org.broadleafcommerce.core.offer.service.exception.OfferMaxUseExceededException;
 import org.broadleafcommerce.core.order.domain.Order;
@@ -30,15 +30,17 @@ import java.util.Set;
 import javax.annotation.Resource;
 
 /**
- * Checks the offers being used in the order to make sure that the customer
- * has not exceeded the max uses for the offer.
+ * <p>Checks the offers being used in the order to make sure that the customer
+ * has not exceeded the max uses for the {@link Offer}.</p>
  * 
- * This will also verify that max uses for the code
+ * This will also verify that max uses for any {@link OfferCode}s that were used to retrieve the {@link Offer}s.
+ * 
+ * @author Phillip Verheyden (phillipuniverse)
  */
 public class VerifyCustomerMaxOfferUsesActivity extends BaseActivity<CheckoutContext> {
 
-    @Resource(name="blOfferAuditDao")
-    protected OfferAuditDao offerAuditDao;
+    @Resource(name="blOfferAuditService")
+    protected OfferAuditService offerAuditService;
     
     @Resource(name = "blOfferService")
     protected OfferService offerService;
@@ -50,7 +52,7 @@ public class VerifyCustomerMaxOfferUsesActivity extends BaseActivity<CheckoutCon
         
         for (Offer offer : appliedOffers) {
             if (offer.isLimitedUsePerCustomer()) {
-                Long currentUses = offerAuditDao.countUsesByCustomer(order.getCustomer().getId(), offer.getId());
+                Long currentUses = offerAuditService.countUsesByCustomer(order.getCustomer().getId(), offer.getId());
                 if (currentUses >= offer.getMaxUsesPerCustomer()) {
                     throw new OfferMaxUseExceededException("The customer has used this offer more than the maximum allowed number of times.");
                 }
@@ -60,7 +62,7 @@ public class VerifyCustomerMaxOfferUsesActivity extends BaseActivity<CheckoutCon
         //TODO: allow lenient checking on offer code usage
         for (OfferCode code : order.getAddedOfferCodes()) {
             if (code.isLimitedUse()) {
-                Long currentCodeUses = offerAuditDao.countOfferCodeUses(code.getId());
+                Long currentCodeUses = offerAuditService.countOfferCodeUses(code.getId());
                 if (currentCodeUses >= code.getMaxUses()) {
                     throw new OfferMaxUseExceededException("Offer code " + code.getOfferCode() + " with id " + code.getId()
                             + " has been than the maximum allowed number of times.");
