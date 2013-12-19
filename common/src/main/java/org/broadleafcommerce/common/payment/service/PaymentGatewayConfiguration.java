163bc377f56f08ad9b40d7ec8a205538d44657ca
Phillip Verheyden <pverheyden@gmail.com>
2013-12-19 17:43:31 -0600

    #485 - Move the payment gateway configuration into common so that the framework can get a handle to it

---
+++ common/src/main/java/org/broadleafcommerce/common/payment/service/PaymentGatewayConfiguration.java
@@ -0,0 +1,128 @@
+/*
+ * #%L
+ * BroadleafCommerce Common Libraries
+ * %%
+ * Copyright (C) 2009 - 2013 Broadleaf Commerce
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/*
+ * Copyright 2008-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.broadleafcommerce.common.payment.service;
+
+import org.broadleafcommerce.common.web.payment.expression.PaymentGatewayFieldExtensionHandler;
+import org.broadleafcommerce.common.web.payment.processor.TRCreditCardExtensionHandler;
+
+/**
+ * @author Elbert Bautista (elbertbautista)
+ */
+public class PaymentGatewayConfiguration {
+
+    protected PaymentGatewayConfigurationService configurationService;
+    protected PaymentGatewayTransactionService transactionService;
+    protected PaymentGatewayTransactionConfirmationService transactionConfirmationService;
+    protected PaymentGatewayReportingService reportingService;
+    protected PaymentGatewayCreditCardService creditCardService;
+    protected PaymentGatewayCustomerService customerService;
+    protected PaymentGatewaySubscriptionService subscriptionService;
+
+    protected TRCreditCardExtensionHandler creditCardExtensionHandler;
+    protected PaymentGatewayFieldExtensionHandler fieldExtensionHandler;
+
+    public PaymentGatewayConfigurationService getConfigurationService() {
+        return configurationService;
+    }
+
+    public void setConfigurationService(PaymentGatewayConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+
+    public PaymentGatewayTransactionService getTransactionService() {
+        return transactionService;
+    }
+
+    public void setTransactionService(PaymentGatewayTransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    public PaymentGatewayTransactionConfirmationService getTransactionConfirmationService() {
+        return transactionConfirmationService;
+    }
+
+    public void setTransactionConfirmationService(PaymentGatewayTransactionConfirmationService transactionConfirmationService) {
+        this.transactionConfirmationService = transactionConfirmationService;
+    }
+
+    public PaymentGatewayReportingService getReportingService() {
+        return reportingService;
+    }
+
+    public void setReportingService(PaymentGatewayReportingService reportingService) {
+        this.reportingService = reportingService;
+    }
+
+    public PaymentGatewayCreditCardService getCreditCardService() {
+        return creditCardService;
+    }
+
+    public void setCreditCardService(PaymentGatewayCreditCardService creditCardService) {
+        this.creditCardService = creditCardService;
+    }
+
+    public PaymentGatewayCustomerService getCustomerService() {
+        return customerService;
+    }
+
+    public void setCustomerService(PaymentGatewayCustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    public PaymentGatewaySubscriptionService getSubscriptionService() {
+        return subscriptionService;
+    }
+
+    public void setSubscriptionService(PaymentGatewaySubscriptionService subscriptionService) {
+        this.subscriptionService = subscriptionService;
+    }
+
+    public TRCreditCardExtensionHandler getCreditCardExtensionHandler() {
+        return creditCardExtensionHandler;
+    }
+
+    public void setCreditCardExtensionHandler(TRCreditCardExtensionHandler creditCardExtensionHandler) {
+        this.creditCardExtensionHandler = creditCardExtensionHandler;
+    }
+
+    public PaymentGatewayFieldExtensionHandler getFieldExtensionHandler() {
+        return fieldExtensionHandler;
+    }
+
+    public void setFieldExtensionHandler(PaymentGatewayFieldExtensionHandler fieldExtensionHandler) {
+        this.fieldExtensionHandler = fieldExtensionHandler;
+    }
+}
