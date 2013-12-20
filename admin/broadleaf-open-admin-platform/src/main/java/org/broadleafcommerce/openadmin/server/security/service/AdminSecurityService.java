f7135e6ef676ab5135dfbc7573f38ad38f86c298
Andre Azzolini <apazzolini@gmail.com>
2013-12-20 11:54:42 -0600

    Cherry pick change from develop to add default permissions for admin access after successful login.
    Addresses #355 and #614.
    
    Notes:
    - Fixed issue with salt not being applied during add (if the salt was on the id field).
    - Fixed issue with salt being applied twice in some cases.
    - Password is not required but if left blank on add it will generate a random password.   If using a salt, no user will be able to login with this password.
    - If password is left blank on the update admin then the password field will be ignored.

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/security/service/AdminSecurityService.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/security/service/AdminSecurityService.java
@@ -1,19 +1,22 @@
 /*
- * Copyright 2008-2013 the original author or authors.
- *
+ * #%L
+ * BroadleafCommerce Open Admin Platform
+ * %%
+ * Copyright (C) 2009 - 2013 Broadleaf Commerce
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
-
 package org.broadleafcommerce.openadmin.server.security.service;
 
 import org.broadleafcommerce.common.security.util.PasswordChange;
@@ -32,6 +35,8 @@ import java.util.List;
  *
  */
 public interface AdminSecurityService {
+
+    public static final String[] DEFAULT_PERMISSIONS = { "PERMISSION_OTHER_DEFAULT", "PERMISSION_ALL_USER_SANDBOX" };
 
     List<AdminUser> readAllAdminUsers();
     AdminUser readAdminUserById(Long id);
@@ -116,8 +121,9 @@ public interface AdminSecurityService {
      * not a {@link SaltSource} configured ({@link #getSaltSource()} returns null) then this also returns null.
      * 
      * @param user
+     * @param unencodedPassword
      * @return the salt for the current admin user
      */
-    public Object getSalt(AdminUser user);
+    public Object getSalt(AdminUser user, String unencodedPassword);
 
 }
