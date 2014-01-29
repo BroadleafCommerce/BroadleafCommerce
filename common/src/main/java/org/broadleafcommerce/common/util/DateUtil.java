65f26c0d8e28661002adbdca9d5e4ccc7d2491b3
Brian Polster <bpolster@broadleafcommerce.org>
2014-01-29 14:57:35 -0600

    #311 - Added some static variables to DateUtil

--- common/src/main/java/org/broadleafcommerce/common/util/DateUtil.java
+++ common/src/main/java/org/broadleafcommerce/common/util/DateUtil.java
@@ -29,6 +29,11 @@ public class DateUtil {
     public static final long ONE_DAY_MILLIS = ONE_HOUR_MILLIS * 24;
     public static final long ONE_WEEK_MILLIS = ONE_DAY_MILLIS * 7;
 
+    public static final long ONE_HOUR_SECONDS = 60 * 60;
+    public static final long ONE_DAY_SECONDS = ONE_HOUR_SECONDS * 24;
+    public static final long ONE_WEEK_SECONDS = ONE_DAY_SECONDS * 7;
+    public static final long SIX_MONTHS_SECONDS = ONE_DAY_SECONDS * 6 * 30;
+
     public static boolean isActive(Date startDate, Date endDate, boolean includeTime) {
         Long date = SystemTime.asMillis(includeTime);
         return !(startDate == null || startDate.getTime() > date || (endDate != null && endDate.getTime() < date));
