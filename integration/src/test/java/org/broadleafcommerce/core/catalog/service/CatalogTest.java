70adf13348b479aa0cbe2ed56f07e927b1487962
Phillip Verheyden <pverheyden@gmail.com>
2014-01-30 17:56:14 -0600

    #655 - Fix broken test

--- integration/src/test/java/org/broadleafcommerce/core/catalog/service/CatalogTest.java
+++ integration/src/test/java/org/broadleafcommerce/core/catalog/service/CatalogTest.java
@@ -33,11 +33,12 @@ import org.broadleafcommerce.test.BaseTest;
 import org.springframework.transaction.annotation.Transactional;
 import org.testng.annotations.Test;
 
-import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Resource;
 
 @SuppressWarnings("deprecation")
 public class CatalogTest extends BaseTest {
@@ -217,9 +218,9 @@ public class CatalogTest extends BaseTest {
         sku.setDiscountable(false);
         assert sku.isDiscountable() == false;
 
-        assert sku.isAvailable() == null;
+        assert sku.isAvailable() == true;
         sku.setAvailable(null);
-        assert sku.isAvailable() == null;
+        assert sku.isAvailable() == true;
         sku.setAvailable(true);
         assert sku.isAvailable() == true;
         sku.setAvailable(false);
