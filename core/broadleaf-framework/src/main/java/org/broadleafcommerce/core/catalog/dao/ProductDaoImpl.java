ce5a2a2000a894748fc2fe23f211c65f6e2870c7
jefffischer <jfischer@broadleafcommerce.org>
2014-02-07 15:28:01 -0600

    #686 : Fix DISTINCT select problem in Oracle and Sql Server when retrieving products

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/dao/ProductDaoImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/dao/ProductDaoImpl.java
@@ -117,9 +117,9 @@ public class ProductDaoImpl implements ProductDao {
         CriteriaQuery<Product> criteria = builder.createQuery(Product.class);
         Root<ProductImpl> product = criteria.from(ProductImpl.class);
 
-        product.fetch("defaultSku", JoinType.LEFT);
+        FetchParent fetchParent = product.fetch("defaultSku", JoinType.LEFT);
         if (!dialectHelper.isOracle() && !dialectHelper.isSqlServer()) {
-            product.fetch("skuMedia", JoinType.LEFT);
+            fetchParent.fetch("skuMedia", JoinType.LEFT);
         }
         criteria.select(product);
 
