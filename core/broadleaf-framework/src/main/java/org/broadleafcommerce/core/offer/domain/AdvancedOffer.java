5ba08cfed896373a33240affcc579b27822e0e23
Brian Polster <bpolster@broadleafcommerce.org>
2014-02-07 17:11:29 -0600

    Documentation for AdvancedOffer interface #311

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/domain/AdvancedOffer.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/domain/AdvancedOffer.java
@@ -31,16 +31,41 @@ import java.util.List;
  */
 public interface AdvancedOffer {
 
+    /**
+     * List of Tiers an offer supports.   Implemented in external module.
+     * @return
+     */
     List<OfferTier> getOfferTiers();
 
+    /**
+     * Sets the list of Tiers.
+     * @param offerTiers
+     */
     void setOfferTiers(List<OfferTier> offerTiers);
 
+    /**
+     * Returns true if this is a tiered offer meaning that the amount depends on the
+     * quantity being purchased.
+     * @return
+     */
     boolean isTieredOffer();
 
+    /**
+     * Sets whether or not this is a tiered offer.
+     * @param isTieredOffer
+     */
     void setTieredOffer(boolean isTieredOffer);
     
+    /**
+     * Sets the {@link OfferTimeZoneType} 
+     * @return
+     */
     public OfferTimeZoneType getOfferTimeZoneType();
 
+    /**
+     * Returns the {@link OfferTimeZoneType}
+     * @param offerTimeZoneType
+     */
     public void setOfferTimeZoneType(OfferTimeZoneType offerTimeZoneType);
 
 
--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/domain/OfferTier.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/domain/OfferTier.java
@@ -23,23 +23,62 @@ package org.broadleafcommerce.core.offer.domain;
 import java.math.BigDecimal;
 
 
-
+/**
+ * Represents a tier and amount combination for an offer.   For example, an offer might allow a
+ * 10% off if a user purchases 1 -5 but then allow 15% off if they purchase more than 5.    
+ * @author bpolster
+ *
+ */
 public interface OfferTier extends Comparable<OfferTier>{
 
+    /**
+     * Returns the unique id of the offer tier.
+     * @return
+     */
     Long getId();
 
+    /**
+     * Sets the id of the offer tier.
+     * @param id
+     */
     void setId(Long id);
 
+    /**
+     * Returns the amount of the offer.   The amount could be a percentage, fixed price, or amount-off
+     * depending on the parent {@link Offer#getDiscountType()}
+     * @return
+     */
     BigDecimal getAmount();
 
-    Long getMinQuantity();
-
+    /**
+     * Sets the amount of the tier.
+     * @param amount
+     */
     void setAmount(BigDecimal amount);
 
+    /**
+     * The minimum number needed to qualify for this tier.
+     * @return
+     */
+    Long getMinQuantity();
+
+
+    /**
+     * Sets the minimum number need to qualify for this tier.
+     * @param minQuantity
+     */
     void setMinQuantity(Long minQuantity);
 
+    /**
+     * Returns the associated offer.
+     * @return
+     */
     Offer getOffer();
 
+    /**
+     * Sets the associated offer.
+     * @param offer
+     */
     void setOffer(Offer offer);
 
 }
