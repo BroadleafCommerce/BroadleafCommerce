b2e8a50c740de04fa555caef3145d6e5bb2f98ab
Andre Azzolini <apazzolini@gmail.com>
2014-02-10 11:51:03 -0600

    Leverage the static String instead of hardcoding it

--- src/main/java/com/broadleafcommerce/customfield/domain/CustomFieldImpl.java
+++ src/main/java/com/broadleafcommerce/customfield/domain/CustomFieldImpl.java
@@ -22,6 +22,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.broadleafcommerce.common.extensibility.jpa.copy.DirectCopyTransform;
 import org.broadleafcommerce.common.extensibility.jpa.copy.DirectCopyTransformMember;
+import org.broadleafcommerce.common.extensibility.jpa.copy.DirectCopyTransformTypes;
 import org.broadleafcommerce.common.presentation.AdminPresentation;
 import org.broadleafcommerce.common.presentation.AdminPresentationClass;
 import org.broadleafcommerce.common.presentation.RequiredOverride;
@@ -48,7 +49,7 @@ import javax.persistence.TableGenerator;
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region="blStandardElements")
 @AdminPresentationClass(friendlyName = "CustomFieldImpl_baseCustomField")
 @DirectCopyTransform({
-        @DirectCopyTransformMember(templateTokens = {"multiTenantSite"})
+        @DirectCopyTransformMember(templateTokens = DirectCopyTransformTypes.MULTITENANT_SITE)
 })
 public class CustomFieldImpl implements CustomField {
 
