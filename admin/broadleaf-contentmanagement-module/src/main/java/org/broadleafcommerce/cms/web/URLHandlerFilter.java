4bb0c866a57a0d3099774f4c167441161afb3d12
Brian Polster <bpolster@broadleafcommerce.org>
2014-02-10 23:56:18 -0600

    #695 Added property `preserveQueryStringOnRedirect` that defaults to false in the framework for backwards compatibility but should be set to true for most installations.   The demo will be updated to include this property set to true going forward.

--- admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/web/URLHandlerFilter.java
+++ admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/web/URLHandlerFilter.java
@@ -19,19 +19,29 @@
  */
 package org.broadleafcommerce.cms.web;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.broadleafcommerce.cms.url.domain.URLHandler;
 import org.broadleafcommerce.cms.url.service.URLHandlerService;
 import org.broadleafcommerce.cms.url.type.URLRedirectType;
+import org.broadleafcommerce.common.util.BLCSystemProperty;
 import org.broadleafcommerce.common.util.UrlUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Responsible for setting up the site and locale used by Broadleaf Commerce components.
@@ -43,6 +53,8 @@ public class URLHandlerFilter extends OncePerRequestFilter {
     
     @Resource(name = "blURLHandlerService")
     private URLHandlerService urlHandlerService;
+
+    private static final Log LOG = LogFactory.getLog(URLHandlerFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -63,11 +75,13 @@ public class URLHandlerFilter extends OncePerRequestFilter {
                 request.getRequestDispatcher(handler.getNewURL()).forward(request, response);               
             } else if (URLRedirectType.REDIRECT_PERM == handler.getUrlRedirectType()) {
                 String url = UrlUtil.fixRedirectUrl(contextPath, handler.getNewURL());
+                url = fixQueryString(request, url);
                 response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
                 response.setHeader( "Location", url);
                 response.setHeader( "Connection", "close" );
             } else if (URLRedirectType.REDIRECT_TEMP == handler.getUrlRedirectType()) {
                 String url = UrlUtil.fixRedirectUrl(contextPath, handler.getNewURL());
+                url = fixQueryString(request, url);
                 response.sendRedirect(url);             
             }           
         } else {
@@ -81,20 +95,62 @@ public class URLHandlerFilter extends OncePerRequestFilter {
      * 
      * @param url
      * @return
+     * @throws Exception 
      */
-    protected String fixRedirectUrl(String contextPath, String url) {
-        if (url.indexOf("//")  < 0) {
+    protected String fixQueryString(HttpServletRequest request, String url) {
+        if (getPreserveQueryStringOnRedirect()) {
+            try {
+                Map parameterMap = request.getParameterMap();
+                if (parameterMap != null && parameterMap.size() > 0) {
+                    Set<String> queryParams = getExistingQueryParams(url);
 
-            if (contextPath != null && (! "".equals(contextPath))) {
-                if (! url.startsWith("/")) {
-                    url = "/" + url;
+                    String symbol = "?";
+                    for (Object keyVal : parameterMap.keySet()) {
+                        String key = (String) keyVal;
+                        if (!queryParams.contains(key)) {
+                            if (url.contains("?")) {
+                                symbol = "&";
+                            }
+                            String param = URLEncoder.encode(key, "UTF-8");
+                            String value = URLEncoder.encode(request.getParameter(key), "UTF-8");
+                            url = url + symbol + param;
+                            if (!StringUtils.isEmpty(value)) {
+                                url = url + "=" + value;
+                            }
+                        }
+                    }
                 }
-                if (! url.startsWith(contextPath)) {
-                    url = contextPath + url;
-                }
-            }           
+            } catch (Exception e) {
+                LOG.error("Error adjusting query string in URLHandlerFilter", e);
+            }
+            return url;
         }
+
         return url;
-        
+    }
+
+    public static Set<String> getExistingQueryParams(String url) throws UnsupportedEncodingException {
+        Set<String> query_params = new HashSet<String>();
+        int pos = url.indexOf("?");
+        if (pos > 0) {
+            String query = url.substring(pos);
+            String[] pairs = query.split("&");
+            for (String pair : pairs) {
+                int idx = pair.indexOf("=");
+                String param="";
+                String value = null;
+                if (idx > 0) {
+                    param = pair.substring(0, idx);
+                } else {
+                    param=pair;
+                }
+                query_params.add(param);
+            }
+        }
+        return query_params;
+    }
+
+    protected boolean getPreserveQueryStringOnRedirect() {
+        return BLCSystemProperty.resolveBooleanSystemProperty("preserveQueryStringOnRedirect");
     }
 }
