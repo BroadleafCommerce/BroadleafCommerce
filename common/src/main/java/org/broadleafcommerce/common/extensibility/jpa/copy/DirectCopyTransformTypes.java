e31a04699505f6ca8ce73565625726547abe100c
jefffischer <jfischer@broadleafcommerce.org>
2014-02-17 18:23:13 -0600

    #691 - Allow internal entity extensions for Category, Product, ProductOption and Offer

--- common/src/main/java/org/broadleafcommerce/common/extensibility/jpa/copy/DirectCopyTransformTypes.java
+++ common/src/main/java/org/broadleafcommerce/common/extensibility/jpa/copy/DirectCopyTransformTypes.java
@@ -25,10 +25,7 @@ package org.broadleafcommerce.common.extensibility.jpa.copy;
 public class DirectCopyTransformTypes {
 
     public static final String SANDBOX = "sandbox";
-    public static final String SANDBOX_CATEGORY_INVOKE = "sandboxCategoryInvocation";
-    public static final String SANDBOX_PRODUCT_INVOKE = "sandboxProductInvocation";
-    public static final String SANDBOX_PRODUCTOPTION_INVOKE = "sandboxProductOptionInvocation";
-    public static final String SANDBOX_OFFER_INVOKE = "sandboxOfferInvocation";
+    public static final String SANDBOX_PRECLONE_INFORMATION = "sandboxPreCloneInformation";
     public static final String MULTITENANT_SITE = "multiTenantSite";
     public static final String MULTITENANT_CATALOG = "multiTenantCatalog";
     public static final String MULTITENANT_SITEMARKER = "multiTenantSiteMarker";
