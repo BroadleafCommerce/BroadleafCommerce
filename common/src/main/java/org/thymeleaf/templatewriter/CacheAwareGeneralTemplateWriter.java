55312f93b1ead96bb497756a8ffdbd4b369f4fa0
Andre Azzolini <apazzolini@gmail.com>
2014-02-19 23:49:14 -0600

    Cleanup and remove random cache usage

--- common/src/main/java/org/thymeleaf/templatewriter/CacheAwareGeneralTemplateWriter.java
+++ common/src/main/java/org/thymeleaf/templatewriter/CacheAwareGeneralTemplateWriter.java
@@ -17,21 +17,6 @@
  * limitations under the License.
  * #L%
  */
-/*
- * Copyright 2008-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.thymeleaf.templatewriter;
 
@@ -48,7 +33,6 @@ import org.thymeleaf.dom.Node;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.Random;
 
 /**
  * Wrapper for Thymeleaf's {@link AbstractGeneralTemplateWriter} that provides content caching
@@ -67,23 +51,6 @@ public class CacheAwareGeneralTemplateWriter extends AbstractGeneralTemplateWrit
         this.delegateWriter = delegateWriter;
     }
     
-    public void printChildProperties(Node node) {
-        System.out.println("found " + node);
-        if (node.getNodePropertyNames().size() > 0) {
-            System.out.println("found properties on node + " + node.getNodePropertyNames());
-        }
-        
-        if (node instanceof org.thymeleaf.dom.Element) {
-            org.thymeleaf.dom.Element e = (org.thymeleaf.dom.Element) node;
-            
-            if (e.hasChildren()) {
-                for (Node child : e.getChildren()) {
-                    printChildProperties(child);
-                }
-            }
-        }
-    }
-
     @Override
     public void writeNode(final Arguments arguments, final Writer writer, final Node node) 
             throws IOException {
@@ -96,23 +63,15 @@ public class CacheAwareGeneralTemplateWriter extends AbstractGeneralTemplateWrit
         
         String cacheKey = e.getAttributeValueFromNormalizedName("cachekey");
         
-        boolean forceGenerate = new Random().nextBoolean();
-
         if (StringUtils.isNotBlank(cacheKey)) {
             Element element = getCache().get(cacheKey);
             String valueToWrite;
             
-            if (forceGenerate) { 
-                element = null;
-            }
-            
-            long start = System.currentTimeMillis();
             if (element != null && element.getObjectValue() != null) {
                 if (LOG.isTraceEnabled()) {
                     LOG.trace("Read template from cache - " + cacheKey);
                 }
                 valueToWrite = (String) element.getObjectValue();
-                System.out.println("It took " + (System.currentTimeMillis() - start) + "ms to read from cache");
             } else {
                 e.removeAttribute("cachekey");
 
@@ -122,7 +81,6 @@ public class CacheAwareGeneralTemplateWriter extends AbstractGeneralTemplateWrit
 
                 element = new Element(cacheKey, valueToWrite);
                 getCache().put(element);
-                System.out.println("It took " + (System.currentTimeMillis() - start) + "ms to generate");
             }
             
             writer.write(valueToWrite);
