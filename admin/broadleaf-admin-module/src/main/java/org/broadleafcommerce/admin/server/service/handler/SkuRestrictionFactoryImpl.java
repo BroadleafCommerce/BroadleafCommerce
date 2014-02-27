6d0bb62fd7ac6fe81a3398dd8c5584900afda533
Phillip Verheyden <pverheyden@gmail.com>
2014-02-27 11:27:51 -0600

    Fixes #746 - Use subqueries to properly filter out Skus when searching

--- admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/server/service/handler/SkuRestrictionFactoryImpl.java
+++ admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/server/service/handler/SkuRestrictionFactoryImpl.java
@@ -21,6 +21,7 @@ package org.broadleafcommerce.admin.server.service.handler;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.broadleafcommerce.core.catalog.domain.SkuImpl;
 import org.broadleafcommerce.openadmin.server.service.persistence.module.criteria.FieldPath;
 import org.broadleafcommerce.openadmin.server.service.persistence.module.criteria.FieldPathBuilder;
 import org.broadleafcommerce.openadmin.server.service.persistence.module.criteria.Restriction;
@@ -34,13 +35,17 @@ import org.broadleafcommerce.openadmin.server.service.persistence.module.criteri
 import org.broadleafcommerce.openadmin.server.service.persistence.module.criteria.predicate.PredicateProvider;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.From;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
 
 /**
  * This class takes into account that filters should be applied on either the root Sku property itself OR the defaultSku
@@ -56,7 +61,7 @@ public class SkuRestrictionFactoryImpl implements RestrictionFactory {
 
     protected static final String DEFAULT_SKU_PATH_PREFIX = "product.defaultSku.";
 
-    protected String skuPropertyPrefix;
+    protected String skuPropertyPrefix = "";
 
     @Override
     public Restriction getRestriction(final String type, String propertyId) {
@@ -68,7 +73,6 @@ public class SkuRestrictionFactoryImpl implements RestrictionFactory {
                 public Predicate buildPredicate(CriteriaBuilder builder, FieldPathBuilder fieldPathBuilder,
                                                 From root, String ceilingEntity, String fullPropertyName,
                                                 Path explicitPath, List directValues) {
-                    
                     FieldPath fieldPath = fieldPathBuilder.getFieldPath(root, fullPropertyName);
                     if ((StringUtils.isNotEmpty(skuPropertyPrefix) && fullPropertyName.startsWith(skuPropertyPrefix))
                                             || (StringUtils.isEmpty(skuPropertyPrefix) && CollectionUtils.isEmpty(fieldPath.getAssociationPath()))) {
@@ -76,6 +80,7 @@ public class SkuRestrictionFactoryImpl implements RestrictionFactory {
                         Path defaultSkuPropertyPath = fieldPathBuilder.getPath(root,
                                 getSkuPropertyPrefix() + DEFAULT_SKU_PATH_PREFIX + fullPropertyName.replace(getSkuPropertyPrefix(),  ""), builder);
                         Path productPath = fieldPathBuilder.getPath(root, getSkuPropertyPrefix() + "product", builder);
+                        Path skuIdPath = fieldPathBuilder.getPath(root, getSkuPropertyPrefix() + "id", builder);
                         Predicate propertyExpression;
                         Predicate defaultSkuExpression;
                         if (delegateRestriction.getPredicateProvider() instanceof LikePredicateProvider) {
@@ -128,26 +133,64 @@ public class SkuRestrictionFactoryImpl implements RestrictionFactory {
                         }
 
                         return buildCompositePredicate(builder, targetPropertyPath, productPath, propertyExpression,
-                                defaultSkuExpression);
+                                defaultSkuExpression, skuIdPath, fieldPathBuilder.getCriteria());
                     }
                     return delegateRestriction.getPredicateProvider().buildPredicate(builder, fieldPathBuilder, root,
                             ceilingEntity, fullPropertyName, explicitPath, directValues);
                 }
         });
     }
-
+    
+    /**
+     * Builds a predicate designed to work for with Skus that have their value explicitly set on the filtering property as
+     * well as optionally looking at the defaultSku related to this Sku
+     * 
+     * @param builder responsible for giving restrictions
+     * @param targetPropertyPath the actual property path of the property being filtered on for the Sku
+     * @param productPath the path to the product relationship (should just be "product")
+     * @param propertyExpression the expression to filter on the actual property of the Sku (e.g. name LIKE %<value>%)
+     * @param defaultSkuExpression the expression to filter on the related defaultSku of this Sku (e.g. product.defaultSku.name LIKE %<value>%)
+     * @param root the original Root of the query, used to build further subqueries
+     * @param criteria the original CriteriaQuery to attach subqueries to
+     * @return
+     */
     protected Predicate buildCompositePredicate(CriteriaBuilder builder, Path targetPropertyPath, Path productPath,
-                                                Predicate propertyExpression, Predicate defaultSkuExpression) {
-        // The first part of this expression successfully grabs default Skus, the second part successfully grabs
-        // the additional Skus; need a way to get them TOGETHER
+                                                Predicate propertyExpression, Predicate defaultSkuExpression, Path skuIdPath, CriteriaQuery criteria) {
+        // First do a subquery to find all of the additional Skus that match the given expression
+        // This subquery will return all of the sku IDs that match the additional Sku expression. The WHERE clause of
+        // this is basically something like:
+        //  WHERE sku.name == null AND sku.product != null AND sku.product.defaultSku.name LIKE %<val>%
+        Subquery<Long> additionalSkusSubQuery = criteria.subquery(Long.class);
+        Root<SkuImpl> subRoot = additionalSkusSubQuery.from(SkuImpl.class);
+        additionalSkusSubQuery.select(subRoot.get("id").as(Long.class));
+        List<Predicate> subRestrictions = new ArrayList<Predicate>();
+        subRestrictions.add(builder.and(
+                                builder.isNull(targetPropertyPath),
+                                builder.isNotNull(productPath),
+                                defaultSkuExpression
+                            ));
+        additionalSkusSubQuery.where(subRestrictions.toArray(new Predicate[subRestrictions.size()]));
+        
+        // Now do another sub query to get all the Skus that actually have the name explicitly set. This will return
+        // all of the default Skus or additional Skus where the name has been explicitly set
+        // This query is something like:
+        // WHERE sku.name != null AND sku.name LIKE %<val>%
+        Subquery<Long> defaultSkusSubquery = criteria.subquery(Long.class);
+        Root<SkuImpl> defaultSkusRoot = defaultSkusSubquery.from(SkuImpl.class);
+        defaultSkusSubquery.select(defaultSkusRoot.get("id").as(Long.class));
+        List<Predicate> defaultSkusRestrictions = new ArrayList<Predicate>();
+        defaultSkusRestrictions.add(builder.and(
+                                        builder.isNotNull(targetPropertyPath),
+                                        propertyExpression
+                                    ));
+        defaultSkusSubquery.where(defaultSkusRestrictions.toArray(new Predicate[defaultSkusRestrictions.size()]));
+        
+        // Now that we've built the subqueries, do an OR to select all Skus whose IDs are apart of the additional Skus
+        // or defaultSkus subquery
         return builder.or(
-                builder.and(builder.isNotNull(targetPropertyPath), propertyExpression)//,
-//                builder.and(
-//                    builder.isNull(targetPropertyPath),
-//                    builder.isNotNull(productPath),
-//                    defaultSkuExpression
-//                )//,
-        );
+                    skuIdPath.in(additionalSkusSubQuery),
+                    skuIdPath.in(defaultSkusSubquery)
+                );
     }
 
     public RestrictionFactory getDelegate() {
