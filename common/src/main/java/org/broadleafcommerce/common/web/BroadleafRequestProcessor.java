a4378e98c54c4935ef1b49451d69528010b46562
Andre Azzolini <apazzolini@gmail.com>
2014-03-12 16:38:11 -0500

    #691 - Reorder the execution of theme and sandbox

--- common/src/main/java/org/broadleafcommerce/common/web/BroadleafRequestProcessor.java
+++ common/src/main/java/org/broadleafcommerce/common/web/BroadleafRequestProcessor.java
@@ -149,12 +149,14 @@ public class BroadleafRequestProcessor extends AbstractBroadleafWebRequestProces
             previewSandBoxContext.setPreviewMode(true);
             SandBoxContext.setSandBoxContext(previewSandBoxContext);
         }
-        // Note that this must happen after the request context is set up as resolving a theme is dependent on site
-        Theme theme = themeResolver.resolveTheme(request);
         brc.setLocale(locale);
         brc.setBroadleafCurrency(currency);
         brc.setSandBox(currentSandbox);
+
+        // Note that this must happen after the request context is set up as resolving a theme is dependent on site
+        Theme theme = themeResolver.resolveTheme(request);
         brc.setTheme(theme);
+
         brc.setMessageSource(messageSource);
         brc.setTimeZone(timeZone);
         brc.setRequestDTO(requestDTO);
