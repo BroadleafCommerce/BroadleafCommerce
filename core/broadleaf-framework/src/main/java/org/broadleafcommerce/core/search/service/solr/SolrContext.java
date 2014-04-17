e6fe1737bfab543d01402a065f05d5eec921c9cf
Kelly Tisdell <ktisdell@broadleafcommerce.org>
2014-04-17 15:35:50 -0500

    #819. Added concept of admin server for Solr, to support swapping of cores in Solr 4.4 and higher.(cherry picked from commit deccf1354f31f66e4b7e6436281fba5a35e8a179)

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/SolrContext.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/SolrContext.java
@@ -18,8 +18,6 @@ package org.broadleafcommerce.core.search.service.solr;
 
 import org.apache.solr.client.solrj.SolrServer;
 
-
-
 /**
  * Provides a class that will statically hold the Solr server
  * 
@@ -30,6 +28,7 @@ public class SolrContext {
     public static final String PRIMARY = "primary";
     public static final String REINDEX = "reindex";
 
+    protected static SolrServer adminServer = null;
     protected static SolrServer primaryServer = null;
     protected static SolrServer reindexServer = null;
 
@@ -39,6 +38,34 @@ public class SolrContext {
 
     public static void setReindexServer(SolrServer server) {
         reindexServer = server;
+    }
+
+    public static void setAdminServer(SolrServer server) {
+        adminServer = server;
+    }
+
+    /**
+     * The adminServer is just a reference to a SolrServer component for connecting to Solr.  In newer 
+     * versions of Solr, 4.4 and beyond, auto discovery of cores is  
+     * provided.  When using a stand-alone server or server cluster, 
+     * the admin server, for swapping cores, is a different URL. For example, 
+     * one needs to use http://solrserver:8983/solr, which formerly acted as the admin server 
+     * AND as the primary core.  As of Solr 4.4, one needs to specify the cores separately: 
+     * http://solrserver:8983/solr/primary and http://solrserver:8983/solr/reindex, 
+     * and use http://solrserver:8983/solr for swapping cores.
+     * 
+     * By default, this method attempts to return an admin server if configured. Otherwise, 
+     * it returns the primary server if the admin server is null, which is backwards compatible 
+     * with the way that BLC worked prior to this change.
+     * 
+     * @return
+     */
+    public static SolrServer getAdminServer() {
+        if (adminServer != null) {
+            return adminServer;
+        }
+        //If the admin server hasn't been set, return the primary server.
+        return getServer();
     }
 
     public static SolrServer getServer() {
--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/SolrHelperServiceImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/SolrHelperServiceImpl.java
@@ -76,7 +76,7 @@ public class SolrHelperServiceImpl implements SolrHelperService {
             car.setAction(CoreAdminAction.SWAP);
 
             try {
-                SolrContext.getServer().request(car);
+                SolrContext.getAdminServer().request(car);
             } catch (Exception e) {
                 LOG.error(e);
                 throw new ServiceException("Unable to swap cores", e);
@@ -201,6 +201,7 @@ public class SolrHelperServiceImpl implements SolrHelperService {
                 .toString();
     }
 
+    @Override
     public String getLocalePrefix() {
         if (BroadleafRequestContext.getBroadleafRequestContext() != null) {
             Locale locale = BroadleafRequestContext.getBroadleafRequestContext().getLocale();
