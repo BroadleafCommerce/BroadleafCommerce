e628e31fd5d313b741ac52bbdb6770551ad19687
Andre Azzolini <apazzolini@gmail.com>
2014-05-19 16:04:49 -0500

    #921 - Json strings aren't escaped for admin hiddenfields objects

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/form/component/ListGridRecord.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/form/component/ListGridRecord.java
@@ -27,6 +27,7 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.broadleafcommerce.openadmin.web.form.entity.Field;
+import org.codehaus.jettison.json.JSONObject;
 
 public class ListGridRecord {
 
@@ -137,9 +138,9 @@ public class ListGridRecord {
         for (int j=0;j<hiddenFields.size();j++) {
             sb.append("{\"name\":\"");
             sb.append(hiddenFields.get(j).getName());
-            sb.append("\",\"val\":\"");
-            sb.append(hiddenFields.get(j).getValue());
-            sb.append("\"}");
+            sb.append("\",\"val\":");
+            sb.append(JSONObject.quote(hiddenFields.get(j).getValue()));
+            sb.append("}");
             if (j < hiddenFields.size()-1) {
                 sb.append(",");
             }
