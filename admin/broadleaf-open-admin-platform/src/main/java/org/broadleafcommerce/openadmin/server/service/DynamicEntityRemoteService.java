852325cf9e239a7d7bf2da237754241260fc505c
Jerry Ocanas <jerry77oz@gmail.com>
2014-06-15 15:53:14 -0500

    #592, #631 - Renamed antisamy file to avoid thymeleaf error when antisamy kicks in and allow for error msg to bubble up to the admin.

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/DynamicEntityRemoteService.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/DynamicEntityRemoteService.java
@@ -123,14 +123,13 @@ public class DynamicEntityRemoteService implements DynamicEntityService, Dynamic
         } catch (CleanStringException e) {
             StringBuilder sb = new StringBuilder();
             for (int j=0;j<e.getCleanResults().getNumberOfErrors();j++){
+                sb.append("\n");
                 sb.append(j+1);
                 sb.append(") ");
                 sb.append((String) e.getCleanResults().getErrorMessages().get(j));
                 sb.append("\n");
             }
-            sb.append("\nNote - ");
-            sb.append(exploitProtectionService.getAntiSamyPolicyFileLocation());
-            sb.append(" policy in effect. Set a new policy file to modify validation behavior/strictness.");
+            sb.append("\nNote - Antisamy policy in effect. Set a new policy file to modify validation behavior/strictness.");
             entity.addValidationError(currentProperty.getName(), sb.toString());
         }
     }
