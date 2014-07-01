db3ac2a27bafdf079cbc9f7299bc2121b7985e83
Andre Azzolini <apazzolini@gmail.com>
2014-07-01 16:18:34 -0500

    #986 - Add the ability to deny requests instead of queuing for locked operations

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/service/OrderLockManager.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/service/OrderLockManager.java
@@ -50,6 +50,17 @@ public interface OrderLockManager {
      *         the operation that required a lock has completed.
      */
     public Object acquireLock(Order order);
+
+    /**
+     * This method differs from {@link #acquireLock(Order)} in that it will not block if the lock is currently
+     * held by a different caller. In the case that the lock was not able to be immediately acquired, this method
+     * will return null.
+     * 
+     * @see #acquireLock(Order)
+     * @param order
+     * @return the lock if it was immediately acquireable, null otherwise
+     */
+    public Object acquireLockIfAvailable(Order order);
     
     /**
      * Releases the given lockObject and notifies any threads that are waiting on that object that they are able to
