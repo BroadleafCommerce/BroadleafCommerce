c47428fb0783c065f81dbdf641f1155879791b33
Kelly Tisdell <ktisdell@broadleafcommerce.org>
2014-07-30 15:31:08 -0500

    #1026. Ensuring that sessions are not used when we are not supposed to be using them.

--- common/src/main/java/org/broadleafcommerce/common/web/util/RepeatSubmitProtectionFilter.java
+++ common/src/main/java/org/broadleafcommerce/common/web/util/RepeatSubmitProtectionFilter.java
@@ -19,6 +19,16 @@
  */
 package org.broadleafcommerce.common.web.util;
 
+import org.broadleafcommerce.common.util.BLCRequestUtils;
+import org.broadleafcommerce.common.web.BroadleafRequestContext;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -27,51 +37,63 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class RepeatSubmitProtectionFilter implements Filter {
 
-    private Map<String, List<String>> requests = new HashMap<String, List<String>>(100);
+    private final Map<String, List<String>> requests = new HashMap<String, List<String>>(100);
 
+    @Override
     public void destroy() {
         //do nothing
     }
 
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String sessionId;
-        String requestURI;
-        synchronized(requests) {
-            sessionId = ((HttpServletRequest) request).getSession().getId();
-            requestURI = ((HttpServletRequest) request).getRequestURI();
-            if (requests.containsKey(sessionId) && requests.get(sessionId).contains(requestURI)) {
-                //we are currently already processing this request
-                ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_NO_CONTENT);
-                return;
+        boolean useSession = true;
+        if (BroadleafRequestContext.getBroadleafRequestContext() != null
+                && BroadleafRequestContext.getBroadleafRequestContext().getWebRequest() != null) {
+            if (!BLCRequestUtils.isOKtoUseSession(BroadleafRequestContext.getBroadleafRequestContext().getWebRequest())) {
+                useSession = false;
             }
-            List<String> myRequests = requests.get(sessionId);
-            if (myRequests == null) {
-                myRequests = new ArrayList<String>();
-                requests.put(sessionId, myRequests);
-            }
-            myRequests.add(requestURI);
+        } else if (!BLCRequestUtils.isOKtoUseSession(new ServletWebRequest((HttpServletRequest) request))) {
+            useSession = false;
         }
-        try {
-            chain.doFilter(request, response);
-        } finally {
+
+        if (useSession) {
+            String sessionId;
+            String requestURI;
             synchronized(requests) {
+                sessionId = ((HttpServletRequest) request).getSession().getId();
+                requestURI = ((HttpServletRequest) request).getRequestURI();
+                if (requests.containsKey(sessionId) && requests.get(sessionId).contains(requestURI)) {
+                    //we are currently already processing this request
+                    ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_NO_CONTENT);
+                    return;
+                }
                 List<String> myRequests = requests.get(sessionId);
-                myRequests.remove(requestURI);
-                if (myRequests.isEmpty()) {
-                    requests.remove(sessionId);
+                if (myRequests == null) {
+                    myRequests = new ArrayList<String>();
+                    requests.put(sessionId, myRequests);
+                }
+                myRequests.add(requestURI);
+            }
+            try {
+                chain.doFilter(request, response);
+            } finally {
+                synchronized (requests) {
+                    List<String> myRequests = requests.get(sessionId);
+                    myRequests.remove(requestURI);
+                    if (myRequests.isEmpty()) {
+                        requests.remove(sessionId);
+                    }
                 }
             }
+        } else {
+            chain.doFilter(request, response);
         }
     }
 
+    @Override
     public void init(FilterConfig arg0) throws ServletException {
         //do nothing
     }
