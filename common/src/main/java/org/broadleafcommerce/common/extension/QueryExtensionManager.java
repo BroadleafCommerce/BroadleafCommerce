0a2ecee2d9dfa3b6715efdb2dc1f4e9347d6e358
jefffischer <jfischer@broadleafcommerce.org>
2014-08-12 12:40:06 -0500

    #1072 - Add query refinement API in the form of an extension handler (cherry picked from commit 68df3b7)

---
+++ common/src/main/java/org/broadleafcommerce/common/extension/QueryExtensionManager.java
@@ -0,0 +1,37 @@
+/*
+ * #%L
+ * BroadleafCommerce Framework
+ * %%
+ * Copyright (C) 2009 - 2013 Broadleaf Commerce
+ * %%
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Broadleaf Commerce, LLC
+ * The intellectual and technical concepts contained
+ * herein are proprietary to Broadleaf Commerce, LLC
+ * and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Broadleaf Commerce, LLC.
+ * #L%
+ */
+package org.broadleafcommerce.common.extension;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Extension manager for any Service or Dao that wishes to allow contribution of restrictions
+ * to fetch queries. This practice is generally employed by modules that need to embellish fetch
+ * queries from core, or even other modules.
+ *
+ * @see QueryExtensionHandler
+ * @author Jeff Fischer
+ */
+@Service("blManualQueryExtensionManager")
+public class QueryExtensionManager extends ExtensionManager<QueryExtensionHandler> {
+
+    public QueryExtensionManager() {
+        super(QueryExtensionHandler.class);
+    }
+
+}
