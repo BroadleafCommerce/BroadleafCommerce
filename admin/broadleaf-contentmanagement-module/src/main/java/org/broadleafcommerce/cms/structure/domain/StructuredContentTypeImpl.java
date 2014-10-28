03dd39834d4641c4f0f9cdb9c6cfb39931305947
Reginald C Cole <rcole@broadleafcommerce.com>
2014-10-28 17:31:15 -0500

    multi tenant copier-refactor

--- admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/structure/domain/StructuredContentTypeImpl.java
+++ admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/structure/domain/StructuredContentTypeImpl.java
@@ -19,16 +19,6 @@
  */
 package org.broadleafcommerce.cms.structure.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
 import org.broadleafcommerce.common.admin.domain.AdminMainEntity;
 import org.broadleafcommerce.common.copy.CreateResponse;
 import org.broadleafcommerce.common.copy.MultiTenantCopyContext;
@@ -40,11 +30,12 @@ import org.broadleafcommerce.common.presentation.AdminPresentation;
 import org.broadleafcommerce.common.presentation.AdminPresentationClass;
 import org.broadleafcommerce.common.presentation.PopulateToOneFieldsEnum;
 import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Index;
+import org.hibernate.annotations.*;
 import org.hibernate.annotations.Parameter;
-import org.springframework.context.annotation.Profile;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * Created by bpolster.
@@ -143,6 +134,7 @@ public class StructuredContentTypeImpl implements StructuredContentType, AdminMa
         cloned.setDescription(description);
         cloned.setName(name);
         CreateResponse<StructuredContentFieldTemplate> clonedTemplate = structuredContentFieldTemplate.createOrRetrieveCopyInstance(context);
+        cloned.setStructuredContentFieldTemplate(clonedTemplate.getClone());
         return createResponse;
     }
 }
