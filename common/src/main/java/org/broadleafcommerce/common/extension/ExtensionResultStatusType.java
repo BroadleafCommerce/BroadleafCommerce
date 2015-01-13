b3abed55c58ddc5cdf6f15eaff7b4b6cf0cca8c8
Phillip Verheyden <pverheyden@gmail.com>
2015-01-13 15:34:01 -0600

    #1089 - Un-deprecate ExtensionResultStatusType.HANDLED and enhance the Javadocs surrounding the various values

--- common/src/main/java/org/broadleafcommerce/common/extension/ExtensionResultStatusType.java
+++ common/src/main/java/org/broadleafcommerce/common/extension/ExtensionResultStatusType.java
@@ -22,24 +22,34 @@ package org.broadleafcommerce.common.extension;
 
 public enum ExtensionResultStatusType {
     /**
-     * @deprecated this is ambiguous; recommend explicitly using {@link ExtensionResultStatusType#HANDLED_CONTINUE} or
-     * {@link ExtensionResultStatusType#HANDLED_CONTINUE} instead
+     * <p>
+     * In short, this value should only be returned from {@link ExtensionManager}s and not from {@link ExtensionHandler}s.
+     * 
+     * <p>
+     * When you are returning results from an {@link ExtensionHandler} then it is less ambiguous to {@link ExtensionManager}s
+     * to instead return {@link ExtensionResultStatusType#HANDLED_CONTINUE} or
+     * {@link ExtensionResultStatusType#HANDLED_CONTINUE} instead. However, when checking the result of invoking an
+     * {@link ExtensionManager} to see if it was handled by <b>ANY</b> {@link ExtensionHandler} then it makes sense
+     * to return this enum instead
      */
-    @Deprecated
     HANDLED,
     
     /**
-     * Extension handled and recommends that the manger continue
+     * The extension was handled by the {@link ExtensionHandler} and it recommends continuing with additional
+     * {@link ExtensionHandler}s (if more are available).
      */
     HANDLED_CONTINUE,
     
     /**
-     * Extension handled and recommends that the manger stop
+     * An {@link ExtensionHandler} has handled this and it recommends that the {@link ExtensionManager} should not invoke
+     * any more {@link ExtensionHandler}s that it may have to execute
      */
     HANDLED_STOP,
     
     /**
-     * Extension did not handle the request
+     * This was not handled by the {@link ExtensionHandler}. In the context of the result of an {@link ExtensionManager},
+     * this indicates that it was not executed by any of the registered {@link ExtensionHandler}s, or that none were
+     * registered in the first place
      */
     NOT_HANDLED
 }
