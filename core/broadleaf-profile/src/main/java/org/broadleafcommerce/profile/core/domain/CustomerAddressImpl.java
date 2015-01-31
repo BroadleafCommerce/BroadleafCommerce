89cf56ec17a284a4236a8ca969bca8acf93eb455
jefffischer <jfischer@broadleafcommerce.org>
2015-01-31 10:28:26 -0600

    cleanup imports

--- core/broadleaf-profile/src/main/java/org/broadleafcommerce/profile/core/domain/CustomerAddressImpl.java
+++ core/broadleaf-profile/src/main/java/org/broadleafcommerce/profile/core/domain/CustomerAddressImpl.java
@@ -19,20 +19,8 @@
  */
 package org.broadleafcommerce.profile.core.domain;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
+import org.broadleafcommerce.common.copy.CreateResponse;
+import org.broadleafcommerce.common.copy.MultiTenantCopyContext;
 import org.broadleafcommerce.common.persistence.ArchiveStatus;
 import org.broadleafcommerce.common.presentation.AdminPresentation;
 import org.broadleafcommerce.common.presentation.AdminPresentationClass;
