bfa7f6bcee49f7165f94500b43107bc4289ce3c5
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2015-02-10 16:48:36 -0600

    A group of Offer exceptions were added so to support showing the user that adding a promo code multiple times or adding an expired promo code did not complete. Refactored the OfferMaxUseExceededException to extend the new OrderException class along with two other exceptions.

---
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/service/exception/OfferAlreadyAddedException.java
@@ -0,0 +1,12 @@
+package org.broadleafcommerce.core.offer.service.exception;
+
+
+public class OfferAlreadyAddedException extends OfferException {
+
+    private static final long serialVersionUID = 1L;
+
+    public OfferAlreadyAddedException(String string) {
+        super(string);
+    }
+
+}
---
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/service/exception/OfferExpiredException.java
@@ -0,0 +1,11 @@
+package org.broadleafcommerce.core.offer.service.exception;
+
+public class OfferExpiredException extends OfferException {
+    
+    private static final long serialVersionUID = 1L;
+
+    public OfferExpiredException(String string) {
+        super(string);
+    }
+    
+}
