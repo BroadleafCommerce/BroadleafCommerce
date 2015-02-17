3136cda91a76916735054ffc4a7f010a0d74a774
Alan Christensen <alanchr@gmail.com>
2015-02-17 11:37:31 -0600

    Removed CategoryXref.CategoryXrefPK
    Moved category/subCategory into CategoryXref. Modeled after current BLC master.
    When 1 of these was serialized and put into an ehcache it was taking up a huge amount
    of space. Removing the nested/embedded PK fixed the issue.

--- src-framework/org/broadleafcommerce/catalog/domain/CategoryXref.java
+++ src-framework/org/broadleafcommerce/catalog/domain/CategoryXref.java
@@ -20,6 +20,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -44,19 +45,36 @@ public class CategoryXref implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /** The category id. */
-    @EmbeddedId
-    CategoryXrefPK categoryXrefPK = new CategoryXrefPK();
-
-    public CategoryXrefPK getCategoryXrefPK() {
-        return categoryXrefPK;
-    }
-
-    public void setCategoryXrefPK(final CategoryXrefPK categoryXrefPK) {
-        this.categoryXrefPK = categoryXrefPK;
-    }
+    @Id
+    @Column(name = "ID")
+    protected String id;
 
     @Column(name = "DISPLAY_ORDER")
     private Long displayOrder;
+
+    @ManyToOne(targetEntity = CategoryImpl.class, optional=false)
+    @JoinColumn(name = "CATEGORY_ID")
+    protected Category category = new CategoryImpl();
+
+    @ManyToOne(targetEntity = CategoryImpl.class, optional=false)
+    @JoinColumn(name = "SUB_CATEGORY_ID")
+    protected Category subCategory = new CategoryImpl();
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(final Category category) {
+        this.category = category;
+    }
+
+    public Category getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(final Category subCategory) {
+        this.subCategory = subCategory;
+    }
 
     public Long getDisplayOrder() {
         return displayOrder;
@@ -66,67 +84,24 @@ public class CategoryXref implements Serializable {
         this.displayOrder = displayOrder;
     }
 
-    public Category getCategory() {
-        return categoryXrefPK.getCategory();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!getClass().isAssignableFrom(o.getClass())) return false;
+
+        CategoryXref that = (CategoryXref) o;
+
+        if (category != null ? !category.equals(that.category) : that.category != null) return false;
+        if (subCategory != null ? !subCategory.equals(that.subCategory) : that.subCategory != null) return false;
+
+        return true;
     }
 
-    public void setCategory(Category category) {
-        categoryXrefPK.setCategory(category);
+    @Override
+    public int hashCode() {
+        int result = category != null ? category.hashCode() : 0;
+        result = 31 * result + (subCategory != null ? subCategory.hashCode() : 0);
+        return result;
     }
-
-    public Category getSubCategory() {
-        return categoryXrefPK.getSubCategory();
-    }
-
-    public void setSubCategory(Category subCategory) {
-        categoryXrefPK.setSubCategory(subCategory);
-    }
-
-    public static class CategoryXrefPK implements Serializable {
-
-        /** The Constant serialVersionUID. */
-        private static final long serialVersionUID = 1L;
-
-        @ManyToOne(targetEntity = CategoryImpl.class, optional=false)
-        @JoinColumn(name = "CATEGORY_ID")
-        protected Category category = new CategoryImpl();
-
-        @ManyToOne(targetEntity = CategoryImpl.class, optional=false)
-        @JoinColumn(name = "SUB_CATEGORY_ID")
-        protected Category subCategory = new CategoryImpl();
-
-        public Category getCategory() {
-            return category;
-        }
-
-        public void setCategory(final Category category) {
-            this.category = category;
-        }
-
-        public Category getSubCategory() {
-            return subCategory;
-        }
-
-        public void setSubCategory(final Category subCategory) {
-            this.subCategory = subCategory;
-        }
-
-        @Override
-        public boolean equals(final Object obj) {
-            if (obj == null) return false;
-            else if (!(obj instanceof CategoryXrefPK)) return false;
-
-            return category.getId().equals(((CategoryXrefPK) obj).getCategory().getId())
-            && subCategory.getId().equals(((CategoryXrefPK) obj).getSubCategory().getId());
-        }
-
-
-        @Override
-        public int hashCode() {
-            return category.hashCode() + subCategory.hashCode();
-        }
-
-
-    }
-
 }
