9a86610e66e8445f24e69a3c4b1cc8386173538b
Nick Crum <ncrum@broadleafcommerce.com>
2015-03-27 15:02:17 -0500

    closes BroadleafCommerce/QA#403 by adding a Spock Spec to test buildIncrementalSkuIndex in SolrIndexServiceImpl

---
+++ core/broadleaf-framework/src/test/groovy/org/broadleafcommerce/core/spec/search/service/solr/SolrIndexServiceSpec.groovy
@@ -0,0 +1,112 @@
+package org.broadleafcommerce.core.spec.search.service.solr
+
+import org.broadleafcommerce.common.locale.service.LocaleService
+import org.broadleafcommerce.common.sandbox.SandBoxHelper
+import org.broadleafcommerce.core.catalog.dao.ProductDao
+import org.broadleafcommerce.core.catalog.dao.SkuDao
+import org.broadleafcommerce.core.catalog.domain.ProductImpl
+import org.broadleafcommerce.core.catalog.domain.Sku
+import org.broadleafcommerce.core.catalog.domain.SkuImpl
+import org.broadleafcommerce.core.search.dao.FieldDao
+import org.broadleafcommerce.core.search.dao.SolrIndexDao
+import org.broadleafcommerce.core.search.domain.Field
+import org.broadleafcommerce.core.search.service.solr.SolrHelperService
+import org.broadleafcommerce.core.search.service.solr.SolrIndexServiceImpl
+import org.broadleafcommerce.core.search.service.solr.SolrSearchServiceExtensionHandler
+import org.broadleafcommerce.core.search.service.solr.SolrSearchServiceExtensionManager
+import org.springframework.transaction.PlatformTransactionManager
+
+import spock.lang.Specification
+
+
+class SolrIndexServiceSpec extends Specification {
+    
+    SolrIndexServiceImpl service;
+    SolrIndexDao mockSolrIndexDao = Mock()
+    FieldDao mockFieldDao = Mock()
+    PlatformTransactionManager mockTransactionManager = Mock()
+    ProductDao mockProductDao = Mock()
+    SkuDao mockSkuDao = Mock()
+    LocaleService mockLocaleService = Mock()
+    SolrHelperService mockShs = Mock()
+    SolrSearchServiceExtensionManager mockExtensionManager = Mock()
+    SandBoxHelper mockSandBoxHelper = Mock()
+    
+    
+    def setup() {
+        service = Spy(SolrIndexServiceImpl)
+        service.solrIndexDao = mockSolrIndexDao
+        service.fieldDao = mockFieldDao
+        service.transactionManager = mockTransactionManager
+        service.productDao = mockProductDao
+        service.skuDao = mockSkuDao
+        service.localeService = mockLocaleService
+        service.shs = mockShs
+        service.extensionManager = mockExtensionManager
+        service.sandBoxHelper = mockSandBoxHelper
+    }
+    
+    def "Test that Categories are being properly associated to skus when creating the solr index"(){
+        setup:
+        mockExtensionManager.getProxy() >> Mock(SolrSearchServiceExtensionHandler)
+        mockFieldDao.readAllSkuFields() >> new ArrayList<Field>()
+        
+        service.buildDocument(*_) >> null
+        
+        SkuImpl testSku1 = Mock(SkuImpl)
+        SkuImpl testSku2 = Mock(SkuImpl)
+        ProductImpl testProduct1 = Mock(ProductImpl)
+        ProductImpl testProduct2 = Mock(ProductImpl)
+        
+        testProduct1.getId() >> 1
+        testProduct2.getId() >> 2
+        
+        testSku1.getProduct() >> testProduct1
+        testSku2.getProduct() >> testProduct2
+        
+        List<Sku> skus = [testSku1, testSku2]
+        
+        List<Long> productIds = [1, 2]
+     
+        when:
+        service.buildIncrementalSkuIndex(skus,false)
+        
+        then:
+        1 * mockSolrIndexDao.populateProductCatalogStructure(productIds, _)
+        
+    }
+    
+    def "Test that Categories are being properly associated to skus when creating the solr index for out of order skus"(){
+        setup:
+        mockExtensionManager.getProxy() >> Mock(SolrSearchServiceExtensionHandler)
+        mockFieldDao.readAllSkuFields() >> new ArrayList<Field>()
+        
+        service.buildDocument(*_) >> null
+        
+        SkuImpl testSku1 = Mock(SkuImpl)
+        SkuImpl testSku2 = Mock(SkuImpl)
+        SkuImpl testSku3 = Mock(SkuImpl)
+        ProductImpl testProduct1 = Mock(ProductImpl)
+        ProductImpl testProduct2 = Mock(ProductImpl)
+        ProductImpl testProduct3 = Mock(ProductImpl)
+        
+        testProduct1.getId() >> 1
+        testProduct2.getId() >> 2
+        testProduct3.getId() >> 3
+        
+        testSku1.getProduct() >> testProduct1
+        testSku2.getProduct() >> testProduct2
+        testSku3.getProduct() >> testProduct3
+        
+        List<Sku> skus = [testSku3, testSku1, testSku2]
+        
+        List<Long> productIds = [3, 1, 2]
+     
+        when:
+        service.buildIncrementalSkuIndex(skus,false)
+        
+        then:
+        1 * mockSolrIndexDao.populateProductCatalogStructure(productIds, _)
+        
+    }
+}
