d140f582103eb857e94d261b9b894f4c91ff1926
Chris Kittrell <ckittrell@broadleafcommerce.com>
2015-03-31 10:40:33 -0500

    Add general CSS & JS files from AdminUX project

---
+++ admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/new-admin.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+  equalheight = function(container){
+
+  var currentTallest = 0,
+       currentRowStart = 0,
+       rowDivs = new Array(),
+       $el,
+       topPosition = 0;
+   $(container).each(function() {
+
+     $el = $(this);
+     $($el).height('auto')
+     topPostion = $el.position().top;
+
+     if (currentRowStart != topPostion) {
+       for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+         rowDivs[currentDiv].height(currentTallest);
+       }
+       rowDivs.length = 0; // empty the array
+       currentRowStart = topPostion;
+       currentTallest = $el.height();
+       rowDivs.push($el);
+     } else {
+       rowDivs.push($el);
+       currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+    }
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+   });
+  }
+
+  // $(window).load(function() {
+  //   equalheight('.class');
+  // });
+
+  $('#datetimepicker').datetimepicker({
+    format:'l, F d, Y \@ g:ia',
+    step: 10,
+  });
+
+  $('.help-tip').tipr({
+    'speed': 300,
+    'mode': 'top'
+  });
+
+});
