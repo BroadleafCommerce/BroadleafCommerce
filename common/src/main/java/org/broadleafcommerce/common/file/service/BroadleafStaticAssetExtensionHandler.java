a3eae2967c9cf9e75695e5bda55a88327a4c36d5
Chris Kittrell <ckittrell@broadleafcommerce.com>
2015-04-01 15:04:21 -0500

    BroadleafCommerce/QA#331 - Update Static Asset admin entity-form

---
+++ common/src/main/java/org/broadleafcommerce/common/file/service/BroadleafStaticAssetExtensionHandler.java
@@ -0,0 +1,34 @@
+/*
+ * #%L
+ * BroadleafCommerce CMS Module
+ * %%
+ * Copyright (C) 2009 - 2014 Broadleaf Commerce
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.broadleafcommerce.common.file.service;
+
+import org.broadleafcommerce.common.extension.ExtensionHandler;
+import org.broadleafcommerce.common.extension.ExtensionResultStatusType;
+import org.broadleafcommerce.common.site.domain.Site;
+import org.springframework.ui.Model;
+
+/**
+ * @author Chris Kittrell (ckittrell)
+ */
+public interface BroadleafStaticAssetExtensionHandler extends ExtensionHandler {
+
+    public ExtensionResultStatusType removeShareOptionsForMTStandardSite(Model model, Site currentSite);
+
+}
