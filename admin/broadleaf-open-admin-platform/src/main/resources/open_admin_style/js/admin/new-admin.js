cbdb674bc6675df0440987b438c8eaaa867d8db0
Jonathan Fleschler <jon.fleschler@gmail.com>
2015-04-09 13:01:04 -0500

    Updated CSS and JS.  Removed old JS from the footer.

--- admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/new-admin.js
+++ admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/new-admin.js
@@ -1,67 +1,68 @@
-/*
- * #%L
- * BroadleafCommerce Open Admin Platform
- * %%
- * Copyright (C) 2009 - 2015 Broadleaf Commerce
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 $(document).ready(function(){
 
-  equalheight = function(container){
+    equalheight = function(container){
 
-  var currentTallest = 0,
-       currentRowStart = 0,
-       rowDivs = new Array(),
-       $el,
-       topPosition = 0;
-   $(container).each(function() {
+        var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0;
+        $(container).each(function() {
 
-     $el = $(this);
-     $($el).height('auto')
-     topPostion = $el.position().top;
+            $el = $(this);
+            $($el).height('auto')
+            topPostion = $el.position().top;
 
-     if (currentRowStart != topPostion) {
-       for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-         rowDivs[currentDiv].height(currentTallest);
-       }
-       rowDivs.length = 0; // empty the array
-       currentRowStart = topPostion;
-       currentTallest = $el.height();
-       rowDivs.push($el);
-     } else {
-       rowDivs.push($el);
-       currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+            if (currentRowStart != topPostion) {
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
+                rowDivs.length = 0; // empty the array
+                currentRowStart = topPostion;
+                currentTallest = $el.height();
+                rowDivs.push($el);
+            } else {
+                rowDivs.push($el);
+                currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+            }
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+        });
     }
-     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-       rowDivs[currentDiv].height(currentTallest);
-     }
-   });
-  }
 
-  // $(window).load(function() {
-  //   equalheight('.class');
-  // });
+    $(window).load(function() {
+        equalheight('.equals .equal');
+    });
 
-  $('#datetimepicker').datetimepicker({
-    format:'l, F d, Y \@ g:ia',
-    step: 10,
-  });
 
-  $('.help-tip').tipr({
-    'speed': 300,
-    'mode': 'top'
-  });
+    $(window).resize(function(){
+        equalheight('.equals .equal');
+    });
+
+    $('#datetimepicker').datetimepicker({
+        format:'l, F d, Y \@ g:ia',
+        step: 10,
+    });
+
+    $('.help-tip').tipr({
+        'speed': 300,
+        'mode': 'top'
+    });
+
+    $('.datepicker').pickadate({
+        format: 'yyyy-mm-dd'
+    });
+
+    $('.timepicker').pickatime({
+        format: 'h:i A',
+        formatSubmit: 'H:i:s',
+        interval: 15
+    });
+
+    $('#data-table').DataTable({
+        "order": [[ 0, "asc" ]],
+        paging: true
+    });
 
 });
