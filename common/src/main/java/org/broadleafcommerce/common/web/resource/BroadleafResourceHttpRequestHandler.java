223db250b45e80e941b2033b8f084f326e1b725c
Brian Polster <bpolster@broadleafcommerce.org>
2015-05-06 01:33:56 -0500

    Resource configuration fixes

---
+++ common/src/main/java/org/broadleafcommerce/common/web/resource/BroadleafResourceHttpRequestHandler.java
@@ -0,0 +1,80 @@
+package org.broadleafcommerce.common.web.resource;
+
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+import org.springframework.web.servlet.resource.ResourceResolver;
+import org.springframework.web.servlet.resource.ResourceTransformer;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+/**
+ * Provides a PostConstruct method that sorts the {@link ResourceResolver}, {@link ResourceTransformer}, 
+ * or location ({@link Resource}) collections based on the {@link Ordered} interface.
+ * 
+ *  
+ * @author bpolster
+ *
+ */
+public class BroadleafResourceHttpRequestHandler extends ResourceHttpRequestHandler {
+
+    @PostConstruct
+    protected void sortCollections() {
+        OrderedComparator oc = new OrderedComparator();
+
+        if (getLocations() != null) {
+            Collections.sort(getLocations(), oc);
+        }
+
+        if (getResourceResolvers() != null) {
+            Collections.sort(getResourceResolvers(), oc);
+        }
+
+        if (getResourceTransformers() != null) {
+            Collections.sort(getResourceTransformers(), oc);
+        }
+    }
+
+    /**
+     * Items that implement Ordered will sort by the value of {@link Ordered#getOrder()}.
+     * 
+     * <p>
+     * Nulls are considered greater except that a getOrder with a value of Integer.MAX_VALUE 
+     * will always sort at the end (even after nulls). 
+     *      
+     */
+    protected class OrderedComparator implements Comparator<Object> {
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            if (o1 instanceof Ordered && o2 instanceof Ordered) {
+                return ((Ordered) o1).getOrder() - ((Ordered) o2).getOrder();
+            }
+
+            if (o1 instanceof Ordered) {
+                if (((Ordered) o1).getOrder() == Integer.MAX_VALUE) {
+                    // Put MAX_VALUE items at the end of the list (even behind nulls)
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+
+            if (o2 instanceof Ordered) {
+                if (((Ordered) o2).getOrder() == Integer.MAX_VALUE) {
+                    // Put MAX_VALUE items at the end of the list (even behind nulls)
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+
+            // Neither is ordered (respect the natural order)
+            return 0;
+        }
+
+    }
+}
