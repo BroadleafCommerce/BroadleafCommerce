46e65e52573ac37f6701322c0e5bbfae8c8f4423
Philip Baggett <pbaggett@broadleafcommerce.com>
2015-05-08 10:46:17 -0500

    #1324 - Fixed exception thrown when cleaning up failed persistence attempt on subclassed entity.

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/persistence/module/BasicPersistenceModule.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/persistence/module/BasicPersistenceModule.java
@@ -24,6 +24,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -1361,12 +1362,10 @@ public class BasicPersistenceModule implements PersistenceModule, RecordHelper, 
             getPersistenceManager().getDynamicEntityDao().getStandardEntityManager().detach(instance);
         }
         //Remove the id field value, if it's set
-        Field idField;
-        try {
-            idField = instance.getClass().getDeclaredField((String) getPersistenceManager().
-                    getDynamicEntityDao().getIdMetadata(instance.getClass()).get("name"));
-        } catch (NoSuchFieldException e1) {
-            throw ExceptionHelper.refineException(e1);
+        String idFieldName = (String) getPersistenceManager().getDynamicEntityDao().getIdMetadata(instance.getClass()).get("name");
+        Field idField = FieldUtils.getField(instance.getClass(), idFieldName, true);
+        if (idField == null) {
+            throw ExceptionHelper.refineException(new NoSuchFieldException("Entity " + instance.getClass().getName() + " does not contain id field " + idFieldName));
         }
         idField.setAccessible(true);
         if (idField.get(instance) != null) {
