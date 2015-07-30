f2ef9eb57df364824370fc5e4b084d06b22aea84
Gonzalo Diaz <gdiaz@broadleafcommerce.com>
2015-07-30 15:11:07 -0500

    BroadleafCommerce/QA#732 - add paging parameters to getRecordforAllSubcollections() - parse request on controller method, in order to detect such parameters

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/AdminEntityService.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/AdminEntityService.java
@@ -253,6 +253,20 @@ public interface AdminEntityService {
             throws ServiceException;
 
     /**
+     * overloading containing paging parameters
+     * @param ppr
+     * @param containingEntity
+     * @param startIndex
+     * @param maxIndex
+     * @param sectionCrumb
+     * @return
+     * @throws ServiceException
+     */
+    public Map<String, DynamicResultSet> getRecordsForAllSubCollections(PersistencePackageRequest ppr,
+            Entity containingEntity, Integer startIndex, Integer maxIndex, List<SectionCrumb> sectionCrumb)
+            throws ServiceException;
+
+    /**
      * Adds an item into the specified collection
      * 
      * @param entityForm
