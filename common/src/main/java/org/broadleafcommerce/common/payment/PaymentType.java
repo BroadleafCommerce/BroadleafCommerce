855e76c8eeb0a8019d99df1f52de69156e328259
Phillip Verheyden <pverheyden@gmail.com>
2015-10-22 17:03:50 -0500

    BroadleafCommerce/QA#977 - Customer Payment payment types should be considered a final payment

--- common/src/main/java/org/broadleafcommerce/common/payment/PaymentType.java
+++ common/src/main/java/org/broadleafcommerce/common/payment/PaymentType.java
@@ -47,7 +47,7 @@ public class PaymentType implements Serializable, BroadleafEnumerationType {
     public static final PaymentType MONEY_ORDER = new PaymentType("MONEY_ORDER", "Money Order", false);
     public static final PaymentType CUSTOMER_CREDIT = new PaymentType("CUSTOMER_CREDIT", "Customer Credit", false);
     public static final PaymentType COD = new PaymentType("COD", "Collect On Delivery", false);
-    public static final PaymentType CUSTOMER_PAYMENT = new PaymentType("CUSTOMER_PAYMENT", "Customer Payment", false);
+    public static final PaymentType CUSTOMER_PAYMENT = new PaymentType("CUSTOMER_PAYMENT", "Customer Payment", true);
     /**
      * Intended for modules like PayPal Express Checkout
      *
