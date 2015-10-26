b1e70d75faf7e6976777725e740266d20138f52b
jefffischer <jfischer@broadleafcommerce.org>
2015-10-26 16:42:26 -0500

    BroadleafCommerce/QA#607 - Fix API calls after merging in solr refactor

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/SolrHelperServiceImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/SolrHelperServiceImpl.java
@@ -257,7 +257,7 @@ public class SolrHelperServiceImpl implements SolrHelperService {
     public Long getCategoryId(Long category) {
         Long[] returnId = new Long[1];
         //TODO (qa 607) Need to review the performance of retrieval of the Category instance here every time (using the id version breaks for indexing derived catalogs)
-        ExtensionResultStatusType result = extensionManager.getProxy().getCategoryId(genericEntityDao.readGenericEntity(CategoryImpl.class, category), returnId);
+        ExtensionResultStatusType result = searchExtensionManager.getProxy().getCategoryId(genericEntityDao.readGenericEntity(CategoryImpl.class, category), returnId);
         if (result.equals(ExtensionResultStatusType.HANDLED)) {
             return returnId[0];
         }
