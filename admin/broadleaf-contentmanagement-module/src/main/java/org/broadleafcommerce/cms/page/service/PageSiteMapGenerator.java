0c19f14ac326e92f3d7dc8f72afe584d8fb9bc88
Chris Kittrell <ckittrell@broadleafcommerce.com>
2015-12-01 15:12:56 -0600

    BroadleafCommerce/QA#1162 - Update AdminAuditable type checks & type casts to use AdminAudit (interface)

--- admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/page/service/PageSiteMapGenerator.java
+++ admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/page/service/PageSiteMapGenerator.java
@@ -27,6 +27,7 @@ import org.broadleafcommerce.common.sitemap.service.SiteMapBuilder;
 import org.broadleafcommerce.common.sitemap.service.SiteMapGenerator;
 import org.broadleafcommerce.common.sitemap.service.type.SiteMapGeneratorType;
 import org.broadleafcommerce.common.sitemap.wrapper.SiteMapURLWrapper;
+import org.broadleafcommerce.openadmin.audit.AdminAudit;
 import org.broadleafcommerce.openadmin.audit.AdminAuditable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -103,8 +104,8 @@ public class PageSiteMapGenerator implements SiteMapGenerator {
     }
 
     protected Date generateDate(Page page) {
-        if (page instanceof AdminAuditable) {
-            return ((AdminAuditable) page).getDateUpdated();
+        if (page instanceof AdminAudit) {
+            return ((AdminAudit) page).getDateUpdated();
         } else {
             return new Date();
         }
