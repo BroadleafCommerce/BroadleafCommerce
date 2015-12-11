d35ececab8c8e60fc0fa44f3ffb73f98a00a336b
Elbert Bautista <ebautista@broadleafcommerce.org>
2015-12-11 14:08:09 -0600

    BroadleafCommerce/QA#1143 - refactor the ValidateAndConfirmPaymentActivity to move "confirm" flow into its own strategy class, update spock tests to reflect new flow

---
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/checkout/service/strategy/OrderPaymentConfirmationStrategy.java
@@ -0,0 +1,40 @@
+/*
+ * #%L
+ * BroadleafCommerce Framework
+ * %%
+ * Copyright (C) 2009 - 2015 Broadleaf Commerce
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.broadleafcommerce.core.checkout.service.strategy;
+
+import org.broadleafcommerce.common.payment.dto.PaymentResponseDTO;
+import org.broadleafcommerce.common.vendor.service.exception.PaymentException;
+import org.broadleafcommerce.core.checkout.service.exception.CheckoutException;
+import org.broadleafcommerce.core.checkout.service.workflow.CheckoutSeed;
+import org.broadleafcommerce.core.payment.domain.OrderPayment;
+import org.broadleafcommerce.core.payment.domain.PaymentTransaction;
+import org.broadleafcommerce.core.workflow.ProcessContext;
+import org.broadleafcommerce.core.workflow.WorkflowException;
+
+/**
+ * @author Elbert Bautista (elbertbautista)
+ */
+public interface OrderPaymentConfirmationStrategy {
+
+    /**
+     * Strategy to determine how to "confirm" an OrderPayment at checkout
+     */
+    public PaymentResponseDTO confirmTransaction(PaymentTransaction tx, ProcessContext<CheckoutSeed> context) throws PaymentException, WorkflowException, CheckoutException;
+}
