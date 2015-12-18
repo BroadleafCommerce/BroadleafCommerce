ed9f2b875ae1c15984543c5a0f15dbf049159fc1
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2015-12-17 19:36:49 -0600

    BroadleafCommerce/QA#1198 - Copied wrappers from broadleaf-framework-web to new broadleaf-rest-api package

---
+++ core/broadleaf-rest-api/src/main/java/org/broadleafcommerce/web/api/v2/wrapper/CustomerAddressWrapper.java
@@ -0,0 +1,101 @@
+/*
+ * #%L
+ * BroadleafCommerce Framework Web
+ * %%
+ * Copyright (C) 2009 - 2013 Broadleaf Commerce
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.broadleafcommerce.web.api.v2.wrapper;
+
+import org.broadleafcommerce.profile.core.domain.CustomerAddress;
+import org.broadleafcommerce.profile.core.service.CustomerAddressService;
+import org.springframework.context.ApplicationContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * This is a JAXB wrapper around CustomerAddress.
+ *
+ * @author Jeff Fischer
+ */
+@XmlRootElement(name = "customerAddress")
+@XmlAccessorType(value = XmlAccessType.FIELD)
+public class CustomerAddressWrapper extends BaseWrapper implements APIWrapper<CustomerAddress>, APIUnwrapper<CustomerAddress> {
+
+    @XmlElement
+    protected Long id;
+
+    @XmlElement
+    protected String addressName;
+
+    @XmlElement
+    protected AddressWrapper address;
+
+    @Override
+    public void wrapDetails(CustomerAddress model, HttpServletRequest request) {
+        this.id = model.getId();
+        this.addressName = model.getAddressName();
+        if (model.getAddress() != null) {
+            this.address = (AddressWrapper) context.getBean(AddressWrapper.class.getName());
+            this.address.wrapDetails(model.getAddress(), request);
+        }
+    }
+
+    @Override
+    public void wrapSummary(CustomerAddress model, HttpServletRequest request) {
+        wrapDetails(model, request);
+    }
+
+    @Override
+    public CustomerAddress unwrap(HttpServletRequest request, ApplicationContext context) {
+        CustomerAddressService customerAddressService = (CustomerAddressService) context.getBean("blCustomerAddressService");
+        CustomerAddress customerAddress = customerAddressService.create();
+        customerAddress.setId(this.id);
+        if (this.address != null) {
+            customerAddress.setAddress(address.unwrap(request, context));
+        }
+        customerAddress.setAddressName(this.addressName);
+        return customerAddress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddressName() {
+        return addressName;
+    }
+
+    public void setAddressName(String addressName) {
+        this.addressName = addressName;
+    }
+
+    public AddressWrapper getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressWrapper address) {
+        this.address = address;
+    }
+}
---
+++ core/broadleaf-rest-api/src/main/java/org/broadleafcommerce/web/api/v2/wrapper/DimensionWrapper.java
@@ -0,0 +1,210 @@
+/*
+ * #%L
+ * BroadleafCommerce Framework Web
+ * %%
+ * Copyright (C) 2009 - 2013 Broadleaf Commerce
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.broadleafcommerce.web.api.v2.wrapper;
+
+import org.broadleafcommerce.common.util.DimensionUnitOfMeasureType;
+import org.broadleafcommerce.common.vendor.service.type.ContainerShapeType;
+import org.broadleafcommerce.common.vendor.service.type.ContainerSizeType;
+import org.broadleafcommerce.core.catalog.domain.Dimension;
+import org.springframework.context.ApplicationContext;
+
+import java.math.BigDecimal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.annotation.XmlElement;
+
+/**
+ * JAXB wrapper for Dimension
+ * <p/>
+ * User: Kelly Tisdell
+ * Date: 4/10/12
+ */
+public class DimensionWrapper extends BaseWrapper implements APIWrapper<Dimension>, APIUnwrapper<Dimension> {
+
+    @XmlElement
+    protected BigDecimal width;
+
+    @XmlElement
+    protected BigDecimal height;
+
+    @XmlElement
+    protected BigDecimal depth;
+
+    @XmlElement
+    protected BigDecimal girth;
+
+    @XmlElement
+    protected String container;
+
+    @XmlElement
+    protected String size;
+    
+    @XmlElement
+    protected String dimensionUnitOfMeasure;
+    
+    @Override
+    public void wrapDetails(Dimension model, HttpServletRequest request) {
+        this.width = model.getWidth();
+        this.depth = model.getDepth();
+        this.height = model.getHeight();
+        this.girth = model.getGirth();
+
+        if (model.getDimensionUnitOfMeasure() != null) {
+            this.dimensionUnitOfMeasure = model.getDimensionUnitOfMeasure().getType();
+        }
+
+        if (model.getSize() != null) {
+            this.size = model.getSize().getType();
+        }
+
+        if (model.getContainer() != null) {
+            this.container = model.getContainer().getType();
+        }
+    }
+
+    @Override
+    public void wrapSummary(Dimension model, HttpServletRequest request) {
+        wrapDetails(model, request);
+    }
+
+    
+    /**
+     * @return the width
+     */
+    public BigDecimal getWidth() {
+        return width;
+    }
+
+    
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(BigDecimal width) {
+        this.width = width;
+    }
+
+    
+    /**
+     * @return the height
+     */
+    public BigDecimal getHeight() {
+        return height;
+    }
+
+    
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(BigDecimal height) {
+        this.height = height;
+    }
+
+    
+    /**
+     * @return the depth
+     */
+    public BigDecimal getDepth() {
+        return depth;
+    }
+
+    
+    /**
+     * @param depth the depth to set
+     */
+    public void setDepth(BigDecimal depth) {
+        this.depth = depth;
+    }
+
+    
+    /**
+     * @return the girth
+     */
+    public BigDecimal getGirth() {
+        return girth;
+    }
+
+    
+    /**
+     * @param girth the girth to set
+     */
+    public void setGirth(BigDecimal girth) {
+        this.girth = girth;
+    }
+
+    
+    /**
+     * @return the container
+     */
+    public String getContainer() {
+        return container;
+    }
+
+    
+    /**
+     * @param container the container to set
+     */
+    public void setContainer(String container) {
+        this.container = container;
+    }
+
+    
+    /**
+     * @return the size
+     */
+    public String getSize() {
+        return size;
+    }
+
+    
+    /**
+     * @param size the size to set
+     */
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    
+    /**
+     * @return the dimensionUnitOfMeasure
+     */
+    public String getDimensionUnitOfMeasure() {
+        return dimensionUnitOfMeasure;
+    }
+
+    
+    /**
+     * @param dimensionUnitOfMeasure the dimensionUnitOfMeasure to set
+     */
+    public void setDimensionUnitOfMeasure(String dimensionUnitOfMeasure) {
+        this.dimensionUnitOfMeasure = dimensionUnitOfMeasure;
+    }
+
+    public Dimension unwrap(HttpServletRequest request, ApplicationContext context) {
+        Dimension dim = new Dimension();
+        dim.setContainer(ContainerShapeType.getInstance(this.container));
+        dim.setDimensionUnitOfMeasure(DimensionUnitOfMeasureType.getInstance(this.dimensionUnitOfMeasure));
+        dim.setDepth(this.depth);
+        dim.setGirth(this.girth);
+        dim.setHeight(this.height);
+        dim.setSize(ContainerSizeType.getInstance(this.size));
+        dim.setWidth(this.width);
+        return dim;
+    }
+}
