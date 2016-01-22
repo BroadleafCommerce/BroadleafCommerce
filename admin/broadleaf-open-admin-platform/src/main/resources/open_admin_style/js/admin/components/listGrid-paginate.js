689c915433bf341ece29331c3b81592fd3705969
Jonathan Fleschler <jon.fleschler@gmail.com>
2016-01-22 07:49:41 -0600

    BroadleafCommerce/QA#910 - Changed category tree listgrid to fill screen height when not in a modal

--- admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/listGrid-paginate.js
+++ admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/listGrid-paginate.js
@@ -586,13 +586,19 @@
                 if ($wrapper.find('.mCS_no_scrollbar').length > 0 && $modalBody.length === 0) {
                     BLCAdmin.listGrid.paginate.updateUrlFromScroll($wrapper.find('tbody'));
                 }
-            } else if ($table.data('listgridtype') === 'asset_grid' || $table.data('listgridtype') === 'asset_grid_folder') {
+            } else if ($table.data('listgridtype') === 'asset_grid'
+                || $table.data('listgridtype') === 'asset_grid_folder'
+                || $table.data('listgridtype') === 'tree') {
                 var $window = $(window);
                 var wrapperHeight = $window.height() - $wrapper.offset().top - 50;
 
                 if ($modalBody.length > 0) {
-                    wrapperHeight = $tbody.closest('.select-group').height()
+                    wrapperHeight = $tbody.closest('.select-group').outerHeight();
                 }
+
+                // adjust for header and footer height
+                wrapperHeight -= $wrapper.prev('.listgrid-header-wrapper:visible').outerHeight();
+                wrapperHeight -= $wrapper.next('.listgrid-table-footer:visible').outerHeight();
 
                 $wrapper.css('max-height', wrapperHeight);
                 $wrapper.find('.mCustomScrollBox').css('max-height', wrapperHeight);
@@ -629,12 +635,6 @@
                 $wrapper.find('.mCustomScrollBox').css('max-height', maxHeight);
                 $modalBody.css('overflow-y', 'auto');
 
-            } else if ($table.data('listgridtype') === 'tree') {
-                var maxHeight = BLCAdmin.listGrid.paginate.computeActualMaxHeight($tbody, treeListGridHeight);
-                $wrapper.css('max-height', maxHeight);
-                $wrapper.find('.mCustomScrollBox').css('max-height', maxHeight);
-
-                $wrapper.mCustomScrollbar('update');
             } else {
                 // not in a modal, not the only grid on the screen, my size should be equal to max size of a grid
                 // There is a possibility, if pagination is limited on the packed, that
