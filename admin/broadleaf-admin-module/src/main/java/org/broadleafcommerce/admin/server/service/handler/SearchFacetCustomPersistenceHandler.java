6ef4f99310e22c5ff9123c553eef89fe3b0491bb
nathandmoore <moorednathan@gmail.com>
2016-02-24 10:28:53 -0600

    Found a better solution: Catching the `PersistenceException` instead of creating a DAO and Service for `CategorySearchFacet` and checking if the `SearchFacet` is in use as a `CategorySearchFacet`.

--- admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/server/service/handler/SearchFacetCustomPersistenceHandler.java
+++ admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/server/service/handler/SearchFacetCustomPersistenceHandler.java
@@ -17,6 +17,7 @@ import org.broadleafcommerce.openadmin.server.service.persistence.module.RecordH
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import javax.persistence.PersistenceException;
 import java.util.Map;
 
 /**
@@ -46,30 +47,21 @@ public class SearchFacetCustomPersistenceHandler extends CustomPersistenceHandle
             throws ServiceException {
         Entity entity = persistencePackage.getEntity();
         try {
-            PersistencePerspective persistencePerspective = persistencePackage.getPersistencePerspective();
-            Map<String, FieldMetadata> adminProperties = helper.getSimpleMergedProperties(SearchFacet.class.getName(), persistencePerspective);
-            Object primaryKey = helper.getPrimaryKey(entity, adminProperties);
-            SearchFacet adminInstance = (SearchFacet) dynamicEntityDao.retrieve(Class.forName(entity.getType()[0]), primaryKey);
-            adminInstance = (SearchFacet) helper.createPopulatedInstance(adminInstance, entity, adminProperties, false);
-            adminInstance = dynamicEntityDao.merge(adminInstance);
-
-            if(csfService.readCategorySearchFacetsBySearchFacet(adminInstance.getId()).size() > 0) {
-                // check if search facet is in use by a category (i.e., is it a CategorySearchFacet?)
-                entity.addGlobalValidationError("Cannot delete this SearchFacet because it is referenced by a Category");
-                throw new ValidationException(entity, "SearchFacet_error_inUse");
+            OperationType opType = persistencePackage.getPersistencePerspective().getOperationTypes().getRemoveType();
+            if (opType != null) {
+                helper.getCompatibleModule(opType).remove(persistencePackage);
             } else {
-                // if not, remove like normal
-                // check if there is a removeType specified
-                OperationType opType = persistencePackage.getPersistencePerspective().getOperationTypes().getRemoveType();
-                if (opType != null) {
-                    helper.getCompatibleModule(opType).remove(persistencePackage);
-                } else {
-                    helper.getCompatibleModule(OperationType.BASIC).remove(persistencePackage);
-                }
+                helper.getCompatibleModule(OperationType.BASIC).remove(persistencePackage);
             }
         } catch (Exception e) {
-            LOG.error("Unable to execute persistence activity", e);
-            throw new ServiceException("Unable to remove entity for " + entity.getType()[0], e);
+            if (e.getCause() instanceof PersistenceException) {
+                // If a persistence exception (should be a foreign key constraint)
+                LOG.error("Unable to execute persistence activity because entity in use", e);
+                throw new ValidationException(entity, "Unable to remove entity for this Search Facet: In use by a Category.");
+            } else {
+                LOG.error("Unable to execute persistence activity", e);
+                throw new ServiceException("Unable to remove entity for " + entity.getType()[0], e);
+            }
         }
     }
 }
