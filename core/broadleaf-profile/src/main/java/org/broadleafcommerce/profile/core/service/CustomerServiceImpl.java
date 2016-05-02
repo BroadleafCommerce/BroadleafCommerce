67680d15a4ac054c210deefcbd125cffa70901e0
Reginald C Cole <rcole@broadleafcommerce.com>
2016-05-02 11:41:10 -0500

    Corrected spacing on ListGrids
    - Changed CustomerService method to use canonical name of Class.
    
    - Addresses Broadleafcommerce/QA#1753

--- core/broadleaf-profile/src/main/java/org/broadleafcommerce/profile/core/service/CustomerServiceImpl.java
+++ core/broadleaf-profile/src/main/java/org/broadleafcommerce/profile/core/service/CustomerServiceImpl.java
@@ -298,9 +298,9 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Long findNextCustomerId() {
-        return idGenerationService.findNextId("org.broadleafcommerce.profile.core.domain.Customer");
+        return idGenerationService.findNextId(Customer.class.getCanonicalName());
     }
-    
+
     @Override
     public Customer createNewCustomer() {
         return createCustomerFromId(null);
