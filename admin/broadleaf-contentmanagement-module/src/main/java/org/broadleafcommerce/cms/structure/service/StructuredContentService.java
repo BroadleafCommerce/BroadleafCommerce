d24fa45842becf05a1235ed4b4029671e70675e2
nathandmoore <moorednathan@gmail.com>
2016-07-21 08:34:47 -0500

    BroadleafCommerce/QA#2011 - Added deprecated annotation

--- admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/structure/service/StructuredContentService.java
+++ admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/structure/service/StructuredContentService.java
@@ -217,8 +217,10 @@ public interface StructuredContentService {
      * @param locale
      * @param contentType
      * @return cache key for DTOList
+     *
      * @deprecated use {@link #buildTypeKeyWithSecure(SandBox, Long, Locale, String, Boolean)}
      */
+    @Deprecated
     public String buildTypeKey(SandBox currentSandbox, Long site, Locale locale, String contentType);
 
     /**
@@ -250,8 +252,10 @@ public interface StructuredContentService {
      *
      * @param nameKey
      * @param typeKey
+     * 
      * @deprecated use {@link #removeItemFromCacheByKey(String)}
      */
+    @Deprecated
     public void removeItemFromCache(String nameKey, String typeKey);
 
     /**
