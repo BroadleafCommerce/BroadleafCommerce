0ae528f707cf715c7595f922806f957ed190551a
Travis Wright <twright@broadleafcommerce.com>
2016-08-01 14:18:21 -0500

    BroadleafCommerce/QA#2046 - Removes the unused cmsUrlPrefix hidden field

--- admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/admin/web/service/AssetFormBuilderServiceImpl.java
+++ admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/admin/web/service/AssetFormBuilderServiceImpl.java
@@ -77,10 +77,6 @@ public class AssetFormBuilderServiceImpl implements AssetFormBuilderService {
             
             // Add a hidden field for the large thumbnail path
             record.getHiddenFields().add(new Field()
-                .withName("cmsUrlPrefix")
-                .withValue(staticAssetUrlPrefix));
-            
-            record.getHiddenFields().add(new Field()
                 .withName("thumbnailKey")
                 .withValue("?smallAdminThumbnail"));
             
