69154e1cce6626eccf8da0a9119ca670b994a82d
Jonathan Fleschler <jon.fleschler@gmail.com>
2016-08-16 08:12:04 -0500

    Added ProductProcessor extension hook

---
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/service/extension/ProductProcessorExtensionHandler.java
@@ -0,0 +1,38 @@
+/*
+ * #%L
+ * BroadleafCommerce Framework
+ * %%
+ * Copyright (C) 2009 - 2016 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
+package org.broadleafcommerce.core.catalog.service.extension;
+
+import org.broadleafcommerce.common.extension.ExtensionHandler;
+import org.broadleafcommerce.common.extension.ExtensionResultStatusType;
+import org.broadleafcommerce.core.catalog.domain.Product;
+
+import java.util.List;
+
+/**
+ * @author Jon Fleschler (jfleschler)
+ */
+public interface ProductProcessorExtensionHandler extends ExtensionHandler {
+
+    /**
+     * Takes a product, performs any special logic and returns a list of associated products to be handled
+     * @param product
+     * @param returnList
+     * @return
+     */
+    public ExtensionResultStatusType expandProduct(Product product, List<Long> returnList);
+}
