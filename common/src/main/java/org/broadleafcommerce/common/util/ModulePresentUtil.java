634495e9d574f13c00098202d2a777a1162faacd
nathandmoore <moorednathan@gmail.com>
2016-08-25 10:18:25 -0500

    BroadleafCommerce/QA#2090 - Added javadoc for the class and method.

--- common/src/main/java/org/broadleafcommerce/common/util/ModulePresentUtil.java
+++ common/src/main/java/org/broadleafcommerce/common/util/ModulePresentUtil.java
@@ -9,12 +9,22 @@ import java.util.Map;
 import javax.annotation.Nonnull;
 
 /**
- * Conditional class that checks for the presence of the Enterprise module.
+ * Conditional class that checks for the presence of a specified module.
  *
  * @author Nathan Moore (nathanmoore).
  */
 public class ModulePresentUtil {
 
+    /**
+     * Treats the {@link org.springframework.context.ApplicationContext} as a
+     * {@link ListableBeanFactory} to produce a {@link Map} of
+     * {@link org.springframework.context.annotation.Bean}. When it finds a
+     * Bean in the module specified, it returns true. Otherwise, it returns false.
+     *
+     * @param moduleInQuestion Name of the module being looked for.
+     *
+     * @return whether the module in question is present in the project
+     */
     public static boolean isPresent(@Nonnull final String moduleInQuestion) {
         ListableBeanFactory factory = ApplicationContextHolder.getApplicationContext();
         Map<String, ModuleLifecycleLoggingBean> beanMap = BeanFactoryUtils.beansOfTypeIncludingAncestors(factory, ModuleLifecycleLoggingBean.class, false, false);
