23823e25dba5d5e56c0f4146a2ff2427ed521eab
Cade Rea <crea@broadleafcommerce.com>
2016-09-08 17:55:52 -0500

    Removed unnecessary type checking and cast.

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/service/workflow/CartOperationProcessContextFactory.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/service/workflow/CartOperationProcessContextFactory.java
@@ -35,14 +35,8 @@ public class CartOperationProcessContextFactory implements ProcessContextFactory
      * Creates the necessary context for cart operations
      */
     public ProcessContext<CartOperationRequest> createContext(CartOperationRequest seedData) throws WorkflowException {
-        if (!(seedData instanceof CartOperationRequest)){
-            throw new WorkflowException("Seed data instance is incorrect. " +
-                    "Required class is " + CartOperationRequest.class.getName() + " " +
-                    "but found class: " + seedData.getClass().getName());
-        }
-        
-        ProcessContext<CartOperationRequest> context = new DefaultProcessContextImpl<CartOperationRequest>();
-        context.setSeedData((CartOperationRequest) seedData);
+        ProcessContext<CartOperationRequest> context = new DefaultProcessContextImpl<>();
+        context.setSeedData(seedData);
         return context;
     }
 
