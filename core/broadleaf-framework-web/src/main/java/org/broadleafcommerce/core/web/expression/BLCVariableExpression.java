260de339d265ce27f80e2f7795e6bd5bc6303c8e
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-09-14 12:04:27 -0500

    BroadleafCommerce/QA#2125 - Fixed the getPrice variable expression to work like the PriceTextDisplayProcessor

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/expression/BLCVariableExpression.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/expression/BLCVariableExpression.java
@@ -18,6 +18,10 @@
 
 package org.broadleafcommerce.core.web.expression;
 
+import org.apache.commons.lang3.StringUtils;
+import org.broadleafcommerce.common.currency.util.BroadleafCurrencyUtils;
+import org.broadleafcommerce.common.money.Money;
+import org.broadleafcommerce.common.util.StringUtil;
 import org.broadleafcommerce.common.web.BroadleafRequestContext;
 import org.broadleafcommerce.common.web.condition.TemplatingExistCondition;
 import org.broadleafcommerce.common.web.expression.BroadleafVariableExpression;
@@ -26,7 +30,8 @@ import org.broadleafcommerce.core.catalog.domain.Product;
 import org.broadleafcommerce.core.catalog.service.CatalogURLService;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
+
+import java.text.NumberFormat;
 
 import javax.annotation.Resource;
 
@@ -80,5 +85,25 @@ public class BLCVariableExpression implements BroadleafVariableExpression {
             }
         }
         return currentUrl;
+    }
+
+    /**
+     * Returns the price at the correct scale and rounding for the default currency
+     * @see Money#defaultCurrency()
+     * @param amount
+     * @return
+     */
+    public String getPrice(String amount) {
+        Money price = Money.ZERO;
+        String sanitizedAmount = StringUtil.removeNonNumerics(amount);
+        if (StringUtils.isNotEmpty(sanitizedAmount)) {
+            price = new Money(sanitizedAmount);
+            BroadleafRequestContext brc = BroadleafRequestContext.getBroadleafRequestContext();
+            if (brc.getJavaLocale() != null) {
+                NumberFormat formatter = BroadleafCurrencyUtils.getNumberFormatFromCache(brc.getJavaLocale(), price.getCurrency());
+                return formatter.format(price.getAmount());
+            }
+        }
+        return "$ " + price.getAmount().toString();
     }
 }
