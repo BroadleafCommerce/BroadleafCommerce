187f1652dce5493c67225f84977538dd0faf4159
Jonathan Fleschler <jon.fleschler@gmail.com>
2016-09-21 12:03:51 -0500

    BroadleafCommerce/QA#2161 - Add boolean to offer determining whether or not it applies to addons

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/domain/Offer.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/domain/Offer.java
@@ -23,8 +23,7 @@ import org.broadleafcommerce.common.persistence.Status;
 import org.broadleafcommerce.core.offer.service.type.OfferDiscountType;
 import org.broadleafcommerce.core.offer.service.type.OfferItemRestrictionRuleType;
 import org.broadleafcommerce.core.offer.service.type.OfferType;
-import org.broadleafcommerce.core.promotionMessage.domain.PromotionMessage;
-import org.broadleafcommerce.core.promotionMessage.domain.type.PromotionMessageType;
+import org.broadleafcommerce.core.order.domain.OrderItem;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -86,6 +85,18 @@ public interface Offer extends Status, Serializable,MultiTenantCloneable<Offer> 
     public OfferItemRestrictionRuleType getOfferItemTargetRuleType();
 
     public void setOfferItemTargetRuleType(OfferItemRestrictionRuleType restrictionRuleType);
+
+    /**
+     * Returns whether or not this offer should apply to an {@link OrderItem}s child order items
+     * @return applyToChildItems
+     */
+    Boolean getApplyToChildItems();
+
+    /**
+     * Sets whether or not this offer should apply to an {@link OrderItem}s child order items
+     * @param applyToChildItems
+     */
+    void setApplyToChildItems(boolean applyToChildItems);
 
     /**
      * Returns false if this offer is not combinable with other offers of the same type.
