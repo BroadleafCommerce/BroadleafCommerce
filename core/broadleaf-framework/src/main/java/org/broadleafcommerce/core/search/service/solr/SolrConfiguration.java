ae08813208335ab1efcc40ea7398da753b220b2c
Chad Harchar <charchar@broadleafcommerce.org>
2016-10-19 17:39:26 -0500

    BroadleafCommerce/QA#2244 - Fixed a NPE caused by checking the wrong solr server for assignable

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/SolrConfiguration.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/SolrConfiguration.java
@@ -220,7 +220,7 @@ public class SolrConfiguration implements InitializingBean {
                     }
                 }
             }
-            if (BroadleafCloudSolrClient.class.isAssignableFrom(primaryServer.getClass())) {
+            if (BroadleafCloudSolrClient.class.isAssignableFrom(server.getClass())) {
                 ((BroadleafCloudSolrClient) server).setReindexClient(true);
                 ((BroadleafCloudSolrClient) server).setSolrConfig(this);
             }
