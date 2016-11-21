de6dbbdfccc3c7749bf081c8823ac942f3b42323
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-11-21 15:15:11 -0600

    Commented and changed PageServiceImpl and PageSiteMapGenerator to work with being moved to content microservice

--- content/src/main/java/org/broadleafcommerce/cms/page/service/PageServiceImpl.java
+++ content/src/main/java/org/broadleafcommerce/cms/page/service/PageServiceImpl.java
@@ -17,7 +17,6 @@
  */
 package org.broadleafcommerce.cms.page.service;
 
-import org.apache.commons.beanutils.BeanComparator;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.broadleafcommerce.cms.file.service.StaticAssetService;
@@ -25,23 +24,12 @@ import org.broadleafcommerce.cms.page.dao.PageDao;
 import org.broadleafcommerce.cms.page.domain.Page;
 import org.broadleafcommerce.cms.page.domain.PageField;
 import org.broadleafcommerce.cms.page.domain.PageTemplate;
-import org.broadleafcommerce.common.cache.CacheStatType;
-import org.broadleafcommerce.common.cache.StatisticsService;
-import org.broadleafcommerce.common.extension.ExtensionResultHolder;
-import org.broadleafcommerce.common.locale.domain.Locale;
-import org.broadleafcommerce.common.locale.service.LocaleService;
-import org.broadleafcommerce.common.locale.util.LocaleUtil;
 import org.broadleafcommerce.common.page.dto.NullPageDTO;
 import org.broadleafcommerce.common.page.dto.PageDTO;
-import org.broadleafcommerce.common.rule.RuleProcessor;
-import org.broadleafcommerce.common.site.domain.Site;
-import org.broadleafcommerce.common.template.TemplateOverrideExtensionManager;
-import org.broadleafcommerce.common.web.BroadleafRequestContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -64,21 +52,21 @@ public class PageServiceImpl implements PageService {
 
     @Resource(name="blPageDao")
     protected PageDao pageDao;
-    
-    @Resource(name="blPageRuleProcessors")
-    protected List<RuleProcessor<PageDTO>> pageRuleProcessors;    
 
-    @Resource(name="blLocaleService")
-    protected LocaleService localeService;
+// TODO microservices - deal with rule processing
+//    @Resource(name="blPageRuleProcessors")
+//    protected List<RuleProcessor<PageDTO>> pageRuleProcessors;
+
+// TODO microservices - deal with locale
+//    @Resource(name="blLocaleService")
+//    protected LocaleService localeService;
     
     @Resource(name="blStaticAssetService")
     protected StaticAssetService staticAssetService;
 
-    @Resource(name="blStatisticsService")
-    protected StatisticsService statisticsService;
-
-    @Resource(name = "blTemplateOverrideExtensionManager")
-    protected TemplateOverrideExtensionManager templateOverrideManager;
+// TODO microservices - deal with statistics services
+//    @Resource(name="blStatisticsService")
+//    protected StatisticsService statisticsService;
 
     @Resource(name = "blPageServiceUtility")
     protected PageServiceUtility pageServiceUtility;
@@ -125,80 +113,85 @@ public class PageServiceImpl implements PageService {
     /*
      * Retrieve the page if one is available for the passed in uri.
      */
-    @Override
-    public PageDTO findPageByURI(Locale locale, String uri, Map<String,Object> ruleDTOs, boolean secure) {
-        List<PageDTO> returnList = null;
-
-        if (uri != null) {
-            List<Page> pageList = pageDao.findPageByURI(uri);
-            returnList = buildPageDTOList(pageList, secure, uri, locale);
-        }
-        
-        PageDTO dto = evaluatePageRules(returnList, locale, ruleDTOs);
-        
-        if (dto.getId() != null) {
-            Page page = findPageById(dto.getId());
-
-            ExtensionResultHolder<PageDTO> newDTO = new ExtensionResultHolder<>();
-
-            // Allow an extension point to override the page to render.
-            extensionManager.getProxy().overridePageDto(newDTO, dto, page);
-            if (newDTO.getResult() != null) {
-                dto = newDTO.getResult();
-            }
-        }
-        
-        if (dto != null) {
-            dto = pageServiceUtility.hydrateForeignLookups(dto);
-        }
-        
-        return dto;
-    }
+// TODO microservices - deal with locale
+//    @Override
+//    public PageDTO findPageByURI(Locale locale, String uri, Map<String,Object> ruleDTOs, boolean secure) {
+//        List<PageDTO> returnList = null;
+//
+//        if (uri != null) {
+//            List<Page> pageList = pageDao.findPageByURI(uri);
+//            returnList = buildPageDTOList(pageList, secure, uri, locale);
+//        }
+//        
+//        PageDTO dto = evaluatePageRules(returnList, locale, ruleDTOs);
+//        
+//        if (dto.getId() != null) {
+//            Page page = findPageById(dto.getId());
+//
+//            ExtensionResultHolder<PageDTO> newDTO = new ExtensionResultHolder<>();
+//
+//            // Allow an extension point to override the page to render.
+//            extensionManager.getProxy().overridePageDto(newDTO, dto, page);
+//            if (newDTO.getResult() != null) {
+//                dto = newDTO.getResult();
+//            }
+//        }
+//        
+//        if (dto != null) {
+//            dto = pageServiceUtility.hydrateForeignLookups(dto);
+//        }
+//        
+//        return dto;
+//    }
 
     /*
      * Converts a list of pages to a list of pageDTOs, and caches the list.
      */
-    @Override
-    public List<PageDTO> buildPageDTOList(List<Page> pageList, boolean secure, String identifier, Locale locale) {
-        List<PageDTO> dtoList = new ArrayList<>();
-        BroadleafRequestContext context = BroadleafRequestContext.getBroadleafRequestContext();
+// TODO microservices - deal with locale
+// TODO microservices - deal with broadleafrequestcontext
+//    @Override
+//    public List<PageDTO> buildPageDTOList(List<Page> pageList, boolean secure, String identifier, Locale locale) {
+//        List<PageDTO> dtoList = new ArrayList<>();
+//        BroadleafRequestContext context = BroadleafRequestContext.getBroadleafRequestContext();
+//
+//        if (context.isProductionSandBox()) {
+//            dtoList = buildPageDTOListUsingCache(pageList, identifier, locale, secure);
+//        } else {
+//            // no caching actions needed if not production sandbox
+//            addPageListToPageDTOList(pageList, secure, dtoList);
+//        }
+//
+//        return copyDTOList(dtoList);
+//    }
 
-        if (context.isProductionSandBox()) {
-            dtoList = buildPageDTOListUsingCache(pageList, identifier, locale, secure);
-        } else {
-            // no caching actions needed if not production sandbox
-            addPageListToPageDTOList(pageList, secure, dtoList);
-        }
+// TODO microservices - deal with locale
+//    protected List<PageDTO> buildPageDTOListUsingCache(List<Page> pageList, String identifier, Locale locale, boolean secure) {
+//        List<PageDTO> dtoList = getCachedPageDTOList(pageList, identifier, locale, secure);
+//
+//        if (dtoList == null || dtoList.isEmpty()) {
+//            addPageListToPageDTOList(pageList, secure, dtoList);
+//
+//            if (dtoList != null && !dtoList.isEmpty()) {
+//                Collections.sort(dtoList, new BeanComparator("priority"));
+//                addPageListToCache(dtoList, identifier, locale, secure);
+//            }
+//        }
+//
+//        return dtoList;
+//    }
 
-        return copyDTOList(dtoList);
-    }
-
-    protected List<PageDTO> buildPageDTOListUsingCache(List<Page> pageList, String identifier, Locale locale, boolean secure) {
-        List<PageDTO> dtoList = getCachedPageDTOList(pageList, identifier, locale, secure);
-
-        if (dtoList == null || dtoList.isEmpty()) {
-            addPageListToPageDTOList(pageList, secure, dtoList);
-
-            if (dtoList != null && !dtoList.isEmpty()) {
-                Collections.sort(dtoList, new BeanComparator("priority"));
-                addPageListToCache(dtoList, identifier, locale, secure);
-            }
-        }
-
-        return dtoList;
-    }
-
-    protected List<PageDTO> getCachedPageDTOList(List<Page> pageList, String identifier, Locale locale, boolean secure) {
-        List<PageDTO> dtoList = new ArrayList<>();
-        String key = buildKey(identifier, locale, secure);
-        List<PageDTO> cachedList = getPageListFromCache(key);
-
-        if (cachedList != null && cachedList.size() == pageList.size()) {
-            dtoList = cachedList;
-        }
-
-        return dtoList;
-    }
+// TODO microservices - deal with locale
+//    protected List<PageDTO> getCachedPageDTOList(List<Page> pageList, String identifier, Locale locale, boolean secure) {
+//        List<PageDTO> dtoList = new ArrayList<>();
+//        String key = buildKey(identifier, locale, secure);
+//        List<PageDTO> cachedList = getPageListFromCache(key);
+//
+//        if (cachedList != null && cachedList.size() == pageList.size()) {
+//            dtoList = cachedList;
+//        }
+//
+//        return dtoList;
+//    }
 
     protected void addPageListToPageDTOList(List<Page> pageList, boolean secure, List<PageDTO> dtoList) {
         if (pageList != null) {
@@ -211,47 +204,51 @@ public class PageServiceImpl implements PageService {
         }
     }
 
-    protected List<PageDTO> getPageListFromCache(String key) {
-        if (key != null) {
-            Element cacheElement = getPageCache().get(key);
+// TODO microservices - deal with statistics services
+//    protected List<PageDTO> getPageListFromCache(String key) {
+//        if (key != null) {
+//            Element cacheElement = getPageCache().get(key);
+//
+//            if (cacheElement != null && cacheElement.getObjectValue() != null) {
+//                statisticsService.addCacheStat(CacheStatType.PAGE_CACHE_HIT_RATE.toString(), true);
+//                return (List<PageDTO>) cacheElement.getObjectValue();
+//            }
+//            statisticsService.addCacheStat(CacheStatType.PAGE_CACHE_HIT_RATE.toString(), false);
+//        }
+//
+//        return null;
+//    }
 
-            if (cacheElement != null && cacheElement.getObjectValue() != null) {
-                statisticsService.addCacheStat(CacheStatType.PAGE_CACHE_HIT_RATE.toString(), true);
-                return (List<PageDTO>) cacheElement.getObjectValue();
-            }
-            statisticsService.addCacheStat(CacheStatType.PAGE_CACHE_HIT_RATE.toString(), false);
-        }
+// TODO microservices - deal with locale
+//    protected void addPageListToCache(List<PageDTO> pageList, String identifier, Locale locale, boolean secure) {
+//        String key = buildKey(identifier, locale, secure);
+//        getPageCache().put(new Element(key, pageList));
+//        addPageMapCacheEntry(identifier, key);
+//    }
 
-        return null;
-    }
-
-    protected void addPageListToCache(List<PageDTO> pageList, String identifier, Locale locale, boolean secure) {
-        String key = buildKey(identifier, locale, secure);
-        getPageCache().put(new Element(key, pageList));
-        addPageMapCacheEntry(identifier, key);
-    }
-
-    @SuppressWarnings("unchecked")
-    protected void addPageMapCacheEntry(String identifier, String key) {
-        BroadleafRequestContext context = BroadleafRequestContext.getBroadleafRequestContext();
-        Site site = context.getNonPersistentSite();
-        Long siteId = (site != null) ? site.getId() : null;
-        Long sandBoxId = context.getSandBoxId();
-
-        String mapKey = getPageMapCacheKey(identifier, sandBoxId, siteId);
-
-        if (mapKey != null) {
-            Element e = getPageMapCache().get(mapKey);
-
-            if (e == null || e.getObjectValue() == null) {
-                List<String> keys = new ArrayList<>();
-                keys.add(key);
-                getPageMapCache().put(new Element(mapKey, keys));
-            } else {
-                ((List<String>) e.getObjectValue()).add(mapKey);
-            }
-        }
-    }
+// TODO microservices - deal with broadleafrequestcontext
+// TODO microservices - deal with site
+//    @SuppressWarnings("unchecked")
+//    protected void addPageMapCacheEntry(String identifier, String key) {
+//        BroadleafRequestContext context = BroadleafRequestContext.getBroadleafRequestContext();
+//        Site site = context.getNonPersistentSite();
+//        Long siteId = (site != null) ? site.getId() : null;
+//        Long sandBoxId = context.getSandBoxId();
+//
+//        String mapKey = getPageMapCacheKey(identifier, sandBoxId, siteId);
+//
+//        if (mapKey != null) {
+//            Element e = getPageMapCache().get(mapKey);
+//
+//            if (e == null || e.getObjectValue() == null) {
+//                List<String> keys = new ArrayList<>();
+//                keys.add(key);
+//                getPageMapCache().put(new Element(mapKey, keys));
+//            } else {
+//                ((List<String>) e.getObjectValue()).add(mapKey);
+//            }
+//        }
+//    }
 
     @Override
     public String getPageMapCacheKey(String uri, Long sandBoxId, Long site) {
@@ -259,39 +256,43 @@ public class PageServiceImpl implements PageService {
         return uri + "-" + sandBoxId + "-" + siteString;
     }
 
-    protected String buildKey(String identifier, Locale locale, Boolean secure) {
-        BroadleafRequestContext context = BroadleafRequestContext.getBroadleafRequestContext();
-        Long sandBoxId = context.getSandBoxId();
-        Site site = context.getNonPersistentSite();
-        Long siteId = (site != null) ? site.getId() : null;
-        locale = findLanguageOnlyLocale(locale);
-        StringBuilder key = new StringBuilder(identifier);
+// TODO microservices - deal with locale
+// TODO microservices - deal with broadleafrequestcontext
+// TODO microservices - deal with site
+//    protected String buildKey(String identifier, Locale locale, Boolean secure) {
+//        BroadleafRequestContext context = BroadleafRequestContext.getBroadleafRequestContext();
+//        Long sandBoxId = context.getSandBoxId();
+//        Site site = context.getNonPersistentSite();
+//        Long siteId = (site != null) ? site.getId() : null;
+//        locale = findLanguageOnlyLocale(locale);
+//        StringBuilder key = new StringBuilder(identifier);
+//
+//        if (locale != null) {
+//            key.append("-").append(locale.getLocaleCode());
+//        }
+//        if (secure != null) {
+//            key.append("-").append(secure);
+//        }
+//        if (sandBoxId != null) {
+//            key.append("-").append(sandBoxId);
+//        }
+//        if (siteId != null) {
+//            key.append("-").append(siteId);
+//        }
+//
+//        return key.toString();
+//    }
 
-        if (locale != null) {
-            key.append("-").append(locale.getLocaleCode());
-        }
-        if (secure != null) {
-            key.append("-").append(secure);
-        }
-        if (sandBoxId != null) {
-            key.append("-").append(sandBoxId);
-        }
-        if (siteId != null) {
-            key.append("-").append(siteId);
-        }
-
-        return key.toString();
-    }
-
-    protected Locale findLanguageOnlyLocale(Locale locale) {
-        if (locale != null ) {
-            Locale languageOnlyLocale = localeService.findLocaleByCode(LocaleUtil.findLanguageCode(locale));
-            if (languageOnlyLocale != null) {
-                return languageOnlyLocale;
-            }
-        }
-        return locale;
-    }
+// TODO microservices - deal with locale
+//    protected Locale findLanguageOnlyLocale(Locale locale) {
+//        if (locale != null ) {
+//            Locale languageOnlyLocale = localeService.findLocaleByCode(LocaleUtil.findLanguageCode(locale));
+//            if (languageOnlyLocale != null) {
+//                return languageOnlyLocale;
+//            }
+//        }
+//        return locale;
+//    }
 
     @Override
     public Cache getPageCache() {
@@ -325,41 +326,43 @@ public class PageServiceImpl implements PageService {
         return dtoListCopy;
     }
 
-    protected PageDTO evaluatePageRules(List<PageDTO> pageDTOList, Locale locale, Map<String, Object> ruleDTOs) {
-        if (pageDTOList == null) {
-            return NULL_PAGE;
-        }
-
-        // First check to see if we have a page that matches on the full locale.
-        for (PageDTO page : pageDTOList) {
-            if (locale != null && locale.getLocaleCode() != null) {
-                if (locale.getLocaleCode().equals(page.getLocaleCode())) {
-                    if (passesPageRules(page, ruleDTOs)) {
-                        return page;
-                    }
-                }
-            }
-        }
-
-        // Otherwise, we look for a match using just the language.
-        for (PageDTO page : pageDTOList) {
-            if (passesPageRules(page, ruleDTOs)) {
-                return page;
-            }
-        }
-
-        return NULL_PAGE;
-    }
+// TODO microservices - deal with locale
+//    protected PageDTO evaluatePageRules(List<PageDTO> pageDTOList, Locale locale, Map<String, Object> ruleDTOs) {
+//        if (pageDTOList == null) {
+//            return NULL_PAGE;
+//        }
+//
+//        // First check to see if we have a page that matches on the full locale.
+//        for (PageDTO page : pageDTOList) {
+//            if (locale != null && locale.getLocaleCode() != null) {
+//                if (locale.getLocaleCode().equals(page.getLocaleCode())) {
+//                    if (passesPageRules(page, ruleDTOs)) {
+//                        return page;
+//                    }
+//                }
+//            }
+//        }
+//
+//        // Otherwise, we look for a match using just the language.
+//        for (PageDTO page : pageDTOList) {
+//            if (passesPageRules(page, ruleDTOs)) {
+//                return page;
+//            }
+//        }
+//
+//        return NULL_PAGE;
+//    }
 
     protected boolean passesPageRules(PageDTO page, Map<String, Object> ruleDTOs) {
-        if (pageRuleProcessors != null) {
-            for (RuleProcessor<PageDTO> processor : pageRuleProcessors) {
-                boolean matchFound = processor.checkForMatch(page, ruleDTOs);
-                if (! matchFound) {
-                    return false;
-                }
-            }
-        }
+// TODO microservices - deal with rule processing
+//        if (pageRuleProcessors != null) {
+//            for (RuleProcessor<PageDTO> processor : pageRuleProcessors) {
+//                boolean matchFound = processor.checkForMatch(page, ruleDTOs);
+//                if (! matchFound) {
+//                    return false;
+//                }
+//            }
+//        }
         return true;
     }
 
--- content/src/main/java/org/broadleafcommerce/cms/page/service/PageSiteMapGenerator.java
+++ content/src/main/java/org/broadleafcommerce/cms/page/service/PageSiteMapGenerator.java
@@ -19,19 +19,10 @@ package org.broadleafcommerce.cms.page.service;
 
 import org.broadleafcommerce.cms.page.dao.PageDao;
 import org.broadleafcommerce.cms.page.domain.Page;
-import org.broadleafcommerce.common.file.service.BroadleafFileUtils;
-import org.broadleafcommerce.common.sitemap.domain.SiteMapGeneratorConfiguration;
-import org.broadleafcommerce.common.sitemap.service.SiteMapBuilder;
-import org.broadleafcommerce.common.sitemap.service.SiteMapGenerator;
-import org.broadleafcommerce.common.sitemap.service.type.SiteMapGeneratorType;
-import org.broadleafcommerce.common.sitemap.wrapper.SiteMapURLWrapper;
-import org.broadleafcommerce.openadmin.audit.AdminAudit;
-import org.broadleafcommerce.openadmin.audit.AdminAuditable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.annotation.Resource;
 
@@ -41,72 +32,75 @@ import javax.annotation.Resource;
  * @author Joshua Skorton (jskorton)
  */
 @Component("blPageSiteMapGenerator")
-public class PageSiteMapGenerator implements SiteMapGenerator {
+public class PageSiteMapGenerator  { //TODO microservices - deal with sitemap generation implements SiteMapGenerator {
 
     @Resource(name = "blPageDao")
     protected PageDao pageDao;
 
     @Value("${page.site.map.generator.row.limit}")
     protected int rowLimit;
-
-    @Override
-    public boolean canHandleSiteMapConfiguration(SiteMapGeneratorConfiguration siteMapGeneratorConfiguration) {
-        return SiteMapGeneratorType.PAGE.equals(siteMapGeneratorConfiguration.getSiteMapGeneratorType());
-    }
-
-    @Override
-    public void addSiteMapEntries(SiteMapGeneratorConfiguration smgc, SiteMapBuilder siteMapBuilder) {
-
-        int rowOffset = 0;
-        List<Page> pages;
-        String previousUrl = "";
-
-        do {
-            pages = pageDao.readOnlineAndIncludedPages(rowLimit, rowOffset, "fullUrl");
-            rowOffset += pages.size();
-            for (Page page : pages) {
-
-                if (page.getExcludeFromSiteMap()) {
-                    continue;
-                }
-
-                String currentURL = page.getFullUrl();
-
-                if (previousUrl.equals(currentURL)) {
-                    continue;
-                } else {
-                    previousUrl = currentURL;
-                }
-
-                SiteMapURLWrapper siteMapUrl = new SiteMapURLWrapper();
-
-                // location
-                siteMapUrl.setLoc(generateUri(siteMapBuilder, page));
-
-                // change frequency
-                siteMapUrl.setChangeFreqType(smgc.getSiteMapChangeFreq());
-
-                // priority
-                siteMapUrl.setPriorityType(smgc.getSiteMapPriority());
-
-                // lastModDate
-                siteMapUrl.setLastModDate(generateDate(page));
-
-                siteMapBuilder.addUrl(siteMapUrl);
-            }
-        } while (pages.size() == rowLimit);
-    }
-
-    protected String generateUri(SiteMapBuilder smb, Page page) {
-        return BroadleafFileUtils.appendUnixPaths(smb.getBaseUrl(), page.getFullUrl());
-    }
+    
+//TODO microservices - deal with sitemap generation
+//    @Override
+//    public boolean canHandleSiteMapConfiguration(SiteMapGeneratorConfiguration siteMapGeneratorConfiguration) {
+//        return SiteMapGeneratorType.PAGE.equals(siteMapGeneratorConfiguration.getSiteMapGeneratorType());
+//    }
+//
+//    @Override
+//    public void addSiteMapEntries(SiteMapGeneratorConfiguration smgc, SiteMapBuilder siteMapBuilder) {
+//
+//        int rowOffset = 0;
+//        List<Page> pages;
+//        String previousUrl = "";
+//
+//        do {
+//            pages = pageDao.readOnlineAndIncludedPages(rowLimit, rowOffset, "fullUrl");
+//            rowOffset += pages.size();
+//            for (Page page : pages) {
+//
+//                if (page.getExcludeFromSiteMap()) {
+//                    continue;
+//                }
+//
+//                String currentURL = page.getFullUrl();
+//
+//                if (previousUrl.equals(currentURL)) {
+//                    continue;
+//                } else {
+//                    previousUrl = currentURL;
+//                }
+//
+//                SiteMapURLWrapper siteMapUrl = new SiteMapURLWrapper();
+//
+//                // location
+//                siteMapUrl.setLoc(generateUri(siteMapBuilder, page));
+//
+//                // change frequency
+//                siteMapUrl.setChangeFreqType(smgc.getSiteMapChangeFreq());
+//
+//                // priority
+//                siteMapUrl.setPriorityType(smgc.getSiteMapPriority());
+//
+//                // lastModDate
+//                siteMapUrl.setLastModDate(generateDate(page));
+//
+//                siteMapBuilder.addUrl(siteMapUrl);
+//            }
+//        } while (pages.size() == rowLimit);
+//    }
+//
+//    protected String generateUri(SiteMapBuilder smb, Page page) {
+//        return BroadleafFileUtils.appendUnixPaths(smb.getBaseUrl(), page.getFullUrl());
+//    }
 
     protected Date generateDate(Page page) {
-        if (page instanceof AdminAudit) {
-            return ((AdminAudit) page).getDateUpdated();
-        } else {
-            return new Date();
-        }
+//        TODO microservices - deal with AdminAudit
+//        if (page instanceof AdminAudit) {
+//            return ((AdminAudit) page).getDateUpdated();
+//        } else {
+//            return new Date();
+//        }
+        return new Date();
     }
 
     public PageDao getPageDao() {
