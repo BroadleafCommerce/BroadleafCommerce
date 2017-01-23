d020f0693fdd8ca713212da18f1e9515d64e42b0
Phillip Verheyden <pverheyden@gmail.com>
2017-01-23 16:38:51 -0600

    BroadleafCommerce/QA#1944 - Target servlet 3.0

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/BroadleafResponseWrapper.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/BroadleafResponseWrapper.java
@@ -25,6 +25,7 @@ import org.owasp.esapi.errors.AccessControlException;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 import java.util.Locale;
 
 import javax.servlet.ServletOutputStream;
@@ -46,6 +47,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
         this.response = response;
     }
     
+    @Override
     public int getStatus() {
         return status;
     }
@@ -54,6 +56,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg0
      * @see org.owasp.esapi.HTTPUtilities#addCookie(HttpServletResponse, Cookie)
      */
+    @Override
     public void addCookie(Cookie arg0) {
         ESAPI.httpUtilities().addCookie(response, arg0);
     }
@@ -63,6 +66,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg1
      * @see javax.servlet.http.HttpServletResponse#addDateHeader(java.lang.String, long)
      */
+    @Override
     public void addDateHeader(String arg0, long arg1) {
         response.addDateHeader(arg0, arg1);
     }
@@ -72,6 +76,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg1
      * @see org.owasp.esapi.HTTPUtilities#addHeader(HttpServletResponse, String, String)
      */
+    @Override
     public void addHeader(String arg0, String arg1) {
         ESAPI.httpUtilities().addHeader(response, arg0, arg1);
     }
@@ -81,6 +86,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg1
      * @see javax.servlet.http.HttpServletResponse#addIntHeader(java.lang.String, int)
      */
+    @Override
     public void addIntHeader(String arg0, int arg1) {
         response.addIntHeader(arg0, arg1);
     }
@@ -90,6 +96,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @return
      * @see javax.servlet.http.HttpServletResponse#containsHeader(java.lang.String)
      */
+    @Override
     public boolean containsHeader(String arg0) {
         return response.containsHeader(arg0);
     }
@@ -100,6 +107,8 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @deprecated
      * @see javax.servlet.http.HttpServletResponse#encodeRedirectUrl(java.lang.String)
      */
+    @Deprecated
+    @Override
     public String encodeRedirectUrl(String arg0) {
         return response.encodeRedirectUrl(arg0);
     }
@@ -109,6 +118,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @return
      * @see javax.servlet.http.HttpServletResponse#encodeRedirectURL(java.lang.String)
      */
+    @Override
     public String encodeRedirectURL(String arg0) {
         return response.encodeRedirectURL(arg0);
     }
@@ -119,6 +129,8 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @deprecated
      * @see javax.servlet.http.HttpServletResponse#encodeUrl(java.lang.String)
      */
+    @Deprecated
+    @Override
     public String encodeUrl(String arg0) {
         return response.encodeUrl(arg0);
     }
@@ -128,6 +140,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @return
      * @see javax.servlet.http.HttpServletResponse#encodeURL(java.lang.String)
      */
+    @Override
     public String encodeURL(String arg0) {
         return response.encodeURL(arg0);
     }
@@ -136,6 +149,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @throws IOException
      * @see javax.servlet.ServletResponse#flushBuffer()
      */
+    @Override
     public void flushBuffer() throws IOException {
         response.flushBuffer();
     }
@@ -144,6 +158,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @return
      * @see javax.servlet.ServletResponse#getBufferSize()
      */
+    @Override
     public int getBufferSize() {
         return response.getBufferSize();
     }
@@ -152,6 +167,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @return
      * @see javax.servlet.ServletResponse#getCharacterEncoding()
      */
+    @Override
     public String getCharacterEncoding() {
         return response.getCharacterEncoding();
     }
@@ -160,6 +176,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @return
      * @see javax.servlet.ServletResponse#getContentType()
      */
+    @Override
     public String getContentType() {
         return response.getContentType();
     }
@@ -168,6 +185,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @return
      * @see javax.servlet.ServletResponse#getLocale()
      */
+    @Override
     public Locale getLocale() {
         return response.getLocale();
     }
@@ -177,6 +195,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @throws IOException
      * @see javax.servlet.ServletResponse#getOutputStream()
      */
+    @Override
     public ServletOutputStream getOutputStream() throws IOException {
         return response.getOutputStream();
     }
@@ -186,6 +205,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @throws IOException
      * @see javax.servlet.ServletResponse#getWriter()
      */
+    @Override
     public PrintWriter getWriter() throws IOException {
         return response.getWriter();
     }
@@ -194,6 +214,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @return
      * @see javax.servlet.ServletResponse#isCommitted()
      */
+    @Override
     public boolean isCommitted() {
         return response.isCommitted();
     }
@@ -202,6 +223,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * 
      * @see javax.servlet.ServletResponse#reset()
      */
+    @Override
     public void reset() {
         response.reset();
     }
@@ -210,6 +232,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * 
      * @see javax.servlet.ServletResponse#resetBuffer()
      */
+    @Override
     public void resetBuffer() {
         response.resetBuffer();
     }
@@ -220,6 +243,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @throws IOException
      * @see javax.servlet.http.HttpServletResponse#sendError(int, java.lang.String)
      */
+    @Override
     public void sendError(int arg0, String arg1) throws IOException {
         response.sendError(arg0, arg1);
     }
@@ -229,6 +253,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @throws IOException
      * @see javax.servlet.http.HttpServletResponse#sendError(int)
      */
+    @Override
     public void sendError(int arg0) throws IOException {
         response.sendError(arg0);
     }
@@ -238,6 +263,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @throws IOException
      * @see javax.servlet.http.HttpServletResponse#sendRedirect(java.lang.String)
      */
+    @Override
     public void sendRedirect(String arg0) throws IOException {
         try {
             ESAPI.httpUtilities().sendRedirect(arg0);
@@ -251,6 +277,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg0
      * @see javax.servlet.ServletResponse#setBufferSize(int)
      */
+    @Override
     public void setBufferSize(int arg0) {
         response.setBufferSize(arg0);
     }
@@ -259,6 +286,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg0
      * @see javax.servlet.ServletResponse#setCharacterEncoding(java.lang.String)
      */
+    @Override
     public void setCharacterEncoding(String arg0) {
         response.setCharacterEncoding(arg0);
     }
@@ -267,6 +295,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg0
      * @see javax.servlet.ServletResponse#setContentLength(int)
      */
+    @Override
     public void setContentLength(int arg0) {
         response.setContentLength(arg0);
     }
@@ -275,6 +304,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg0
      * @see javax.servlet.ServletResponse#setContentType(java.lang.String)
      */
+    @Override
     public void setContentType(String arg0) {
         response.setContentType(arg0);
     }
@@ -284,6 +314,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg1
      * @see javax.servlet.http.HttpServletResponse#setDateHeader(java.lang.String, long)
      */
+    @Override
     public void setDateHeader(String arg0, long arg1) {
         response.setDateHeader(arg0, arg1);
     }
@@ -293,6 +324,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg1
      * @see javax.servlet.http.HttpServletResponse#setHeader(java.lang.String, java.lang.String)
      */
+    @Override
     public void setHeader(String arg0, String arg1) {
         response.setHeader(arg0, arg1);
     }
@@ -302,6 +334,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg1
      * @see javax.servlet.http.HttpServletResponse#setIntHeader(java.lang.String, int)
      */
+    @Override
     public void setIntHeader(String arg0, int arg1) {
         response.setIntHeader(arg0, arg1);
     }
@@ -310,6 +343,7 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg0
      * @see javax.servlet.ServletResponse#setLocale(java.util.Locale)
      */
+    @Override
     public void setLocale(Locale arg0) {
         response.setLocale(arg0);
     }
@@ -320,6 +354,8 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @deprecated
      * @see javax.servlet.http.HttpServletResponse#setStatus(int, java.lang.String)
      */
+    @Deprecated
+    @Override
     public void setStatus(int arg0, String arg1) {
         this.status = arg0;
         response.setStatus(arg0, arg1);
@@ -329,9 +365,34 @@ public class BroadleafResponseWrapper implements HttpServletResponse {
      * @param arg0
      * @see javax.servlet.http.HttpServletResponse#setStatus(int)
      */
+    @Override
     public void setStatus(int arg0) {
         this.status = arg0;
         response.setStatus(arg0);
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.http.HttpServletResponse#getHeader(java.lang.String)
+     */
+    @Override
+    public String getHeader(String name) {
+        return response.getHeader(name);
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.http.HttpServletResponse#getHeaders(java.lang.String)
+     */
+    @Override
+    public Collection<String> getHeaders(String name) {
+        return response.getHeaders(name);
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.http.HttpServletResponse#getHeaderNames()
+     */
+    @Override
+    public Collection<String> getHeaderNames() {
+        return response.getHeaderNames();
     }
 
 }
