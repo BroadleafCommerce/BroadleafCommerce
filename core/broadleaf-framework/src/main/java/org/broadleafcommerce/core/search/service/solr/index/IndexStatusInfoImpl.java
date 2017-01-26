cb7b93ea92251a1b7c485ccfc2897ae3b264cb69
Daniel Colgrove <dcolgrove@broadleafcommerce.com>
2017-01-26 10:19:49 -0600

    Enhanced SolrIndexStatus to support partial index error tracking and limited retry attempts.  Includes enhancements to FileSystemSolrIndexStatusProvider to support these two new features (error tracking/dead event) as well as pruning the dead event list.  Addresses https://github.com/BroadleafCommerce/QA/issues/2559

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/index/IndexStatusInfoImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/search/service/solr/index/IndexStatusInfoImpl.java
@@ -30,6 +30,8 @@ public class IndexStatusInfoImpl implements IndexStatusInfo {
 
     private Date lastIndexDate;
     private Map<String, String> additionalInfo = new HashMap<String, String>();
+    private Map<Long, IndexStatusError> indexErrors = new HashMap<Long, IndexStatusError>();
+    private Map<Long, Date> deadIndexEvents = new HashMap<Long, Date>();
 
     @Override
     public Date getLastIndexDate() {
@@ -50,4 +52,25 @@ public class IndexStatusInfoImpl implements IndexStatusInfo {
     public void setAdditionalInfo(Map<String, String> additionalInfo) {
         this.additionalInfo = additionalInfo;
     }
+
+    @Override
+    public Map<Long, IndexStatusError> getIndexErrors() {
+        return indexErrors;
+    }
+
+    @Override
+    public void setIndexErrors(Map<Long, IndexStatusError> indexErrors) {
+        this.indexErrors = indexErrors;
+    }
+
+    @Override
+    public Map<Long, Date> getDeadIndexEvents() {
+        return deadIndexEvents;
+    }
+
+    @Override
+    public void setDeadIndexEvents(Map<Long, Date> deadIndexEvents) {
+        this.deadIndexEvents = deadIndexEvents;
+    }
+    
 }
