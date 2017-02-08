2c86062b556264c07e3323b4e3b616dbca008d9d
danielcolgrove <dcolgrove@broadleafcommerce.com>
2017-02-08 16:29:00 -0600

    Merge pull request #805 from BroadleafCommerce/qa-2576-persist-maps
    Retrieve Sandbox and its children in same session

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/filter/BroadleafAdminRequestProcessor.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/web/filter/BroadleafAdminRequestProcessor.java
@@ -360,6 +360,11 @@ public class BroadleafAdminRequestProcessor extends AbstractBroadleafWebRequestP
             if (BLCRequestUtils.isOKtoUseSession(request)) {
                 request.setAttribute(BroadleafSandBoxResolver.SANDBOX_ID_VAR, sandBox.getId(), WebRequest.SCOPE_GLOBAL_SESSION);
             }
+            //We do this to prevent lazy init exceptions when this context/sandbox combination
+            // is used in a different session that it was initiated in. see QA#2576
+            if(sandBox != null && sandBox.getChildSandBoxes() != null) {
+                sandBox.getChildSandBoxes().size();
+            }
             brc.setSandBox(sandBox);
             brc.setDeployBehavior(deployBehaviorUtil.isProductionSandBoxMode() ? DeployBehavior.CLONE_PARENT : DeployBehavior.OVERWRITE_PARENT);
             brc.getAdditionalProperties().put("adminUser", adminUser);
--- common/src/main/java/org/broadleafcommerce/common/web/BroadleafRequestProcessor.java
+++ common/src/main/java/org/broadleafcommerce/common/web/BroadleafRequestProcessor.java
@@ -165,6 +165,11 @@ public class BroadleafRequestProcessor extends AbstractBroadleafWebRequestProces
             brc.setBroadleafCurrency(currencyDto.getCurrencyToUse());
             brc.setRequestedBroadleafCurrency(currencyDto.getRequestedCurrency());
         }
+        //We do this to prevent lazy init exceptions when this context/sandbox combination
+        // is used in a different session that it was initiated in. see QA#2576
+        if(currentSandbox != null && currentSandbox.getChildSandBoxes() != null) {
+            currentSandbox.getChildSandBoxes().size();
+        }
 
         brc.setSandBox(currentSandbox);
         brc.setDeployBehavior(deployBehaviorUtil.isProductionSandBoxMode() ? DeployBehavior.CLONE_PARENT : DeployBehavior.OVERWRITE_PARENT);
