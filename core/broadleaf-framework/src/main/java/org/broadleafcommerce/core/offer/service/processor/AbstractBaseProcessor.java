35ce2e04a4dac1e6b2e897315ea7834e59b45d67
Reginald C Cole <rcole@broadleafcommerce.com>
2017-03-09 11:47:11 -0600

    Remove the evaluation of Mvel from the Synchronation block.
    - Addresses BroadleafCommerce/QA#2658

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/service/processor/AbstractBaseProcessor.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/offer/service/processor/AbstractBaseProcessor.java
@@ -236,13 +236,14 @@ public abstract class AbstractBaseProcessor implements BaseProcessor {
      * @return a Boolean object containing the result of executing the MVEL expression
      */
     public Boolean executeExpression(String expression, Map<String, Object> vars) {
+        Map<String, Class<?>> contextImports = new HashMap<>();
         synchronized (EXPRESSION_CACHE) {
             expression = usePriceBeforeAdjustments(expression);
-            Map<String, Class<?>> contextImports = new HashMap<>();
             contextImports.put("OfferType", OfferType.class);
             contextImports.put("FulfillmentType", FulfillmentType.class);
-            return MvelHelper.evaluateRule(expression, vars, EXPRESSION_CACHE, contextImports);
         }
+        return MvelHelper.evaluateRule(expression, vars, EXPRESSION_CACHE, contextImports);
+        
     }
 
     protected String usePriceBeforeAdjustments(String expression) {
