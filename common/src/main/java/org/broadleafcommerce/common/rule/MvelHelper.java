73cef4ab91a61805828295307c1c0108eda58a78
Chad Harchar <charchar@broadleafcommerce.org>
2017-03-23 13:32:49 -0500

    BroadleafCommerce/QA#2694 - Prevent modified rule being put in cache, added isInfoEnabled check to MVEL rule exception

--- common/src/main/java/org/broadleafcommerce/common/rule/MvelHelper.java
+++ common/src/main/java/org/broadleafcommerce/common/rule/MvelHelper.java
@@ -143,7 +143,10 @@ public class MvelHelper {
             return true;
         } else {
             // MVEL expression compiling can be expensive so let's cache the expression
-            Serializable exp = expressionCache.get(rule);
+            Serializable exp = null;
+            if (expressionCache != null) {
+                exp = expressionCache.get(rule);
+            }
             if (exp == null) {
                 ParserContext context = new ParserContext();
                 context.addImport("MVEL", MVEL.class);
@@ -154,9 +157,8 @@ public class MvelHelper {
                     }
                 }
                 
-                rule = modifyExpression(rule, ruleParameters, context);
-                
-                exp = MVEL.compileExpression(rule, context);
+                String modifiedRule = modifyExpression(rule, ruleParameters, context);
+                exp = MVEL.compileExpression(modifiedRule, context);
                 expressionCache.put(rule, exp);
             }
 
@@ -178,7 +180,7 @@ public class MvelHelper {
             } catch (Exception e) {
                 //Unable to execute the MVEL expression for some reason
                 //Return false, but notify about the bad expression through logs
-                if (!TEST_MODE) {
+                if (!TEST_MODE && LOG.isInfoEnabled()) {
                     LOG.info("Unable to parse and/or execute the mvel expression (" + rule + "). Reporting to the logs and returning false for the match expression", e);
                 }
                 return false;
@@ -243,7 +245,6 @@ public class MvelHelper {
      * 
      * Should be called from within a valid web request.
      *
-     * @param request
      * @return
      */
     public static Map<String, Object> buildMvelParameters() {
