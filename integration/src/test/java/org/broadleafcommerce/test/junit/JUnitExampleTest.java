02e969785834c153b48a61f8a9256fcf5d05d8d2
Phillip Verheyden <pverheyden@gmail.com>
2017-04-05 09:08:01 -0500

    #869 - Remove the unnecessary JUnit annotations since @RunWith cannot be apart of a meta-annotation. Add example tests for Spock and JUnit and update documentation

---
+++ integration/src/test/java/org/broadleafcommerce/test/junit/JUnitExampleTest.java
@@ -0,0 +1,48 @@
+/*
+ * #%L
+ * BroadleafCommerce Integration
+ * %%
+ * Copyright (C) 2009 - 2017 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
+package org.broadleafcommerce.test.junit;
+/**
+ * 
+ */
+
+import org.broadleafcommerce.core.catalog.service.CatalogService;
+import org.broadleafcommerce.test.config.BroadleafSiteIntegrationTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * 
+ * 
+ * @author Phillip Verheyden (phillipuniverse)
+ */
+@BroadleafSiteIntegrationTest
+@RunWith(SpringRunner.class)
+public class JUnitExampleTest {
+    
+    @Resource
+    private CatalogService catalogService;
+    
+    @Test
+    public void testInjectionWorks() {
+        Assert.assertNotEquals(catalogService, null);
+    }
+}
