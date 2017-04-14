adb1a5cad2260096511d6068b3adef22cf39250b
jefffischer <jfischer@broadleafcommerce.org>
2017-04-14 15:10:23 -0500

    Fix mvel test for cases where the project filesystem path contains whitespace
    (cherry picked from commit c1ba77c)

--- common/src/test/java/org/broadleafcommerce/common/rule/MvelOverloadWorkaroundReproduction.java
+++ common/src/test/java/org/broadleafcommerce/common/rule/MvelOverloadWorkaroundReproduction.java
@@ -17,14 +17,6 @@
  */
 package org.broadleafcommerce.common.rule;
 
-import org.apache.commons.collections.map.MultiValueMap;
-import org.mvel2.MVEL;
-import org.mvel2.ParserContext;
-
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Run the test case as a java main application in a new JVM. This seems to be required to cause the variability in the ordering
  * of the call to Class#getMethods on SelectizeCollectionUtils. In this case, we should be using the refactor of
