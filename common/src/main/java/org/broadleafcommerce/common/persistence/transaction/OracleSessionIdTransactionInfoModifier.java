4ac7f1e7218bef7ca49e0d9c772265ccf08b1ee4
jefffischer <jfischer@broadleafcommerce.org>
2017-06-07 00:09:54 -0500

    BroadleafCommerce/QA#2760 - Fix Oracle SID query

--- common/src/main/java/org/broadleafcommerce/common/persistence/transaction/OracleSessionIdTransactionInfoModifier.java
+++ common/src/main/java/org/broadleafcommerce/common/persistence/transaction/OracleSessionIdTransactionInfoModifier.java
@@ -2,6 +2,12 @@ package org.broadleafcommerce.common.persistence.transaction;
 
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
+import org.hibernate.jdbc.ReturningWork;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.persistence.EntityManager;
 
@@ -14,10 +20,34 @@ public class OracleSessionIdTransactionInfoModifier implements TransactionInfoCu
     public void modify(TransactionInfo info) {
         EntityManager em = info.getEntityManager();
         if (em != null) {
-            Session session = em.unwrap(Session.class);
-            SQLQuery query = session.createSQLQuery("select sys_context('userenv','sid') from dual;");
-            String sessionId = String.valueOf(query.uniqueResult());
-            info.getAdditionalParams().put("sessionid", sessionId);
+            Integer sid = em.unwrap(Session.class).doReturningWork(new ReturningWork<Integer>() {
+                @Override
+                public Integer execute(Connection connection) throws SQLException {
+                    Statement statement = null;
+                    ResultSet resultSet = null;
+                    try {
+                        statement = connection.createStatement();
+                        resultSet = statement.executeQuery("SELECT sys_context('userenv','sid') FROM dual");
+                        Integer response = null;
+                        if (resultSet.next()) {
+                            response = resultSet.getInt(1);
+                        }
+                        return response;
+                    } finally {
+                        if (resultSet != null) {
+                            try{
+                                resultSet.close();
+                            } catch (Throwable e) {}
+                        }
+                        if (statement != null) {
+                            try {
+                                statement.close();
+                            } catch (Throwable e) {}
+                        }
+                    }
+                }
+            });
+            info.getAdditionalParams().put("sessionid", sid==null?"UNKNOWN":String.valueOf(sid));
         }
     }
 
