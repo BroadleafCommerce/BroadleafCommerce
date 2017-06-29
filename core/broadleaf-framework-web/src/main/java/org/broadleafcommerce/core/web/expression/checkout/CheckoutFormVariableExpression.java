03479a88e82fd5ea238eebe24ed6c10cea1627ec
Chris Kittrell <ckittrell@broadleafcommerce.com>
2017-06-29 09:32:46 -0500

    BroadleafCommerce/QA#2835 - Refactor CheckoutFormVariableExpression so that it only helps make decisions on whether or not to render a checkout stage & pre-populate checkout forms

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/expression/checkout/CheckoutFormVariableExpression.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/expression/checkout/CheckoutFormVariableExpression.java
@@ -17,15 +17,26 @@
  */
 package org.broadleafcommerce.core.web.expression.checkout;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.broadleafcommerce.common.money.Money;
+import org.broadleafcommerce.common.payment.PaymentType;
 import org.broadleafcommerce.common.web.expression.BroadleafVariableExpression;
+import org.broadleafcommerce.core.order.domain.FulfillmentGroup;
+import org.broadleafcommerce.core.order.domain.FulfillmentOption;
 import org.broadleafcommerce.core.order.domain.Order;
 import org.broadleafcommerce.core.order.service.FulfillmentGroupService;
-import org.broadleafcommerce.core.web.checkout.stage.CheckoutStageType;
+import org.broadleafcommerce.core.payment.domain.OrderPayment;
+import org.broadleafcommerce.core.web.checkout.model.BillingInfoForm;
+import org.broadleafcommerce.core.web.checkout.model.ShippingInfoForm;
 import org.broadleafcommerce.core.web.order.CartState;
 import org.broadleafcommerce.core.web.order.service.CartStateService;
 import org.broadleafcommerce.presentation.condition.ConditionalOnTemplating;
+import org.broadleafcommerce.profile.core.domain.CustomerAddress;
+import org.broadleafcommerce.profile.core.service.CustomerAddressService;
+import org.broadleafcommerce.profile.web.core.CustomerState;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import javax.annotation.Resource;
 
@@ -42,35 +53,16 @@ public class CheckoutFormVariableExpression implements BroadleafVariableExpressi
     @Resource(name = "blFulfillmentGroupService")
     protected FulfillmentGroupService fulfillmentGroupService;
 
+    @Resource(name = "blCustomerAddressService")
+    protected CustomerAddressService customerAddressService;
+
     @Override
     public String getName() {
         return "checkoutForm";
     }
 
-    public boolean isPreviousStage(String currentStage, String activeStage) {
-        CheckoutStageType currentStageType = CheckoutStageType.getInstance(currentStage);
-        CheckoutStageType activeStageType = CheckoutStageType.getInstance(activeStage);
-
-        return currentStageType.compareTo(activeStageType) < 0;
-    }
-
-    public boolean isActiveStage(String currentStage, String activeStage) {
-        CheckoutStageType currentStageType = CheckoutStageType.getInstance(currentStage);
-        CheckoutStageType activeStageType = CheckoutStageType.getInstance(activeStage);
-
-        return currentStageType.compareTo(activeStageType) == 0;
-    }
-
-    public boolean isLaterStage(String currentStage, String activeStage) {
-        CheckoutStageType currentStageType = CheckoutStageType.getInstance(currentStage);
-        CheckoutStageType activeStageType = CheckoutStageType.getInstance(activeStage);
-
-        return currentStageType.compareTo(activeStageType) > 0;
-    }
-
     public boolean shouldShowShippingInfoStage() {
         Order cart = CartState.getCart();
-
         int numShippableFulfillmentGroups = fulfillmentGroupService.calculateNumShippableFulfillmentGroups(cart);
 
         return numShippableFulfillmentGroups > 0;
@@ -79,6 +71,50 @@ public class CheckoutFormVariableExpression implements BroadleafVariableExpressi
     public boolean shouldShowBillingInfoStage() {
         return !cartStateService.orderContainsThirdPartyPayment()
                 && !cartStateService.orderContainsUnconfirmedCreditCard();
+    }
+
+    public ShippingInfoForm prePopulateShippingInfoForm(ShippingInfoForm shippingInfoForm) {
+        Order cart = CartState.getCart();
+        FulfillmentGroup firstShippableFulfillmentGroup = fulfillmentGroupService.getFirstShippableFulfillmentGroup(cart);
+
+        if (firstShippableFulfillmentGroup != null) {
+            //if the cart has already has fulfillment information
+            if (firstShippableFulfillmentGroup.getAddress() != null) {
+                shippingInfoForm.setAddress(firstShippableFulfillmentGroup.getAddress());
+            } else {
+                //check for a default address for the customer
+                CustomerAddress defaultAddress = customerAddressService.findDefaultCustomerAddress(CustomerState.getCustomer().getId());
+                if (defaultAddress != null) {
+                    shippingInfoForm.setAddress(defaultAddress.getAddress());
+                    shippingInfoForm.setAddressName(defaultAddress.getAddressName());
+                }
+            }
+
+            FulfillmentOption fulfillmentOption = firstShippableFulfillmentGroup.getFulfillmentOption();
+            if (fulfillmentOption != null) {
+                shippingInfoForm.setFulfillmentOption(fulfillmentOption);
+                shippingInfoForm.setFulfillmentOptionId(fulfillmentOption.getId());
+            }
+        }
+
+        return shippingInfoForm;
+    }
+
+    public BillingInfoForm prePopulateBillingInfoForm(BillingInfoForm billingInfoForm) {
+        Order cart = CartState.getCart();
+        billingInfoForm.setEmailAddress(cart.getEmailAddress());
+
+        List<OrderPayment> orderPayments = cart.getPayments();
+        for (OrderPayment payment : CollectionUtils.emptyIfNull(orderPayments)) {
+            boolean isCreditCardPaymentType = PaymentType.CREDIT_CARD.equals(payment.getType());
+            boolean paymentHasBillingAddress = (payment.getBillingAddress() != null);
+
+            if (isCreditCardPaymentType && paymentHasBillingAddress) {
+                billingInfoForm.setAddress(payment.getBillingAddress());
+            }
+        }
+
+        return billingInfoForm;
     }
 
     /**
