03251480ed01cb649cedf5374315a1c93c888b80
Jacob Mitash <jacobmitash@gmail.com>
2017-07-05 15:38:30 -0500

    Added linked data for the nav menu
    BroadleafCommerce/QA#2902

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/linked/data/HomepageLinkedDataServiceImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/linked/data/HomepageLinkedDataServiceImpl.java
@@ -1,3 +1,20 @@
+/*
+ * #%L
+ * BroadleafCommerce Framework
+ * %%
+ * Copyright (C) 2009 - 2017 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
 package org.broadleafcommerce.core.linked.data;
 
 import org.codehaus.jettison.json.JSONArray;
@@ -8,7 +25,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by jacobmitash on 6/28/17.
+ *
+ * @author Jacob Mitash
  */
 @Service("blHomepageLinkedDataService")
 public class HomepageLinkedDataServiceImpl extends AbstractLinkedDataService implements HomepageLinkedDataService {
