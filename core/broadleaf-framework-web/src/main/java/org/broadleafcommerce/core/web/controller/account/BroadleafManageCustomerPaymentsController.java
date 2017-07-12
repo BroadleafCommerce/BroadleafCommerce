9f556f2ab943118087f04da2b980f843c198a9e6
Jacob Mitash <jacobmitash@gmail.com>
2017-07-12 11:31:19 -0500

    BroadleafCommerce/QA#2934 - Moved request/parameter annotations to correct place for saved payments

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/controller/account/BroadleafManageCustomerPaymentsController.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/controller/account/BroadleafManageCustomerPaymentsController.java
@@ -1,12 +1,14 @@
 package org.broadleafcommerce.core.web.controller.account;
 
 import org.broadleafcommerce.common.web.controller.BroadleafAbstractController;
-import org.springframework.stereotype.Controller;
+import org.broadleafcommerce.profile.core.domain.Customer;
+import org.broadleafcommerce.profile.core.domain.CustomerPayment;
+import org.broadleafcommerce.profile.core.service.CustomerPaymentService;
+import org.broadleafcommerce.profile.web.core.CustomerState;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -14,39 +16,53 @@ import javax.servlet.http.HttpServletRequest;
  *
  * @author Jacob Mitash
  */
-@Controller
-@RequestMapping("/account/payments")
 public class BroadleafManageCustomerPaymentsController extends BroadleafAbstractController {
+
+    @Value("${validate.customer.owned.data:true}")
+    protected boolean validateCustomerOwnedData;
 
     protected static String customerPaymentView = "account/manageCustomerPayments";
     protected static String customerPaymentRedirect = "redirect:/account/payments";
 
-    @RequestMapping(method = RequestMethod.GET)
+    @Resource(name = "blCustomerPaymentService")
+    protected CustomerPaymentService customerPaymentService;
+
     public String viewCustomerPayments(HttpServletRequest request, Model model) {
         //TODO: update model
 
         return getCustomerPaymentView();
     }
 
-    @RequestMapping(value = "/{customerPaymentId}/", method = RequestMethod.GET)
-    public String viewCustomerPayment(HttpServletRequest request, Model model, @PathVariable("customerPaymentId") Long customerPaymentId) {
-        //TODO: update model
+    public String viewCustomerPayment(HttpServletRequest request, Model model, Long customerPaymentId) {
+        //TODO: update model, add form
 
         return getCustomerPaymentView();
     }
 
-    @RequestMapping(value = "/{customerPaymentId}/", method = RequestMethod.POST)
-    public String updateCustomerPayment(HttpServletRequest request, Model model, @PathVariable("customerPaymentId") Long customerPaymentId) {
+    public String updateCustomerPayment(HttpServletRequest request, Model model, Long customerPaymentId) {
         //TODO: update model, add form
 
         return getCustomerPaymentRedirect();
     }
 
-    @RequestMapping(value = "/{customerPaymentId}/", method = RequestMethod.POST, params="removePayment=Remove")
-    public String removeCustomerPayment(HttpServletRequest request, Model model, @PathVariable("customerPaymentId") Long customerPaymentId) {
+    public String removeCustomerPayment(HttpServletRequest request, Model model, Long customerPaymentId) {
         //TODO: update model, add form
 
         return getCustomerPaymentRedirect();
+    }
+
+    protected void validateCustomerOwnedData(CustomerPayment customerPayment) {
+        if (validateCustomerOwnedData) {
+            Customer activeCustomer = CustomerState.getCustomer();
+            if (activeCustomer != null
+                    && !(activeCustomer.equals(customerPayment.getCustomer()))) {
+                throw new SecurityException("The active customer does not own the object that they are trying to view, edit, or remove.");
+            }
+
+            if (activeCustomer == null && customerPayment.getCustomer() != null) {
+                throw new SecurityException("The active customer does not own the object that they are trying to view, edit, or remove.");
+            }
+        }
     }
 
     public String getCustomerPaymentView() {
