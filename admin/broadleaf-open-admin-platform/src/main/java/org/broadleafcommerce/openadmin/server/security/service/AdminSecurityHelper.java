ab7d94cb8e1102102561140fe900cd6d48f9253f
Philip Baggett <pbaggett@broadleafcommerce.com>
2017-07-18 16:17:58 -0500

    Changes based on feedback from AXF project.

---
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/security/service/AdminSecurityHelper.java
@@ -0,0 +1,36 @@
+/*
+ * #%L
+ * BroadleafCommerce Open Admin Platform
+ * %%
+ * Copyright (C) 2009 - 2016 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
+package org.broadleafcommerce.openadmin.server.security.service;
+
+import org.broadleafcommerce.openadmin.server.security.domain.AdminPermission;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.Collection;
+
+/**
+ * @author Philip Baggett (pbaggett)
+ */
+public interface AdminSecurityHelper {
+
+    /**
+     * Adds the hierarchy of each admin permission to the collection of granted authorities.
+     *
+     * @param grantedAuthorities the collection that authorities will be added to
+     * @param adminPermissions the collection of permissions who's hierarchies will be put into grantedAuthorities
+     */
+    void addAllPermissionsToAuthorities(Collection<GrantedAuthority> grantedAuthorities, Collection<AdminPermission> adminPermissions);
+}
