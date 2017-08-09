0193e2f7cd3d801ad960bf85337b3d1075d76852
Chris Kittrell <ckittrell@broadleafcommerce.com>
2017-08-09 13:12:59 -0500

    BroadleafCommerce/QA#2959 - Fix the getShouldUseCustomerPaymentDefaultValue() condition

--- core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/checkout/service/CheckoutFormServiceImpl.java
+++ core/broadleaf-framework-web/src/main/java/org/broadleafcommerce/core/web/checkout/service/CheckoutFormServiceImpl.java
@@ -190,7 +190,7 @@ public class CheckoutFormServiceImpl implements CheckoutFormService {
         boolean orderUsingCustomerPayment = (customerPaymentUsedForOrder != null);
         boolean cartHasTemporaryCreditCard = cartStateService.cartHasTemporaryCreditCard();
 
-        return orderUsingCustomerPayment && !cartHasTemporaryCreditCard;
+        return orderUsingCustomerPayment || !cartHasTemporaryCreditCard;
     }
 
     /**
