a1ff5f593e74cc927d373c205598c898e2d2b599
hamza00111 <hamza00111@gmail.com>
2017-10-19 18:56:55 +0200

    Add setters in ExampleBean of MergeBeanDefinitionTest

--- common/src/test/java/org/broadleafcommerce/test/common/context/merge/reader/MergeBeanDefinitionTest.java
+++ common/src/test/java/org/broadleafcommerce/test/common/context/merge/reader/MergeBeanDefinitionTest.java
@@ -61,6 +61,18 @@ public class MergeBeanDefinitionTest {
         public String field1;
         public String field2;
         public String field3;
+		
+        public void setField1(String field1) {
+			this.field1 = field1;
+		}
+		public void setField2(String field2) {
+			this.field2 = field2;
+		}
+		public void setField3(String field3) {
+			this.field3 = field3;
+		}
+        
+        
     }
     
     public static class AnonymousBean { }
