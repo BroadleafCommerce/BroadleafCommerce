438bb13ebc605deee5519cffd24a2601addeee61
Phillip Verheyden <pverheyden@gmail.com>
2018-04-01 02:07:13 -0500

    Add a convenience method to FulfillmentGroupService for all shippable fulfillment groups

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/service/FulfillmentGroupService.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/service/FulfillmentGroupService.java
@@ -40,49 +40,49 @@ public interface FulfillmentGroupService {
     public FulfillmentGroup findFulfillmentGroupById(Long fulfillmentGroupId);
 
     public void delete(FulfillmentGroup fulfillmentGroup);
-    
+
     public FulfillmentGroup addFulfillmentGroupToOrder(FulfillmentGroupRequest fulfillmentGroupRequest, boolean priceOrder) throws PricingException;
-    
+
     public FulfillmentGroup addItemToFulfillmentGroup(FulfillmentGroupItemRequest fulfillmentGroupItemRequest, boolean priceOrder) throws PricingException;
 
     public FulfillmentGroup addItemToFulfillmentGroup(FulfillmentGroupItemRequest fulfillmentGroupItemRequest, boolean priceOrder, boolean save) throws PricingException;
-    
+
     public Order removeAllFulfillmentGroupsFromOrder(Order order, boolean priceOrder) throws PricingException;
 
     /**
      * Removes every fulfillment group item in every fulfillment group in the order
      * that is associated with the given orderItem. Note that it does not save the changes
      * made - instead, the caller is responsible for saving the order further down.
-     * 
+     *
      * @param order
      * @param orderItem
      */
     public void removeOrderItemFromFullfillmentGroups(Order order, OrderItem orderItem);
-    
+
     public FulfillmentGroupFee createFulfillmentGroupFee();
 
     /**
      * Associates shippable FulfillmentGroupItems in the given Order such that they match the structure
-     * of the OrderMultishipOptions associated with the given Order. 
-     * 
+     * of the OrderMultishipOptions associated with the given Order.
+     *
      * @see OrderMultishipOption
-     * 
+     *
      * @param order
      * @return the saved order
-     * @throws PricingException 
+     * @throws PricingException
      */
     public Order matchFulfillmentGroupsToMultishipOptions(Order order, boolean priceOrder) throws PricingException;
 
     /**
      * Collapses all of the shippable fulfillment groups in the given order to the first shippable fulfillment group
      * in the order.
-     * 
+     *
      * @see #matchFulfillmentGroupsToMultishipOptions(Order, boolean)
-     * 
+     *
      * @param order
      * @param priceOrder
      * @return the saved order
-     * @throws PricingException 
+     * @throws PricingException
      */
     public Order collapseToOneShippableFulfillmentGroup(Order order, boolean priceOrder) throws PricingException;
 
@@ -94,27 +94,27 @@ public interface FulfillmentGroupService {
      * @return
      */
     public List<FulfillmentGroup> findUnfulfilledFulfillmentGroups(int start, int maxResults);
-    
+
     /**
      * Reads FulfillmentGroups whose status is PARTIALLY_FULFILLED or PARTIALLY_DELIVERED.
-     * 
+     *
      * @param start
      * @param maxResults
      * @return
      */
     public List<FulfillmentGroup> findPartiallyFulfilledFulfillmentGroups(int start, int maxResults);
-    
+
     /**
-     * Returns FulfillmentGroups whose status is null, or where no processing has yet occured. 
+     * Returns FulfillmentGroups whose status is null, or where no processing has yet occured.
      * Default returns in ascending order according to date that the order was created.
      * @param start
      * @param maxResults
      * @return
      */
     public List<FulfillmentGroup> findUnprocessedFulfillmentGroups(int start, int maxResults);
-    
+
     /**
-     * Reads FulfillmentGroups by status, either ascending or descending according to the date that 
+     * Reads FulfillmentGroups by status, either ascending or descending according to the date that
      * the order was created.
      * @param status
      * @param start
@@ -123,9 +123,9 @@ public interface FulfillmentGroupService {
      * @return
      */
     public List<FulfillmentGroup> findFulfillmentGroupsByStatus(FulfillmentGroupStatusType status, int start, int maxResults, boolean ascending);
-    
+
     /**
-     * Reads FulfillmentGroups by status, ascending according to the date that 
+     * Reads FulfillmentGroups by status, ascending according to the date that
      * the order was created.
      * @param status
      * @param start
@@ -136,22 +136,31 @@ public interface FulfillmentGroupService {
 
     /**
      * Determines if a fulfillment group is shippable based on its fulfillment type.
-     * 
+     *
      * @param fulfillmentType
      * @return
      */
     public boolean isShippable(FulfillmentType fulfillmentType);
-    
+
     /**
      * Returns the first shippable fulfillment group from an order.
      *
      * @param order
+     * @see #getAllShippableFulfillmentGroups(Order)
+     * @see #isShippable(FulfillmentType)
      */
     public FulfillmentGroup getFirstShippableFulfillmentGroup(Order order);
 
     /**
+     * Returns all of the shippable fulfillment groups for an order
+     *
+     * @see #isShippable(FulfillmentType)
+     */
+    public List<FulfillmentGroup> getAllShippableFulfillmentGroups(Order order);
+
+    /**
      * Finds all FulfillmentGroupItems in the given Order that reference the given OrderItem.
-     * 
+     *
      * @param order
      * @param orderItem
      * @return the list of related FulfillmentGroupItems
