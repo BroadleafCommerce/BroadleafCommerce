fe8db8ae7357ceb1641b00ee7ec02444c04a991e
Phillip Verheyden <pverheyden@gmail.com>
2018-04-23 15:08:56 -0500

    Add license header

--- admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/web/CMSFormBuilderExtensionHandler.java
+++ admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/web/CMSFormBuilderExtensionHandler.java
@@ -1,3 +1,20 @@
+/*
+ * #%L
+ * BroadleafCommerce CMS Module
+ * %%
+ * Copyright (C) 2009 - 2018 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
 package org.broadleafcommerce.cms.web;
 
 import org.apache.commons.logging.Log;
