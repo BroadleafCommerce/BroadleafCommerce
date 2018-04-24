84c7fdced7339fa71b4398dffea8d8a42d805cfe
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2018-04-24 14:56:32 -0500

    BroadleafCommerce/QA#2948 Moved packages so that it's not scanned for site deployments

--- admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/admin/web/service/CMSFormBuilderExtensionHandler.java
+++ admin/broadleaf-contentmanagement-module/src/main/java/org/broadleafcommerce/cms/admin/web/service/CMSFormBuilderExtensionHandler.java
@@ -15,7 +15,7 @@
  * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
  * #L%
  */
-package org.broadleafcommerce.cms.web;
+package org.broadleafcommerce.cms.admin.web.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
