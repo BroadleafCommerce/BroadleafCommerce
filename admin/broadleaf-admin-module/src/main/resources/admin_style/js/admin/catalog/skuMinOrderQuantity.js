2612d70c0d3163c3c0c355569e7ee72a8c842d60
Chris Kittrell <ckittrell@broadleafcommerce.com>
2018-06-08 10:37:38 -0500

    Add dependentFieldHandler for defaultSku's min order quantity fields (#1913)

--- admin/broadleaf-admin-module/src/main/resources/admin_style/js/admin/catalog/skuMinOrderQuantity.js
+++ admin/broadleaf-admin-module/src/main/resources/admin_style/js/admin/catalog/skuMinOrderQuantity.js
@@ -17,7 +17,10 @@
  */
 (function($, BLCAdmin) {
 
-    var className = 'org.broadleafcommerce.core.catalog.domain.Sku';
-    BLCAdmin.addDependentFieldHandler(className, '#field-hasMinOrderQuantity', '#field-minOrderQuantity', 'true');
+    var skuClassName = 'org.broadleafcommerce.core.catalog.domain.Sku';
+    BLCAdmin.addDependentFieldHandler(skuClassName, '#field-hasMinOrderQuantity', '#field-minOrderQuantity', 'true');
+
+    var productClassName = 'org.broadleafcommerce.core.catalog.domain.Product';
+    BLCAdmin.addDependentFieldHandler(productClassName, '#field-defaultSku--hasMinOrderQuantity', '#field-defaultSku--minOrderQuantity', 'true');
 
 })(jQuery, BLCAdmin);
