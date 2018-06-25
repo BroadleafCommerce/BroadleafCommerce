3572ef8d6cd7a1a9c53da2e0028a04413aeba2a8
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2018-06-25 18:11:26 -0500

    Support overriding the table name portion of the index name that's created for the columns listed in a Broadleaf Filter. This was added in order to circumvented issues where two entities with similar table names would have identical index names created which causes errors on Hibernate schema creation.

--- common/src/main/java/org/broadleafcommerce/common/weave/ConditionalDirectCopyTransformMemberDto.java
+++ common/src/main/java/org/broadleafcommerce/common/weave/ConditionalDirectCopyTransformMemberDto.java
@@ -44,6 +44,21 @@ public class ConditionalDirectCopyTransformMemberDto implements Serializable {
     protected String conditionalProperty;
     protected Boolean conditionalValue;
 
+    /**
+     * The name that should be used when dynamically generating index names instead of the table name. This is needed
+     * when two entities have table names that create the same dynamic index names. Generally the strategy used to create
+     * the table name part of the index is to use the first two characters of each word in the table name split on the underscores.
+     * 
+     * i.e.
+     * BLC_APPLE_CARROT -> BLAPCA
+     * BLC_APPLY_CAR    -> BLAPCA
+     * 
+     * Since both tables have a collision, BLC_APPLY_CAR can set this property to "BLAPPCA" to avoid collisions
+     * 
+     * @return
+     */
+    protected String overrideIndexNameKey;
+
     public String[] getTemplateTokens() {
         return templateTokens;
     }
@@ -82,5 +97,13 @@ public class ConditionalDirectCopyTransformMemberDto implements Serializable {
 
     public void setConditionalValue(Boolean conditionalValue) {
         this.conditionalValue = conditionalValue;
+    }
+
+    public String getOverrideIndexNameKey() {
+        return overrideIndexNameKey;
+    }
+
+    public void setOverrideIndexNameKey(String overrideIndexNameKey) {
+        this.overrideIndexNameKey = overrideIndexNameKey;
     }
 }
