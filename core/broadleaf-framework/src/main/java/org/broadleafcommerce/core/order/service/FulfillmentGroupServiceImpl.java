ef7e460d32185af5295d9c552a27c81c5e140e70
Dima Myroniuk <dmyroniuk@broadleafcommerce.com>
2018-07-11 18:36:32 +0300

    Fixed an issue where we can not remove the item from the cart with add-ons if the add-on quantity more than 1.

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/service/FulfillmentGroupServiceImpl.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/order/service/FulfillmentGroupServiceImpl.java
@@ -10,7 +10,7 @@
  * the Broadleaf End User License Agreement (EULA), Version 1.1
  * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
  * shall apply.
- *
+ * 
  * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
  * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
  * #L%
@@ -361,7 +361,7 @@ public class FulfillmentGroupServiceImpl implements FulfillmentGroupService {
                     fulfillmentGroupItem = fulfillmentGroupItemDao.create();
                     fulfillmentGroupItem.setFulfillmentGroup(fg);
                     fulfillmentGroupItem.setOrderItem(orderItem);
-                    fulfillmentGroupItem.setQuantity(1);
+                    fulfillmentGroupItem.setQuantity(orderItem.getQuantity());
                     fulfillmentGroupItem = fulfillmentGroupItemDao.save(fulfillmentGroupItem);
                     fg.getFulfillmentGroupItems().add(fulfillmentGroupItem);
                 } else {
