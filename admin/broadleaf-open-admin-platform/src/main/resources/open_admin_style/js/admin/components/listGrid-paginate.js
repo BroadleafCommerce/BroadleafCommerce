956cdff9aa75386bd6ab0a623ccff3fc14ee8ca9
Jacob Mitash <jacobmitash@gmail.com>
2018-07-18 12:19:30 -0500

    Fix foldering listgrids reverting to smaller height when changing folder

--- admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/listGrid-paginate.js
+++ admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/listGrid-paginate.js
@@ -760,7 +760,7 @@
                     && ($table.data('listgridtype') === 'asset_grid'
                         || $table.data('listgridtype') === 'asset_grid_folder'
                         || $table.data('listgridtype') === 'tree'))
-                || ($table.data('listgridtype') === 'main' && $table.closest('#folder-listgrid-container').length)
+                || ($table.data('listgridtype') === 'main' && $table.closest('.folder-listgrid-container').length)
                 || ($table.data('listgridtype') === 'main' && $table.closest('.folder-items-container').length))  {
                 var $window = $(window);
                 var wrapperHeight = $window.height() - $wrapper.offset().top - 50;
