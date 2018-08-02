096c64fb4b945f76a533319ba68989dc92d271d4
Oleksii <omiroshnyk@broadleafcommerce.com>
2018-08-02 17:00:19 +0300

    BroadleafCommerce/QA#3436

--- common/src/main/java/org/broadleafcommerce/common/config/dao/SystemPropertiesDaoImpl.java
+++ common/src/main/java/org/broadleafcommerce/common/config/dao/SystemPropertiesDaoImpl.java
@@ -10,7 +10,7 @@
  * the Broadleaf End User License Agreement (EULA), Version 1.1
  * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
  * shall apply.
- * 
+ *
  * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
  * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
  * #L%
@@ -139,12 +139,10 @@ public class SystemPropertiesDaoImpl extends AbstractCacheMissAware implements S
                         }
 
                         BroadleafRequestContext broadleafRequestContext = BroadleafRequestContext.getBroadleafRequestContext();
-                        if (broadleafRequestContext == null || broadleafRequestContext.getNonPersistentSite() == null) {
-                            if (SiteDiscriminator.class.isAssignableFrom(SystemPropertyImpl.class)) {
-                                for (SystemProperty prop : response) {
-                                    if (((SiteDiscriminator) prop).getSiteDiscriminator() == null) {
-                                        return prop;
-                                    }
+                        if ((broadleafRequestContext == null || broadleafRequestContext.getNonPersistentSite() == null) && SiteDiscriminator.class.isAssignableFrom(SystemPropertyImpl.class)) {
+                            for (SystemProperty prop : response) {
+                                if (((SiteDiscriminator) prop).getSiteDiscriminator() == null) {
+                                    return prop;
                                 }
                             }
                         } else {
