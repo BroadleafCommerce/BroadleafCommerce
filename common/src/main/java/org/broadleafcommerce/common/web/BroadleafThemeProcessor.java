12ee6d2707e9f0db3abcea43b00b3b81d0231102
Stanislav Fedorov <sfedorov@broadleafcommerce.com>
2019-06-11 14:39:50 +0300

    The content targeting functionality that swaps out the theme was moved to separate filter
    https://github.com/BroadleafCommerce/QA/issues/3682

---
+++ common/src/main/java/org/broadleafcommerce/common/web/BroadleafThemeProcessor.java
@@ -0,0 +1,57 @@
+/*
+ * #%L
+ * BroadleafCommerce Common Libraries
+ * %%
+ * Copyright (C) 2009 - 2016 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ *
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
+package org.broadleafcommerce.common.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.broadleafcommerce.common.classloader.release.ThreadLocalManager;
+import org.broadleafcommerce.common.site.domain.Theme;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+import javax.annotation.Resource;
+
+/**
+ *
+ * @author Stanislav Fedorov
+ * @see {@link BroadleafThemeResolverFilter}
+ */
+@Component("blThemeProcessor")
+public class BroadleafThemeProcessor extends AbstractBroadleafWebRequestProcessor {
+
+    protected final Log LOG = LogFactory.getLog(getClass());
+
+    @Resource(name = "blThemeResolver")
+    protected BroadleafThemeResolver themeResolver;
+
+    @Override
+    public void process(WebRequest request) {
+
+        BroadleafRequestContext brc = BroadleafRequestContext.getBroadleafRequestContext();
+
+        // Note that this must happen after the request context is set up as resolving a theme is dependent on site
+        Theme theme = themeResolver.resolveTheme(request);
+        brc.setTheme(theme);
+
+    }
+
+    @Override
+    public void postProcess(WebRequest request) {
+        ThreadLocalManager.remove();
+    }
+
+}
