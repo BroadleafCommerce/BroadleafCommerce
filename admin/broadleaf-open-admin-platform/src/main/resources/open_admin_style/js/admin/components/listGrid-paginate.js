458a51e9a555ea57c71c7849dc94a812bb0416f8
cschneider <cschneider@broadleafcommerce.com>
2019-08-30 12:51:38 -0500

    Fixed index calculation when modifying category order

--- admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/listGrid-paginate.js
+++ admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/listGrid-paginate.js
@@ -526,10 +526,13 @@
         },
 
         getActualRowIndex : function($tr) {
-            var trPlacementTop = $tr.position().top;
-            var rowHeight = this.getRowHeight($tr.closest('tbody'));
-            // The division should produce an integer, assuming the row heights are consistent.
-            return trPlacementTop / rowHeight;
+            var targetRow = $tr[0];
+            var rows = $tr.closest('tbody').find('tr');
+            for(var i = 0; i < rows.length; i++){
+                if(rows[i].isSameNode(targetRow)){
+                    return i;
+                }
+            }
         },
         
         getTopVisibleIndex : function($tbody) {
