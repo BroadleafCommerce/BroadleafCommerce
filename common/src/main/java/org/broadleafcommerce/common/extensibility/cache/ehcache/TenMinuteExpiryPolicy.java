14e932673d91b4bb2f89a20aa7d7901f69f005c0
ktisdell <ktisdell@broadleafcommerce.com>
2019-09-12 10:55:59 -0500

    Removed docs that wrongly asserted that these expiry policies can be used as an alternative to javax.cache.expiry.ExpiryPolicy.

--- common/src/main/java/org/broadleafcommerce/common/extensibility/cache/ehcache/TenMinuteExpiryPolicy.java
+++ common/src/main/java/org/broadleafcommerce/common/extensibility/cache/ehcache/TenMinuteExpiryPolicy.java
@@ -21,18 +21,7 @@ import org.broadleafcommerce.common.extensibility.cache.TimedValueHolder;
 
 /**
  * Convenience class providing a 10 minute expiry policy, along with the ability to override it on a per entry basis when 
- * using a {@link TimedValueHolder} as the cached value. This can also be used in the EhCache XML to specify an expiry policy:
- * 
- * <pre>
- * {@code
- * <cache alias="myCache">
- *        <expiry>
- *            <class>org.broadleafcommerce.common.extensibility.cache.ehcache.TenMinuteExpiryPolicy</class>
- *        </expiry>
- *        <heap>5000</heap>
- * </cache>
- * }
- * </pre>
+ * using a {@link TimedValueHolder} as the cached value. 
  * 
  * @author Kelly Tisdell
  *
