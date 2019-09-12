81260c4f1acca491c924fa7cef7f7307c7520d54
ktisdell <ktisdell@broadleafcommerce.com>
2019-09-12 16:24:04 -0500

    Keeping the docs the way that they were.  This can be used as an ExpiryPolicy in the EhCache XML configuration.

--- common/src/main/java/org/broadleafcommerce/common/extensibility/cache/ehcache/ThirtyMinuteExpiryPolicy.java
+++ common/src/main/java/org/broadleafcommerce/common/extensibility/cache/ehcache/ThirtyMinuteExpiryPolicy.java
@@ -21,7 +21,18 @@ import org.broadleafcommerce.common.extensibility.cache.TimedValueHolder;
 
 /**
  * Convenience class providing a 30 minute expiry policy, along with the ability to override it on a per entry basis when 
- * using a {@link TimedValueHolder} as the cached value. 
+ * using a {@link TimedValueHolder} as the cached value. This can also be used in the EhCache XML to specify an expiry policy:
+ * 
+ * <pre>
+ * {@code
+ * <cache alias="myCache">
+ *        <expiry>
+ *            <class>org.broadleafcommerce.common.extensibility.cache.ehcache.ThirtyMinuteExpiryPolicy</class>
+ *        </expiry>
+ *        <heap>5000</heap>
+ * </cache>
+ * }
+ * </pre>
  * 
  * @author Kelly Tisdell
  *
