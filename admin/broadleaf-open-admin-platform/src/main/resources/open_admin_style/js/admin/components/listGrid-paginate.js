3218d6e4d135392d4e22467af436f6799641afb5
Stanislav Fedorov <sfedorov@broadleafcommerce.com>
2019-11-20 12:07:58 +0200

    Fixed an issue When go back from product screen list grid fails to scroll
    https://github.com/BroadleafCommerce/QA/issues/3826

--- admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/listGrid-paginate.js
+++ admin/broadleaf-open-admin-platform/src/main/resources/open_admin_style/js/admin/components/listGrid-paginate.js
@@ -853,7 +853,9 @@
             if (maxHeight < rowHeight) {
                 maxHeight = rowHeight;
             }
-            
+            if (maxHeight > desiredMaxHeight) {
+                maxHeight = desiredMaxHeight;
+            }
             return maxHeight;
         },
         
