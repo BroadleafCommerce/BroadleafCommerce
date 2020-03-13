bfd8067a05f83e466306b714ec82bf2adda2ee7b
Pavlo Putrenkov <putrenkov55@gmail.com>
2020-03-13 14:19:13 +0200

    QA-3961 Additional lazy fetch tuning https://github.com/BroadleafCommerce/QA/issues/3961

--- core/broadleaf-profile/src/main/java/org/broadleafcommerce/profile/core/domain/CustomerAddressImpl.java
+++ core/broadleaf-profile/src/main/java/org/broadleafcommerce/profile/core/domain/CustomerAddressImpl.java
@@ -39,7 +39,6 @@ import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
