d0d21b1b143a62a114e0f6c5478e54e8e792db9d
Pavlo Putrenkov <putrenkov55@gmail.com>
2020-03-25 16:41:46 +0200

    QA-3974 revert merge conflict https://github.com/BroadleafCommerce/QA/issues/3974

--- common/src/main/java/org/broadleafcommerce/common/demo/ImportSQLConfig.java
+++ common/src/main/java/org/broadleafcommerce/common/demo/ImportSQLConfig.java
@@ -17,6 +17,8 @@
  */
 package org.broadleafcommerce.common.demo;
 
+import org.broadleafcommerce.common.condition.ConditionalOnBroadleafModule;
+import org.broadleafcommerce.common.module.BroadleafModuleRegistration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
@@ -40,6 +42,7 @@ public class ImportSQLConfig {
     }
 
     @Bean
+    @ConditionalOnBroadleafModule(BroadleafModuleRegistration.BroadleafModuleEnum.MULTI_TENANT_SINGLE_SCHEMA)
     public AutoImportSql blCommonLateData() {
         return new AutoImportSql(AutoImportPersistenceUnit.BL_PU,"config/bc/sql/demo/fix_admin_user_data.sql,config/bc/sql/demo/fix_system_property_data.sql", AutoImportStage.PRIMARY_LATE);
     }
