4b0a788aad15417032a1bf8b2665141e08eedd82
Oleksii <omiroshnyk@broadleafcommerce.com>
2021-06-01 11:58:05 +0300

    BroadleafCommerce/QA#4355 - Fixes for propagation of duplicated product

--- common/src/main/java/org/broadleafcommerce/common/persistence/EntityDuplicatorImpl.java
+++ common/src/main/java/org/broadleafcommerce/common/persistence/EntityDuplicatorImpl.java
@@ -33,12 +33,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
 
 import static org.broadleafcommerce.common.copy.MultiTenantCopyContext.MANUAL_DUPLICATION;
+import static org.broadleafcommerce.common.copy.MultiTenantCopyContext.PROPAGATION;
 
 /**
  * @see EntityDuplicator
@@ -166,11 +168,16 @@ public class EntityDuplicatorImpl extends MultiTenantCopier implements EntityDup
 
             context.getCopyHints().put(MANUAL_DUPLICATION, Boolean.TRUE.toString());
             dup = performCopy(context, (MultiTenantCloneable<T>) entity);
-/*
-            if (extensionManager != null) {
-                extensionManager.addToSandbox(dup);
+
+            ExtensionResultHolder<List<MultiTenantCopyContext>> resultHolder = new ExtensionResultHolder<>();
+            extensionManager.getCatalogsForPropagation(context, resultHolder);
+            List<MultiTenantCopyContext> contexts = resultHolder.getResult();
+            for (MultiTenantCopyContext multiTenantCopyContext : contexts) {
+                multiTenantCopyContext.getCopyHints().put(MANUAL_DUPLICATION, Boolean.TRUE.toString());
+                BroadleafRequestContext.getBroadleafRequestContext().getAdditionalProperties().put("MANUAL_DUPLICATION_PROPAGATION","TRUE");
+                multiTenantCopyContext.getCopyHints().put(PROPAGATION, Boolean.TRUE.toString());
+                performCopy(multiTenantCopyContext, (MultiTenantCloneable<T>) dup);
             }
-*/
         } catch (Exception e) {
             throw ExceptionHelper.refineException(RuntimeException.class, RuntimeException.class,
                     String.format("Unable to duplicate entity %s:%s", entityClass.getName(), id), 
@@ -248,7 +255,7 @@ public class EntityDuplicatorImpl extends MultiTenantCopier implements EntityDup
             }
         }, (Class<T>) entity.getClass(), (T) entity, context);
         
-        return context.getClonedVersion((Class<T>) entity.getClass(), 
+        return context.getClonedVersion((Class<T>) entity.getClass(),
                 genericEntityService.getIdentifier(entity));
     }
     
