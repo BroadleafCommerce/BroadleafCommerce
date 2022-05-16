7b53e8f9ce10de96891d79590b676b334117137e
Ritesh Adhikari <radhikari@broadleafcommerce.com>
2022-05-16 09:35:30 -0500

    Fix headers

--- core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/WeaveProductSkuUsage.java
+++ core/broadleaf-framework/src/main/java/org/broadleafcommerce/core/catalog/domain/WeaveProductSkuUsage.java
@@ -4,15 +4,15 @@
  * %%
  * Copyright (C) 2009 - 2021 Broadleaf Commerce
  * %%
- * NOTICE:  All information contained herein is, and remains
- * the property of Broadleaf Commerce, LLC
- * The intellectual and technical concepts contained
- * herein are proprietary to Broadleaf Commerce, LLC
- * and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Broadleaf Commerce, LLC.
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
  * #L%
  */
 
