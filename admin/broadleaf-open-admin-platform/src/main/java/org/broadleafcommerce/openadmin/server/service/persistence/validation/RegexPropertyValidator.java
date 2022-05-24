1f8848e2ce72e9ffe5a7319b26a5da02941e95ec
Orest Katruk <okatruk@broadleafcommerce.com>
2022-05-24 11:39:18 +0300

    QA-4702: rename property and update logic in RegexPropertyValidator; clean code

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/persistence/validation/RegexPropertyValidator.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/service/persistence/validation/RegexPropertyValidator.java
@@ -28,10 +28,10 @@ import org.broadleafcommerce.openadmin.dto.FieldMetadata;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+
 import java.io.Serializable;
 import java.util.Map;
 import java.util.regex.PatternSyntaxException;
-
 
 /**
  * Validates a field against a configured 'regularExpression' item
@@ -48,42 +48,45 @@ public class RegexPropertyValidator extends ValidationConfigurationBasedProperty
      * allow them, which means that this validator will always return true with null values
      */
     protected boolean succeedForNullValues = true;
-    
+
     /**
      * Whether or not this validator should succeed (and thus pass validation) even when the configured regular expression
      * pattern is invalid. While this value defaults to true, it might be beneficial to set this to false in a development
      * environment to debug problems in regular expressions. In either case, this validator will log that there is an invalid
      * regular expression
      */
-    protected boolean suceedForInvalidRegex = true;
+    protected boolean succeedForInvalidRegex = true;
 
     @Resource(name = "blExploitProtectionService")
-    protected ExploitProtectionService eps;
+    protected ExploitProtectionService exploitProtectionService;
 
     @Override
     public PropertyValidationResult validate(Entity entity,
-            Serializable instance,
-            Map<String, FieldMetadata> entityFieldMetadata,
-            Map<String, String> validationConfiguration,
-            BasicFieldMetadata propertyMetadata,
-            String propertyName,
-            String value) {
+                                             Serializable instance,
+                                             Map<String, FieldMetadata> entityFieldMetadata,
+                                             Map<String, String> validationConfiguration,
+                                             BasicFieldMetadata propertyMetadata,
+                                             String propertyName,
+                                             String value) {
         String expression = validationConfiguration.get("regularExpression");
-        
+
         if (value == null) {
             return new PropertyValidationResult(succeedForNullValues, validationConfiguration.get(ConfigurationItem.ERROR_MESSAGE));
         }
-        
+
         try {
-            return new PropertyValidationResult(eps.htmlDecode(value).matches(expression), validationConfiguration.get(ConfigurationItem.ERROR_MESSAGE));
+            return new PropertyValidationResult(
+                    this.exploitProtectionService.htmlDecode(value).matches(expression),
+                    validationConfiguration.get(ConfigurationItem.ERROR_MESSAGE)
+            );
         } catch (PatternSyntaxException e) {
-            String message = "Invalid regular expression pattern '" + StringUtil.sanitize(expression) + "' for " 
+            String message = "Invalid regular expression pattern '" + StringUtil.sanitize(expression) + "' for "
                     + StringUtil.sanitize(propertyName);
             LOG.error(message, e);
-            return new PropertyValidationResult(suceedForInvalidRegex, "Invalid regular expression pattern for " + propertyName);
+            return new PropertyValidationResult(!succeedForInvalidRegex, "Invalid regular expression pattern for " + propertyName);
         }
     }
-    
+
     public boolean isSucceedForNullValues() {
         return succeedForNullValues;
     }
@@ -92,12 +95,12 @@ public class RegexPropertyValidator extends ValidationConfigurationBasedProperty
         this.succeedForNullValues = succeedForNullValues;
     }
 
-    public boolean isSuceedForInvalidRegex() {
-        return suceedForInvalidRegex;
+    public boolean isSucceedForInvalidRegex() {
+        return succeedForInvalidRegex;
     }
 
-    public void setSuceedForInvalidRegex(boolean suceedForInvalidRegex) {
-        this.suceedForInvalidRegex = suceedForInvalidRegex;
+    public void setSucceedForInvalidRegex(boolean succeedForInvalidRegex) {
+        this.succeedForInvalidRegex = succeedForInvalidRegex;
     }
 
 }
