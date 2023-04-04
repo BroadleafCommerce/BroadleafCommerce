ac0e753ffe5052dcb499c8370f9c5595d02a57cc
mburyi <markijanburyy01@gmail.com>
2023-04-04 15:57:56 +0200

    BroadleafCommerce/QA-4943_admin_password_regex_validation
    Created AdminUserRegexValidator to set Regex and validate password with it. Also set new properties in common.properties file, so customers could set their own regex and use this logic.

--- admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/security/domain/AdminUserImpl.java
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/security/domain/AdminUserImpl.java
@@ -45,12 +45,6 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Index;
 import org.hibernate.annotations.Parameter;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -67,6 +61,11 @@ import javax.persistence.MapKey;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * 
@@ -127,7 +126,13 @@ public class AdminUserImpl implements AdminUser, AdminMainEntity, AdminUserAdmin
                             @ConfigurationItem(itemName = ConfigurationItem.ERROR_MESSAGE, itemValue = "passwordNotMatchError"),
                             @ConfigurationItem(itemName = "otherField", itemValue = "passwordConfirm")
                     }
-                    )
+                    ), @ValidationConfiguration(
+                            validationImplementation = "org.broadleafcommerce.openadmin.server.service.persistence.validation.AdminUserRegexValidator",
+                    configurationItems = {
+                            @ConfigurationItem(itemName = ConfigurationItem.ERROR_MESSAGE, itemValue = "passwordNotMatchProvidedRegex"),
+                            @ConfigurationItem(itemName = "regex", itemValue = "passwordMatchesRegex")
+                    }
+            )
             })
     protected String password;
 
