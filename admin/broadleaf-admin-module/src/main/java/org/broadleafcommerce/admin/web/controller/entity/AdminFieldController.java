ca1dce0d97e8fa24157c52515f67b6b4cf4a7164
Stanislav Fedorov <sfedorov@broadleafcommerce.com>
2025-01-28 12:05:14 +0200

    Fixed an issue where Indexed Fields filtering does not work https://github.com/BroadleafCommerce/QA/issues/5345

---
+++ admin/broadleaf-admin-module/src/main/java/org/broadleafcommerce/admin/web/controller/entity/AdminFieldController.java
@@ -0,0 +1,83 @@
+package org.broadleafcommerce.admin.web.controller.entity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.broadleafcommerce.common.admin.domain.AdminMainEntity;
+import org.broadleafcommerce.openadmin.dto.ClassMetadata;
+import org.broadleafcommerce.openadmin.dto.DynamicResultSet;
+import org.broadleafcommerce.openadmin.dto.Entity;
+import org.broadleafcommerce.openadmin.dto.Property;
+import org.broadleafcommerce.openadmin.dto.SectionCrumb;
+import org.broadleafcommerce.openadmin.server.domain.PersistencePackageRequest;
+import org.broadleafcommerce.openadmin.web.controller.entity.AdminBasicEntityController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller("blAdminFieldController")
+@RequestMapping("/" + AdminFieldController.SECTION_KEY)
+public class AdminFieldController extends AdminBasicEntityController {
+
+    public static final String SECTION_KEY = "org.broadleafcommerce.core.search.domain.FieldImpl";
+
+    @Override
+    protected String getSectionKey(Map<String, String> pathVars) {
+        //allow external links to work for ToOne items
+        if (super.getSectionKey(pathVars) != null) {
+            return super.getSectionKey(pathVars);
+        }
+        return SECTION_KEY;
+    }
+
+    @RequestMapping(value = "/selectize", method = RequestMethod.GET)
+    public @ResponseBody Map<String, Object> viewEntityListSelectize(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Model model,
+            @PathVariable Map<String, String> pathVars,
+            @RequestParam MultiValueMap<String, String> requestParams
+    ) throws Exception {
+        String sectionKey = getSectionKey(pathVars);
+        String sectionClassName = getClassNameForSection(sectionKey);
+        List<SectionCrumb> crumbs = getSectionCrumbs(request, null, null);
+        PersistencePackageRequest ppr = getSectionPersistencePackageRequest(sectionClassName, requestParams, crumbs, pathVars)
+                .withCustomCriteria(getCustomCriteria(requestParams));
+
+        ppr.addCustomCriteria(buildSelectizeCustomCriteria());
+
+        ClassMetadata cmd = service.getClassMetadata(ppr).getDynamicResultSet().getClassMetaData();
+        DynamicResultSet drs = service.getRecords(ppr).getDynamicResultSet();
+
+        return constructSelectizeOptionMap(drs, cmd);
+    }
+
+    public Map<String, Object> constructSelectizeOptionMap(DynamicResultSet drs, ClassMetadata cmd) {
+        Map<String, Object> result = new HashMap<>();
+        List<Map<String, String>> options = new ArrayList<>();
+
+        for (Entity e : drs.getRecords()) {
+            Map<String, String> selectizeOption = new HashMap<>();
+
+            Property p = e.findProperty(AdminMainEntity.MAIN_ENTITY_NAME_PROPERTY);
+            if (p != null) {
+                selectizeOption.put("name", p.getValue());
+                selectizeOption.put("id", p.getValue());
+            }
+            options.add(selectizeOption);
+        }
+        result.put("options", options);
+
+        return result;
+    }
+
+}
