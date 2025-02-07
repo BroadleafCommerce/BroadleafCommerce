79a08970d1b59b24ce44bd7780eb987c0c07e4fc
Orest Katruk <okatruk@broadleafcommerce.com>
2025-02-07 02:16:09 +0200

    QA-5346: add BroadleafIdGenerator annotation to replace deprecated GenericGenerator

---
+++ common/src/main/java/org/broadleafcommerce/common/persistence/BroadleafIdGenerator.java
@@ -0,0 +1,35 @@
+/*-
+ * #%L
+ * BroadleafCommerce Common Libraries
+ * %%
+ * Copyright (C) 2009 - 2025 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
+package org.broadleafcommerce.common.persistence;
+
+import org.hibernate.annotations.IdGeneratorType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+@IdGeneratorType(IdOverrideTableGenerator.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ FIELD, METHOD})
+public @interface BroadleafIdGenerator {
+    String segment_value();
+    String entity_name();
+}
