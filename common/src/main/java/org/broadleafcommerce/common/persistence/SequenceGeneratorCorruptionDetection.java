267c1bca53b47ec6576a6ea46596f7eb4dd8e4b6
Stanislav Fedorov <sfedorov@broadleafcommerce.com>
2026-04-24 15:57:28 +0300

    Fix SequenceGeneratorCorruptionDetection issue (#5447)

--- common/src/main/java/org/broadleafcommerce/common/persistence/SequenceGeneratorCorruptionDetection.java
+++ common/src/main/java/org/broadleafcommerce/common/persistence/SequenceGeneratorCorruptionDetection.java
@@ -210,7 +210,7 @@ public class SequenceGeneratorCorruptionDetection implements ApplicationListener
                 if (CollectionUtils.isNotEmpty(results) && results.get(0) != null) {
                     LOG.debug(String.format("Checking for sequence corruption on entity %s", segmentValue));
                     Long maxEntityId = BLCNumberUtils.toLong(results.get(0));
-                    if (maxEntityId + incrementSize + 1 >= maxSequenceId) {
+                    if (maxEntityId + incrementSize >= maxSequenceId) {
                         String invalidSequenceDetectedMsg = String.format("The sequence value for %s in %s was found as %d (or an entry did not exist) but the actual max sequence in"
                                 + " %s's table was found as %d", segmentValue, tableName, maxSequenceId, mappedClass.getName(), maxEntityId);
                         if (automaticallyCorrectInconsistencies) {
